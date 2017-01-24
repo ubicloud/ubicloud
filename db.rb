2f7271a55268127262b0c51c3d40fb983af74ef8
Jeremy Evans <code@jeremyevans.net>
2017-01-24 13:00:59 -0800

    Freeze datasets by default

--- db.rb
+++ db.rb
@@ -5,3 +5,4 @@ require 'sequel'
 # Delete DATABASE_URL from the environment, so it isn't accidently
 # passed to subprocesses.  DATABASE_URL may contain passwords.
 DB = Sequel.connect(ENV.delete('DATABASE_URL'))
+DB.extension :freeze_datasets
