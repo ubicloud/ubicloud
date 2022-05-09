aeda5a7f1abbd800d6581872dfa4520535c8cefe
Jeremy Evans <code@jeremyevans.net>
2022-05-08 20:39:57 -0700

    Add comment to db.rb showing that is where Database/Global extensions should be loaded
    Fixes #24

--- db.rb
+++ db.rb
@@ -8,3 +8,6 @@ require 'sequel/core'
 # Delete APP_DATABASE_URL from the environment, so it isn't accidently
 # passed to subprocesses.  APP_DATABASE_URL may contain passwords.
 DB = Sequel.connect(ENV.delete('APP_DATABASE_URL') || ENV.delete('DATABASE_URL'))
+
+# Load Sequel Database/Global extensions here
+# DB.extension :date_arithmetic
