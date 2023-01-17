3c58494500ad823aafbda471f0d13b0719a5254b
Daniel Farina <daniel@fdr.io>
2023-01-17 15:24:27 -0800

    Basic zeitwerk setup

---
+++ loader.rb
@@ -0,0 +1,7 @@
+require "zeitwerk"
+LOADER = Zeitwerk::Loader.new
+LOADER.push_dir("#{__dir__}/")
+LOADER.push_dir("#{__dir__}/models")
+LOADER.push_dir("#{__dir__}/lib")
+LOADER.enable_reloading
+LOADER.setup
