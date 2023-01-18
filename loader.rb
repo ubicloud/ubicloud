d3017967c4633c0f5901f140467b61881985fad8
Daniel Farina <daniel@fdr.io>
2023-01-18 15:26:01 -0800

    Lazily load the idiomatic Sequel constant name "DB"
    Otherwise, Zeitwerk wants to load "Db"

--- loader.rb
+++ loader.rb
@@ -4,5 +4,6 @@ LOADER = Zeitwerk::Loader.new
 LOADER.push_dir("#{__dir__}/")
 LOADER.push_dir("#{__dir__}/models")
 LOADER.push_dir("#{__dir__}/lib")
+LOADER.inflector.inflect("db" => "DB")
 LOADER.enable_reloading
 LOADER.setup
