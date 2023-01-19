9fb6006fe476084ce61b85ea084cc38e89b68684
Daniel Farina <daniel@fdr.io>
2023-01-19 14:50:08 -0800

    Use a loader hook to get directory-level initialization
    Previously this was eager because model.rb has to do this eagerly, on is
    own.  See 9e22aaada16ca4e57d2e7dfa6532c53058ddb7c8

--- loader.rb
+++ loader.rb
@@ -7,6 +7,11 @@ Loader.push_dir("#{__dir__}/lib")
 Loader.ignore("#{__dir__}/routes")
 Loader.ignore("#{__dir__}/migrate")
 Loader.ignore("#{__dir__}/spec")
+models_dir = "#{__dir__}/models.rb"
+Loader.ignore(models_dir)
+Loader.on_dir_autoloaded(models_dir) do
+  require_relative "models"
+end
 Loader.inflector.inflect("db" => "DB")
 Loader.enable_reloading
 Loader.setup
