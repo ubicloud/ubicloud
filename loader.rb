284e7161d70366b28708daba054444a306440b0e
Daniel Farina <daniel@fdr.io>
2023-01-19 14:37:52 -0800

    Per roda convention, routes use class reopening
    And because of that, the zeitwerk convention does not apply.
    
    https://github.com/jeremyevans/roda-sequel-stack/issues/21

--- loader.rb
+++ loader.rb
@@ -4,6 +4,7 @@ Loader = Zeitwerk::Loader.new
 Loader.push_dir("#{__dir__}/")
 Loader.push_dir("#{__dir__}/models")
 Loader.push_dir("#{__dir__}/lib")
+Loader.ignore("#{__dir__}/routes")
 Loader.ignore("#{__dir__}/migrate")
 Loader.inflector.inflect("db" => "DB")
 Loader.enable_reloading
