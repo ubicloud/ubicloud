235bf88e7f1b1b7f14e35665e604c91cf7e1d101
Daniel Farina <daniel@fdr.io>
2023-01-19 13:56:54 -0800

    Change LOADER to Loader
    I find it easier to type.

--- loader.rb
+++ loader.rb
@@ -1,9 +1,10 @@
 require_relative ".env"
 require "zeitwerk"
-LOADER = Zeitwerk::Loader.new
-LOADER.push_dir("#{__dir__}/")
-LOADER.push_dir("#{__dir__}/models")
-LOADER.push_dir("#{__dir__}/lib")
-LOADER.inflector.inflect("db" => "DB")
-LOADER.enable_reloading
-LOADER.setup
+Loader = Zeitwerk::Loader.new
+Loader.push_dir("#{__dir__}/")
+Loader.push_dir("#{__dir__}/models")
+Loader.push_dir("#{__dir__}/lib")
+Loader.ignore("#{__dir__}/migrate")
+Loader.inflector.inflect("db" => "DB")
+Loader.enable_reloading
+Loader.setup
