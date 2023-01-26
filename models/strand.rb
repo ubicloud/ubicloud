b88dd19e86682891aa552ce6d4e5f8f40daae3b5
Daniel Farina <daniel@fdr.io>
2023-01-26 11:25:18 -0800

    Write the first real test, and implement leasing
    This implements the beginning of a leasing feature, for mutual
    exclusion.  And, it's the first actual test.

--- models/strand.rb
+++ models/strand.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class Strand < Model
+  LEASE_EXPIRATION = 120
+
+  def lease
+    self.class.lease(id) do
+      yield self
+    end
+  end
+
+  def self.lease(id)
+    affected = DB[<<SQL, id].first
+UPDATE strand
+SET lease = now() + '120 seconds'
+WHERE id = ? AND (lease IS NULL OR lease < now())
+RETURNING lease
+SQL
+    return false unless affected
+    lease = affected.fetch(:lease)
+    yield
+
+    num_updated = DB[<<SQL, id, lease].update
+UPDATE strand
+SET lease = NULL
+WHERE id = ? AND lease = ?
+SQL
+    fail "BUG: lease violated" unless num_updated == 1
+    true
+  end
 end
