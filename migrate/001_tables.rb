8fb7d1beb2f031342aa1ff23f76e7d4b698233b3
Daniel Farina <daniel@fdr.io>
2023-02-24 19:03:39 -0800

    Add Sshable model
    This can be used for host servers, or possibly VMs if Clover ever uses
    both in some capacity.  It doesn't matter, as long as there's a host.
    
    Included is field encryption and connection caching.
    
    Note that the connection cache has no limit, and is bound to a
    Thread. The intention is that Threads made in the Dispatcher should be
    short-lived, rather than pooled and reused.
    
    Previous projects of mine have tended to make profuse use of SSH
    connections to simplify the programming model, but this was slow.  By
    binding the cache to the Thread, the programming model stays simple,
    and hopefully the caching code stays simple, and we are not stuck
    debugging complex residue across many executions in a long-lived
    thread.

--- migrate/001_tables.rb
+++ migrate/001_tables.rb
@@ -18,5 +18,11 @@ Sequel.migration do
       foreign_key :consumer, :strand, type: :uuid, null: false
       column :name, :text, collate: '"C"', null: false
     end
+
+    create_table(:sshable) do
+      column :id, :uuid, primary_key: true, default: Sequel.lit("gen_random_uuid()")
+      column :host, :text, collate: '"C"', null: false, unique: true
+      column :private_key, :text, collate: '"C"', null: false
+    end
   end
 end
