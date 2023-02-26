e8b5ec59d06f4d3aac2756b3290bd2cd7a3c75df
Daniel Farina <daniel@fdr.io>
2023-02-26 06:51:34 -0800

    Add dispatcher thread handling test
    This is complicated by the fact that it requires some transactions and
    inter-thread synchronization.

--- spec/scheduling/dispatcher_spec.rb
+++ spec/scheduling/dispatcher_spec.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# A bit of a hack, to re-use a different spec_helper, but otherwise
+# these tests will crash DatabaseCleaner.
+require_relative "../model/spec_helper"
+
 RSpec.describe Scheduling::Dispatcher do
   subject(:di) { described_class.new }
 
@@ -16,6 +20,48 @@ RSpec.describe Scheduling::Dispatcher do
       di.wait_cohort
 
       expect(di.threads).to eq(want)
+    end
+  end
+
+  describe "#start_cohort" do
+    it "can create threads" do
+      # Isolate some thread local variables used for communication
+      # within.
+      Thread.new do
+        th = Thread.current
+        r, w = IO.pipe
+
+        # Set a temporally-unique name that allows the Test strand to
+        # find this thread and read its variables.
+        th.name = "clover_test"
+
+        # Pass part of a pipe: the test will synchronize by blocking
+        # on it having been closed.
+        th[:clover_test_in] = w
+
+        # Ensure the test can be found by "#scan" and runs in a
+        # thread.
+        s = Strand.create(schedule: Time.now, prog: "Test", label: "synchronized")
+        di.start_cohort
+        expect(di.threads.count).to be 1
+        expect(di.threads[0].name).to eq(s.id)
+
+        # Blocks until :clover_test_out has been set.
+        r.read
+        r.close
+
+        # Wait until thread has changed "alive?" status to "false".
+        th.thread_variable_get(:clover_test_out).join
+
+        # Expect a dead thread to get reaped by wait_cohort.
+        di.wait_cohort
+        expect(di.threads).to be_empty
+      ensure
+        # Multiple transactions are required for this test across
+        # threads, so we need to clean up differently than
+        # DatabaseCleaner would.
+        Strand.truncate(cascade: true)
+      end.join
     end
   end
 end
