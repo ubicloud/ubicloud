f566a46859906aec55d37509f263ad089483d6c0
Daniel Farina <daniel@fdr.io>
2023-02-27 15:26:10 -0800

    Write hypervisor console output to a file
    ...rather than attaching it to a foreground tty/pty.  I thought about
    allocating a pty so that there's a channel for input, but it doesn't
    look trivial based on quick reading.  It could be useful in some
    circumstances, like entering keystrokes during boot.
    
    AWS does support serial ports (probably the emulated ones, not the
    virtio ones), but not by default.
    https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-serial-console.html
    
    Per cloud-hypervisor manual,
    https://github.com/cloud-hypervisor/cloud-hypervisor/blob/main/docs/device_model.md#serial-port ,
    use of an emulated serial device rater than virtio-console slows down
    boot.  Given the rarity of need, I can see why, perhaps, AWS chose the
    default it did

--- rhizome/bin/runvm.rb
+++ rhizome/bin/runvm.rb
@@ -11,5 +11,5 @@ end
 q_vm = vm_name.shellescape
 q_guest_mac = File.read("/home/#{vm_name}/guest_mac").strip.shellescape
 exec <<EOS
-ip netns exec #{q_vm} sudo -u #{q_vm} -i -- /opt/cloud-hypervisor/v30.0/cloud-hypervisor --kernel /opt/fw/v0.4.2/hypervisor-fw --disk path=boot.raw --disk path=ubuntu-cloudinit.img --cpus boot=4 --memory size=1024M --net "mac="#{q_guest_mac.shellescape}",tap=tap"#{q_vm.shellescape}",ip=,mask="
+ip netns exec #{q_vm} sudo -u #{q_vm} -i -- /opt/cloud-hypervisor/v30.0/cloud-hypervisor --kernel /opt/fw/v0.4.2/hypervisor-fw --disk path=boot.raw --disk path=ubuntu-cloudinit.img --console file=console.log --cpus boot=4 --memory size=1024M --net "mac="#{q_guest_mac.shellescape}",tap=tap"#{q_vm.shellescape}",ip=,mask="
 EOS
