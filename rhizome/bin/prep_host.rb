663e69082585e045fe6612aa936aa830c23f30a2
Daniel Farina <daniel@fdr.io>
2023-02-27 13:24:22 -0800

    Add automation for preparing the host files and directories
    This includes downloading the hypervisor, boot firmware, network
    settings, and an OS image.  Other programs have been changed to
    comport with these paths.
    
    The hypervisor and firmware have versioned directories, as to assist
    with upgrading "naturally" with VM churn:
    
        /opt
        ├── cloud-hypervisor
        │   └── v30.0
        │       └── cloud-hypervisor
        ├── fw
        │   └── v0.4.2
        │       └── hypervisor-fw
        └── jammy-server-cloudimg-amd64.img

---
+++ rhizome/bin/prep_host.rb
@@ -0,0 +1,43 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "../lib/common"
+require "fileutils"
+
+# cloud-hypervisor version
+ch_v = "30.0"
+
+# YYY: we should check against digests of each artifact, to detect and
+# report any unexpected content changes (e.g., supply chain attack).
+
+ch_dir = "/opt/cloud-hypervisor/v#{ch_v}"
+FileUtils.mkdir_p(ch_dir)
+FileUtils.cd ch_dir do
+  r "curl -L3 -O https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/v#{ch_v}/cloud-hypervisor"
+  FileUtils.chmod "a+x", "cloud-hypervisor"
+end
+
+# rust-hypervisor-firmware version
+fw_v = "0.4.2"
+fw_dir = "/opt/fw/v#{fw_v}"
+FileUtils.mkdir_p(fw_dir)
+FileUtils.cd fw_dir do
+  r "curl -L3 -O https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/#{fw_v}/hypervisor-fw"
+end
+
+# Host-level network packet forwarding, otherwise packets cannot leave
+# the physical interface.
+File.write("/etc/sysctl.d/72-clover-forward-packets.conf", <<CONF)
+net.ipv6.conf.all.forwarding=1
+CONF
+r "sysctl --system"
+
+# OS images.
+
+# For qemu-image convert and mcopy for cloud-init with the nocloud
+# driver.
+r "apt-get -y install qemu-utils mtools"
+
+FileUtils.cd "/opt" do
+  r "curl -O https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+end
