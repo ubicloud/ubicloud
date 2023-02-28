e2cf924766d609d440e6cb0bc2e2500835695a24
Daniel Farina <daniel@fdr.io>
2023-02-27 18:46:07 -0800

    Add infrastructure for computing paths in vm home directory
    I was tired of repeating string interpolations everywhere as I
    attempted splitting prepvm.rb, so I centralized it and use a bit of
    metaprogramming to generate all the symbols I need.

---
+++ rhizome/spec/vm_path_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative "../lib/vm_path"
+
+RSpec.describe VmPath do
+  subject(:vp) { described_class.new("test'vm") }
+
+  it "can compute a path" do
+    expect(vp.guest_mac).to eq("/home/test'vm/guest_mac")
+  end
+
+  it "can escape a path" do
+    expect(vp.q_guest_mac).to eq("/home/test\\'vm/guest_mac")
+  end
+
+  it "will snakeify difficult characters" do
+    expect(vp.boot_raw).to eq("/home/test'vm/boot.raw")
+  end
+
+  it "can read file contents" do
+    expect(File).to receive(:read).with(vp.boot_raw).and_return("\n")
+    expect(vp.read_boot_raw).to eq("")
+  end
+
+  context "when writing" do
+    it "affixes a newline if it is missing" do
+      expect(File).to receive(:write).with(vp.boot_raw, "test content\n")
+      vp.write_boot_raw("test content")
+    end
+
+    it "doesn't add more newlines than necessary" do
+      expect(File).to receive(:write).with(vp.boot_raw, "test content\n")
+      vp.write_boot_raw("test content\n")
+    end
+  end
+end
