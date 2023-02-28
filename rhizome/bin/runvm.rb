8ec87e760ef1c85dd2f8a242d062130e45a13a63
Daniel Farina <daniel@fdr.io>
2023-02-27 18:46:07 -0800

    Add infrastructure for computing paths in vm home directory
    I was tired of repeating string interpolations everywhere as I
    attempted splitting prepvm.rb, so I centralized it and use a bit of
    metaprogramming to generate all the symbols I need.

--- rhizome/bin/runvm.rb
+++ rhizome/bin/runvm.rb
@@ -2,14 +2,16 @@
 # frozen_string_literal: true
 
 require_relative "../lib/common"
+require_relative "../lib/vm_path"
 
 unless (vm_name = ARGV.shift)
   puts "need vm name as argument"
   exit 1
 end
 
+vp = VmPath.new(vm_name)
 q_vm = vm_name.shellescape
-q_guest_mac = File.read("/home/#{vm_name}/guest_mac").strip.shellescape
+q_guest_mac = vp.read_guest_mac.strip.shellescape
 exec <<EOS
 ip netns exec #{q_vm} sudo -u #{q_vm} -i -- /opt/cloud-hypervisor/v30.0/cloud-hypervisor --kernel /opt/fw/v0.4.2/hypervisor-fw --disk path=boot.raw --disk path=ubuntu-cloudinit.img --console file=console.log --cpus boot=4 --memory size=1024M --net "mac="#{q_guest_mac.shellescape}",tap=tap"#{q_vm.shellescape}",ip=,mask="
 EOS
