db2c4e66a906405a57b58e5d8d9bf33d9f3fa041
Daniel Farina <daniel@fdr.io>
2023-03-03 14:19:49 -0800

    Switch to Prog::Test
    It seems a bit more sensible than choosing an arbitrary Prog to test
    with.  Now, if a particular prog is used, it is implied that the test
    has to do with testing with that prog specifically.

--- spec/model/strand_spec.rb
+++ spec/model/strand_spec.rb
@@ -3,23 +3,23 @@
 require_relative "spec_helper"
 
 RSpec.describe Strand do
-  let(:st) { described_class.new(schedule: Time.now, prog: "StartHypervisor", label: "start") }
+  let(:st) { described_class.new(schedule: Time.now, prog: "Test", label: "start") }
 
   it "can take leases" do
     st.save_changes
     did_it = st.lease {
-      next Prog::StartHypervisor.new st.id
+      next Prog::Test.new st.id
     }
     expect(did_it).to be true
   end
 
   it "can load a prog" do
-    expect(st.load).to be_instance_of Prog::StartHypervisor
+    expect(st.load).to be_instance_of Prog::Test
   end
 
   it "can run a label" do
     st.save_changes
-    sh = instance_spy(Prog::StartHypervisor)
+    sh = instance_spy(Prog::Test)
     expect(st).to receive(:load).and_return sh
     st.run
     expect(sh).to have_received(:start)
