19ed50d799c427918d14aac6e03cbf73b0b67def
Daniel Farina <daniel@fdr.io>
2023-03-03 07:01:08 -0800

    Add netaddr to project and use it to parse PG inet and cidr types
    Alas, the stdlib class IPAddr (also used for CIDR) in Ruby has some
    borderline dangerous behavior with `#to_s` ignoring the mask bits
    entirely.
    
    Instead, parse with NetAddr, its `#to_s` does not suffer this defect.
    However, I am somewhat unhappy with introducing an interpreter loop
    into row scanning considering many rows may be scanned where no
    interesting NetAddr features are used.
    
    If one were handily available, what I would like is a Proc, written in
    C, part of the standard library, that only returns its first argument.

--- db.rb
+++ db.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "netaddr"
+
 begin
   require_relative ".env"
 rescue LoadError
@@ -7,7 +9,16 @@ end
 
 require "sequel/core"
 
-DB = Sequel.connect(Config.clover_database_url)
+DB = Sequel.connect(Config.clover_database_url).tap do |db|
+  # Replace dangerous (for cidrs) Ruby IPAddr type that is otherwise
+  # used by sequel_pg.  Has come up more than once in the bug tracker:
+  #
+  # https://github.com/jeremyevans/sequel_pg/issues?q=inet
+  # https://github.com/jeremyevans/sequel_pg/issues/30
+  # https://github.com/jeremyevans/sequel_pg/pull/37
+  db.add_conversion_proc(650, NetAddr.method(:parse_net))
+  db.add_conversion_proc(869, NetAddr.method(:parse_ip))
+end
 
 # Load Sequel Database/Global extensions here
 # DB.extension :date_arithmetic
