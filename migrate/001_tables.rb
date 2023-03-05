27b4745912af63ddcbc93a8f64ef707055ab0312
Daniel Farina <daniel@fdr.io>
2023-03-04 16:52:20 -0800

    Handful of table adjustments
    Suspending the usual rules about editing migrations (again), I make a
    few changes for convenience sake in this early period:
    
    `parent` is renamed to `parent_id`.  This is to better abide Sequel's
    association support, which will want to use the "parent" name for its
    methods.
    
    `schedule` is given a default.  It cleans up many tests, and probably
    code in production as well.  The code that wants to hint at suspending
    execution for a while can do so after executing once.
    
    `retval` has been made NULLable, `exitval` has been added: it occurred
    to me that I shouldn't conflate Prog exit and stack returns, and that
    a NULL state is useful to indicate "hasn't returned" and "hasn't
    exited," a special empty value is more prone to error here.

--- migrate/001_tables.rb
+++ migrate/001_tables.rb
@@ -4,13 +4,14 @@ Sequel.migration do
   change do
     create_table(:strand) do
       column :id, :uuid, primary_key: true, default: Sequel.lit("gen_random_uuid()")
-      foreign_key :parent, :strand, type: :uuid
-      column :schedule, :timestamptz, null: false
+      foreign_key :parent_id, :strand, type: :uuid
+      column :schedule, :timestamptz, null: false, default: Sequel.lit("now()")
       column :lease, :timestamptz
       column :prog, :text, collate: '"C"', null: false
       column :label, :text, collate: '"C"', null: false
       column :stack, :jsonb, null: false, default: "[]"
-      column :retval, :jsonb, null: false, default: "{}"
+      column :exitval, :jsonb
+      column :retval, :jsonb
     end
 
     create_table(:semaphore) do
