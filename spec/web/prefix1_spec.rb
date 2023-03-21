c6b6a162fe09a33999d36335f27205c505c9d8c0
Daniel Farina <daniel@fdr.io>
2023-03-21 16:31:32 -0700

    Add a somewhat more sensible page title algorithm
    In particular, it handles "nil" somewhat well. Perhaps it should be
    out-lined though.

--- spec/web/prefix1_spec.rb
+++ spec/web/prefix1_spec.rb
@@ -5,7 +5,7 @@ require_relative "spec_helper"
 RSpec.describe Clover, "/prefix1" do
   it "has a page title" do
     visit "/prefix1"
-    expect(page.title).to match(/Rodauth Demo/)
+    expect(page.title).to eq("UbiCloud/Login")
     # ...
   end
 end
