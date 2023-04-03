4329be7926c40c88880e35fa2dea137f856eb8df
Hadi Moshayedi <hadi@ubicloud.com>
2023-04-03 15:21:11 -0700

    Creates IPSec tunnels between the new vm and all other VMs on create.
    To do this,
    1. On rhizome side, introduces `rhizome/bin/setup-ipsec` which does
       the necessary setup to have a tunnel between two VMs.
    2. Clover calls `rhizome/bin/setup-ipsec` to create missing tunnels
       when a VMs mesh is refreshed
    3. After a VM is created, we signal the refresh_mesh semaphore for
       all VMs.
    
    TODO: We will revisit this future, to create a mechanism to achieve:
    
    * Ability to gracefully rotate keys, in two phases (using spi
      and/or reqid and policy changes)
    * Use as few SSH sessions as possible (consecutive cmds are cheapin
      a thread thanks to caching).
    * Being able to batch processing reasonably.
    * Allowing re-resynchronization of vmhost ip xfrm state without
      micromanagement of ultra-fine records in the database to address
      abnormalities or development needs.
    * Solve the concurrency issues with Vm.each {} with overlapping
      transaction snapshots to make sure we don't miss VMs that get
      created.

--- model/vm.rb
+++ model/vm.rb
@@ -6,7 +6,12 @@ class Vm < Sequel::Model
   one_to_one :strand, key: :id
   many_to_one :vm_host
   one_to_many :vm_private_subnet, key: :vm_id
+  one_to_many :ipsec_tunnels, key: :src_vm_id
 
   include SemaphoreMethods
-  semaphore :destroy
+  semaphore :destroy, :refresh_mesh
+
+  def private_subnets
+    vm_private_subnet.map { _1.private_subnet }
+  end
 end
