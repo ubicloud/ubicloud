f5482c97cac4449e1e07143abf838b20e03f04f8
Daniel Farina <daniel@ubicloud.com>
2023-04-03 23:18:04 -0700

    Add Semaphore implementation
    Semaphores are a concurrency control mechanism, in this case
    implemented using database transaction rather than in memory as would
    be seen in an operating system.
    
    Generally, the way they work is this:
    
    * Any thread of execution, e.g. web server, personnel using the REPL,
      other Strands, can increment any semaphore anywhere at any time.
      This is safe.  These could be called producers.
    
    * Only one strand, generally, the Nexus strand, is permitted to decr.
      These could be called consumers.
    
    A 2016 blog post about the concurrency pitfalls solved by semaphores
    can be seen here for detailed information:
    https://www.citusdata.com/blog/2016/08/12/state-machines-to-run-databases/
    
    One major alteration from this blog post is that this implementation
    uses a unary numbering system, e.g. previously a counter would be
    incremented or decremented, but now increment makes a new row and
    decrement deletes all rows in a snapshot.  Snapshots are, as a rule,
    read at the beginning of a work-performing transaction to itemize the
    exact amount of semaphore to decrement.
    
    This row-heavy approach bloats the size of each semaphore increment
    hugely (from less than a bit each to many bytes each) but offers
    potential for progress tracking (a long-standing complaint with a
    counter-only approach) and finer-grained locking in the context of a
    relational database.
    
    We can hybridize approaches later by adding a counter to the records
    and incrementing it in some situations, if we need increments and
    decrements to be cheaper in the storage dimension.  The downside is
    that a long transaction that has incremented the counter but not yet
    committed squats on the lock, and more software engineering is
    required to defer modifications of semaphores so the locks are held
    briefly.

--- model/semaphore.rb
+++ model/semaphore.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
+require "ulid"
+require_relative "../model"
+
 class Semaphore < Sequel::Model
+  def self.incr(strand_id, name)
+    DB.transaction do
+      Strand.dataset.where(id: strand_id).update(schedule: Sequel::CURRENT_TIMESTAMP)
+      Semaphore.create(strand_id: strand_id, name: name) do
+        # Use ULIDs since semaphores have temporal locality, and there's
+        # not any harm in letting date information leak from their
+        # identifier.
+        _1.id = ULID.generate.to_uuidish
+      end
+    end
+  end
 end
--- prog/base.rb
+++ prog/base.rb
@@ -3,8 +3,38 @@
 class Prog::Base
   attr_reader :strand
 
-  def initialize(strand)
+  def initialize(strand, snap = nil)
+    @snap = snap || SemSnap.new(strand.id)
     @strand = strand
+  end
+
+  def self.semaphores(names)
+    names = case names
+    in Symbol
+      [names]
+    in [Symbol]
+      names
+    else
+      fail "BUG: only symbols should be passed to self.semaphores"
+    end
+
+    names.each do |name|
+      define_method "incr_#{name}" do
+        @snap.incr(name)
+      end
+
+      define_method "decr_#{name}" do
+        @snap.decr(name)
+      end
+
+      class_eval %{
+def when_#{name}_set?
+  if @snap.set?(#{name.inspect})
+    yield
+  end
+end
+}, __FILE__, __LINE__ - 6
+    end
   end
 
   def nap(seconds = 30)
