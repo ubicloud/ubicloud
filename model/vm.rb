c2c8317bfef794e8764e7019a0815dc5896cad82
Daniel Farina <daniel@ubicloud.com>
2023-04-04 11:33:08 -0700

    Implement rudimentary VM destroy semaphore
    This allows any thread, such as an operator in the REPL, or a web
    server process, or other Strands, to destroy Vms like so:
    
        Vm['id'].incr_destroy
    
    It is "rudimentary" because it's not idempotent nor re-entrant: a
    second attempt to delete, if the first was interrupted, will crash.

--- model/vm.rb
+++ model/vm.rb
@@ -6,4 +6,7 @@ class Vm < Sequel::Model
   one_to_one :strand, key: :id
   many_to_one :vm_host
   one_to_many :vm_private_subnet, key: :vm_id
+
+  include SemaphoreMethods
+  semaphores :destroy
 end
