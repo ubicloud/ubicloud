11737abb03877eb3857106ed91087322fcf8b300
Enes Çakır <enes@cakir.web.tr>
2023-04-07 02:13:32 +0300

    format

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,55 @@
-$(".toggle-mobile-menu").on("click", function(event) {
+$(".toggle-mobile-menu").on("click", function (event) {
     if ($("#mobile-menu").is(":hidden")) {
-        $("#mobile-menu").show(0, function() {
-            $("#mobile-menu .menu-outer").addClass("opacity-100").removeClass("opacity-0");
-            $("#mobile-menu .menu-inner").addClass("translate-x-0").removeClass("-translate-x-full");
+        $("#mobile-menu").show(0, function () {
+            $("#mobile-menu .menu-outer")
+                .addClass("opacity-100")
+                .removeClass("opacity-0");
+            $("#mobile-menu .menu-inner")
+                .addClass("translate-x-0")
+                .removeClass("-translate-x-full");
         });
     } else {
-        $("#mobile-menu .menu-outer").addClass("opacity-0").removeClass("opacity-100");
-        $("#mobile-menu .menu-inner").addClass("-translate-x-full").removeClass("translate-x-0");
-        setTimeout(function(){
+        $("#mobile-menu .menu-outer")
+            .addClass("opacity-0")
+            .removeClass("opacity-100");
+        $("#mobile-menu .menu-inner")
+            .addClass("-translate-x-full")
+            .removeClass("translate-x-0");
+        setTimeout(function () {
             $("#mobile-menu").hide();
         }, 300);
     }
 });
 
-$(".radio-small-cards input[type=radio]").on("change", function(event) {
+$(".radio-small-cards input[type=radio]").on("change", function (event) {
     let name = $(this).attr("name");
     $(`#${name}-radios label`)
         .addClass("ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50")
         .removeClass("ring-2 ring-indigo-600 ring-offset-2 bg-indigo-600 text-white hover:bg-indigo-500");
-    $(this).parent()
+    $(this)
+        .parent()
         .removeClass("ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50")
         .addClass("ring-2 ring-indigo-600 ring-offset-2 bg-indigo-600 text-white hover:bg-indigo-500");
 });
 
-$(".radio-stacked-cards input[type=radio]").on("change", function(event) {
+$(".radio-stacked-cards input[type=radio]").on("change", function (event) {
     let name = $(this).attr("name");
     $(`#${name}-radios label`).removeClass("border-indigo-600 ring-2 ring-indigo-600");
-    $(`#${name}-radios label span.pointer-events-none`).removeClass("border-transparent").addClass("border-2");
+    $(`#${name}-radios label span.pointer-events-none`)
+        .removeClass("border-transparent")
+        .addClass("border-2");
 
-    $(this).parent().addClass("border-indigo-600 ring-2 ring-indigo-600");
-    $(this).parent().children("span.pointer-events-none").removeClass("border-2").addClass("border-transparent");
+    $(this)
+        .parent()
+        .addClass("border-indigo-600 ring-2 ring-indigo-600");
+    $(this)
+        .parent()
+        .children("span.pointer-events-none")
+        .removeClass("border-2")
+        .addClass("border-transparent");
 });
 
-$(".delete-btn").on("click", function(event) {
+$(".delete-btn").on("click", function (event) {
     event.preventDefault();
     let url = $(this).data("url");
     let csrf = $(this).data("csrf");
@@ -50,8 +67,8 @@ $(".delete-btn").on("click", function(event) {
     $.ajax({
         url: url,
         type: 'DELETE',
-        data: {'_csrf': csrf},
-        success: function(result) {
+        data: { '_csrf': csrf },
+        success: function (result) {
             window.location.href = "/vm";
         },
         error: function (xhr, ajaxOptions, thrownError) {
@@ -60,22 +77,22 @@ $(".delete-btn").on("click", function(event) {
     });
 });
 
-$(".copy-content").on("click", function(event) {
+$(".copy-content").on("click", function (event) {
     let content = $(this).data("content");
     let message = $(this).data("message");
     navigator.clipboard.writeText(content);
     notification(message);
 })
 
-$(".back-btn").on("click", function(event) {
+$(".back-btn").on("click", function (event) {
     event.preventDefault();
     history.back();
 })
 
 // Show price change for demo
-$("#location-radios input[type=radio]").on("change", function(event) {
+$("#location-radios input[type=radio]").on("change", function (event) {
     let location = $(this).val();
-    $('#size-radios .size-price').each(function(i, obj) {
+    $('#size-radios .size-price').each(function (i, obj) {
         let prices = $(this).data("prices");
         let price = prices[location] || prices["default"]
         $(this).text(`$${price.toFixed(2)}`)
@@ -86,11 +103,13 @@ function notification(message) {
     let container = $("#notification-template").parent();
     let newNotification = $("#notification-template").clone();
     newNotification.find("p").text(message);
-    newNotification.appendTo(container).show(0, function() {
-        $(this).removeClass("translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2").addClass("translate-y-0 opacity-100 sm:translate-x-0");
+    newNotification.appendTo(container).show(0, function () {
+        $(this)
+            .removeClass("translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2")
+            .addClass("translate-y-0 opacity-100 sm:translate-x-0");
     });
 
-    setTimeout(function(){
+    setTimeout(function () {
         newNotification.remove();
     }, 2000);
 }
