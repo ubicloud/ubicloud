f1f9e555d7b6c78c7e4683c283021e5ecb122669
Enes Cakir <enes@ubicloud.com>
2023-04-25 23:28:18 +0300

    Add UI tests for Vm and VmHost views
    Frontend is difficult to test. JavaScript engine is required to test
    some interactive functionalities. We are planning to use more
    sophisticated frontend test frameworks in future.
    
    But for now, we need to be sure our UI is not broken by commits.
    Testing all pages manually is not very feasible.
    
    Capybara helps to test UI easily. We don't need JavaScript for
    basic functionalities. Actually, Capybara has Selenium driver to
    run JavaScript codes too, but it slow downs tests.

--- routes/object_storage.rb
+++ routes/object_storage.rb
@@ -3,6 +3,7 @@
 class Clover
   hash_branch("object-storage") do |r|
     r.get true do
+      @page_title = "Object Storage"
       view content: render("components/page_header", locals: {title: "This service is under development"})
     end
   end
--- spec/web/spec_helper.rb
+++ spec/web/spec_helper.rb
@@ -6,12 +6,10 @@ raise "test database doesn't end with test" if DB.opts[:database] && !DB.opts[:d
 require "capybara"
 require "capybara/rspec"
 require "rack/test"
+require "argon2"
 
 Gem.suffix_pattern
 
-Clover.plugin :not_found do
-  raise "404 - File Not Found"
-end
 Clover.plugin :error_handler do |e|
   raise e
 end
@@ -34,4 +32,29 @@ RSpec.configure do |config|
     Capybara.reset_sessions!
     Capybara.use_default_driver
   end
+
+  config.before(:suite) do
+    # Create a default user to use in all test if not exits
+    # Database cleaner can't trunca accounts tables because of
+    # account of `ph` user separation.
+    unless DB[:accounts].where(email: "user@example.com").first
+      hash = Argon2::Password.new({
+        t_cost: 1,
+        m_cost: 3,
+        secret: Config.clover_session_secret
+      }).create("0123456789")
+
+      account_id = DB[:accounts].insert(email: "user@example.com", status_id: 2)
+      DB[:account_password_hashes].insert(id: account_id, password_hash: hash)
+    end
+  end
+end
+
+def login(email = "user@example.com", password = "0123456789")
+  visit "/login"
+  fill_in "Email address", with: email
+  fill_in "Password", with: password
+  click_button "Sign in"
+
+  expect(page.title).to eq("Ubicloud - Dashboard")
 end
