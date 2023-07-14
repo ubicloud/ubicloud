dffc983b3e4e4268b822e599c9cb5c006cee34b8
Enes Cakir <enes@ubicloud.com>
2023-07-14 20:05:49 +0300

    Extract project/user and project/policy routes to nested route files
    Project route file has nested user and policy routes. We decided to show
    resources of single project at a time on console. I will move vm routes
    to under the project prefix. So routes/project.rb file will get bigger.
    I decided to move route subgroups to separate files using hash_branches
    with namespace similar to what I did in API routes. It makes routes more
    maintainable.

---
+++ routes/web/project/policy.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class CloverWeb
+  hash_branch(:project_prefix, "policy") do |r|
+    Authorization.authorize(@current_user.id, "Project:policy", @project.id)
+    @serializer = Serializers::Web::AccessPolicy
+
+    r.get true do
+      # For UI simplicity, we are showing only one policy at the moment
+      @policy = serialize(@project.access_policies.first)
+
+      view "project/show_policies"
+    end
+
+    r.is String do |policy_ulid|
+      policy = AccessPolicy.from_ulid(policy_ulid)
+
+      unless policy
+        response.status = 404
+        r.halt
+      end
+
+      r.post true do
+        body = r.params["body"]
+
+        begin
+          fail JSON::ParserError unless JSON.parse(body).is_a?(Hash)
+        rescue JSON::ParserError
+          flash["error"] = "The policy isn't a valid JSON object."
+          return redirect_back_with_inputs
+        end
+
+        policy.update(body: body)
+
+        flash["notice"] = "'#{policy.name}' policy updated for '#{@project.name}'"
+
+        r.redirect "#{@project.path}/policy"
+      end
+    end
+  end
+end
