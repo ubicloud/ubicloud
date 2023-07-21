6691257680627825bae862d00e7b31028fc40591
Enes Cakir <enes@ubicloud.com>
2023-07-21 17:54:01 +0300

    Track all files for actual code coverage
    We are using Unreloader with autoload feature. It's not loading
    files when file isn't used. So when a file not used in any test,
    that file isn't shown in coverage report. But the file should be
    in coverage report zero coverage.
    
    `SimpleCov.track_files` helps to track all files.
    
    This change decreased our current code coverage limits.

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -5,16 +5,26 @@ if (suite = ENV.delete("COVERAGE"))
 
   SimpleCov.start do
     enable_coverage :branch
-    minimum_coverage line: 95, branch: 91.5
-    minimum_coverage_by_file line: 60, branch: 50
+    minimum_coverage line: 90, branch: 90
+    minimum_coverage_by_file line: 0, branch: 50
 
     command_name suite
 
+    # rhizome (dataplane) and controlplane should have separate coverage reports.
+    # They will have different coverage suites in future.
+    add_filter "/rhizome"
+
+    # No need to check coverage for them
+    add_filter "/migrate/"
     add_filter "/spec/"
-    add_filter "/model.rb"
     add_filter "/db.rb"
+    add_filter "/model.rb"
+    add_filter "/loader.rb"
     add_filter "/.env.rb"
+
     add_group("Missing") { |src| src.covered_percent < 100 }
     add_group("Covered") { |src| src.covered_percent == 100 }
+
+    track_files "**/*.rb"
   end
 end
