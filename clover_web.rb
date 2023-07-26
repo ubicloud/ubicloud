5fa05edc331794964c44245a7513826d26e86395
Enes Cakir <enes@ubicloud.com>
2023-07-26 14:23:00 +0300

    Purify access policy JSON before highlighting
    Dependabot created two security alerts titled as "DOM text reinterpreted
    as HTML" for `pre.html(jsonHighlight(pre.text())` lines.
    
    It says "Extracting text from a DOM node and interpreting it as HTML can
    lead to a cross-site scripting vulnerability."
    
    People and ChatGPT recommend to use
    [DOMPurify](https://github.com/cure53/DOMPurify) library to sanitize
    HTML input.
    
    Actually we have very strict Content-Security-Policy and don't allow
    inline scripts.
    
    But to be future-proof, I added DOMPurify. I hope it will resolve these
    security alerts. I couldn't find a way to be sure is it resolving or not
    before merging it.

--- clover_web.rb
+++ clover_web.rb
@@ -24,7 +24,7 @@ class CloverWeb < Roda
     csp.style_src :self
     csp.img_src :self
     csp.form_action :self
-    csp.script_src :self, "https://cdn.jsdelivr.net"
+    csp.script_src :self, "https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js", "https://cdn.jsdelivr.net/npm/dompurify@3.0.5/dist/purify.min.js"
     csp.connect_src :self
     csp.base_uri :none
     csp.frame_ancestors :none
