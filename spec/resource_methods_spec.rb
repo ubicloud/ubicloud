ee8ae03470be05ab038782441d331270472a7d7b
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2023-07-31 06:00:08 +0300

    Scrub encrypted columns before archiving
    This is an improvement over existing soft delete mechanism. We continue to
    archive models that has encrpted columns, but we scrub those columns before
    archiving.

--- spec/resource_methods_spec.rb
+++ spec/resource_methods_spec.rb
@@ -3,9 +3,11 @@
 RSpec.describe ResourceMethods do
   let(:sa) { Sshable.create_with_id(host: "test.localhost", raw_private_key_1: SshKey.generate.keypair) }
 
-  it "archives the model when deleted" do
-    values_hash = sa.values.merge(model_name: "Sshable")
-    expect(DeletedRecord).to receive(:create).with(hash_including(model_values: values_hash))
+  it "archives scrubbed version of the model when deleted" do
+    scrubbed_values_hash = sa.values.merge(model_name: "Sshable")
+    scrubbed_values_hash.delete(:raw_private_key_1)
+    scrubbed_values_hash.delete(:raw_private_key_2)
+    expect(DeletedRecord).to receive(:create).with(hash_including(model_values: scrubbed_values_hash))
     sa.destroy
   end
 end
