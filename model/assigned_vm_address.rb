87f3ed45e580865effd121f1dfc05e88600f525f
Enes Cakir <enes@ubicloud.com>
2023-08-08 15:33:44 +0300

    Create billing record for IPv4 address
    IPv4 is a very scarce resource, and it starts to cost. Other cloud
    providers charge extra when user enable IPv4. We decided to charge
    $3 monthly for IPv4.

--- model/assigned_vm_address.rb
+++ model/assigned_vm_address.rb
@@ -5,6 +5,7 @@ require_relative "../model"
 class AssignedVmAddress < Sequel::Model
   one_to_one :vm, key: :dst_vm_id
   many_to_one :address, key: :address_id
+  one_to_one :active_billing_record, class: :BillingRecord, key: :resource_id, conditions: {Sequel.function(:upper, :span) => nil}
 
   include ResourceMethods
 end
