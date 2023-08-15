30ded75cb398faf105fa6bacce6da41a3448cd1b
Daniel Farina <daniel@ubicloud.com>
2023-08-14 19:22:06 -0700

    Add links validator

--- manual/astro.config.mjs
+++ manual/astro.config.mjs
@@ -1,30 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlightLinksValidator(),
+    starlight({
+      title: "Ubicloud Documenation",
+      social: {
+        github: "https://github.com/ubicloud/ubicloud",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Build your own cloud",
+              link: "/quick-start/build-your-own-cloud/",
+            },
+            { label: "Managed Service", link: "/quick-start/managed-service" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
