532c78694649e92147622dbc2383bad043190859
Enes Cakir <enes@ubicloud.com>
2023-09-01 17:45:44 +0300

    Add remaining credit and discount percentage project detail page
    Show them only if different than zero

--- serializers/web/project.rb
+++ serializers/web/project.rb
@@ -7,6 +7,8 @@ class Serializers::Web::Project < Serializers::Base
       ubid: p.ubid,
       path: p.path,
       name: p.name,
+      credit: p.credit.to_f,
+      discount: p.discount,
       provider: Option::Providers[p.provider]
     }
   end
