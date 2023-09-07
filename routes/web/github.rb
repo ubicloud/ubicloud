23bb665a2ad4c41d65f1925961df7dc0c861a7de
Enes Cakir <enes@ubicloud.com>
2023-09-07 15:04:40 +0300

    Add callback route for new GitHub App installation
    GitHub redirects to callback URL with installation_id and code after an
    user install or configure our GitHub app.
    
    We use project id from session that we persist while initiating install
    flow. If user didn't install our GitHub app via "Connect" button on our
    console, they need to reinstall it. It is requirement, because we need
    project to associate with installation. In future as an improvement, we
    can show project selector if no project id in session.
    
    Callback is just a GET request, anyone can change query parameters and
    set any installation_id. We use oauth code to verify request is from
    GitHub and installation_id is belongs to user.
    
    This blog post has good summary:
    https://roadie.io/blog/avoid-leaking-github-org-data/

---
+++ routes/web/github.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class CloverWeb
+  hash_branch("github") do |r|
+    r.get "callback" do
+      oauth_code = r.params["code"]
+      installation_id = r.params["installation_id"]
+
+      code_response = Github.oauth_client.exchange_code_for_token(oauth_code)
+
+      unless (access_token = code_response[:access_token]) &&
+          (installation_response = Octokit::Client.new(access_token: access_token).get("/user/installations")[:installations].find { _1[:id].to_s == installation_id })
+        flash["error"] = "GitHub App installation failed."
+        r.redirect "/dashboard"
+      end
+
+      if (installation = GithubInstallation[installation_id: installation_id])
+        Authorization.authorize(@current_user.id, "Project:github", installation.project.id)
+        flash["notice"] = "GitHub runner integration is already enabled for #{installation.project.name} project."
+        r.redirect "#{installation.project.path}/github"
+      end
+
+      unless (project = Project[session.delete("github_installation_project_id")])
+        flash["error"] = "Install GitHub App from project's 'GitHub Runners' page."
+        r.redirect "/dashboard"
+      end
+
+      Authorization.authorize(@current_user.id, "Project:github", project.id)
+
+      GithubInstallation.create_with_id(
+        installation_id: installation_id,
+        name: (installation_response[:account][:login] || installation_response[:account][:name]),
+        type: installation_response[:account][:type],
+        project_id: project.id
+      )
+
+      flash["notice"] = "GitHub runner integration is enabled for #{project.name} project."
+      r.redirect "#{project.path}/github"
+    end
+  end
+end
