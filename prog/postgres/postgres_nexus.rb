a259c4508efd64d039619b5789ba52cf69bde472
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2023-09-13 21:26:20 +0300

    Postgres v0.1
    This is the initial commit for PostgreSQL service. It contains bare minimums
    to introduce PostgreSQL service. Currently, we only create a virtual machine,
    install PostgreSQL binaries in it and do some basic configurations. In the
    upcoming commits we will add things like UI, API support, HA, backup/restore,
    firewall rules, DNS names, scaling up/down, certificates and many more. Even
    though this is incomplete, it provides enough infrastructure to build on top
    of it.
    
    Here are few notable design decisions we made in this commit;
    - We don't ask for database name, username or password from user. They will
    all be created by default. Database name and username is postgres, password
    will be auto generated at run-time.
    - Corollary of above, we are giving superuser access to users. This might be
    biggest divergence of our postgres service implementation from the incumbents
    but it is not an unseen practice. There are numerous benefits of this; most
    importantly, it gives more power to users, which could tilt decision to our
    favor for users who want or need superuser access. Secondly, it makes our
    implementation simpler; we don't need to implement special logic to support
    role or extension creation. Users can do these things themselves without
    needing us to provide special API/UI. On the flip side, it is possible to
    shoot yourself on the foot with too much power, and thus create additional
    support burden. I expect this to be rare occasion, but we might want to
    consider our support model for such cases.
    - We are also introducing a new concept, that is not just about postgres in
    this commit; service projects. PostgreSQL service only exposes PostgreSQL
    resource to user not VMs or public IP addresses it creates in the back end.
    Those resources needs to live in somewhere. Service project is exactly that.
    Each service will have their own account and project where they can keep
    the backing resources. In this commit, we are creating one project for all
    PostgreSQL services' resources, but after gathering more data, we might
    change that in the future.
    
    Currently there is no API or GUI interface. To be able to create PostgreSQL
    resources, you need to set ENV["POSTGRES_SERVICE_PROJECT_ID"] in your .env.rb.
    
    Then you can create new postgres database with this command;
    Prog::Postgres::PostgresNexus.assemble(<project-id>, <location>, "test-pg-server", "standard-4", 100)
    
    After it is ready, you can get the connection string like this;
    Postgres::PostgresServer.first.connection_string

---
+++ prog/postgres/postgres_nexus.rb
@@ -0,0 +1,185 @@
+# frozen_string_literal: true
+
+require "forwardable"
+
+require_relative "../../lib/util"
+
+class Prog::Postgres::PostgresNexus < Prog::Base
+  subject_is :postgres_server
+
+  extend Forwardable
+  def_delegators :postgres_server, :vm
+
+  semaphore :restart, :destroy
+
+  def self.assemble(project_id, location, server_name, vm_size, storage_size_gib)
+    unless (project = Project[project_id])
+      fail "No existing project"
+    end
+
+    Validation.validate_vm_size(vm_size)
+    Validation.validate_name(server_name)
+    Validation.validate_location(location, project.provider)
+
+    DB.transaction do
+      ubid = PostgresServer.generate_ubid
+
+      ssh_key = SshKey.generate
+      vm_st = Prog::Vm::Nexus.assemble(
+        ssh_key.public_key,
+        Config.postgres_service_project_id,
+        location: location,
+        name: ubid.to_s,
+        size: vm_size,
+        storage_volumes: [
+          {encrypted: true, size_gib: 30},
+          {encrypted: true, size_gib: storage_size_gib}
+        ],
+        boot_image: "ubuntu-jammy",
+        enable_ip4: true
+      )
+
+      Sshable.create(
+        unix_user: "ubi",
+        host: "temp_#{vm_st.id}",
+        raw_private_key_1: ssh_key.keypair
+      ) { _1.id = vm_st.id }
+
+      postgres_server = PostgresServer.create(
+        project_id: project_id, location: location, server_name: server_name,
+        target_vm_size: vm_size, target_storage_size_gib: storage_size_gib,
+        superuser_password: SecureRandom.base64(15).gsub(/[+\/]/, "+" => "_", "/" => "-"),
+        vm_id: vm_st.id
+      ) { _1.id = ubid.to_uuid }
+      postgres_server.associate_with_project(project)
+
+      Strand.create(prog: "Postgres::PostgresNexus", label: "start") { _1.id = postgres_server.id }
+    end
+  end
+
+  def before_run
+    when_destroy_set? do
+      if strand.label != "destroy"
+        postgres_server.active_billing_records.each do |br|
+          br.update(span: Sequel.pg_range(br.span.begin...Time.now))
+        end
+
+        hop_destroy
+      end
+    end
+  end
+
+  label def start
+    nap 5 unless vm.strand.label == "wait"
+    vm.sshable.update(host: vm.ephemeral_net4)
+    hop_bootstrap_rhizome
+  end
+
+  label def bootstrap_rhizome
+    register_deadline(:wait, 10 * 60)
+
+    bud Prog::BootstrapRhizome, {"target_folder" => "postgres", "subject_id" => vm.id, "user" => "ubi"}
+    hop_wait_bootstrap_rhizome
+  end
+
+  label def wait_bootstrap_rhizome
+    reap
+    hop_mount_data_disk if leaf?
+    donate
+  end
+
+  label def mount_data_disk
+    case vm.sshable.cmd("common/bin/daemonizer --check format_disk")
+    when "Succeeded"
+      vm.sshable.cmd("sudo mkdir -p /dat")
+      device_path = vm.vm_storage_volumes.find { _1.boot == false }.device_path.shellescape
+
+      vm.sshable.cmd("sudo common/bin/add_to_fstab #{device_path} /dat ext4 defaults 0 0")
+      vm.sshable.cmd("sudo mount #{device_path} /dat")
+
+      hop_install_postgres
+    when "Failed", "NotStarted"
+      device_path = vm.vm_storage_volumes.find { _1.boot == false }.device_path.shellescape
+      vm.sshable.cmd("common/bin/daemonizer 'sudo mkfs --type ext4 #{device_path}' format_disk")
+    end
+
+    nap 5
+  end
+
+  label def install_postgres
+    case vm.sshable.cmd("common/bin/daemonizer --check install_postgres")
+    when "Succeeded"
+      postgres_server.incr_restart
+      hop_configure
+    when "Failed", "NotStarted"
+      vm.sshable.cmd("common/bin/daemonizer 'sudo postgres/bin/install_postgres' install_postgres")
+    end
+
+    nap 5
+  end
+
+  label def configure
+    case vm.sshable.cmd("common/bin/daemonizer --check configure")
+    when "Succeeded"
+      hop_create_billing_record
+    when "Failed", "NotStarted"
+      configure_hash = postgres_server.configure_hash
+      vm.sshable.cmd("common/bin/daemonizer 'sudo postgres/bin/configure' configure", stdin: JSON.generate(configure_hash))
+    end
+
+    nap 5
+  end
+
+  label def create_billing_record
+    BillingRecord.create_with_id(
+      project_id: postgres_server.project_id,
+      resource_id: postgres_server.id,
+      resource_name: postgres_server.server_name,
+      billing_rate_id: BillingRate.from_resource_properties("PostgresCores", "standard", postgres_server.location)["id"],
+      amount: vm.cores
+    )
+
+    BillingRecord.create_with_id(
+      project_id: postgres_server.project_id,
+      resource_id: postgres_server.id,
+      resource_name: postgres_server.server_name,
+      billing_rate_id: BillingRate.from_resource_properties("PostgresStorage", "standard", postgres_server.location)["id"],
+      amount: postgres_server.target_storage_size_gib
+    )
+
+    when_restart_set? do
+      hop_restart
+    end
+
+    hop_wait
+  end
+
+  label def restart
+    decr_restart
+    vm.sshable.cmd("sudo postgres/bin/restart")
+    hop_wait
+  end
+
+  label def wait
+    nap 30
+  end
+
+  label def destroy
+    register_deadline(nil, 5 * 60)
+    reap
+
+    decr_destroy
+
+    if vm
+      vm.private_subnets.each { _1.incr_destroy }
+      vm.sshable&.destroy
+      vm.incr_destroy
+      nap 5
+    end
+
+    postgres_server.dissociate_with_project(postgres_server.project)
+    postgres_server.destroy
+
+    pop "postgres server is deleted"
+  end
+end
---
+++ spec/prog/postgres/postgres_nexus_spec.rb
@@ -0,0 +1,253 @@
+# frozen_string_literal: true
+
+require_relative "../../model/spec_helper"
+
+RSpec.describe Prog::Postgres::PostgresNexus do
+  subject(:nx) { described_class.new(Strand.new(id: PostgresServer.generate_uuid)) }
+
+  let(:project) { Project.create_with_id(name: "default", provider: "hetzner").tap { _1.associate_with_project(_1) } }
+
+  let(:postgres_server) { instance_double(PostgresServer, id: "0eb058bb-960e-46fe-aab7-3717f164ab25", project_id: project.id, server_name: "pg-server-name", location: "hetzner-hel1", target_storage_size_gib: 100) }
+  let(:vm) { instance_double(Vm, id: "788525ed-d6f0-4937-a844-323d4fd91946", cores: 1) }
+  let(:sshable) { instance_double(Sshable) }
+
+  before do
+    allow(vm).to receive(:sshable).and_return(sshable)
+    allow(postgres_server).to receive_messages(project: project, vm: vm)
+    allow(nx).to receive(:postgres_server).and_return(postgres_server)
+  end
+
+  describe ".assemble" do
+    let(:postgres_project) { Project.create_with_id(name: "default", provider: "hetzner").tap { _1.associate_with_project(_1) } }
+
+    before do
+      allow(Config).to receive(:postgres_service_project_id).and_return(postgres_project.id)
+    end
+
+    it "validates input" do
+      expect {
+        described_class.assemble(SecureRandom.uuid, "hetzner-hel1", "pg-server-name", "standard-2", 100)
+      }.to raise_error RuntimeError, "No existing project"
+
+      expect {
+        described_class.assemble(project.id, "hetzner-xxx", "pg-server-name", "standard-2", 100)
+      }.to raise_error Validation::ValidationFailed, "Validation failed for following fields: provider"
+
+      expect {
+        described_class.assemble(project.id, "hetzner-hel1", "pg/server/name", "standard-2", 100)
+      }.to raise_error Validation::ValidationFailed, "Validation failed for following fields: name"
+
+      expect {
+        described_class.assemble(project.id, "hetzner-hel1", "pg-server-name", "standard-128", 100)
+      }.to raise_error Validation::ValidationFailed, "Validation failed for following fields: size"
+    end
+
+    it "creates postgres server and vm with sshable" do
+      st = described_class.assemble(project.id, "hetzner-hel1", "pg-server-name", "standard-2", 100)
+
+      postgres_server = PostgresServer[st.id]
+      expect(postgres_server).not_to be_nil
+      expect(postgres_server.vm).not_to be_nil
+      expect(postgres_server.vm.sshable).not_to be_nil
+    end
+  end
+
+  describe "#before_run" do
+    it "hops to destroy and stops billing records when needed" do
+      br = instance_double(BillingRecord, span: (Time.now - 100)..(Time.now))
+      expect(br).to receive(:update).twice
+      expect(postgres_server).to receive(:active_billing_records).and_return([br, br])
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect { nx.before_run }.to hop("destroy")
+    end
+
+    it "does not hop to destroy if already in the destroy state" do
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect(nx.strand).to receive(:label).and_return("destroy")
+      expect { nx.before_run }.not_to hop("destroy")
+    end
+  end
+
+  describe "#start" do
+    it "naps if vm not ready" do
+      expect(vm).to receive(:strand).and_return(Strand.new(label: "prep"))
+      expect { nx.start }.to nap(5)
+    end
+
+    it "update sshable host and hops" do
+      expect(vm).to receive(:strand).and_return(Strand.new(label: "wait"))
+      expect(vm).to receive(:ephemeral_net4).and_return("1.1.1.1")
+      expect(sshable).to receive(:update).with(host: "1.1.1.1")
+      expect { nx.start }.to hop("bootstrap_rhizome")
+    end
+  end
+
+  describe "#bootstrap_rhizome" do
+    it "buds a bootstrap rhizome process" do
+      expect(nx).to receive(:register_deadline)
+      expect(nx).to receive(:bud).with(Prog::BootstrapRhizome, {"target_folder" => "postgres", "subject_id" => vm.id, "user" => "ubi"})
+      expect { nx.bootstrap_rhizome }.to hop("wait_bootstrap_rhizome")
+    end
+  end
+
+  describe "#wait_bootstrap_rhizome" do
+    before { expect(nx).to receive(:reap) }
+
+    it "hops to mount_data_disk if there are no sub-programs running" do
+      expect(nx).to receive(:leaf?).and_return true
+
+      expect { nx.wait_bootstrap_rhizome }.to hop("mount_data_disk")
+    end
+
+    it "donates if there are sub-programs running" do
+      expect(nx).to receive(:leaf?).and_return false
+      expect(nx).to receive(:donate).and_call_original
+
+      expect { nx.wait_bootstrap_rhizome }.to nap(0)
+    end
+  end
+
+  describe "#mount_data_disk" do
+    it "formats data disk if format command is not sent yet or failed" do
+      expect(vm).to receive(:vm_storage_volumes).and_return([instance_double(VmStorageVolume, boot: true, device_path: "/dev/vda"), instance_double(VmStorageVolume, boot: false, device_path: "/dev/vdb")]).twice
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer 'sudo mkfs --type ext4 /dev/vdb' format_disk").twice
+
+      # NotStarted
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check format_disk").and_return("NotStarted")
+      expect { nx.mount_data_disk }.to nap(5)
+
+      # Failed
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check format_disk").and_return("Failed")
+      expect { nx.mount_data_disk }.to nap(5)
+    end
+
+    it "mounts data disk if format disk is succeeded and hops to install_postgres" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check format_disk").and_return("Succeeded")
+      expect(vm).to receive(:vm_storage_volumes).and_return([instance_double(VmStorageVolume, boot: true, device_path: "/dev/vda"), instance_double(VmStorageVolume, boot: false, device_path: "/dev/vdb")])
+      expect(sshable).to receive(:cmd).with("sudo mkdir -p /dat")
+      expect(sshable).to receive(:cmd).with("sudo common/bin/add_to_fstab /dev/vdb /dat ext4 defaults 0 0")
+      expect(sshable).to receive(:cmd).with("sudo mount /dev/vdb /dat")
+      expect { nx.mount_data_disk }.to hop("install_postgres")
+    end
+
+    it "naps if script return unknown status" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check format_disk").and_return("Unknown")
+      expect { nx.mount_data_disk }.to nap(5)
+    end
+  end
+
+  describe "#install_postgres" do
+    it "triggers install_postgres if install_postgres command is not sent yet or failed" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer 'sudo postgres/bin/install_postgres' install_postgres").twice
+
+      # NotStarted
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check install_postgres").and_return("NotStarted")
+      expect { nx.install_postgres }.to nap(5)
+
+      # Failed
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check install_postgres").and_return("Failed")
+      expect { nx.install_postgres }.to nap(5)
+    end
+
+    it "hops to configure if install_postgres command is succeeded and increments restart semaphore" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check install_postgres").and_return("Succeeded")
+      expect(postgres_server).to receive(:incr_restart)
+      expect { nx.install_postgres }.to hop("configure")
+    end
+
+    it "naps if script return unknown status" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check install_postgres").and_return("Unknown")
+      expect { nx.install_postgres }.to nap(5)
+    end
+  end
+
+  describe "#configure" do
+    it "triggers configure if configure command is not sent yet or failed" do
+      expect(postgres_server).to receive(:configure_hash).and_return("dummy-configure-hash").twice
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer 'sudo postgres/bin/configure' configure", stdin: JSON.generate("dummy-configure-hash")).twice
+
+      # NotStarted
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check configure").and_return("NotStarted")
+      expect { nx.configure }.to nap(5)
+
+      # Failed
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check configure").and_return("Failed")
+      expect { nx.configure }.to nap(5)
+    end
+
+    it "hops to create_billing_record if configure command is succeeded" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check configure").and_return("Succeeded")
+      expect { nx.configure }.to hop("create_billing_record")
+    end
+
+    it "naps if script return unknown status" do
+      expect(sshable).to receive(:cmd).with("common/bin/daemonizer --check configure").and_return("Unknown")
+      expect { nx.configure }.to nap(5)
+    end
+  end
+
+  describe "#create_billing_record" do
+    it "creates billing record for cores and storage then hops" do
+      expect(nx).to receive(:when_restart_set?)
+
+      expect(BillingRecord).to receive(:create_with_id).with(
+        project_id: postgres_server.project_id,
+        resource_id: postgres_server.id,
+        resource_name: postgres_server.server_name,
+        billing_rate_id: BillingRate.from_resource_properties("PostgresCores", "standard", postgres_server.location)["id"],
+        amount: vm.cores
+      )
+
+      expect(BillingRecord).to receive(:create_with_id).with(
+        project_id: postgres_server.project_id,
+        resource_id: postgres_server.id,
+        resource_name: postgres_server.server_name,
+        billing_rate_id: BillingRate.from_resource_properties("PostgresStorage", "standard", postgres_server.location)["id"],
+        amount: postgres_server.target_storage_size_gib
+      )
+
+      expect { nx.create_billing_record }.to hop("wait")
+    end
+
+    it "hops to restart if restart semaphore is incremented" do
+      expect(nx).to receive(:when_restart_set?).and_yield
+      expect { nx.create_billing_record }.to hop("restart")
+    end
+  end
+
+  describe "#restart" do
+    it "restarts and hops to wait" do
+      expect(nx).to receive(:decr_restart)
+      expect(sshable).to receive(:cmd).with("sudo postgres/bin/restart")
+      expect { nx.restart }.to hop("wait")
+    end
+  end
+
+  describe "#wait" do
+    it "naps" do
+      expect { nx.wait }.to nap(30)
+    end
+  end
+
+  describe "#destroy" do
+    before { expect(nx).to receive(:reap).at_least(:once) }
+
+    it "triggers vm deletion and waits until it is deleted" do
+      expect(sshable).to receive(:destroy)
+      expect(vm).to receive(:private_subnets).and_return([])
+      expect(vm).to receive(:incr_destroy)
+      expect { nx.destroy }.to nap(5)
+
+      expect(vm).to receive(:sshable).and_return(nil)
+      expect(vm).to receive(:private_subnets).and_return([])
+      expect(vm).to receive(:incr_destroy)
+      expect { nx.destroy }.to nap(5)
+
+      expect(postgres_server).to receive(:vm).and_return(nil)
+      expect(postgres_server).to receive(:dissociate_with_project)
+      expect(postgres_server).to receive(:destroy)
+
+      expect { nx.destroy }.to exit({"msg" => "postgres server is deleted"})
+    end
+  end
+end
