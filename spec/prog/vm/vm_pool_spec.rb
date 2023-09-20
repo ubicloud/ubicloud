4ed3778c9142cc3773a3509912501ad2932df3b1
Furkan Sahin <furkan@ubicloud.com>
2023-09-20 17:32:46 +0200

    Adds Vm Pools
    With this commit, we are adding the initial version of vm pools. The
    idea is to reduce the provisioning time for github runners down by
    eliminating the initial provisioning time.
    
    We simply have a strand in a default project that keeps provisioning VMs
    until the statically defined `size` is hit. As long as there are
    available VMs, GithubRunner first picks them from the pool. If not, the
    provisioning of a new VM starts. Whenever the VM size in the pool drops
    below `size`, we start provisioning a new VM.
    
    In this version of the pool, we have 1 pool per runner_label. In future
    versions, we might specialize it even further for a customer and isolate
    their pool, if needed.

---
+++ spec/prog/vm/vm_pool_spec.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require_relative "../../model/spec_helper"
+
+RSpec.describe Prog::Vm::VmPool do
+  subject(:nx) { described_class.new(st) }
+
+  let(:st) { Strand.new }
+
+  describe ".assemble" do
+    it "creates the entity and strand properly" do
+      st = described_class.assemble(size: 3, vm_size: "standard-2", boot_image: "img", location: "hetzner-fsn1")
+      pool = VmPool[st.id]
+      expect(pool).not_to be_nil
+      expect(pool.size).to eq(3)
+      expect(pool.vm_size).to eq("standard-2")
+      expect(pool.boot_image).to eq("img")
+      expect(pool.location).to eq("hetzner-fsn1")
+      expect(st.label).to eq("create_new_vm")
+    end
+  end
+
+  describe "#create_new_vm" do
+    let(:prj) {
+      Project.create_with_id(name: "default", provider: "hetzner").tap { _1.associate_with_project(_1) }
+    }
+
+    it "creates a new vm and hops to wait" do
+      expect(Config).to receive(:vm_pool_project_id).and_return(prj.id)
+      st = described_class.assemble(size: 3, vm_size: "standard-2", boot_image: "img", location: "hetzner-fsn1")
+      st.update(label: "create_new_vm")
+      expect(SshKey).to receive(:generate).and_call_original
+      expect(nx).to receive(:vm_pool).and_return(VmPool[st.id]).at_least(:once)
+      expect {
+        nx.create_new_vm
+      }.to hop("wait")
+      pool = VmPool[st.id]
+      expect(pool.vms.count).to eq(1)
+      expect(pool.vms.first.sshable).not_to be_nil
+    end
+  end
+
+  describe "#wait" do
+    let(:pool) {
+      VmPool.create_with_id(size: 0, vm_size: "standard-2", boot_image: "img", location: "hetzner-fsn1")
+    }
+
+    it "waits if nothing to do" do
+      expect(nx).to receive(:vm_pool).and_return(pool).at_least(:once)
+      expect {
+        nx.wait
+      }.to nap(30)
+    end
+
+    it "hops to create_new_vm, if vm count is less" do
+      pool.update(size: 1)
+      expect(nx).to receive(:vm_pool).and_return(pool).at_least(:once)
+      expect {
+        nx.wait
+      }.to hop("create_new_vm")
+    end
+  end
+
+  describe "#destroy" do
+    let(:pool) {
+      VmPool.create_with_id(size: 0, vm_size: "standard-2", boot_image: "img", location: "hetzner-fsn1")
+    }
+
+    it "increments vms' destroy semaphore and hops to wait_vms_destroy" do
+      vm = instance_double(Vm)
+      expect(nx).to receive(:vm_pool).and_return(pool)
+      expect(pool).to receive(:vms).and_return([vm])
+      expect(vm).to receive(:incr_destroy)
+      expect {
+        nx.destroy
+      }.to hop("wait_vms_destroy")
+    end
+  end
+
+  describe "#wait_vms_destroy" do
+    let(:pool) {
+      VmPool.create_with_id(size: 0, vm_size: "standard-2", boot_image: "img", location: "hetzner-fsn1")
+    }
+
+    it "pops if vms are all destroyed" do
+      expect(nx).to receive(:vm_pool).and_return(pool).at_least(:once)
+      expect(pool).to receive(:destroy)
+      expect(pool).to receive(:vms).and_return([])
+      expect(nx).to receive(:pop).with("pool destroyed")
+
+      nx.wait_vms_destroy
+    end
+
+    it "naps if there are still vms" do
+      expect(nx).to receive(:vm_pool).and_return(pool).at_least(:once)
+      expect(pool).to receive(:vms).and_return([true])
+      expect {
+        nx.wait_vms_destroy
+      }.to nap(10)
+    end
+  end
+
+  describe "#before_run" do
+    it "hops to destroy when needed" do
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect { nx.before_run }.to hop("destroy")
+    end
+
+    it "does not hop to destroy if already in the destroy state" do
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect(nx.strand).to receive(:label).and_return("destroy")
+      expect { nx.before_run }.not_to hop("destroy")
+    end
+  end
+end
