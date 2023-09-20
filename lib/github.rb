4409fbc35a8a5dc0b4a0fdf7230aa6a89b0d3ae0
Enes Cakir <enes@ubicloud.com>
2023-09-20 18:14:33 +0300

    Add a helper to redeliver failed GitHub webhook deliveries
    GitHub does not resend webhook deliveries that have failed. It is
    necessary to inspect the failed deliveries and redeliver them in case of
    missed deliveries due to deployments or incidents.
    
    The "/app/hook/deliveries" endpoint lacks a filter, necessitating
    pagination until the specified date. It also returns all attempts for
    each delivery packet. Each delivery packet has a unique "guid".
    
    We group deliveries by "guid". If there isn't a successful delivery
    within a "guid" group, we initiate redelivery for that particular
    packet.
    
        # Get failed deliveries for last 10 minutes
        Github.failed_deliveries(Time.now - 10 * 60)
    
        # Redeliver failed deliveries for last 10 minutes
        Github.redeliver_failed_deliveries(Time.now - 10 * 60)
    
    Since it paginates until the specified date is reached, avoid providing
    a very old date as it could result in an excessive number of pagination
    requests.
    
    GitHub also recommends to use such a script to redeliver failed
    deliveries
    
    https://docs.github.com/en/webhooks/using-webhooks/automatically-redelivering-failed-deliveries-for-a-github-app-webhook

--- lib/github.rb
+++ lib/github.rb
@@ -31,4 +31,29 @@ module Github
   def self.runner_labels
     @@runner_labels ||= YAML.load_file("config/github_runner_labels.yml").to_h { [_1["name"], _1] }
   end
+
+  def self.failed_deliveries(since)
+    client = Github.app_client
+    all_deliveries = client.get("/app/hook/deliveries?per_page=100")
+    while (next_url = client.last_response.rels[:next]&.href) && (since < all_deliveries.last[:delivered_at])
+      all_deliveries += client.get(next_url)
+    end
+
+    all_deliveries
+      .reject { _1[:delivered_at] < since }
+      .group_by { _1[:guid] }
+      .values
+      .reject { |group| group.any? { _1[:status] == "OK" } }
+      .map { |group| group.max_by { _1[:delivered_at] } }
+  end
+
+  def self.redeliver_failed_deliveries(since)
+    client = Github.app_client
+    failed_deliveries = Github.failed_deliveries(since)
+    failed_deliveries.each do |delivery|
+      Clog.emit("redelivering failed delivery") { {delivery: delivery} }
+      client.post("/app/hook/deliveries/#{delivery[:id]}/attempts")
+    end
+    Clog.emit("redelivered failed deliveries")
+  end
 end
