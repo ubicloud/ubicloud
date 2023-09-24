89b54bcdd1d899d88a08e2051a8eec2f070c4abb
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2023-09-25 02:13:54 +0300

    Add PostgreSQL migration file

---
+++ migrate/20230926_postgres.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table(:postgres_server) do
+      column :id, :uuid, primary_key: true, default: nil
+      column :created_at, :timestamptz, null: false, default: Sequel.lit("now()")
+      column :updated_at, :timestamptz, null: false, default: Sequel.lit("now()")
+      column :project_id, :project, type: :uuid, null: false
+      column :location, :text, collate: '"C"', null: false
+      column :server_name, :text, collate: '"C"', null: false, unique: true
+      column :target_vm_size, :text, collate: '"C"', null: false
+      column :target_storage_size_gib, :bigint, null: false
+      column :superuser_password, :text, collate: '"C"', null: false # TODO encrypt this
+      column :vm_id, :vm, type: :uuid
+    end
+  end
+end
