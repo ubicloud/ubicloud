4eb7751652fdfb2208060998163989801fa25d7f
Furkan Sahin <furkan@ubicloud.com>
2023-09-26 11:52:56 +0200

    Adds logs to see pool usage

--- prog/vm/github_runner.rb
+++ prog/vm/github_runner.rb
@@ -54,9 +54,11 @@ class Prog::Vm::GithubRunner < Prog::Base
         amount: 1
       )
 
+      puts "Pool is used for #{label}"
       return vm
     end
 
+    puts "Pool is empty for #{label}, creating a new VM"
     ubid = GithubRunner.generate_ubid
     ssh_key = SshKey.generate
     # We use unencrypted storage for now, because provisioning 86G encrypted
