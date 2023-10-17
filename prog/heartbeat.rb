683afdbc0e38dafb47e4d7eaadd42fb5af9aa4c3
Enes Cakir <enes@ubicloud.com>
2023-10-17 13:22:14 +0300

    Set connection timeout for the heartbeat requests
    The heartbeat prog attempts to connect to the database and send GET
    requests to the heartbeat URL every 10 seconds. Although this process is
    relatively straightforward, we've noticed that some heartbeat runs can
    take 10-20 seconds at production, which could indicate a connection
    issue.
    
    Excon, by default, has a 60-second timeout. I reduced this to 2 seconds
    to prevent excessive blocking of the respirate thread. If the respirate
    cannot send a request to the heartbeat URL within 2 seconds, there may
    be an issue either with our system or our 3rd-party monitoring service.

--- prog/heartbeat.rb
+++ prog/heartbeat.rb
@@ -5,7 +5,10 @@ require "excon"
 class Prog::Heartbeat < Prog::Base
   label def wait
     DB["SELECT 1"].first
-    Excon.get(Config.heartbeat_url)
+    Excon.get(Config.heartbeat_url, read_timeout: 2, write_timeout: 2, connect_timeout: 2)
+    nap 10
+  rescue Excon::Error::Timeout => e
+    Clog.emit("heartbeat request timed out") { {exception: {message: e.message}} }
     nap 10
   end
 end
