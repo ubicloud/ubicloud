d4f29e78337804b00ab366aca969214d753a3ab7
Hadi Moshayedi <hadi@ubicloud.com>
2023-10-20 12:04:05 -0700

    E2E CI tests host reboot.
    We had couple of host reboot related issues unnoticed, hence testing host
    reboot behaviour in E2E CI.

--- spec/prog/test/vm_group_spec.rb
+++ spec/prog/test/vm_group_spec.rb
@@ -14,20 +14,20 @@ RSpec.describe Prog::Test::VmGroup do
   end
 
   describe "#setup_vms" do
-    it "hops to wait_children_created" do
-      expect { vg_test.setup_vms }.to hop("wait_children_created", "Test::VmGroup")
+    it "hops to wait_children_ready" do
+      expect { vg_test.setup_vms }.to hop("wait_children_ready", "Test::VmGroup")
     end
   end
 
-  describe "#wait_children_created" do
+  describe "#wait_children_ready" do
     it "hops to children_ready if children idle" do
       expect(vg_test).to receive(:children_idle).and_return(true)
-      expect { vg_test.wait_children_created }.to hop("children_ready", "Test::VmGroup")
+      expect { vg_test.wait_children_ready }.to hop("children_ready", "Test::VmGroup")
     end
 
     it "donates if children not idle" do
       expect(vg_test).to receive(:children_idle).and_return(false)
-      expect { vg_test.wait_children_created }.to nap(0)
+      expect { vg_test.wait_children_ready }.to nap(0)
     end
   end
 
@@ -41,14 +41,53 @@ RSpec.describe Prog::Test::VmGroup do
   end
 
   describe "#wait_subtests" do
-    it "hops to destroy_vms if children idle" do
+    it "hops to destroy_vms if children idle and not test_reboot" do
       expect(vg_test).to receive(:children_idle).and_return(true)
+      expect(vg_test).to receive(:frame).and_return({"test_reboot" => false})
       expect { vg_test.wait_subtests }.to hop("destroy_vms", "Test::VmGroup")
+    end
+
+    it "hops to test_reboot if children idle and test_reboot" do
+      expect(vg_test).to receive(:children_idle).and_return(true)
+      expect(vg_test).to receive(:frame).and_return({"test_reboot" => true})
+      expect { vg_test.wait_subtests }.to hop("test_reboot", "Test::VmGroup")
     end
 
     it "donates if children not idle" do
       expect(vg_test).to receive(:children_idle).and_return(false)
       expect { vg_test.wait_subtests }.to nap(0)
+    end
+  end
+
+  describe "#test_reboot" do
+    it "hops to wait_reboot" do
+      host = instance_double(VmHost)
+      expect(vg_test).to receive(:host).and_return(host)
+      expect(host).to receive(:incr_reboot).with(no_args)
+      expect { vg_test.test_reboot }.to hop("wait_reboot", "Test::VmGroup")
+    end
+  end
+
+  describe "#wait_reboot" do
+    let(:st) {
+      instance_double(Strand)
+    }
+
+    before do
+      host = instance_double(VmHost)
+      allow(vg_test).to receive(:host).and_return(host)
+      allow(host).to receive(:strand).and_return(st)
+    end
+
+    it "naps if strand is busy" do
+      expect(st).to receive(:label).and_return("reboot")
+      expect { vg_test.wait_reboot }.to nap(30)
+    end
+
+    it "runs vm tests if reboot done" do
+      expect(st).to receive(:label).and_return("wait")
+      expect(st).to receive(:semaphores).and_return([])
+      expect { vg_test.wait_reboot }.to hop("wait_children_ready", "Test::VmGroup")
     end
   end
 
@@ -108,6 +147,16 @@ RSpec.describe Prog::Test::VmGroup do
       st = Strand.create_with_id(prog: "Prog", label: "label")
       allow(vg_test).to receive(:strand).and_return(st)
       expect(vg_test.children_idle).to be(true)
+    end
+  end
+
+  describe "#host" do
+    it "returns first VM's host" do
+      sshable = Sshable.create_with_id
+      host = VmHost.create(location: "A") { _1.id = sshable.id }
+      vm = Vm.create_with_id(unix_user: "root", public_key: "", name: "xyz", location: "a", boot_image: "b", family: "z", cores: 1, vm_host_id: host.id)
+      expect(vg_test).to receive(:frame).and_return({"vms" => [vm.id]})
+      expect(vg_test.host).to eq(host)
     end
   end
 end
