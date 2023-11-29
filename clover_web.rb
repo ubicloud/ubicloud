0fffb831d59d34943bdbb57d4d92d7ec293f89ea
Enes Cakir <enes@ubicloud.com>
2023-11-30 01:39:57 +0300

    Add a datepicker component to UI
    We require a date picker to select the target restore date for our
    PostgreSQL databases. This is our first time needing such a feature.
    Several alternative date picker libraries exist, including flatpickr,
    jQuery UI Datepicker, Datepicker.js, Air Datepicker, and pickadate.js. I
    evaluated these libraries based on various factors: popularity,
    aesthetic appeal, dependencies, package size, and the ability to set
    minimum/maximum dates.
    
    I chose [flatpickr](https://github.com/flatpickr/flatpickr). It offers a
    modern interface with no dependencies and displays the native OS picker
    on mobile devices, thus providing an excellent UX.  I pass the picker
    configurations from Ruby to JavaScript using data-* attributes.
    
    To optimize page loads, I introduced the `@enable_datepicker` flag. This
    ensures that the package files are loaded only on pages that require a
    date picker.

--- clover_web.rb
+++ clover_web.rb
@@ -17,10 +17,10 @@ class CloverWeb < Roda
 
   plugin :content_security_policy do |csp|
     csp.default_src :none
-    csp.style_src :self
+    csp.style_src :self, "https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css"
     csp.img_src :self, "data: image/svg+xml"
     csp.form_action :self, "https://checkout.stripe.com"
-    csp.script_src :self, "https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js", "https://cdn.jsdelivr.net/npm/dompurify@3.0.5/dist/purify.min.js"
+    csp.script_src :self, "https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js", "https://cdn.jsdelivr.net/npm/dompurify@3.0.5/dist/purify.min.js", "https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js"
     csp.connect_src :self
     csp.base_uri :none
     csp.frame_ancestors :none
