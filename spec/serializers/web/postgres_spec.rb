cf1a67a83ef6ff26bcb2fe99a8db38e55df08cc3
Enes Cakir <enes@ubicloud.com>
2023-11-30 13:40:24 +0300

    Add earliest/latest restore time helpers to the pg timeline
    We need to determine earliest/latest restore times to provide a specific
    time range for the user on the UI.
    
    We don't allow to restore the destroyed databases for now. So the latest
    restore time is the current time.
    
    The earliest restore time is little bit tricky. Determining the earliest
    restore time is straightforward; simply query the blob storage and
    select the oldest entry. However, we aim to avoid querying the blob
    storage service each time a user opens the page in the UI. The
    "earliest_backup_completed_at" column is used to cache the value,
    eliminating the need to query the blob storage every time. The
    "earliest_backup_completed_at" value can be changed when a new backup is
    created or an existing backup is deleted. It's nil when the server is
    created, so we get it from the blob storage until the first backup
    completed. Currently, we lack a backup cleanup feature.  Once it is
    implemented, we can invoke the "refresh_earliest_backup_completion_time"
    method at the appropriate points.
    
    We return earliest/latest restore times only if the server is primary.

---
+++ spec/serializers/web/postgres_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative "../../spec_helper"
+
+RSpec.describe Serializers::Web::Postgres do
+  let(:pg) { PostgresResource.new(server_name: "pg-server-name").tap { _1.id = "69c0f4cd-99c1-8ed0-acfe-7b013ce2fa0b" } }
+
+  it "can serialize when no earliest/latest restore times" do
+    expect(pg).to receive(:strand).and_return(instance_double(Strand, label: "start")).twice
+    expect(pg).to receive(:timeline).and_return(instance_double(PostgresTimeline, earliest_restore_time: nil, latest_restore_time: nil)).twice
+    expect(pg).to receive(:server).and_return(instance_double(PostgresServer, primary?: true, vm: nil)).twice
+    data = described_class.new(:detailed).serialize(pg)
+    expect(data[:earliest_restore_time]).to be_nil
+    expect(data[:latest_restore_time]).to be_nil
+  end
+
+  it "can serialize when have earliest/latest restore times" do
+    time = Time.now
+    expect(pg).to receive(:strand).and_return(instance_double(Strand, label: "start")).twice
+    expect(pg).to receive(:timeline).and_return(instance_double(PostgresTimeline, earliest_restore_time: time, latest_restore_time: time)).twice
+    expect(pg).to receive(:server).and_return(instance_double(PostgresServer, primary?: true, vm: nil)).twice
+    data = described_class.new(:detailed).serialize(pg)
+    expect(data[:earliest_restore_time]).to eq(time.iso8601)
+    expect(data[:latest_restore_time]).to eq(time.iso8601)
+  end
+
+  it "can serialize when not primary" do
+    expect(pg).to receive(:strand).and_return(instance_double(Strand, label: "start")).twice
+    expect(pg).to receive(:server).and_return(instance_double(PostgresServer, primary?: false, vm: nil)).twice
+    data = described_class.new(:detailed).serialize(pg)
+    expect(data[:earliest_restore_time]).to be_nil
+    expect(data[:latest_restore_time]).to be_nil
+  end
+end
