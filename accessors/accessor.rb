842bc17a25e9bf8bc82f4777eb42de9432ba5cfc
Burak Velioglu <velioglub@ubicloud.com>
2023-12-11 12:26:08 +0300

    Add accessor

---
+++ accessors/accessor.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module AccessorMethods
+  def get(project, user, param)
+    raise NoMethodError
+  end
+
+  def get_all(project, user, param)
+    raise NoMethodError
+  end
+
+  def post(project, user, param)
+    raise NoMethodError
+  end
+
+  def delete(project, user)
+    raise NoMethodError
+  end
+end
