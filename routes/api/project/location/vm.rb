7ac80e46320f8dc45fb02aa7ff3f626f6f0c99f9
Burak Velioglu <velioglub@ubicloud.com>
2024-02-05 20:42:20 -0800

    Add validation to the VM API Post endpoint

--- routes/api/project/location/vm.rb
+++ routes/api/project/location/vm.rb
@@ -23,15 +23,19 @@ class CloverApi
         Authorization.authorize(@current_user.id, "Vm:create", @project.id)
         fail Validation::ValidationFailed.new({billing_info: "Project doesn't have valid billing information"}) unless @project.has_valid_payment_method?
 
+        required_parameters = ["public_key"]
+        allowed_optional_parameters = ["size", "unix_user", "location", "boot_image"]
+
+        request_body = r.body.read
+        Validation.validate_request_body(request_body, required_parameters, allowed_optional_parameters)
+
+        request_body_params = JSON.parse(request_body)
+
         st = Prog::Vm::Nexus.assemble(
-          r.params["public_key"],
+          request_body_params["public_key"],
           @project.id,
           name: vm_name,
-          unix_user: r.params["unix_user"],
-          size: r.params["size"],
-          location: @location,
-          boot_image: r.params["boot_image"],
-          enable_ip4: !!r.params["enable_ip4"]
+          **request_body_params.except(*required_parameters).transform_keys { |key| key.to_sym }
         )
 
         serialize(st.subject)
