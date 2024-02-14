9d8b6ab468ee6ef175c1e7b422af4b818fc072a2
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-02-14 14:26:31 +0100

    Revert "Load UnicodeNormalize before freezing core"
    This reverts commit 4cfcceb22c85d914cd88e6e20af3d20ef01382e8.

--- loader.rb
+++ loader.rb
@@ -99,9 +99,5 @@ def clover_freeze
   # Also for at least puma, but not itemized by the roda-sequel-stack
   # project for some reason.
   require "nio4r"
-
-  # this Ruby standard library method patches core classes.
-  "".unicode_normalize(:nfc)
-
-  Refrigerator.freeze_core
+  Refrigerator.freeze
 end
