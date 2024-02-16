08e08b3db77e6730083ced0c925f77d1f1b4f30b
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-02-16 17:42:56 +0100

    Copy the Postgres CA files to the filesystem
    Now, if Config.clover_database_url or Config.postgres_monitor_database_url use
    verify-full, which they should, they can also refer to the CA file stored at
    var/ca_bundles/postgres_monitor_db.crt and verify the full certificate chain.

--- db.rb
+++ db.rb
@@ -3,7 +3,10 @@
 require "netaddr"
 require "sequel/core"
 require_relative "config"
+require_relative "lib/util"
 
+db_ca_bundle_filename = File.join(Dir.pwd, "var", "ca_bundles", "db_ca_bundle.crt")
+Util.safe_write_to_file(db_ca_bundle_filename, Config.clover_database_root_certs)
 DB = Sequel.connect(Config.clover_database_url, max_connections: Config.db_pool - 1, pool_timeout: Config.database_timeout).tap do |db|
   # Replace dangerous (for cidrs) Ruby IPAddr type that is otherwise
   # used by sequel_pg.  Has come up more than once in the bug tracker:
@@ -15,6 +18,8 @@ DB = Sequel.connect(Config.clover_database_url, max_connections: Config.db_pool 
   db.add_conversion_proc(869, NetAddr.method(:parse_ip))
 end
 
+postgres_monitor_db_ca_bundle_filename = File.join(Dir.pwd, "var", "ca_bundles", "postgres_monitor_db.crt")
+Util.safe_write_to_file(postgres_monitor_db_ca_bundle_filename, Config.postgres_monitor_database_root_certs)
 POSTGRES_MONITOR_DB = Sequel.connect(Config.postgres_monitor_database_url, max_connections: Config.db_pool, pool_timeout: Config.database_timeout) if Config.postgres_monitor_database_url
 
 # Load Sequel Database/Global extensions here
--- lib/util.rb
+++ lib/util.rb
@@ -64,4 +64,14 @@ module Util
   def self.exception_to_hash(ex)
     {exception: {message: ex.message, class: ex.class.to_s, backtrace: ex.backtrace, cause: ex.cause.inspect}}
   end
+
+  def self.safe_write_to_file(filename, content)
+    FileUtils.mkdir_p(File.dirname(filename))
+    temp_filename = filename + ".tmp"
+    File.open("#{temp_filename}.lock", File::RDWR | File::CREAT) do |lock_file|
+      lock_file.flock(File::LOCK_EX)
+      File.write(temp_filename, content)
+      File.rename(temp_filename, filename)
+    end
+  end
 end
