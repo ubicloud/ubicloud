6fe43bebe90dba1f2377b767871810b757c3a53d
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-02-26 08:00:59 +0100

    Don't use DB variable in migration scripts
    Changing old migration scripts is usually frowned upon, but in this case the
    change does not affect the state of the database. The change is using the run
    function instead of accessing the tables using DB[:billing_rate], because DB
    is a global variable and when multiple threads with different DB values run
    at the same time, they break each other.
    
    One might ask, when there can be multiple threads with different DB values. The
    answer is when we parallelize unit tests using tests processes, each with their
    own databases. The next commit introduces parallel unit tests.

--- migrate/20230808_add_ipv4_billing.rb
+++ migrate/20230808_add_ipv4_billing.rb
@@ -2,7 +2,7 @@
 
 Sequel.migration do
   up do
-    DB[:billing_rate].truncate(cascade: true)
+    run "TRUNCATE billing_rate CASCADE"
     copy_into :billing_rate, data: <<COPY
 139d9a67-8182-8578-a303-235cabd5161c	VmCores	standard	hetzner-fsn1	0.000171296
 08c502f7-df5d-8978-9896-feafa0ec5c40	VmCores	standard	hetzner-hel1	0.000154167
--- migrate/20230809_drop_billing_rate.rb
+++ migrate/20230809_drop_billing_rate.rb
@@ -20,7 +20,7 @@ Sequel.migration do
       index [:resource_type, :resource_family, :location], unique: true
     end
 
-    DB[:billing_rate].truncate(cascade: true)
+    run "TRUNCATE billing_rate CASCADE"
     copy_into :billing_rate, data: <<COPY
 139d9a67-8182-8578-a303-235cabd5161c	VmCores	standard	hetzner-fsn1	0.000171296
 08c502f7-df5d-8978-9896-feafa0ec5c40	VmCores	standard	hetzner-hel1	0.000154167
