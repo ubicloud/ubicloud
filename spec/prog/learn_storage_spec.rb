ubicloud/ubicloud

commit 6e56364d17d503d6a31c18a19372dbf100a6a4ab

Improve LearnStorage prog to allow rerunning
When we run the `LearnStorage` prog on an existing VM host that already
run it, it fails due to a 'duplicate storage device' error. If the
storage device already exists, we should avoid creating a new record.

Fixes #1147

prog/learn_storage.rb
@@ -24,13 +24,16 @@ class Prog::LearnStorage < Prog::Base
 
   def df_command(path = "") = "df -B1 --output=target,size,avail #{path}"
 
+  def storage_device(name)
+    vm_host.storage_devices.find { _1.name == name } || StorageDevice.new_with_id(vm_host_id: vm_host.id, name: name)
+  end
+
   def make_model_instances
     devices = DfRecord.parse_all(sshable.cmd(df_command))
     if devices.none? { _1.optional_name }
       rec = DfRecord.parse_all(sshable.cmd(df_command("/var/storage"))).first
       [
-        StorageDevice.new_with_id(
-          vm_host_id: vm_host.id, name: "DEFAULT",
+        storage_device("DEFAULT").set(
           # reserve 5G the host.
           available_storage_gib: [rec.avail_gib - 5, 0].max,
           total_storage_gib: rec.size_gib
@@ -39,8 +42,7 @@ class Prog::LearnStorage < Prog::Base
     else
       devices.filter_map do |rec|
         next unless (name = rec.optional_name)
-        StorageDevice.new_with_id(
-          vm_host_id: vm_host.id, name: name,
+        storage_device(name).set(
           available_storage_gib: rec.avail_gib,
           total_storage_gib: rec.size_gib
         )

spec/prog/learn_storage_spec.rb
@@ -17,13 +17,27 @@ EOS
         StorageDevice.map(&:name).sort
       }.from([]).to(%w[stor1 stor2])
     end
+
+    it "exits, updating existing StorageDevice model instances" do
+      vmh = Prog::Vm::HostNexus.assemble("::1").subject
+      ls = described_class.new(Strand.new(stack: [{"subject_id" => vmh.id}]))
+      expect(ls.sshable).to receive(:cmd).with("df -B1 --output=target,size,avail ").and_return(<<EOS)
+Mounted on                   1B-blocks        Avail
+/var/storage/devices/stor1   205520896     99571712
+/var/storage/devices/stor2  3331416064   3331276800
+EOS
+      StorageDevice.create_with_id(vm_host_id: vmh.id, name: "stor1", available_storage_gib: 3, total_storage_gib: 15)
+      expect { ls.start }.to exit({"total_storage_gib" => 3, "available_storage_gib" => 3, "msg" => "created StorageDevice records"}).and change {
+        StorageDevice.map(&:name).sort
+      }.from(%w[stor1]).to(%w[stor1 stor2])
+    end
   end
 
   describe Prog::LearnStorage, "#make_model_instances" do
     subject(:ls) { described_class.new(Strand.new) }
 
     let(:sshable) { instance_double(Sshable) }
-    let(:vmh) { instance_double(VmHost, id: "746976d6-315b-8f71-95e6-367c4ac068d7") }
+    let(:vmh) { instance_double(VmHost, id: "746976d6-315b-8f71-95e6-367c4ac068d7", storage_devices: []) }
 
     before do
       expect(ls).to receive(:sshable).and_return(sshable).at_least(:once)

commit e2688fd9c4da610b666c39fe154dca2e4d1534c7

Use upsert for LearnStorage idempotency
This is an alternative to "find or create" imperative logic that was
pushed down to the leaves of the program to offer a consistent
interface via the `storage_device(name)` method.

This caused tests that did not require persistence to start writing to
the database as a side effect.

Now, model instances can be instantiated with "new" and passed around
and only persisted at the last phase with an upsert.

6e56364d17d503d6a31c18a19372dbf100a6a4ab introduced the imperative
code to fix an idempotency issue in
89f9dbd7f2b12c08eb993d947e64003c68c90606.  This can be seen as a
refinement of that fix.

The test has been enhanced to be stricter, showing the update of
records and how the exit value from the prog is a sum.

model/storage_device.rb
@@ -4,6 +4,7 @@ require_relative "../model"
 
 class StorageDevice < Sequel::Model
   include ResourceMethods
+  plugin :insert_conflict
 
   many_to_one :vm_host
 

prog/learn_storage.rb
@@ -24,16 +24,13 @@ class Prog::LearnStorage < Prog::Base
 
   def df_command(path = "") = "df -B1 --output=target,size,avail #{path}"
 
-  def storage_device(name)
-    vm_host.storage_devices.find { _1.name == name } || StorageDevice.new_with_id(vm_host_id: vm_host.id, name: name)
-  end
-
   def make_model_instances
     devices = DfRecord.parse_all(sshable.cmd(df_command))
     if devices.none? { _1.optional_name }
       rec = DfRecord.parse_all(sshable.cmd(df_command("/var/storage"))).first
       [
-        storage_device("DEFAULT").set(
+        StorageDevice.new_with_id(
+          vm_host_id: vm_host.id, name: "DEFAULT",
           # reserve 5G the host.
           available_storage_gib: [rec.avail_gib - 5, 0].max,
           total_storage_gib: rec.size_gib
@@ -42,7 +39,8 @@ class Prog::LearnStorage < Prog::Base
     else
       devices.filter_map do |rec|
         next unless (name = rec.optional_name)
-        storage_device(name).set(
+        StorageDevice.new_with_id(
+          vm_host_id: vm_host.id, name: name,
           available_storage_gib: rec.avail_gib,
           total_storage_gib: rec.size_gib
         )
@@ -52,10 +50,17 @@ class Prog::LearnStorage < Prog::Base
 
   label def start
     total, avail = make_model_instances.each_with_object([0, 0]) { |sd, accum|
-      sd.save_changes
+      sd.skip_auto_validations(:unique) do
+        sd.insert_conflict(target: [:vm_host_id, :name],
+          update: {
+            total_storage_gib: Sequel[:excluded][:total_storage_gib],
+            available_storage_gib: Sequel[:excluded][:available_storage_gib]
+          }).save_changes
+      end
       accum[0] += sd.total_storage_gib
       accum[1] += sd.available_storage_gib
     }
+
     pop({"total_storage_gib" => total, "available_storage_gib" => avail,
          "msg" => "created StorageDevice records"})
   end

spec/prog/learn_storage_spec.rb
@@ -23,13 +23,24 @@ EOS
       ls = described_class.new(Strand.new(stack: [{"subject_id" => vmh.id}]))
       expect(ls.sshable).to receive(:cmd).with("df -B1 --output=target,size,avail ").and_return(<<EOS)
 Mounted on                   1B-blocks        Avail
-/var/storage/devices/stor1   205520896     99571712
-/var/storage/devices/stor2  3331416064   3331276800
+/var/storage/devices/stor1  6205520896   3099571712
+/var/storage/devices/stor2  3331416064   1531276800
 EOS
-      StorageDevice.create_with_id(vm_host_id: vmh.id, name: "stor1", available_storage_gib: 3, total_storage_gib: 15)
-      expect { ls.start }.to exit({"total_storage_gib" => 3, "available_storage_gib" => 3, "msg" => "created StorageDevice records"}).and change {
-        StorageDevice.map(&:name).sort
-      }.from(%w[stor1]).to(%w[stor1 stor2])
+      StorageDevice.create_with_id(vm_host_id: vmh.id, name: "stor1", available_storage_gib: 100, total_storage_gib: 100)
+      expect { ls.start }.to exit({"total_storage_gib" => 8, "available_storage_gib" => 3, "msg" => "created StorageDevice records"}).and change {
+        StorageDevice.map { |sd|
+          sd.values.slice(
+            :name, :available_storage_gib, :total_storage_gib
+          )
+        }.sort_by { _1[:name] }
+      }.from(
+        [{name: "stor1", total_storage_gib: 100, available_storage_gib: 100}]
+      ).to(
+        [
+          {name: "stor1", total_storage_gib: 5, available_storage_gib: 2},
+          {name: "stor2", total_storage_gib: 3, available_storage_gib: 1}
+        ]
+      )
     end
   end
 
@@ -37,7 +48,7 @@ EOS
     subject(:ls) { described_class.new(Strand.new) }
 
     let(:sshable) { instance_double(Sshable) }
-    let(:vmh) { instance_double(VmHost, id: "746976d6-315b-8f71-95e6-367c4ac068d7", storage_devices: []) }
+    let(:vmh) { instance_double(VmHost, id: "746976d6-315b-8f71-95e6-367c4ac068d7") }
 
     before do
       expect(ls).to receive(:sshable).and_return(sshable).at_least(:once)