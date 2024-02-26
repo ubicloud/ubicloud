93c2b5585a89b87636a0cd6adcb6095fbc680710
Hadi Moshayedi <hadi@ubicloud.com>
2024-02-26 14:54:36 -0800

    Change the hugepages arithmetic.
    Previously, at an arm64 host with 128g memory & 80 cores we tried to get
    128 hugepages (2 + 128*79/80), which might put the host under pressure.
    
    This PR enforces a static allocation of 5GB of memory for the host
    across all configurations, enhancing the predictability of memory
    available for services running on the host.
    
    In current deployments we have allocated either 248 or 249 huepages per
    256g host, so this doesn't reduce the capacity of current deployments.

--- prog/setup_hugepages.rb
+++ prog/setup_hugepages.rb
@@ -5,8 +5,11 @@ class Prog::SetupHugepages < Prog::Base
 
   label def start
     hugepage_size = "1G"
-    # put away 1 core of overhead for host, and reserve 2x1G for 2 SPDK installations
-    hugepage_cnt = 2 + (vm_host.total_mem_gib * (vm_host.total_cores - 1)) / vm_host.total_cores
+
+    # Reserve 5G of overhead for the host. SPDK will use 2 of the hugepages +
+    # upto about 1G of the 5G as not all SPDK allocations are from hugepages.
+    hugepage_cnt = vm_host.total_mem_gib - 5
+
     sshable.cmd("sudo sed -i '/^GRUB_CMDLINE_LINUX=\"/ s/\"$/ hugetlb_free_vmemmap=on default_hugepagesz=#{hugepage_size} hugepagesz=#{hugepage_size} hugepages=#{hugepage_cnt}&/' /etc/default/grub")
     sshable.cmd("sudo update-grub")
 
