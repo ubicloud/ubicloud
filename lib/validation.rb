86a34bd9985b4a965514a55268c66eacf67c0ff8
Burak Velioglu <velioglub@ubicloud.com>
2024-03-04 21:47:36 -0800

    Update password validation to accept single password
    While updating password via API, sending single passwords would be
    enough. Updating password validation with this commit to use them
    with PG APIs.

--- lib/validation.rb
+++ lib/validation.rb
@@ -98,16 +98,20 @@ module Validation
     fail ValidationFailed.new({param => msg})
   end
 
-  def self.validate_postgres_superuser_password(original_password, repeat_password)
+  def self.validate_postgres_superuser_password(original_password, repeat_password = nil)
     messages = []
     messages.push("Password must have 12 characters minimum.") if original_password.size < 12
     messages.push("Password must have at least one lowercase letter.") unless original_password.match?(/[a-z]/)
     messages.push("Password must have at least one uppercase letter.") unless original_password.match?(/[A-Z]/)
     messages.push("Password must have at least one digit.") unless original_password.match?(/[0-9]/)
-    messages.push("Passwords must match.") if original_password != repeat_password
+    messages.push("Passwords must match.") if repeat_password && original_password != repeat_password
 
     unless messages.empty?
-      fail ValidationFailed.new({"original_password" => messages.map { _1 }})
+      if repeat_password
+        fail ValidationFailed.new({"original_password" => messages.map { _1 }})
+      else
+        fail ValidationFailed.new({"password" => messages.map { _1 }})
+      end
     end
   end
 
