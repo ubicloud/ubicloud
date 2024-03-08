dc171fc1d3b5650502335647e885df384996eb89
Daniel Farina <daniel@ubicloud.com>
2024-03-07 16:03:37 -0800

    Add application type checking to heartbeat
    A trick for more robust ditch-effort monitoring is to ensure at least
    one backend for each expected process type is connected to Postgres.
    
    In principle this could be made stricter by checking for the quantity
    of each backend relative to expectations, but these expectations are
    quite unstable given changes in pooling and compute acquisition, so
    it's more suited to a not-yet-designed Prog that would offer something
    more rich than what "heartbeat" is intended to provide.

--- prog/heartbeat.rb
+++ prog/heartbeat.rb
@@ -3,8 +3,14 @@
 require "excon"
 
 class Prog::Heartbeat < Prog::Base
+  CONNECTED_APPLICATION_QUERY = <<SQL
+SELECT count(DISTINCT application_name)
+FROM pg_stat_activity
+WHERE application_name ~ '^(bin/respirate|bin/monitor|.*/puma)$'
+SQL
+
   label def wait
-    DB["SELECT 1"].first
+    nap 10 unless DB.get(CONNECTED_APPLICATION_QUERY) == 3
     Excon.get(Config.heartbeat_url, read_timeout: 2, write_timeout: 2, connect_timeout: 2)
     nap 10
   rescue Excon::Error::Timeout => e
