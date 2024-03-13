ubicloud/ubicloud

commit 540b34854f6ba13b1e546b5741b4523c50b7bd62

Add globally blocked dnsnames to firewall rules
The nftables that is used for firewall rules gets 2 new sets. These are
used to block incoming and outgoing traffic to select IP addresses.

prog/vnet/update_firewall_rules.rb
@@ -13,6 +13,8 @@ class Prog::Vnet::UpdateFirewallRules < Prog::Base
     allowed_ingress_ip6_port_set = consolidate_rules(rules.select { _1.ip6? && _1.port_range })
     guest_ephemeral = subdivide_network(vm.ephemeral_net6).first.to_s
 
+    globally_blocked_ipv4s, globally_blocked_ipv6s = generate_globally_blocked_lists
+
     vm.vm_host.sshable.cmd("sudo ip netns exec #{vm.inhost_name} nft --file -", stdin: <<TEMPLATE)
 # An nftables idiom for idempotent re-create of a named entity: merge
 # in an empty table (a no-op if the table already exists) and then
@@ -58,6 +60,18 @@ table inet fw_table {
     elements = { #{generate_private_ip6_list} }
   }
 
+  set globally_blocked_ipv4s {
+    type ipv4_addr;
+    flags interval;
+#{globally_blocked_ipv4s.empty? ? "" : "elements = {#{globally_blocked_ipv4s}}"}
+  }
+
+  set globally_blocked_ipv6s {
+    type ipv6_addr;
+    flags interval;
+#{globally_blocked_ipv6s.empty? ? "" : "elements = {#{globally_blocked_ipv6s}}"}
+  }
+
   flowtable ubi_flowtable {
     hook ingress priority filter
     devices = { #{vm.nics.map(&:ubid_to_tap_name).join(",")}, vethi#{vm.inhost_name} }
@@ -67,6 +81,10 @@ table inet fw_table {
     type filter hook forward priority filter; policy drop;
     meta l4proto { tcp, udp } flow offload @ubi_flowtable
     meta l4proto { tcp, udp } th dport 111 drop
+    ip saddr @globally_blocked_ipv4s drop
+    ip6 saddr @globally_blocked_ipv6s drop
+    ip daddr @globally_blocked_ipv4s drop
+    ip6 daddr @globally_blocked_ipv6s drop
     ip saddr @private_ipv4_cidrs ct state established,related,new counter accept
     ip daddr @private_ipv4_cidrs ct state established,related counter accept
     ip6 saddr @private_ipv6_cidrs ct state established,related,new counter accept
@@ -86,6 +104,22 @@ table inet fw_table {
 TEMPLATE
 
     pop "firewall rule is added"
+  end
+
+  def generate_globally_blocked_lists
+    globally_blocked_ipv4s = []
+    globally_blocked_ipv6s = []
+
+    GloballyBlockedDnsname.each do |globally_blocked_dnsname|
+      ips = globally_blocked_dnsname.ip_list
+      ips.each do |ip|
+        globally_blocked_ipv4s << "#{ip}/32" if ip.ipv4?
+        globally_blocked_ipv6s << "#{ip}/128" if ip.ipv6?
+      end
+    end
+    summ_ipv4 = NetAddr.summ_IPv4Net(globally_blocked_ipv4s.map { NetAddr::IPv4Net.parse(_1.to_s) })
+    summ_ipv6 = NetAddr.summ_IPv6Net(globally_blocked_ipv6s.map { NetAddr::IPv6Net.parse(_1.to_s) })
+    [summ_ipv4.join(", "), summ_ipv6.join(", ")]
   end
 
   # This method is needed to properly consolidate port_ranges + cidrs.

spec/prog/vnet/update_firewall_rules_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Prog::Vnet::UpdateFirewallRules do
 
   describe "update_firewall_rules" do
     it "populates elements if there are fw rules" do
+      GloballyBlockedDnsname.create_with_id(dns_name: "blockedhost.com", ip_list: Sequel.lit("ARRAY['123.123.123.123'::inet, '2a00:1450:400e:811::200e'::inet]"))
       expect(nx).to receive(:vm).and_return(vm).at_least(:once)
       expect(vm).to receive(:firewalls).and_return([instance_double(Firewall, name: "fw_table", firewall_rules: [
         instance_double(FirewallRule, ip6?: false, cidr: NetAddr::IPv4Net.parse("0.0.0.0/0"), port_range: nil),
@@ -76,6 +77,18 @@ elements = {fd00::/64 . 0-9999,fd00::1/128 . 10000-65535}
     elements = { fd00::/64 }
   }
 
+  set globally_blocked_ipv4s {
+    type ipv4_addr;
+    flags interval;
+elements = {123.123.123.123/32}
+  }
+
+  set globally_blocked_ipv6s {
+    type ipv6_addr;
+    flags interval;
+elements = {2a00:1450:400e:811::200e/128}
+  }
+
   flowtable ubi_flowtable {
     hook ingress priority filter
     devices = { tap0, vethix }
@@ -85,6 +98,10 @@ elements = {fd00::/64 . 0-9999,fd00::1/128 . 10000-65535}
     type filter hook forward priority filter; policy drop;
     meta l4proto { tcp, udp } flow offload @ubi_flowtable
     meta l4proto { tcp, udp } th dport 111 drop
+    ip saddr @globally_blocked_ipv4s drop
+    ip6 saddr @globally_blocked_ipv6s drop
+    ip daddr @globally_blocked_ipv4s drop
+    ip6 daddr @globally_blocked_ipv6s drop
     ip saddr @private_ipv4_cidrs ct state established,related,new counter accept
     ip daddr @private_ipv4_cidrs ct state established,related counter accept
     ip6 saddr @private_ipv6_cidrs ct state established,related,new counter accept
@@ -154,6 +171,18 @@ table inet fw_table {
     elements = { fd00::/64 }
   }
 
+  set globally_blocked_ipv4s {
+    type ipv4_addr;
+    flags interval;
+
+  }
+
+  set globally_blocked_ipv6s {
+    type ipv6_addr;
+    flags interval;
+
+  }
+
   flowtable ubi_flowtable {
     hook ingress priority filter
     devices = { tap0, vethix }
@@ -163,6 +192,10 @@ table inet fw_table {
     type filter hook forward priority filter; policy drop;
     meta l4proto { tcp, udp } flow offload @ubi_flowtable
     meta l4proto { tcp, udp } th dport 111 drop
+    ip saddr @globally_blocked_ipv4s drop
+    ip6 saddr @globally_blocked_ipv6s drop
+    ip daddr @globally_blocked_ipv4s drop
+    ip6 daddr @globally_blocked_ipv6s drop
     ip saddr @private_ipv4_cidrs ct state established,related,new counter accept
     ip daddr @private_ipv4_cidrs ct state established,related counter accept
     ip6 saddr @private_ipv6_cidrs ct state established,related,new counter accept

model/globally_blocked_dnsname.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "../model"
+
+class GloballyBlockedDnsname < Sequel::Model
+  include ResourceMethods
+
+  def self.ubid_type
+    UBID::TYPE_ETC
+  end
+end

commit 1059fe9f3e5842388bc884b4da96815e6be9c004

Fix nil ip_list issue in globally_blocked_dnsname
The commit 540b34854f6ba13b1e546b5741b4523c50b7bd62 introduced an
iteration via .each over the ip_list column of globally_blocked_dnsname.
However, when we add a new entry to the table, until the first iteration
of the Prog::ResolveGloballyBlockedDnsnames, the ip_list is empty.
Therefore, the Prog::UpdateFirewallRules starts to fail with nil ref.
This commit fixes that.

prog/vnet/update_firewall_rules.rb
@@ -111,7 +111,7 @@ TEMPLATE
     globally_blocked_ipv6s = []
 
     GloballyBlockedDnsname.each do |globally_blocked_dnsname|
-      ips = globally_blocked_dnsname.ip_list
+      ips = globally_blocked_dnsname.ip_list || []
       ips.each do |ip|
         globally_blocked_ipv4s << "#{ip}/32" if ip.ipv4?
         globally_blocked_ipv6s << "#{ip}/128" if ip.ipv6?

spec/prog/vnet/update_firewall_rules_spec.rb
@@ -124,6 +124,9 @@ ADD_RULES
     end
 
     it "does not pass elements if there are not fw rules" do
+      # An address to block but not discovered the ip_list, yet.
+      GloballyBlockedDnsname.create_with_id(dns_name: "blockedhost.com", ip_list: nil)
+
       expect(nx).to receive(:vm).and_return(vm).at_least(:once)
       expect(vm).to receive(:firewalls).and_return([])
       expect(vm.vm_host.sshable).to receive(:cmd).with("sudo ip netns exec x nft --file -", stdin: <<ADD_RULES)