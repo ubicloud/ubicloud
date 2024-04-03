f243da1a304ce774a52a44f715d451d201318531
Enes Cakir <enes@ubicloud.com>
2024-04-03 17:13:37 +0300

    Improve invoice charging function
    We manually charge invoices. I've resolved some issues we encountered
    during the charging process.
    
    Firstly, I switched the logging method from 'puts' to 'Clog'.
    
    We attempted to charge each payment method listed in the billing
    information sequentially. Sometimes, the Stripe Ruby SDK throws an error
    when a payment fails. In such cases, we need to catch the error and
    proceed to charge the next payment method.
    
    If we successfully charge an invoice, there's no need to continue trying
    other payment methods, so we break the loop.
    
    If all payment methods fail, we leave the invoice unpaid.

--- spec/model/invoice_spec.rb
+++ spec/model/invoice_spec.rb
@@ -6,77 +6,90 @@ RSpec.describe Invoice do
   subject(:invoice) { described_class.new(id: "50d5aae4-311c-843b-b500-77fbc7778050", content: {"cost" => 10}, status: "unpaid") }
 
   let(:billing_info) { BillingInfo.create_with_id(stripe_id: "cs_1234567890") }
-  let(:payment_method) { PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_1234567890") }
+
+  before do
+    allow(invoice).to receive(:reload)
+    allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
+  end
 
   describe ".charge" do
     it "not charge if Stripe not enabled" do
       allow(Config).to receive(:stripe_secret_key).and_return(nil)
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Billing is not enabled. Set STRIPE_SECRET_KEY to enable billing.\n").to_stdout
+      expect(Clog).to receive(:emit).with("Billing is not enabled. Set STRIPE_SECRET_KEY to enable billing.").and_call_original
+      expect(invoice.charge).to be true
     end
 
     it "not charge if already charged" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
+      expect(Clog).to receive(:emit).with("Invoice already charged.").and_call_original
       invoice.status = "paid"
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] already charged: paid\n").to_stdout
+      expect(invoice.charge).to be true
     end
 
     it "not charge if less than minimum charge threshold" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
       invoice.content["cost"] = 0.4
       expect(invoice).to receive(:update).with(status: "below_minimum_threshold")
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] cost is less than minimum charge cost: $0.4\n").to_stdout
+      expect(Clog).to receive(:emit).with("Invoice cost is less than minimum charge cost.").and_call_original
+      expect(invoice.charge).to be true
     end
 
     it "not charge if doesn't have billing info" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] doesn't have billing info\n").to_stdout
+      expect(Clog).to receive(:emit).with("Invoice doesn't have billing info.").and_call_original
+      expect(invoice.charge).to be false
     end
 
     it "not charge if no payment methods" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
       invoice.content["billing_info"] = {"id" => billing_info.id}
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] couldn't charge with any payment method\n").to_stdout
+      expect(Clog).to receive(:emit).with("Invoice doesn't have billing info.").and_call_original
+      expect(invoice.charge).to be false
     end
 
-    it "not charge if payment method fails" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
+    it "not charge if all payment methods fails" do
       invoice.content["billing_info"] = {"id" => billing_info.id}
+      payment_method1 = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_1", order: 1)
+      payment_method2 = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_2", order: 2)
+
       # rubocop:disable RSpec/VerifiedDoubles
-      expect(Stripe::PaymentIntent).to receive(:create).and_return(double(Stripe::PaymentIntent, status: "failed")).at_least(:once)
+      expect(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1000, customer: billing_info.stripe_id, payment_method: payment_method1.stripe_id))
+        .and_raise(Stripe::CardError.new("Unsufficient funds", {}))
+      expect(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1000, customer: billing_info.stripe_id, payment_method: payment_method2.stripe_id))
+        .and_raise(Stripe::CardError.new("Card declined", {}))
       # rubocop:enable RSpec/VerifiedDoubles
-      expect do
-        expect(invoice.charge).to be_nil
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] couldn't charge with PaymentMethod[#{payment_method.ubid}]: failed
-Invoice[1va3atns1h3j3pm07fyy7ey050] couldn't charge with any payment method\n").to_stdout
+      expect(Clog).to receive(:emit).with("Invoice couldn't charged.").and_call_original.twice
+      expect(Clog).to receive(:emit).with("Invoice couldn't charged with any payment method.").and_call_original
+      expect(invoice.charge).to be false
     end
 
-    it "can charge" do
-      allow(Config).to receive(:stripe_secret_key).and_return("secret_key")
+    it "fails if PaymentIntent does not raise an exception in case of failure" do
       invoice.content["billing_info"] = {"id" => billing_info.id}
+      payment_method = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_1", order: 1)
 
       # rubocop:disable RSpec/VerifiedDoubles
-      expect(Stripe::PaymentIntent).to receive(:create).and_return(double(Stripe::PaymentIntent, status: "succeeded", id: "pi_1234567890")).with(hash_including(
-        amount: 1000,
-        customer: billing_info.stripe_id,
-        payment_method: payment_method.stripe_id
-      )).at_least(:once)
+      expect(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1000, customer: billing_info.stripe_id, payment_method: payment_method.stripe_id))
+        .and_return(double(Stripe::PaymentIntent, id: "payment-intent-id", status: "failed"))
+      # rubocop:enable RSpec/VerifiedDoubles
+      expect(Clog).to receive(:emit).with("BUG: payment intent should succeed here").and_call_original
+      expect(Clog).to receive(:emit).with("Invoice couldn't charged with any payment method.").and_call_original
+      expect(invoice.charge).to be false
+    end
+
+    it "can charge from a correct payment method even some of them are not working" do
+      invoice.content["billing_info"] = {"id" => billing_info.id}
+      payment_method1 = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_1", order: 1)
+      payment_method2 = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_2", order: 2)
+      payment_method3 = PaymentMethod.create_with_id(billing_info_id: billing_info.id, stripe_id: "pm_3", order: 3)
+      # rubocop:disable RSpec/VerifiedDoubles
+      expect(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1000, customer: billing_info.stripe_id, payment_method: payment_method1.stripe_id))
+        .and_raise(Stripe::CardError.new("Declined", {}))
+      expect(Stripe::PaymentIntent).to receive(:create).with(hash_including(amount: 1000, customer: billing_info.stripe_id, payment_method: payment_method2.stripe_id))
+        .and_return(double(Stripe::PaymentIntent, status: "succeeded", id: "pi_1234567890"))
+      expect(Stripe::PaymentIntent).not_to receive(:create).with(hash_including(payment_method: payment_method3.stripe_id))
       # rubocop:enable RSpec/VerifiedDoubles
       expect(invoice).to receive(:save).with(columns: [:status, :content])
-      expect do
-        expect(invoice.charge).to eq("pi_1234567890")
-      end.to output("Invoice[1va3atns1h3j3pm07fyy7ey050] charged with PaymentMethod[#{payment_method.ubid}] for $10.0\n").to_stdout
+      expect(Clog).to receive(:emit).with("Invoice couldn't charged.").and_call_original
+      expect(Clog).to receive(:emit).with("Invoice charged.").and_call_original
+      expect(invoice.charge).to be true
       expect(invoice.status).to eq("paid")
-      expect(invoice.content["payment_method"]["id"]).to eq(payment_method.id)
+      expect(invoice.content["payment_method"]["id"]).to eq(payment_method2.id)
       expect(invoice.content["payment_intent"]).to eq("pi_1234567890")
     end
   end
