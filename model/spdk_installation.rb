c7b7ff41cb378b8bf79dc8801a4bf20d9f6ade6d
Hadi Moshayedi <hadi@ubicloud.com>
2024-04-17 12:29:36 -0700

    Remove references to SPDK_LEGACY_VERSION.
    We don't have any legacy SPDK installations in production anymore, so we
    can remove these.

--- model/spdk_installation.rb
+++ model/spdk_installation.rb
@@ -2,9 +2,6 @@
 
 require_relative "../model"
 
-# YYY: Remove all checks against this after upgrading all legacy systems
-LEGACY_SPDK_VERSION = "LEGACY_SPDK_VERSION"
-
 class SpdkInstallation < Sequel::Model
   many_to_one :vm_host
   one_to_many :vm_storage_volumes
