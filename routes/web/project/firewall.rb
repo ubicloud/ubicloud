18c8dbcd40e19806b99432fa85985fc265e4d361
Furkan Sahin <furkan@ubicloud.com>
2024-04-30 14:54:53 +0200

    Add Firewall Create page

--- routes/web/project/firewall.rb
+++ routes/web/project/firewall.rb
@@ -10,5 +10,31 @@ class CloverWeb
       view "firewall/index"
     end
 
+    r.on "create" do
+      r.get true do
+        Authorization.authorize(@current_user.id, "Firewall:create", @project.id)
+        @firewall = serialize(@project.firewalls_dataset.authorized(@current_user.id, "Firewall:view").all)
+        @subnets = Serializers::Web::PrivateSubnet.serialize(@project.private_subnets_dataset.authorized(@current_user.id, "PrivateSubnet:view").all)
+        view "firewall/create"
+      end
+    end
+
+    r.post true do
+      Authorization.authorize(@current_user.id, "Firewall:create", @project.id)
+      Validation.validate_name(r.params["name"])
+
+      fw = Firewall.create_with_id(
+        name: r.params["name"],
+        description: r.params["description"]
+      )
+      fw.associate_with_project(@project)
+
+      ps = PrivateSubnet.from_ubid(r.params["private-subnet-id"])
+      fw.associate_with_private_subnet(ps) if ps
+
+      flash["notice"] = "'#{r.params["name"]}' is created"
+
+      r.redirect "#{@project.path}#{Firewall[fw.id].path}"
+    end
   end
 end
