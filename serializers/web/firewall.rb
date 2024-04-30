473d6049a5648dd666cfbdccff8dca1a44171d65
Furkan Sahin <furkan@ubicloud.com>
2024-04-30 14:49:14 +0200

    Add Firewall Index page

--- serializers/web/firewall.rb
+++ serializers/web/firewall.rb
@@ -3,9 +3,11 @@
 class Serializers::Web::Firewall < Serializers::Base
   def self.base(firewall)
     {
+      ubid: firewall.ubid,
       id: firewall.id,
       name: firewall.name,
       description: firewall.description,
+      path: firewall.path,
       firewall_rules: firewall.firewall_rules.sort_by { |fwr| fwr.cidr.version && fwr.cidr.to_s }.map { |fw| Serializers::Web::FirewallRule.serialize(fw) }
     }
   end
