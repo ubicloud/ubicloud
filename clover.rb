00143c11ca12d76374fd34f676f4df762c5d0c3e
Burak Velioglu <velioglub@ubicloud.com>
2024-05-05 23:04:55 -0700

    Direct requests coming from api subdomain to api endpoints
    Directing requests coming from api subdomain to api endpoints. So,
    clients can send api requests to api.ubicloud.com instead of
    console.ubicloud.com/api as they do now.
    
    Also, continue directing requests coming to /api to api endpoints
    in test and development environments to make development and test
    easier.

--- clover.rb
+++ clover.rb
@@ -16,9 +16,19 @@ class Clover < Roda
   end
 
   route do |r|
-    r.on "api" do
+    subdomain = r.host.split(".").first
+    if subdomain == "api"
       r.run CloverApi
     end
+
+    # To make test and development easier
+    # :nocov:
+    unless Config.production?
+      r.on "api" do
+        r.run CloverApi
+      end
+    end
+    # :nocov:
 
     r.run CloverWeb
   end
