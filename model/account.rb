28710977458e10f95310470ccf0a8b67fe953039
Burak Velioglu <velioglub@ubicloud.com>
2024-05-07 17:30:21 -0700

    Tag related payment methods while suspending an account
    We are suspending an account if we think it is a fraud. With this
    commit we are getting all projects of which the account is member of, then
    tag all payment methods of those projects as fraud.

--- model/account.rb
+++ model/account.rb
@@ -27,5 +27,7 @@ class Account < Sequel::Model(:accounts)
   def suspend
     update(suspended_at: Time.now)
     DB[:account_active_session_keys].where(account_id: id).delete(force: true)
+
+    projects.each { _1.billing_info&.payment_methods_dataset&.update(fraud: true) }
   end
 end
