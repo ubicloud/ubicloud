a3c9f8c25df27e2c3dc40c8cd2f33ae95cec6668
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-05-21 04:24:15 +0300

    Merge web and api serializers for network interfaces

--- serializers/common/nic.rb
+++ serializers/common/nic.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-class Serializers::Web::Nic < Serializers::Base
+class Serializers::Common::Nic < Serializers::Base
   def self.base(nic)
     {
-      id: nic.id,
-      ubid: nic.ubid,
+      id: nic.ubid,
       name: nic.name,
       private_ipv4: nic.private_ipv4.network.to_s,
       private_ipv6: nic.private_ipv6.nth(2).to_s,
-      vm_name: nic.vm&.name,
-      subnet_name: nic.private_subnet.name
+      vm_name: nic.vm&.name
     }
   end
 
