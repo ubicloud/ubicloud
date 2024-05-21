c4b8d9b8155e8effd232296bf69c388e8cf0b2a2
Benjamin Satzger <ben@ubicloud.com>
2024-05-21 13:52:39 +0200

    Introduce new firmware class
    The new implementation downloads the firmware from
    https://github.com/ubicloud/build-edk2-firmware and stores it
    in `/opt/fw`. As an example, firmware with version 202402 would
    be located at `/opt/fw/CLOUDHV-202402.fd`. The firmware's
    SHA-256 digest is verified during the download.

--- rhizome/host/lib/cloud_hypervisor.rb
+++ rhizome/host/lib/cloud_hypervisor.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require "fileutils"
 require_relative "../../common/lib/arch"
 
 module CloudHypervisor
-  FirmwareClass = Struct.new(:version, :name) {
+  FirmwareClassLegacy = Struct.new(:version, :name) {
     def url
       "https://github.com/fdr/edk2/releases/download/#{version}/#{name}"
     end
@@ -14,12 +15,44 @@ module CloudHypervisor
   }
 
   FIRMWARE = if Arch.arm64?
-    FirmwareClass.new("edk2-stable202308", "CLOUDHV_EFI.fd")
+    FirmwareClassLegacy.new("edk2-stable202308", "CLOUDHV_EFI.fd")
   elsif Arch.x64?
-    FirmwareClass.new("edk2-stable202302", "CLOUDHV.fd")
+    FirmwareClassLegacy.new("edk2-stable202302", "CLOUDHV.fd")
   else
     fail "BUG: unexpected architecture"
   end
+
+  FirmwareClass = Struct.new(:version, :sha256) {
+    def url
+      "https://github.com/ubicloud/build-edk2-firmware/releases/download/edk2-stable#{version}-#{Arch.sym}/CLOUDHV-#{Arch.sym}.fd"
+    end
+
+    def firmware_root
+      "/opt/fw"
+    end
+
+    def path
+      "#{firmware_root}/CLOUDHV-#{version}.fd"
+    end
+
+    def download
+      return if File.exist?(path)
+      FileUtils.mkdir_p(firmware_root)
+      sha256_curl = nil
+      safe_create_file_with_content(path) do |f|
+        sha256_curl = curl_firmware(f)
+        fail "Invalid SHA-256 digest" unless sha256 == sha256_curl
+      end
+      sha256_curl
+    end
+
+    def curl_firmware(file)
+      r("bash -c 'curl -f -L3 #{url.shellescape} | tee >(openssl dgst -sha256) > #{file.path.shellescape}'").split(" ").last
+    end
+  }
+
+  NEW_FIRMWARE = FirmwareClass.new(Arch.render(x64: "202402", arm64: "202211"),
+    Arch.render(x64: "2cf0e7bd7164cbe1d353ccfe176f41e57b8036492eb7b0a94f9b04c0c973764d", arm64: "3e34934478870a2ce67eb76bef6f9f38eb6a8b16849115b64473f05c2ccb922a"))
 
   VersionClass = Struct.new(:version) {
     def ch_remote_url
