14dbfb425976d501ef0634bddb96abc291089b02
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-05-22 20:59:47 +0300

    Merge web and api serializers for projects
    Polymorphic authorization checks and options based serializers made serializers
    strong enough to format slightly different outputs based on the needs of web
    and api endpoints. This commit merges the two project serializers into one.

--- serializers/common/project.rb
+++ serializers/common/project.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
-class Serializers::Api::Project < Serializers::Base
+class Serializers::Common::Project < Serializers::Base
   def self.serialize_internal(p, options = {})
-    {
+    base = {
       id: p.ubid,
       name: p.name,
       credit: p.credit.to_f,
       discount: p.discount
     }
+
+    if options[:include_path]
+      base[:path] = p.path
+    end
+
+    base
   end
 end
