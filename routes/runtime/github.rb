560bec2b9e4b411625dbdfef201e6e6907f12936
Enes Cakir <enes@ubicloud.com>
2024-05-29 19:30:23 +0300

    Add reserveCache endpoint
    The client reserves a cache entry before beginning to upload content to
    the blob storage. S3 compatible blob storage necessitates sending a
    `CreateMultipartUpload` request prior to the content upload. This
    returns an `UploadId`.
    
    The 'POST /caches' endpoint, ('reserveCache' [^1]), creates a cache
    entry using the given "key" and "version" parameters. This prevents any
    other runner from starting to upload the same cache. It returns an
    `upload_id` from the blob storage and a list of presigned URLs for each
    32MB part, which the client will use to upload each chunk.
    
    The cache client uses the `cacheId` in some logics. This is unnecessary
    for our forked version since we already have `uploadId`. However, to
    avoid extensive modifications to the client code, a fixed `cacheId` is
    returned for each request.
    
    [^1]: https://github.com/ubicloud/toolkit/blob/d1df13e178816d69d96bdc5c753b36a66ad03728/packages/cache/src/internal/cacheHttpClient.ts#L204

--- routes/runtime/github.rb
+++ routes/runtime/github.rb
@@ -7,5 +7,41 @@ class CloverRuntime
     end
 
     repository.setup_blob_storage unless repository.access_key
+
+    r.on "caches" do
+      # reserveCache
+      r.post true do
+        key = r.params["key"]
+        version = r.params["version"]
+        size = r.params["cacheSize"].to_i
+        fail CloverError.new(400, "InvalidRequest", "Wrong parameters") if key.nil? || version.nil? || size == 0
+
+        unless (scope = runner.workflow_job&.dig("head_branch"))
+          # YYYY: If the webhook not delivered yet, we can try to get the branch from the API
+          Clog.emit("The runner does not have a workflow job") { {no_workflow_job: {ubid: runner.ubid, repository_ubid: repository.ubid}} }
+          fail CloverError.new(400, "InvalidRequest", "No workflow job data available")
+        end
+
+        if size > 10 * 1024 * 1024 * 1024
+          fail CloverError.new(400, "InvalidRequest", "The cache size is over the 10GB limit")
+        end
+
+        entry = GithubCacheEntry.create_with_id(repository_id: runner.repository.id, key: key, version: version, size: size, scope: scope, created_by: runner.id)
+
+        upload_id = repository.blob_storage_client.create_multipart_upload(bucket: repository.bucket_name, key: entry.blob_key).upload_id
+        entry.update(upload_id: upload_id)
+
+        max_chunk_size = 32 * 1024 * 1024 # 32MB
+        presigned_urls = (1..size.fdiv(max_chunk_size).ceil).map do
+          repository.url_presigner.presigned_url(:upload_part, bucket: repository.bucket_name, key: entry.blob_key, upload_id: upload_id, part_number: _1, expires_in: 900)
+        end
+
+        {
+          uploadId: upload_id,
+          presignedUrls: presigned_urls,
+          chunkSize: max_chunk_size
+        }
+      end
+    end
   end
 end
