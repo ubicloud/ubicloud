b2bce5b0847aa3056f4cdd2e8927a0e014a93c7d
Enes Cakir <enes@ubicloud.com>
2024-05-29 19:30:23 +0300

    Add reserveCache endpoint
    The client reserves a cache entry before beginning to upload content to
    the blob storage. S3 compatible blob storage necessitates sending a
    `CreateMultipartUpload` request prior to the content upload. This
    returns an `UploadId`.
    
    The 'POST /caches' endpoint, ('reserveCache' [^1]), creates a cache
    entry using the given "key" and "version" parameters. This prevents any
    other runner from starting to upload the same cache. It returns an
    `upload_id` from the blob storage and a list of presigned URLs for each
    32MB part, which the client will use to upload each chunk.
    
    The cache client uses the `cacheId` in some logics. This is unnecessary
    for our forked version since we already have `uploadId`. However, to
    avoid extensive modifications to the client code, a fixed `cacheId` is
    returned for each request.
    
    [^1]: https://github.com/ubicloud/toolkit/blob/d1df13e178816d69d96bdc5c753b36a66ad03728/packages/cache/src/internal/cacheHttpClient.ts#L204

--- spec/routes/runtime/github_spec.rb
+++ spec/routes/runtime/github_spec.rb
@@ -43,4 +43,67 @@ RSpec.describe Clover, "github" do
 
     expect(last_response.status).to eq(400)
   end
+
+  describe "cache endpoints" do
+    let(:repository) { GithubRepository.create_with_id(name: "test", default_branch: "main", access_key: "123") }
+    let(:runner) { GithubRunner.create_with_id(vm_id: create_vm.id, repository_name: "test", label: "ubicloud", repository_id: repository.id, workflow_job: {head_branch: "dev"}) }
+    let(:url_presigner) { instance_double(Aws::S3::Presigner, presigned_request: "aa") }
+    let(:blob_storage_client) { instance_double(Aws::S3::Client) }
+
+    before do
+      login_runtime(runner.vm)
+      allow(Aws::S3::Presigner).to receive(:new).and_return(url_presigner)
+      allow(Aws::S3::Client).to receive(:new).and_return(blob_storage_client)
+    end
+
+    describe "reserves cache" do
+      it "fails if one of the parameters are missing" do
+        [
+          ["k1", "v1", nil],
+          [nil, "v1", 10],
+          ["k1", nil, 10]
+        ].each do |key, version, size|
+          params = {key: key, version: version, cacheSize: size}.compact
+          post "/runtime/github/caches", params
+
+          expect(last_response.status).to eq(400)
+          expect(JSON.parse(last_response.body)["error"]["message"]).to eq("Wrong parameters")
+        end
+      end
+
+      it "fails if the runner doesn't have a scope" do
+        runner.update(workflow_job: nil)
+        post "/runtime/github/caches", {key: "k1", version: "v1", cacheSize: 100}
+
+        expect(last_response.status).to eq(400)
+        expect(JSON.parse(last_response.body)["error"]["message"]).to eq("No workflow job data available")
+      end
+
+      it "fails if cache is bigger than 10GB" do
+        post "/runtime/github/caches", {key: "k1", version: "v1", cacheSize: 11 * 1024 * 1024 * 1024}
+
+        expect(last_response.status).to eq(400)
+        expect(JSON.parse(last_response.body)["error"]["message"]).to eq("The cache size is over the 10GB limit")
+      end
+
+      it "returns presigned urls and upload id for the reserved cache" do
+        expect(blob_storage_client).to receive(:create_multipart_upload).and_return(instance_double(Aws::S3::Types::CreateMultipartUploadOutput, upload_id: "upload-id"))
+        expect(url_presigner).to receive(:presigned_url).with(:upload_part, hash_including(bucket: repository.bucket_name, upload_id: "upload-id")) do |_, params|
+          "url-#{params[:part_number]}"
+        end.exactly(3).times
+        post "/runtime/github/caches", {key: "k1", version: "v1", cacheSize: 75 * 1024 * 1024}
+
+        expect(last_response.status).to eq(200)
+        response = JSON.parse(last_response.body)
+        expect(response["uploadId"]).to eq("upload-id")
+        expect(response["presignedUrls"]).to eq(["url-1", "url-2", "url-3"])
+
+        entry = repository.cache_entries.first
+        expect(entry.key).to eq("k1")
+        expect(entry.version).to eq("v1")
+        expect(entry.size).to eq(75 * 1024 * 1024)
+        expect(entry.upload_id).to eq("upload-id")
+      end
+    end
+  end
 end
