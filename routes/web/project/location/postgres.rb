4fab539d925aa75697928b52fb5a053768f597a5
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-06-01 03:48:30 +0300

    Add helper class for postgres routes
    The business logic in both web and api endpoints are combined in the helper
    class. Currently the combination is made simply adding a branch for each option
    and copying the business logic from each endpoint to their respective branches.
    Rubocop also moved some common lines out of the branches, but other than that
    we did not try to converge single logic and eliminate the branches as much as
    possible. This is intentional as I wanted to keep this commit as boring and add
    a separate commit with the more interesting convergence stuff.
    
    In its current form, this commit does not provide much benefit and in fact it
    increases the total line count. However it paves the way for making complex
    improvements.

--- routes/web/project/location/postgres.rb
+++ routes/web/project/location/postgres.rb
@@ -9,101 +9,39 @@ class CloverWeb
         response.status = 404
         r.halt
       end
-      @pg = Serializers::Postgres.serialize(pg, {detailed: true, include_path: true})
+
+      route_helper = Routes::Common::PostgresHelper.new(app: self, request: r, user: @current_user, location: @location, resource: pg)
 
       r.get true do
-        Authorization.authorize(@current_user.id, "Postgres:view", pg.id)
-        view "postgres/show"
+        route_helper.get
       end
 
       r.delete true do
-        Authorization.authorize(@current_user.id, "Postgres:delete", pg.id)
-        pg.incr_destroy
-        return {message: "Deleting #{pg.name}"}.to_json
+        route_helper.delete
       end
 
       r.on "firewall-rule" do
         r.post true do
-          Authorization.authorize(@current_user.id, "Postgres:Firewall:edit", pg.id)
-          parsed_cidr = Validation.validate_cidr(r.params["cidr"])
-
-          DB.transaction do
-            PostgresFirewallRule.create_with_id(
-              postgres_resource_id: pg.id,
-              cidr: parsed_cidr.to_s
-            )
-            pg.incr_update_firewall_rules
-          end
-
-          flash["notice"] = "Firewall rule is created"
-
-          r.redirect "#{@project.path}#{pg.path}"
+          route_helper.post_firewall_rule
         end
 
         r.is String do |firewall_rule_ubid|
           r.delete true do
-            Authorization.authorize(@current_user.id, "Postgres:Firewall:edit", pg.id)
-            fwr = PostgresFirewallRule.from_ubid(firewall_rule_ubid)
-            unless fwr
-              response.status = 404
-              r.halt
-            end
-
-            DB.transaction do
-              fwr.destroy
-              pg.incr_update_firewall_rules
-            end
-
-            return {message: "Firewall rule deleted"}.to_json
+            route_helper.delete_firewall_rule(firewall_rule_ubid)
           end
         end
       end
 
       r.post "restore" do
-        Authorization.authorize(@current_user.id, "Postgres:create", @project.id)
-        Authorization.authorize(@current_user.id, "Postgres:view", pg.id)
-
-        st = Prog::Postgres::PostgresResourceNexus.assemble(
-          project_id: @project.id,
-          location: pg.location,
-          name: r.params["name"],
-          target_vm_size: pg.target_vm_size,
-          target_storage_size_gib: pg.target_storage_size_gib,
-          parent_id: pg.id,
-          restore_target: r.params["restore_target"]
-        )
-
-        flash["notice"] = "'#{r.params["name"]}' will be ready in a few minutes"
-
-        r.redirect "#{@project.path}#{st.subject.path}"
+        route_helper.restore
       end
 
       r.post "reset-superuser-password" do
-        Authorization.authorize(@current_user.id, "Postgres:create", @project.id)
-        Authorization.authorize(@current_user.id, "Postgres:view", pg.id)
-
-        unless pg.representative_server.primary?
-          flash["error"] = "Superuser password cannot be updated during restore!"
-          return redirect_back_with_inputs
-        end
-
-        Validation.validate_postgres_superuser_password(r.params["original_password"], r.params["repeat_password"])
-
-        pg.update(superuser_password: r.params["original_password"])
-        pg.representative_server.incr_update_superuser_password
-
-        flash["notice"] = "The superuser password will be updated in a few seconds"
-
-        r.redirect "#{@project.path}#{pg.path}"
+        route_helper.reset_superuser_password
       end
 
       r.post "restart" do
-        Authorization.authorize(@current_user.id, "Postgres:edit", pg.id)
-        pg.servers.each do |s|
-          s.incr_restart
-        rescue Sequel::ForeignKeyConstraintViolation
-        end
-        r.redirect "#{@project.path}#{pg.path}"
+        route_helper.restart
       end
     end
   end
