c448a98ad6737f5ef292be0cb9f19b5e8a62a60d
Benjamin Satzger <ben@ubicloud.com>
2024-06-03 22:58:47 +0200

    Leverage utility function to download firmware
    Use a utility function from rhizome lib to download firmware instead of
    a custom curl_firmware method.

--- rhizome/host/spec/cloud_hypervisor_spec.rb
+++ rhizome/host/spec/cloud_hypervisor_spec.rb
@@ -3,31 +3,45 @@
 require_relative "../lib/cloud_hypervisor"
 
 RSpec.describe CloudHypervisor do
-  subject(:fw) { CloudHypervisor::FirmwareClass.new("202402", "thesha") }
+  describe "#firmware" do
+    subject(:fw) { CloudHypervisor::FirmwareClass.new("202402", "thesha") }
 
-  describe "#download" do
-    it "can use an existing firmware" do
-      expect(File).to receive(:exist?).with("/opt/fw/CLOUDHV-202402.fd").and_return(true)
-      expect(fw).not_to receive(:curl_firmware)
-      fw.download
+    let(:gh_fw_url) { "https://github.com/ubicloud/build-edk2-firmware/releases/download/edk2-stable202402-x64/CLOUDHV-x64.fd" }
+    let(:firmware_path) { "/opt/fw/CLOUDHV-202402.fd" }
+    let(:firmware_tmp_path) { "/opt/fw/CLOUDHV-202402.fd.tmp" }
+
+    context "when firmware already exists" do
+      before do
+        allow(File).to receive(:exist?).with(firmware_path).and_return(true)
+      end
+
+      it "does not download the firmware" do
+        expect(fw).not_to receive(:curl_file)
+        fw.download
+      end
     end
 
-    it "can download a firmware" do
-      expect(File).to receive(:exist?).with("/opt/fw/CLOUDHV-202402.fd").and_return(false)
-      expect(FileUtils).to receive(:mkdir_p).with("/opt/fw")
-      f = instance_double(File)
-      expect(fw).to receive(:safe_write_to_file).with("/opt/fw/CLOUDHV-202402.fd").and_yield(f)
-      expect(fw).to receive(:curl_firmware).with(f).and_return("thesha")
-      fw.download
-    end
+    context "when firmware does not exist" do
+      before do
+        allow(File).to receive(:exist?).with(firmware_path).and_return(false)
+        allow(FileUtils).to receive(:mkdir_p).with("/opt/fw")
+      end
 
-    it "fails if sha is incorrect" do
-      expect(File).to receive(:exist?).with("/opt/fw/CLOUDHV-202402.fd").and_return(false)
-      expect(FileUtils).to receive(:mkdir_p).with("/opt/fw")
-      f = instance_double(File)
-      expect(fw).to receive(:safe_write_to_file).with("/opt/fw/CLOUDHV-202402.fd").and_yield(f)
-      expect(fw).to receive(:curl_firmware).with(f).and_return("anothersha")
-      expect { fw.download }.to raise_error("Invalid SHA-256 digest")
+      it "downloads the firmware" do
+        f = instance_double(File, path: firmware_tmp_path)
+        allow(Arch).to receive(:sym).and_return(:x64)
+        expect(fw).to receive(:safe_write_to_file).with(firmware_path).and_yield(f)
+        expect(fw).to receive(:curl_file).with(gh_fw_url, firmware_tmp_path).and_return("thesha")
+        fw.download
+      end
+
+      it "raises an error if the SHA-256 digest is incorrect" do
+        f = instance_double(File, path: firmware_tmp_path)
+        allow(Arch).to receive(:sym).and_return(:x64)
+        expect(fw).to receive(:safe_write_to_file).with(firmware_path).and_yield(f)
+        expect(fw).to receive(:curl_file).with(gh_fw_url, firmware_tmp_path).and_return("anothersha")
+        expect { fw.download }.to raise_error("Invalid SHA-256 digest")
+      end
     end
   end
 end
