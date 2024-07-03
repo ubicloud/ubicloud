bc11688e0cb59edd253f1b0b65791db01df81a4f
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-07-03 14:12:00 +0200

    Add base class for common routes
    We are working towards combining business logic in web and api endpoints. For
    that purpose, we will introduce helper classes that will be shared between the
    two routes. This is the base class for those helper classes.
    
    The base class is quite simple at the moment. It only includes an initializer
    and few helper methods to easily access app object's fields.
    
    What this and following commits provide is mostly a refactor and deduplication.
    There are almost no changes in the actual behavior of the app. In the few cases
    where there are changes, they are made on the side of web endpoints and caller
    part of the code is also updated respectively.

---
+++ routes/common/base.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Routes::Common::Base
+  module AppMode
+    API = :api
+    WEB = :web
+  end
+
+  def initialize(app:, request:, user:, location:, resource:)
+    @app = app
+    @request = request
+    @user = user
+    @resource = resource
+    @location = location
+    @mode = if app.instance_of?(::CloverApi)
+      AppMode::API
+    elsif app.instance_of?(::CloverWeb)
+      AppMode::WEB
+    else
+      raise "Unknown app mode"
+    end
+  end
+
+  def project
+    @app.instance_variable_get(:@project)
+  end
+
+  def response
+    @app.response
+  end
+
+  def flash
+    @app.flash
+  end
+end
