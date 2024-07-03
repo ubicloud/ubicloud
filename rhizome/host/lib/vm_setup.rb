7aa764d4764b5347640aff2474fead3143b57df1
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-07-03 03:40:09 +0200

    Allow passing multiple ssh keys while setting up VM
    We used to allow passing only one ssh key while setting up a VM. For the VMs
    that belongs to services, it would be useful to pass multiple ssh keys. For
    example, we can pass array of operator keys to be put into the VMs.
    
    An alternative approach would be adding SSH keys to the VM using the sshable
    interface. However, that would happen after certain steps in the service setup,
    thus wouldn't be useful for debugging bootstrapping issues. It would also
    require each service to implement key addition logic.

--- rhizome/host/lib/vm_setup.rb
+++ rhizome/host/lib/vm_setup.rb
@@ -43,9 +43,9 @@ class VmSetup
     @vp ||= VmPath.new(@vm_name)
   end
 
-  def prep(unix_user, public_key, nics, gua, ip4, local_ip4, max_vcpus, cpu_topology, mem_gib, ndp_needed, storage_params, storage_secrets, swap_size_bytes, pci_devices)
+  def prep(unix_user, public_keys, nics, gua, ip4, local_ip4, max_vcpus, cpu_topology, mem_gib, ndp_needed, storage_params, storage_secrets, swap_size_bytes, pci_devices)
     setup_networking(false, gua, ip4, local_ip4, nics, ndp_needed, multiqueue: max_vcpus > 1)
-    cloudinit(unix_user, public_key, nics, swap_size_bytes)
+    cloudinit(unix_user, public_keys, nics, swap_size_bytes)
     storage(storage_params, storage_secrets, true)
     hugepages(mem_gib)
     prepare_pci_devices(pci_devices)
@@ -58,9 +58,9 @@ class VmSetup
     storage(storage_params, storage_secrets, false)
   end
 
-  def reassign_ip6(unix_user, public_key, nics, gua, ip4, local_ip4, max_vcpus, cpu_topology, mem_gib, ndp_needed, storage_params, storage_secrets, swap_size_bytes, pci_devices)
+  def reassign_ip6(unix_user, public_keys, nics, gua, ip4, local_ip4, max_vcpus, cpu_topology, mem_gib, ndp_needed, storage_params, storage_secrets, swap_size_bytes, pci_devices)
     setup_networking(false, gua, ip4, local_ip4, nics, ndp_needed, multiqueue: max_vcpus > 1)
-    cloudinit(unix_user, public_key, nics, swap_size_bytes)
+    cloudinit(unix_user, public_keys, nics, swap_size_bytes)
     hugepages(mem_gib)
     storage(storage_params, storage_secrets, false)
     install_systemd_unit(max_vcpus, cpu_topology, mem_gib, storage_params, nics, pci_devices)
@@ -394,7 +394,7 @@ add element inet drop_unused_ip_packets allowed_ipv4_addresses { #{ip_net} }
     r "ip netns exec #{q_vm} bash -c 'nft -f #{vp.q_nftables_conf}'"
   end
 
-  def cloudinit(unix_user, public_key, nics, swap_size_bytes)
+  def cloudinit(unix_user, public_keys, nics, swap_size_bytes)
     vp.write_meta_data(<<EOS)
 instance-id: #{yq(@vm_name)}
 local-hostname: #{yq(@vm_name)}
@@ -441,7 +441,7 @@ ethernets:
 #{ethernets}
 EOS
 
-    write_user_data(unix_user, public_key, swap_size_bytes)
+    write_user_data(unix_user, public_keys, swap_size_bytes)
 
     FileUtils.rm_rf(vp.cloudinit_img)
     r "mkdosfs -n CIDATA -C #{vp.q_cloudinit_img} 8192"
@@ -462,7 +462,7 @@ EOS
     SWAP_CONFIG
   end
 
-  def write_user_data(unix_user, public_key, swap_size_bytes)
+  def write_user_data(unix_user, public_keys, swap_size_bytes)
     vp.write_user_data(<<EOS)
 #cloud-config
 users:
@@ -470,7 +470,7 @@ users:
     sudo: ALL=(ALL) NOPASSWD:ALL
     shell: /bin/bash
     ssh_authorized_keys:
-      - #{yq(public_key)}
+#{public_keys.map { "      - #{yq(_1)}" }.join("\n")}
 
 ssh_pwauth: False
 
