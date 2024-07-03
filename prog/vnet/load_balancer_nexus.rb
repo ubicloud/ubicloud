679aad821d50f54b75153475b92dab3fe7caa4bf
Furkan Sahin <furkan@ubicloud.com>
2024-07-03 14:58:04 +0200

    Load Balancer DNS integration
    We need to provide a hostname to the customers to be able to face a
    single address to provide continuous connectivity even when the
    underlying IP addresses of the load balancer changes. We accomplish this
    by integrating load balancer with our existing DNS service. We will
    create a new dns zone and manage individual host names depending on the
    load balancer activity.

--- prog/vnet/load_balancer_nexus.rb
+++ prog/vnet/load_balancer_nexus.rb
@@ -2,7 +2,7 @@
 
 class Prog::Vnet::LoadBalancerNexus < Prog::Base
   subject_is :load_balancer
-  semaphore :destroy, :update_load_balancer
+  semaphore :destroy, :update_load_balancer, :rewrite_dns_records
 
   def self.assemble(private_subnet_id, name: nil, algorithm: "round_robin", src_port: nil, dst_port: nil)
 
@@ -33,6 +33,11 @@ class Prog::Vnet::LoadBalancerNexus < Prog::Base
       hop_update_vm_load_balancers
     end
 
+    when_rewrite_dns_records_set? do
+      rewrite_dns_records
+      decr_rewrite_dns_records
+    end
+
     nap 5
   end
 
@@ -57,6 +62,11 @@ class Prog::Vnet::LoadBalancerNexus < Prog::Base
   label def destroy
     decr_destroy
     strand.children.map { _1.destroy }
+    # The following if statement makes sure that it's OK to not have dns_zone
+    # only if it's not in production.
+    if (dns_zone = Prog::Vnet::LoadBalancerNexus.dns_zone) && (Config.production? || dns_zone)
+      dns_zone.delete_record(record_name: load_balancer.hostname)
+    end
 
     load_balancer.vms.each do |vm|
       bud Prog::Vnet::UpdateLoadBalancer, {"subject_id" => vm.id, "load_balancer_id" => load_balancer.id}, :remove_load_balancer
@@ -75,5 +85,18 @@ class Prog::Vnet::LoadBalancerNexus < Prog::Base
     end
 
     nap 5
+  end
+
+  def rewrite_dns_records
+    Prog::Vnet::LoadBalancerNexus.dns_zone&.delete_record(record_name: load_balancer.hostname)
+
+    load_balancer.vms_to_dns.map do |vm|
+      Prog::Vnet::LoadBalancerNexus.dns_zone&.insert_record(record_name: load_balancer.hostname, type: "A", ttl: 10, data: vm.ephemeral_net4.to_s) if vm.ephemeral_net4
+      Prog::Vnet::LoadBalancerNexus.dns_zone&.insert_record(record_name: load_balancer.hostname, type: "AAAA", ttl: 10, data: vm.ephemeral_net6.nth(2).to_s)
+    end
+  end
+
+  def self.dns_zone
+    @@dns_zone ||= DnsZone[project_id: Config.load_balancer_service_project_id, name: Config.load_balancer_service_hostname]
   end
 end
