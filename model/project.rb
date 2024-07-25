c18d23f3f0c38e2181c225fea507442ad68841ff
Burak Velioglu <velioglub@ubicloud.com>
2024-07-25 18:12:04 +0300

    Add cache proxy for projects with transparent cache flag
    This commit introduces support for transparent cache, enabling the
    use of our own infrastructure for saving and restoring caches without
    changing anything on the test workflow files. The proxy is now run on
    runner VMs to intercept and handle all cache-related requests. The
    proxy will only be activated for projects where the transparent_cache
    flag is set to true.
    
    Key changes include:
    
    Proxy Setup: A proxy is deployed on the runner VMs to handle cache
    requests. This proxy intercepts requests intended for GitHub's cache
    API and redirects them to our infrastructure.
    
    Forked Runner Script: A forked version of the runner script is downloaded
    and used instead of the pre-existing one on the VM. This modification
    is necessary because redirecting requests from GitHub's cache URI isn't
    straightforward. By setting CUSTOM_ACTIONS_CACHE_URL to localhost:8080,
    all cache requests are directed to the proxy running locally on port 8080.
    
    Blob Storage Redirection: Requests to localhost.blob.core.windows.net
    are also redirected to the local proxy, as GitHub's toolkit directly
    accesses blob storage during file restoration rather than routing through
    the cache API.
    
    Note that the current implementation downloads and runs the forked
    runner and proxy while initiating runner vm for ease of testing. In
    future iterations, both steps will be integrated into the image generation
    process to reduce VM setup time.

--- model/project.rb
+++ model/project.rb
@@ -132,5 +132,5 @@ class Project < Sequel::Model
     end
   end
 
-  feature_flag :postgresql_base_image, :vm_public_ssh_keys
+  feature_flag :postgresql_base_image, :vm_public_ssh_keys, :transparent_cache
 end
