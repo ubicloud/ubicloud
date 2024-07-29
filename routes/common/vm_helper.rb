7ff8b3f45ad89521ce141b00aeda6c77dd247cd3
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-07-30 01:29:03 +0200

    Combine business logic for Vm resource deletion endpoints
    For web, we used to return message in the response, however it is not used in
    the frontend. We also returned 404 when the resource is not found, but that is
    already handled as success case. This commit removes both thus makes overall
    logic to more similar to API endpoints.

--- routes/common/vm_helper.rb
+++ routes/common/vm_helper.rb
@@ -93,12 +93,8 @@ class Routes::Common::VmHelper < Routes::Common::Base
   def delete
     Authorization.authorize(@user.id, "Vm:delete", @resource.id)
     @resource.incr_destroy
-    if @mode == AppMode::API
-      response.status = 204
-      @request.halt
-    else
-      {message: "Deleting #{@resource.name}"}.to_json
-    end
+    response.status = 204
+    @request.halt
   end
 
   def get_create
