d071be58cf90cb15f67d3064b51ef1d6a2c94698
Furkan Sahin <furkan@ubicloud.com>
2024-07-31 18:00:53 +0200

    Load Balancer's Cert integration
    This commit simply implements the load balancer's certificate
    integration by utilizing the FSMs implemented in the previous commit.

---
+++ model/certs_load_balancers.rb
@@ -0,0 +1,16 @@
+#  frozen_string_literal: true
+
+require_relative "../model"
+
+class CertsLoadBalancers < Sequel::Model
+  many_to_one :load_balancer
+  many_to_one :cert
+  include ResourceMethods
+
+  def destroy
+    DB.transaction do
+      cert.incr_destroy
+      super
+    end
+  end
+end
