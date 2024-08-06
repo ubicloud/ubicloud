e816969eabe6f726dfd0a90a5c16db8e74b4f03f
Furkan Sahin <furkan@ubicloud.com>
2024-08-06 09:01:51 +0200

    ACME Certificate integration related tests
    This also includes the load balancer's certificate related test changes.

--- spec/model/resource_methods_spec.rb
+++ spec/model/resource_methods_spec.rb
@@ -4,7 +4,7 @@ require_relative "../spec_helper"
 
 RSpec.describe ResourceMethods do
   it "hides sensitive and long columns" do
-    [GithubRunner, PostgresResource, Vm, VmHost, MinioCluster, MinioServer].each do |klass|
+    [GithubRunner, PostgresResource, Vm, VmHost, MinioCluster, MinioServer, Cert].each do |klass|
       inspect_output = klass.new.inspect
       klass.redacted_columns.each do |column_key|
         expect(inspect_output).not_to include column_key.to_s
