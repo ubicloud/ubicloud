d8bfbe029d0354313ec041554d661228a1d3d2f9
Furkan Sahin <sahinffurkan@gmail.com>
2024-09-13 13:28:30 +0200

    Revert "Revert "Update health probes to use the hostname and check return type""
    This reverts commit 3bf4312908631f68a19fa180a7d25d33fc6f1747.

--- prog/vnet/load_balancer_health_probes.rb
+++ prog/vnet/load_balancer_health_probes.rb
@@ -12,7 +12,7 @@ class Prog::Vnet::LoadBalancerHealthProbes < Prog::Base
       cmd = if load_balancer.health_check_protocol == "tcp"
         "sudo ip netns exec #{vm.inhost_name} nc -z -w #{load_balancer.health_check_timeout} #{vm.nics.first.private_ipv4.network} #{load_balancer.dst_port} && echo 200 || echo 400"
       else
-        "sudo ip netns exec #{vm.inhost_name} curl --insecure --max-time #{load_balancer.health_check_timeout} --silent --output /dev/null --write-out '%{http_code}' #{load_balancer.health_check_protocol}://#{vm.nics.first.private_ipv4.network}:#{load_balancer.dst_port}#{load_balancer.health_check_endpoint}"
+        "sudo ip netns exec #{vm.inhost_name} curl --insecure --resolve #{load_balancer.hostname}:#{load_balancer.dst_port}:#{vm.nics.first.private_ipv4.network} --max-time #{load_balancer.health_check_timeout} --silent --output /dev/null --write-out '%{http_code}' #{load_balancer.health_check_protocol}://#{load_balancer.hostname}:#{load_balancer.dst_port}#{load_balancer.health_check_endpoint}"
       end
 
       vm.vm_host.sshable.cmd(cmd)
@@ -21,7 +21,7 @@ class Prog::Vnet::LoadBalancerHealthProbes < Prog::Base
     end
 
     vm_state, vm_state_counter = load_balancer.load_balancers_vms_dataset.where(vm_id: vm.id).get([:state, :state_counter])
-    threshold, health_check = (response_code == "200") ?
+    threshold, health_check = (response_code.to_i == 200) ?
       [load_balancer.health_check_up_threshold, "up"] :
       [load_balancer.health_check_down_threshold, "down"]
     counter = (vm_state == health_check) ? vm_state_counter + 1 : 1
