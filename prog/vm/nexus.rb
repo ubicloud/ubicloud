68246e6afe392df6db3dbd1c3f02da13231ee232
Furkan Sahin <sahinffurkan@gmail.com>
2024-09-13 13:03:14 +0200

    Revert "Fix VM removal when its attached to a load balancer"
    This reverts commit 4a00e9050470ea0193bd4bd8fe21dcc11a0f7deb.

--- prog/vm/nexus.rb
+++ prog/vm/nexus.rb
@@ -437,7 +437,7 @@ class Prog::Vm::Nexus < Prog::Base
       nap 30
     end
 
-    vm.load_balancer.remove_vm(vm) if vm.load_balancer
+    vm.load_balancer.remove_vm(vm)
 
     vm.vm_host.sshable.cmd("sudo host/bin/setup-vm delete_net #{q_vm}")
 
