5722d64c87e5ed84455d4d43ee3764fc4e4fec87
Benjamin Satzger <ben@ubicloud.com>
2024-09-26 14:13:14 +0200

    New boot image types for AI inferencing
    This commit introduces new boot image types related to AI inferencing.
    The new types are: AI base and AI model boot images. The AI base boot
    image is a base image that contains the necessary software to run
    inferencing. The AI model boot image is a boot image that contains the
    AI model to be run on the AI base boot image.

--- prog/download_boot_image.rb
+++ prog/download_boot_image.rb
@@ -14,7 +14,7 @@ class Prog::DownloadBootImage < Prog::Base
   end
 
   def download_from_blob_storage?
-    image_name.start_with?("github", "postgres") || Config.e2e_test?
+    image_name.start_with?("github", "postgres", "ai-") || Config.e2e_test?
   end
 
   def default_boot_image_version(image_name)
@@ -37,6 +37,8 @@ class Prog::DownloadBootImage < Prog::Base
       Config.github_gpu_ubuntu_2204_version
     when "postgres-ubuntu-2204"
       Config.postgres_ubuntu_2204_version
+    when "ai-ubuntu-2404-nvidia"
+      Config.ai_ubuntu_2404_nvidia_version
     else
       fail "Unknown boot image: #{image_name}"
     end
@@ -51,11 +53,13 @@ class Prog::DownloadBootImage < Prog::Base
           "github" => "raw",
           "postgres" => "raw",
           "ubuntu" => "img",
-          "almalinux" => "qcow2"
+          "almalinux" => "qcow2",
+          "ai" => "raw"
         }
         image_family = image_name.split("-").first
         suffix = suffixes.fetch(image_family, nil)
-        blob_storage_client.get_presigned_url("GET", Config.ubicloud_images_bucket_name, "#{image_name}-#{vm_host.arch}-#{version}.#{suffix}", 60 * 60).to_s
+        arch = image_name.start_with?("ai-model") ? "-" : "-#{vm_host.arch}-"
+        blob_storage_client.get_presigned_url("GET", Config.ubicloud_images_bucket_name, "#{image_name}#{arch}#{version}.#{suffix}", 60 * 60).to_s
       elsif image_name == "ubuntu-noble"
         arch = (vm_host.arch == "x64") ? "amd64" : "arm64"
         "https://cloud-images.ubuntu.com/releases/noble/release-#{version}/ubuntu-24.04-server-cloudimg-#{arch}.img"
@@ -102,7 +106,10 @@ class Prog::DownloadBootImage < Prog::Base
       ["github-gpu-ubuntu-2204", "x64", "20240818.1.0"] => "c366cc99107b1ea9c12cc6cfc03073a90f3aad011e333a59e0b6cfdc36776568",
       ["postgres-ubuntu-2204", "x64", "20240226.1.0"] => "f8a2b78189239717355b54ecf62a504a349c96b1ab6a21919984f58c2a367617",
       ["postgres-ubuntu-2204", "x64", "20240702.3.0"] => "02dfa6e844fa3e72224f2f0f1811e039221acee07d6b05b507d49ae17f84d0ce",
-      ["postgres-paradedb-ubuntu-2204", "x64", "20240926.1.0"] => "9249a64a516e7e39170cea7427c0cf9c32627061755773294a19e3367f114824"
+      ["postgres-paradedb-ubuntu-2204", "x64", "20240926.1.0"] => "9249a64a516e7e39170cea7427c0cf9c32627061755773294a19e3367f114824",
+      ["ai-ubuntu-2404-nvidia", "x64", "20240928.1.0"] => "b222788ebac32d08a6fcb4d3dfe1f0420f5da9cba1fe90fd56be834d51b8e054",
+      ["ai-model-gemma-2-2b-it", "x64", "20240918.1.0"] => "b726ead6d5f48fb8e6de7efb48fb22367c9a7c155cfee71a3a7e5527be5df08e",
+      ["ai-model-llama-3-2-3b-it", "x64", "20240926.1.0"] => "02b0b34fa84843ba1d09dc7f5d3877cf79dbbb3dcaa087ea16af72b95e35a354"
     }
 
     # YYY: In future all images should be checked for sha256 sum, so the nil
