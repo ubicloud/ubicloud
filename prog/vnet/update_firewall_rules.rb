9496bf12b43b3f08f8c6872a4b4a9de1623e5431
Furkan Sahin <furkan@ubicloud.com>
2024-10-08 18:03:27 +0200

    apply firewall rules to private subnet addresses as well

--- prog/vnet/update_firewall_rules.rb
+++ prog/vnet/update_firewall_rules.rb
@@ -7,11 +7,9 @@ class Prog::Vnet::UpdateFirewallRules < Prog::Base
 
   label def update_firewall_rules
     rules = vm.firewalls.map(&:firewall_rules).flatten
-    allowed_ingress_ip4 = NetAddr.summ_IPv4Net(rules.select { !_1.ip6? && !_1.port_range }.map { _1.cidr }).map(&:to_s)
-    allowed_ingress_ip6 = NetAddr.summ_IPv6Net(rules.select { _1.ip6? && !_1.port_range }.map { _1.cidr }).map(&:to_s)
     allowed_ingress_ip4_port_set = consolidate_rules(rules.select { !_1.ip6? && _1.port_range })
     allowed_ingress_ip6_port_set = consolidate_rules(rules.select { _1.ip6? && _1.port_range })
-    guest_ephemeral = subdivide_network(vm.ephemeral_net6).first.to_s
+    guest_ephemeral, clover_ephemeral = subdivide_network(vm.ephemeral_net6).map(&:to_s)
 
     globally_blocked_ipv4s, globally_blocked_ipv6s = generate_globally_blocked_lists
 
@@ -22,18 +20,6 @@ class Prog::Vnet::UpdateFirewallRules < Prog::Base
 table inet fw_table;
 delete table inet fw_table;
 table inet fw_table {
-  set allowed_ipv4_cidrs {
-    type ipv4_addr;
-    flags interval;
-#{allowed_ingress_ip4.any? ? "elements = {#{allowed_ingress_ip4.join(",")}}" : ""}
-  }
-
-  set allowed_ipv6_cidrs {
-    type ipv6_addr;
-    flags interval;
-#{allowed_ingress_ip6.any? ? "elements = {#{allowed_ingress_ip6.join(",")}}" : ""}
-  }
-
   set allowed_ipv4_port_tuple {
     type ipv4_addr . inet_service;
     flags interval;
@@ -79,26 +65,70 @@ table inet fw_table {
 
   chain forward_ingress {
     type filter hook forward priority filter; policy drop;
-    meta l4proto { tcp, udp } flow offload @ubi_flowtable
+
+    # Offload to ubi_flowtable. This is used to offload already filtered
+    # traffic to reduce the latency.
+    # meta l4proto { tcp, udp } flow offload @ubi_flowtable
+
+    # Destination port 111 is reserved for the portmapper. We block it to
+    # prevent abuse.
     meta l4proto { tcp, udp } th dport 111 drop
+
+    # Drop all traffic from globally blocked IPs. This is mainly used to
+    # block access to malicious IPs that are known to cause issues on the
+    # internet.
     ip saddr @globally_blocked_ipv4s drop
     ip6 saddr @globally_blocked_ipv6s drop
     ip daddr @globally_blocked_ipv4s drop
     ip6 daddr @globally_blocked_ipv6s drop
+
+    # If we are using @private_ipv4_cidrs as source address, we allow all
+    # established,related,new traffic because this is outgoing traffic.
     ip saddr @private_ipv4_cidrs ct state established,related,new counter accept
-    ip daddr @private_ipv4_cidrs ct state established,related counter accept
+
+    # If we are using clover_ephemeral, that means we are using ipsec. We need
+    # to allow traffic for the private communication and block via firewall
+    # rules through @allowed_ipv4_port_tuple and @allowed_ipv6_port_tuple in the
+    # next section of rules.
+    ip6 daddr #{clover_ephemeral} ct state established,related,new counter accept
+    ip6 saddr #{clover_ephemeral} ct state established,related,new counter accept
+
+    # Allow TCP and UDP traffic for allowed_ipv4_port_tuple and
+    # allowed_ipv6_port_tuple into the VM using any address, such as;
+    #  - public ipv4
+    #  - private ipv4
+    #  - public ipv6 (guest_ephemeral)
+    #  - private ipv6
+    #  - private clover ephemeral ipv6
+    ip saddr . tcp dport @allowed_ipv4_port_tuple ct state established,related,new counter accept
+    ip saddr . udp dport @allowed_ipv4_port_tuple ct state established,related,new counter accept
+    ip6 saddr . tcp dport @allowed_ipv6_port_tuple ct state established,related,new counter accept
+    ip6 saddr . udp dport @allowed_ipv6_port_tuple ct state established,related,new counter accept
+
+    # Allow outgoing traffic from the VM using the following addresses as
+    # source address.
     ip6 saddr @private_ipv6_cidrs ct state established,related,new counter accept
-    ip6 daddr @private_ipv6_cidrs ct state established,related,new counter accept
     ip6 saddr #{guest_ephemeral} ct state established,related,new counter accept
-    ip6 daddr #{guest_ephemeral} ct state established,related,new counter accept
-    ip saddr @allowed_ipv4_cidrs ip daddr @private_ipv4_cidrs counter accept
-    ip6 saddr @allowed_ipv6_cidrs ip6 daddr #{guest_ephemeral} counter accept
-    ip saddr . tcp dport @allowed_ipv4_port_tuple ip daddr @private_ipv4_cidrs counter accept
-    ip6 saddr . tcp dport @allowed_ipv6_port_tuple ip6 daddr #{guest_ephemeral} counter accept
-    ip saddr . udp dport @allowed_ipv4_port_tuple ip daddr @private_ipv4_cidrs counter accept
-    ip6 saddr . udp dport @allowed_ipv6_port_tuple ip6 daddr #{guest_ephemeral} counter accept
+    ip6 saddr #{clover_ephemeral} ct state established,related,new counter accept
+
+    # Allow incoming traffic to the VM using the following addresses as
+    # destination address. This is needed to allow the return traffic.
+    ip6 daddr @private_ipv6_cidrs ct state established,related counter accept
+    ip6 daddr #{guest_ephemeral} ct state established,related counter accept
+    ip6 daddr #{clover_ephemeral} ct state established,related counter accept
+
+    # ???
+    ip daddr @private_ipv4_cidrs ct state established,related counter accept
+
+    # Allow ping for all
     ip saddr 0.0.0.0/0 icmp type echo-request counter accept
+    ip daddr 0.0.0.0/0 icmp type echo-request counter accept
+    ip saddr 0.0.0.0/0 icmp type echo-reply counter accept
+    ip daddr 0.0.0.0/0 icmp type echo-reply counter accept
     ip6 saddr ::/0 icmpv6 type echo-request counter accept
+    ip6 daddr ::/0 icmpv6 type echo-request counter accept
+    ip6 saddr ::/0 icmpv6 type echo-reply counter accept
+    ip6 daddr ::/0 icmpv6 type echo-reply counter accept
   }
 }
 TEMPLATE
@@ -210,11 +240,11 @@ TEMPLATE
   end
 
   def generate_private_ip4_list
-    vm.nics.map { NetAddr::IPv4Net.parse(_1.private_ipv4.network.to_s + "/26").to_s }.join(",")
+    vm.nics.map { _1.private_ipv4.to_s }.join(",")
   end
 
   def generate_private_ip6_list
-    vm.nics.map { NetAddr::IPv6Net.parse(_1.private_ipv6.network.to_s + "/64").to_s }.join(",")
+    vm.nics.map { _1.private_ipv6.to_s }.join(",")
   end
 
   def subdivide_network(net)
