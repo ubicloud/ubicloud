29412eead2e4e73eb84a3942697eb8bf0eff4421
Jeremy Evans <jeremy@ubicloud.com>
2024-10-23 05:57:28 -0700

    Fix NoMethodError when logging in user via remember token
    When a user is logged in via the remember token, @current_user was
    previously set to nil, because it was set before load_memory,
    probably to make it available in the rodauth routes (I'm guessing).
    
    If @current_user isn't already set after requiring authentication,
    then set it at the point so it is available in all of the routes.

--- clover_web.rb
+++ clover_web.rb
@@ -391,6 +391,7 @@ class CloverWeb < Roda
       r.redirect rodauth.login_route
     end
     rodauth.require_authentication
+    @current_user ||= Account[rodauth.session_value]
 
     r.hash_branches("")
   end
