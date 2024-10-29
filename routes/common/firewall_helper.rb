dd351054386a147a30fc5a7a55f88b4047c759c6
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-10-29 22:30:03 +0100

    post

--- routes/common/firewall_helper.rb
+++ routes/common/firewall_helper.rb
@@ -22,4 +22,30 @@ class Routes::Common::FirewallHelper < Routes::Common::Base
       @app.view "networking/firewall/index"
     end
   end
+
+  def post(name)
+    Authorization.authorize(@user.id, "Firewall:create", project.id)
+
+    allowed_optional_parameters = ["description", "private_subnet_id"]
+    request_body_params = Validation.validate_request_body(params, [], allowed_optional_parameters)
+
+    Validation.validate_name(name)
+    
+    firewall = Firewall.create_with_id(
+      name: name,
+      description: request_body_params["description"],
+      location: @location
+    )
+    firewall.associate_with_project(@project)
+
+    ps = PrivateSubnet.from_ubid(request_body_params["private_subnet_id"]) if request_body_params["private_subnet_id"]
+    firewall.associate_with_private_subnet(ps) if ps
+
+    if @mode == AppMode::API
+      Serializers::Firewall.serialize(firewall)
+    else
+      flash["notice"] = "'#{name}' is created"
+      @request.redirect "#{@project.path}#{Firewall[fw.id].path}"
+    end
+  end
 end
--- routes/web/project/firewall.rb
+++ routes/web/project/firewall.rb
@@ -19,23 +19,8 @@ class CloverWeb
     end
 
     r.post true do
-      Authorization.authorize(current_account.id, "Firewall:create", @project.id)
-      Validation.validate_name(r.params["name"])
-      location = LocationNameConverter.to_internal_name(r.params["location"])
-
-      fw = Firewall.create_with_id(
-        name: r.params["name"],
-        description: r.params["description"],
-        location: location
-      )
-      fw.associate_with_project(@project)
-
-      ps = PrivateSubnet.from_ubid(r.params["private_subnet_id"])
-      fw.associate_with_private_subnet(ps) if ps
-
-      flash["notice"] = "'#{r.params["name"]}' is created"
-
-      r.redirect "#{@project.path}#{Firewall[fw.id].path}"
+      firewall_endpoint_helper.instance_variable_set(:@location, LocationNameConverter.to_internal_name(r.params["location"]))
+      firewall_endpoint_helper.post(r.params["name"])
     end
   end
 end
