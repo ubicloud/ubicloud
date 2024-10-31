8e658d2ad86415f16679bfe9394932f12dec5a8f
Jeremy Evans <jeremy@ubicloud.com>
2024-10-31 14:01:27 -0700

    Remove clover_*.rb files
    These showed up as uncovered after the previous commit, probably
    because the constants were set in clover.rb

--- loader.rb
+++ loader.rb
@@ -88,7 +88,7 @@ module Routes; end
 module Routes::Common; end
 
 autoload_normal.call("model", flat: true)
-%w[lib clover.rb clover_web.rb clover_api.rb clover_runtime.rb routes/clover_base.rb routes/clover_error.rb].each { autoload_normal.call(_1) }
+%w[lib clover.rb routes/clover_base.rb routes/clover_error.rb].each { autoload_normal.call(_1) }
 %w[scheduling prog serializers routes/common].each { autoload_normal.call(_1, include_first: true) }
 
 if ENV["LOAD_FILES_SEPARATELY_CHECK"] == "1"
