9372ac29fa9319f0bcf9d84b325ed157100cfe75
Jeremy Evans <jeremy@ubicloud.com>
2024-10-31 17:29:43 -0700

    Merge the project/location/vm routes
    Eventually, we will merge the Routes::Common::VmHelper directly
    into the route block, but the project/vm routes still use it, so
    that will have to wait.

--- routes/web/project/location/vm.rb
+++ routes/web/project/location/vm.rb
@@ -1,22 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:project_location_prefix, "vm") do |r|
-    r.on String do |vm_name|
-      unless (vm = @project.vms_dataset.where(location: @location).where { {Sequel[:vm][:name] => vm_name} }.first)
-        response.status = 404
-        r.halt
-      end
-
-      vm_endpoint_helper = Routes::Common::VmHelper.new(app: self, request: r, user: current_account, location: @location, resource: vm)
-
-      r.get true do
-        vm_endpoint_helper.get
-      end
-
-      r.delete true do
-        vm_endpoint_helper.delete
-      end
-    end
-  end
-end
+require_relative "../../../merged/project/location/vm"
