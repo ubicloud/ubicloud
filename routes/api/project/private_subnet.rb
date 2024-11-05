371bda7a2a9e56454f1ea67694e81535301419e6
Jeremy Evans <jeremy@ubicloud.com>
2024-11-05 13:18:29 -0800

    Merge the project/private_subnet route branches

--- routes/api/project/private_subnet.rb
+++ routes/api/project/private_subnet.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:api_project_prefix, "private-subnet") do |r|
-    ps_endpoint_helper = Routes::Common::PrivateSubnetHelper.new(app: self, request: r, user: current_account, location: nil, resource: nil)
-
-    r.get true do
-      ps_endpoint_helper.list
-    end
-  end
-end
+require_relative "../../merged/project/private_subnet"
