be4f9d7408fbc5400bf7606f039ed7198a184302
Jeremy Evans <jeremy@ubicloud.com>
2024-11-06 10:50:17 -0800

    Merge the project/firewall route branches
    The r.get true route is still only a basic merge.  It will be
    cleaned up in subsequent commits.

--- routes/api/project/firewall.rb
+++ routes/api/project/firewall.rb
@@ -1,18 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:api_project_prefix, "firewall") do |r|
-    r.get true do
-      result = @project.firewalls_dataset.authorized(current_account.id, "Firewall:view").eager(:firewall_rules).paginated_result(
-        start_after: r.params["start_after"],
-        page_size: r.params["page_size"],
-        order_column: r.params["order_column"]
-      )
-
-      {
-        items: Serializers::Firewall.serialize(result[:records]),
-        count: result[:count]
-      }
-    end
-  end
-end
+require_relative "../../merged/project/firewall"
