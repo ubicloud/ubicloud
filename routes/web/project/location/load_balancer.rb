46bc00cec205acf81d6f9a4e253d424515326792
Enes Cakir <enes@ubicloud.com>
2024-11-07 01:11:13 +0300

    Merge the project/location/load_balancer route branches

--- routes/web/project/location/load_balancer.rb
+++ routes/web/project/location/load_balancer.rb
@@ -1,37 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:project_location_prefix, "load-balancer") do |r|
-    r.on String do |lb_name|
-      pss = @project.private_subnets_dataset.where(location: @location).all
-      lb = pss.flat_map { _1.load_balancers_dataset.where { {Sequel[:load_balancer][:name] => lb_name} }.all }.first
-
-      unless lb
-        response.status = r.delete? ? 204 : 404
-        r.halt
-      end
-
-      load_balancer_endpoint_helper = Routes::Common::LoadBalancerHelper.new(app: self, request: r, user: current_account, resource: lb, location: nil)
-
-      r.get true do
-        load_balancer_endpoint_helper.get
-      end
-
-      r.delete true do
-        load_balancer_endpoint_helper.delete
-      end
-
-      r.on "attach-vm" do
-        r.post true do
-          load_balancer_endpoint_helper.post_attach_vm
-        end
-      end
-
-      r.on "detach-vm" do
-        r.post true do
-          load_balancer_endpoint_helper.post_detach_vm
-        end
-      end
-    end
-  end
-end
+require_relative "../../../merged/project/location/load_balancer"
