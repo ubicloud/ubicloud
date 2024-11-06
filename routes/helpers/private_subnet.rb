9971c86b3f84690d1f769634a5192b72254c90a5
Jeremy Evans <jeremy@ubicloud.com>
2024-11-05 19:11:51 -0800

    Perform authorization in same place for api and web requests in private_subnet_list

--- routes/helpers/private_subnet.rb
+++ routes/helpers/private_subnet.rb
@@ -2,10 +2,11 @@
 
 class Clover
   def private_subnet_list
+    dataset = @project.private_subnets_dataset.authorized(current_account.id, "PrivateSubnet:view")
+
     if api?
-      dataset = @project.private_subnets_dataset
       dataset = dataset.where(location: @location) if @location
-      result = dataset.authorized(current_account.id, "PrivateSubnet:view").eager(nics: [:private_subnet]).paginated_result(
+      result = dataset.eager(nics: [:private_subnet]).paginated_result(
         start_after: request.params["start_after"],
         page_size: request.params["page_size"],
         order_column: request.params["order_column"]
@@ -16,7 +17,7 @@ class Clover
         count: result[:count]
       }
     else
-      @pss = Serializers::PrivateSubnet.serialize(@project.private_subnets_dataset.authorized(current_account.id, "PrivateSubnet:view").all, {include_path: true})
+      @pss = Serializers::PrivateSubnet.serialize(dataset.all, {include_path: true})
       view "networking/private_subnet/index"
     end
   end
