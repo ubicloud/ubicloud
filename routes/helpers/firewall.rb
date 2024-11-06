8498120a57bb72ddfbce7b294fa381cf13cc7b00
Jeremy Evans <jeremy@ubicloud.com>
2024-11-06 13:09:38 -0800

    Add firewall_post helper method
    This DRYs up code in the project/firewall and
    project/location/firewall routes.

--- routes/helpers/firewall.rb
+++ routes/helpers/firewall.rb
@@ -18,4 +18,33 @@ class Clover
       count: result[:count]
     }
   end
+
+  def firewall_post(firewall_name)
+    Authorization.authorize(current_account.id, "Firewall:create", @project.id)
+    Validation.validate_name(firewall_name)
+    @location ||= LocationNameConverter.to_internal_name(request.params["location"])
+
+    description = if api?
+      Validation.validate_request_body(request.body.read, [], ["description"])["description"] || ""
+    else
+      request.params["description"]
+    end
+
+    firewall = Firewall.create_with_id(
+      name: firewall_name,
+      description:,
+      location: @location
+    )
+    firewall.associate_with_project(@project)
+
+    if api?
+      Serializers::Firewall.serialize(firewall)
+    else
+      private_subnet = PrivateSubnet.from_ubid(request.params["private-subnet-id"])
+      firewall.associate_with_private_subnet(private_subnet) if private_subnet
+
+      flash["notice"] = "'#{firewall_name}' is created"
+      request.redirect "#{@project.path}#{firewall.path}"
+    end
+  end
 end
