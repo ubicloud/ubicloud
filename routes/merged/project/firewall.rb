5eeedb041424bd882c599588c05ffa7c1dc8eb97
Jeremy Evans <jeremy@ubicloud.com>
2024-11-06 10:50:17 -0800

    Merge the project/firewall route branches
    The r.get true route is still only a basic merge.  It will be
    cleaned up in subsequent commits.

---
+++ routes/merged/project/firewall.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+class Clover
+  branch = lambda do |r|
+    r.get true do
+      if api?
+        result = @project.firewalls_dataset.authorized(current_account.id, "Firewall:view").eager(:firewall_rules).paginated_result(
+          start_after: r.params["start_after"],
+          page_size: r.params["page_size"],
+          order_column: r.params["order_column"]
+        )
+
+        {
+          items: Serializers::Firewall.serialize(result[:records]),
+          count: result[:count]
+        }
+      else
+        authorized_firewalls = @project.firewalls_dataset.authorized(current_account.id, "Firewall:view").all
+        @firewalls = Serializers::Firewall.serialize(authorized_firewalls, {include_path: true})
+
+        view "networking/firewall/index"
+      end
+    end
+
+    r.on web? do
+      r.on "create" do
+        r.get true do
+          Authorization.authorize(current_account.id, "Firewall:create", @project.id)
+          authorized_subnets = @project.private_subnets_dataset.authorized(current_account.id, "PrivateSubnet:edit").all
+          @subnets = Serializers::PrivateSubnet.serialize(authorized_subnets)
+          @default_location = @project.default_location
+          view "networking/firewall/create"
+        end
+      end
+
+      r.post true do
+        Authorization.authorize(current_account.id, "Firewall:create", @project.id)
+        Validation.validate_name(r.params["name"])
+        location = LocationNameConverter.to_internal_name(r.params["location"])
+
+        fw = Firewall.create_with_id(
+          name: r.params["name"],
+          description: r.params["description"],
+          location: location
+        )
+        fw.associate_with_project(@project)
+
+        ps = PrivateSubnet.from_ubid(r.params["private-subnet-id"])
+        fw.associate_with_private_subnet(ps) if ps
+
+        flash["notice"] = "'#{r.params["name"]}' is created"
+
+        r.redirect "#{@project.path}#{Firewall[fw.id].path}"
+      end
+    end
+  end
+
+  hash_branch(:project_prefix, "firewall", &branch)
+  hash_branch(:api_project_prefix, "firewall", &branch)
+end
