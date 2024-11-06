561a6e1d2955b705604be41bae212edb42b7c51b
Enes Cakir <enes@ubicloud.com>
2024-11-07 01:35:26 +0300

    Merge the project/load_balancer route branches

--- routes/api/project/load_balancer.rb
+++ routes/api/project/load_balancer.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:api_project_prefix, "load-balancer") do |r|
-    load_balancer_endpoint_helper = Routes::Common::LoadBalancerHelper.new(app: self, request: r, user: current_account, location: nil, resource: nil)
-
-    r.get true do
-      load_balancer_endpoint_helper.list
-    end
-
-    r.on String do |lb_name|
-      r.post true do
-        load_balancer_endpoint_helper.post(name: lb_name)
-      end
-    end
-  end
-end
+require_relative "../../merged/project/load_balancer"
