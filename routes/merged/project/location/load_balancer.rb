eb7aee0499524d82be6c2d3c40c4d5428c556399
Enes Cakir <enes@ubicloud.com>
2024-11-07 02:37:52 +0300

    Inline LoadBalancerHelper#patch

--- routes/merged/project/location/load_balancer.rb
+++ routes/merged/project/location/load_balancer.rb
@@ -109,7 +109,42 @@ class Clover
       end
 
       r.patch api? do
-        lb_endpoint_helper.patch
+        Authorization.authorize(current_account.id, "LoadBalancer:edit", lb.id)
+        request_body_params = Validation.validate_request_body(json_params, %w[algorithm src_port dst_port health_check_endpoint vms])
+        lb.update(
+          algorithm: request_body_params["algorithm"],
+          src_port: Validation.validate_port(:src_port, request_body_params["src_port"]),
+          dst_port: Validation.validate_port(:dst_port, request_body_params["dst_port"]),
+          health_check_endpoint: request_body_params["health_check_endpoint"]
+        )
+
+        request_body_params["vms"].each do |vm_id|
+          vm_id = vm_id.delete("\"")
+          vm = Vm.from_ubid(vm_id)
+          unless vm
+            response.status = 404
+            r.halt
+          end
+
+          Authorization.authorize(current_account.id, "Vm:view", vm.id)
+          if vm.reload.load_balancer && vm.load_balancer.id != lb.id
+            fail Validation::ValidationFailed.new("vm_id" => "VM is already attached to a load balancer")
+          elsif vm.load_balancer && vm.load_balancer.id == lb.id
+            next
+          end
+
+          lb.add_vm(vm)
+        end
+
+        lb.vms.map { _1.ubid }.reject { request_body_params["vms"].map { |vm_id| vm_id.delete("\"") }.include?(_1) }.each do |vm_id|
+          vm = Vm.from_ubid(vm_id)
+          lb.evacuate_vm(vm)
+          lb.remove_vm(vm)
+        end
+
+        response.status = 200
+        lb.incr_update_load_balancer
+        Serializers::LoadBalancer.serialize(lb.reload, {detailed: true})
       end
     end
   end
