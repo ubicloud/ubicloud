022999ce4ba88f377a90e343c345ee276d83b794
Jeremy Evans <jeremy@ubicloud.com>
2024-11-06 16:16:58 -0800

    Merge project/location routes

--- routes/web/project/location.rb
+++ routes/web/project/location.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-class Clover
-  hash_branch(:project_prefix, "location") do |r|
-    r.on String do |location_display_name|
-      @location = LocationNameConverter.to_internal_name(location_display_name)
-
-      r.hash_branches(:project_location_prefix)
-    end
-  end
-end
+require_relative "../../merged/project/location"
