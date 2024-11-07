9371f6c1c6e6affcf6bc8576c1cc8fa67385c13e
Enes Cakir <enes@ubicloud.com>
2024-11-07 03:19:01 +0300

    Perform authorization in same place for api and web requests in load_balancer_list

--- routes/helpers/load_balancer.rb
+++ routes/helpers/load_balancer.rb
@@ -3,9 +3,10 @@
 class Clover
   def load_balancer_list
     dataset = @project.load_balancers_dataset
+    dataset = dataset.authorized(current_account.id, "LoadBalancer:view")
     dataset = dataset.join(:private_subnet, id: Sequel[:load_balancer][:private_subnet_id]).where(location: @location).select_all(:load_balancer) if @location
     if api?
-      result = dataset.authorized(current_account.id, "LoadBalancer:view").paginated_result(
+      result = dataset.paginated_result(
         start_after: request.params["start_after"],
         page_size: request.params["page_size"],
         order_column: request.params["order_column"]
@@ -16,7 +17,7 @@ class Clover
         count: result[:count]
       }
     else
-      @lbs = Serializers::LoadBalancer.serialize(dataset.authorized(current_account.id, "LoadBalancer:view").all, {include_path: true})
+      @lbs = Serializers::LoadBalancer.serialize(dataset.all, {include_path: true})
       view "networking/load_balancer/index"
     end
   end
