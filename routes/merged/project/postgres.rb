ec069d8a2a5da0c76aae3d0bd6207117821c139b
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-11-15 00:53:57 +0300

    Reduce nesting for project/postgres create route

--- routes/merged/project/postgres.rb
+++ routes/merged/project/postgres.rb
@@ -12,20 +12,18 @@ class Clover
         postgres_post(name: r.params["name"])
       end
 
-      r.on "create" do
-        r.get true do
-          Authorization.authorize(current_account.id, "Postgres:create", @project.id)
+      r.get "create" do
+        Authorization.authorize(current_account.id, "Postgres:create", @project.id)
 
-          flavor = r.params["flavor"] || PostgresResource::Flavor::STANDARD
-          Validation.validate_postgres_flavor(flavor)
+        flavor = r.params["flavor"] || PostgresResource::Flavor::STANDARD
+        Validation.validate_postgres_flavor(flavor)
 
-          @flavor = flavor
-          @prices = fetch_location_based_prices("PostgresCores", "PostgresStorage")
-          @has_valid_payment_method = @project.has_valid_payment_method?
-          @enabled_postgres_sizes = Option::VmSizes.select { @project.quota_available?("PostgresCores", _1.vcpu / 2) }.map(&:name)
+        @flavor = flavor
+        @prices = fetch_location_based_prices("PostgresCores", "PostgresStorage")
+        @has_valid_payment_method = @project.has_valid_payment_method?
+        @enabled_postgres_sizes = Option::VmSizes.select { @project.quota_available?("PostgresCores", _1.vcpu / 2) }.map(&:name)
 
-          view "postgres/create"
-        end
+        view "postgres/create"
       end
     end
   end
