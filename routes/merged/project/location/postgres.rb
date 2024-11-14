0c223fadc3b3e7c1a114d9169cbf00e9d5b85863
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2024-11-14 07:05:14 +0300

    Merge the project/location/postgres route branches

---
+++ routes/merged/project/location/postgres.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+class Clover
+  branch = lambda do |r|
+    pg_endpoint_helper = Routes::Common::PostgresHelper.new(app: self, request: r, user: current_account, location: @location, resource: nil)
+
+    r.get api? do
+      pg_endpoint_helper.list
+    end
+
+    r.on NAME_OR_UBID do |pg_name, pg_ubid|
+      if pg_name
+        r.post true do
+          pg_endpoint_helper.post(name: pg_name)
+        end
+
+        filter = {Sequel[:postgres_resource][:name] => pg_name}
+      else
+        filter = {Sequel[:postgres_resource][:id] => UBID.to_uuid(pg_ubid)}
+      end
+
+      filter[:location] = @location
+      pg = @project.postgres_resources_dataset.first(filter)
+      pg_endpoint_helper.instance_variable_set(:@resource, pg)
+
+      unless pg
+        response.status = request.delete? ? 204 : 404
+        request.halt
+      end
+
+      request.get true do
+        pg_endpoint_helper.get
+      end
+
+      request.delete true do
+        pg_endpoint_helper.delete
+      end
+
+      if web?
+        request.post "restart" do
+          pg_endpoint_helper.restart
+        end
+      end
+
+      if api?
+        request.post "failover" do
+          pg_endpoint_helper.failover
+        end
+      end
+
+      request.on "firewall-rule" do
+        if api?
+          request.get true do
+            pg_endpoint_helper.get_firewall_rule
+          end
+        end
+
+        request.post true do
+          pg_endpoint_helper.post_firewall_rule
+        end
+
+        request.is String do |firewall_rule_ubid|
+          request.delete true do
+            pg_endpoint_helper.delete_firewall_rule(firewall_rule_ubid)
+          end
+        end
+      end
+
+      request.on "metric-destination" do
+        request.post true do
+          pg_endpoint_helper.post_metric_destination
+        end
+
+        request.is String do |metric_destination_ubid|
+          request.delete true do
+            pg_endpoint_helper.delete_metric_destination(metric_destination_ubid)
+          end
+        end
+      end
+
+      request.post "restore" do
+        pg_endpoint_helper.restore
+      end
+
+      request.post "reset-superuser-password" do
+        pg_endpoint_helper.reset_superuser_password
+      end
+    end
+
+    # 204 response for invalid names
+    r.is String do |pg_name|
+      r.post do
+        pg_endpoint_helper.post(name: pg_name)
+      end
+
+      r.delete do
+        response.status = 204
+        nil
+      end
+    end
+  end
+
+  hash_branch(:api_project_location_prefix, "postgres", &branch)
+  hash_branch(:project_location_prefix, "postgres", &branch)
+end
