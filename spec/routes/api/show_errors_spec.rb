ab02cf8536a24576210e643b57e54e8284d0d871
Jeremy Evans <jeremy@ubicloud.com>
2024-11-18 13:50:37 -0800

    Support SHOW_ERRORS environment variable when testing
    This environment variable makes the error handler raise the
    exception instead swallowing it.  This makes debugging issues
    significantly simpler.

---
+++ spec/routes/api/show_errors_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "spec_helper"
+
+RSpec.describe Clover do
+  before do
+    unless (@show_errors = ENV["SHOW_ERRORS"])
+      ENV["SHOW_ERRORS"] = "1"
+    end
+    login_api(create_account.email)
+  end
+
+  after do
+    ENV.delete("SHOW_ERRORS") unless @show_errors
+  end
+
+  it "supports SHOW_ERRORS environment variable when testing" do
+    expect { post "/api/project", {}.to_json }.to raise_error Validation::ValidationFailed
+  end
+end
