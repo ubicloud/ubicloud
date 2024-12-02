48337d252130f1bd499b4f670ef44733513cc2b1
Furkan Sahin <furkan@ubicloud.com>
2024-12-02 14:34:40 +0100

    Revert "Enable Docker in runner with IPv6"
    This reverts commit d98fd5c9c30db54b9cc05612af17b472b825bb36.

--- prog/vm/github_runner.rb
+++ prog/vm/github_runner.rb
@@ -201,14 +201,6 @@ class Prog::Vm::GithubRunner < Prog::Base
   end
 
   label def setup_environment
-    docker_daemon_json = JSON.generate({
-      "ipv6" => true,
-      "fixed-cidr-v6" => "#{vm.ephemeral_net6.nth(2)}:dc/124",
-      "experimental" => false,
-      "dns" => [vm.nics.first.private_subnet.net4.nth(2), "fd00:0b1c:100d:53::"],
-      "dns-opts" => ["single-request-reopen"]
-    })
-
     command = <<~COMMAND
       # To make sure the script errors out if any command fails
       set -ueo pipefail
@@ -228,10 +220,6 @@ class Prog::Vm::GithubRunner < Prog::Base
       # ubicloud/cache package which forked from the official actions/cache package, sends requests to UBICLOUD_CACHE_URL using this token.
       echo "UBICLOUD_RUNTIME_TOKEN=#{vm.runtime_token}
       UBICLOUD_CACHE_URL=#{Config.base_url}/runtime/github/" | sudo tee -a /etc/environment
-
-      # We need to configure Docker to work with IPv6 properly. If the docker daemon config file exists, we append the new configuration to it.
-      ([ -f "/etc/docker/daemon.json" ] && sudo cat /etc/docker/daemon.json || echo '{}') | jq '. += #{docker_daemon_json}' | sudo tee /etc/docker/daemon.json
-      sudo systemctl restart docker
     COMMAND
 
     if github_runner.installation.cache_enabled
