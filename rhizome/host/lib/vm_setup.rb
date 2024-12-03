cb833fd0cc57901c16abccbbe67239def89b7562
Furkan Sahin <furkan@ubicloud.com>
2024-12-03 17:23:07 +0100

    Lease DHCP4 for 6 hours
    The default is 1 hour for dnsmasq per the manual however, as far as I
    can see, it is 2 minutes for us. I caught at just at the renewal;
    ```
    runneradmin@vm086b75:/tmp$ ip a
    1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN
    group default qlen 1000
        link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
        inet 127.0.0.1/8 scope host lo
           valid_lft forever preferred_lft forever
        inet6 ::1/128 scope host
           valid_lft forever preferred_lft forever
    2: ens3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1400 qdisc mq state UP
    group default qlen 1000
        link/ether 9a:b1:f0:66:29:8a brd ff:ff:ff:ff:ff:ff
        altname enp0s3
    inet 10.95.34.36/26 metric 100 brd 10.95.34.63 scope global dynamic
    ens3
           valid_lft 120sec preferred_lft 120sec
    inet6 2a01:4f8:161:24db:98cc::2/128 scope global dynamic
    noprefixroute
           valid_lft 82409sec preferred_lft 82409sec
    inet6 fd0d:83f8:ff01:194b:ec40::2/128 scope global dynamic
    noprefixroute
           valid_lft 82409sec preferred_lft 82409sec
        inet6 fe80::98b1:f0ff:fe66:298a/64 scope link
           valid_lft forever preferred_lft forever
    3: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue
    state DOWN group default
        link/ether 02:42:b6:3a:c0:02 brd ff:ff:ff:ff:ff:ff
        inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
           valid_lft forever preferred_lft forever
    ```
    
    This doesn't make sense especially for runners because the private ipv4
    address of the VM will never change the whole lifetime of the runner.
    The timeout limit for github actions runners are 6 hours, so I simply
    set it to that value.
    
    dhcp-range parameter of dnsmasq is a little bit weird that if we put
    both the mask and lease time, it fails to start. Since the address is
    defined from start to end and we put the same address, the prefix len
    can be replaced with 6h.

--- rhizome/host/lib/vm_setup.rb
+++ rhizome/host/lib/vm_setup.rb
@@ -441,12 +441,15 @@ DHCP
     interfaces = nics.map { "interface=#{_1.tap}" }.join("\n")
     dnsmasq_address_ip6 = NetAddr::IPv6.parse("fd00:0b1c:100d:53::")
     runner_config = if boot_image.include?("github")
-      <<~ADDRESSES
+      <<~ADDITIONAL_RUNNER_CONFIG
       address=/ubicloudhostplaceholder.blob.core.windows.net/#{nics.first.net4.split("/").first}
       address=/.docker.io/::
-      ADDRESSES
+      dhcp-range=#{guest_network.nth(2)},#{guest_network.nth(2)},6h
+      ADDITIONAL_RUNNER_CONFIG
     else
-      ""
+      <<~ADDITIONAL_CONFIG
+      dhcp-range=#{guest_network.nth(2)},#{guest_network.nth(2)},#{guest_network.netmask.prefix_len}
+      ADDITIONAL_CONFIG
     end
     vp.write_dnsmasq_conf(<<DNSMASQ_CONF)
 pid-file=
@@ -458,7 +461,6 @@ bogus-priv
 no-resolv
 #{raparams}
 #{interfaces}
-dhcp-range=#{guest_network.nth(2)},#{guest_network.nth(2)},#{guest_network.netmask.prefix_len}
 #{private_ip_dhcp}
 server=9.9.9.9@#{dns_ipv4}
 server=149.112.112.112@#{dns_ipv4}
