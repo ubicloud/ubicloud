4e350146fa444c4768d280805aac708f24fe2c9b
Jeremy Evans <jeremy@ubicloud.com>
2024-12-04 16:37:34 -0800

    Remove AppliedTag

--- model/access_tag.rb
+++ model/access_tag.rb
@@ -4,9 +4,6 @@ require_relative "../model"
 
 class AccessTag < Sequel::Model
   many_to_one :project
-  one_to_many :applied_tags
-
-  plugin :association_dependencies, applied_tags: :destroy
 
   include ResourceMethods
 end
