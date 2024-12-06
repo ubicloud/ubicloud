83cc031b20a7cb7b935c43b38ee2bc0a5988d7c7
Benjamin Satzger <ben@ubicloud.com>
2024-12-06 18:56:51 +0100

    Add Inference Tokens UI
    Introduced an “Inference Tokens” page to manage tokens for inference
    endpoints. Users can create and delete tokens directly from this interface.
    The tokens are required to send requests to into inference endpoints.
    Requests are billed to the project the token belongs to.

---
+++ routes/project/inference_token.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Clover
+  hash_branch(:project_prefix, "inference-token") do |r|
+    r.on web? do
+      next unless @project.get_ff_inference_ui
+
+      r.get true do
+        dataset = dataset_authorize(@project.api_keys_dataset.where(used_for: "inference_endpoint"), "InferenceToken:view")
+        dataset = dataset.where(is_valid: true)
+        dataset = dataset.order(:created_at)
+        @inference_tokens = Serializers::InferenceToken.serialize(dataset.all)
+        view "inference/token/index"
+      end
+
+      r.post true do
+        authorize("InferenceToken:create", @project.id)
+        it = DB.transaction { ApiKey.create_inference_token(@project) }
+        flash["notice"] = "Created inference token with id #{it.ubid}"
+        r.redirect "#{@project.path}/inference-token"
+      end
+
+      r.delete String do |ubid|
+        if (it = ApiKey.from_ubid(ubid))
+          authorize("InferenceToken:delete", it.id)
+          it.destroy
+          flash["notice"] = "Inference token deleted successfully"
+        end
+        204
+      end
+    end
+  end
+end
