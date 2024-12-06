3173aaf72f71d5d19185850e2251e02c87c2c6fe
Benjamin Satzger <ben@ubicloud.com>
2024-12-06 16:14:51 +0100

    Remove create_api_key from project model
    The create_api_key method has been removed from the project model to
    reflect the introduction of multiple API key types. These include keys
    that are not directly tied to a specific project, making the method less
    universally applicable.

--- model/project.rb
+++ model/project.rb
@@ -117,10 +117,6 @@ class Project < Sequel::Model
     effective_quota_value(resource_type) >= current_resource_usage(resource_type) + requested_additional_usage
   end
 
-  def create_api_key(used_for: "inference_endpoint")
-    ApiKey.create_with_id(owner_table: Project.table_name, owner_id: id, used_for: used_for)
-  end
-
   def validate
     super
     if new? || changed_columns.include?(:name)
