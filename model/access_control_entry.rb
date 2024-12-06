825c349f4464b8526f92a1b8d265af17bba9ade2
Jeremy Evans <jeremy@ubicloud.com>
2024-12-06 09:23:06 -0800

    Add AccessControlEntry validation
    This validates that subjects, actions, and objects in the access
    control entry are validate and related to the current project.
    
    In a few places in the specs where we are testing access control
    entries, skip validation as it isn't important to what is being
    tested.  In a couple other places, make small changes to work
    with the validation.

--- model/access_control_entry.rb
+++ model/access_control_entry.rb
@@ -9,6 +9,30 @@ class AccessControlEntry < Sequel::Model
 
   # use __id__ if you want the internal object id
   def_column_alias :object_id, :object_id
+
+  def validate
+    if project_id
+      {subject_id:, action_id:, object_id:}.each do |field, value|
+        next unless value
+        ubid = UBID.from_uuidish(value).to_s
+
+        valid = case field
+        when :subject_id
+          SubjectTag.valid_member?(project_id, ubid.start_with?("et") ? ApiKey[value] : UBID.decode(ubid))
+        when :action_id
+          ActionTag.valid_member?(project_id, UBID.decode(ubid))
+        else
+          ObjectTag.valid_member?(project_id, UBID.decode(ubid))
+        end
+
+        unless valid
+          errors.add(field, "is not related to this project")
+        end
+      end
+    end
+
+    super
+  end
 end
 
 # Table: access_control_entry
