ea359157049bebea0461f0afad99d4c83e262ee5
Jeremy Evans <jeremy@ubicloud.com>
2024-12-09 15:35:08 -0800

    Add UI for tag management
    This allows, for subject, action, and object tags:
    
    * Creating tags
    * Renaming tags
    * Removing tags
    * Adding members to tags
    * Removing members from tags
    
    The Admin subject tag is special, and cannot be renamed, removed, or included
    in other tags.  It also cannot have all accounts removed from it, to ensure
    there is at least one account with full access.
    
    When adding members to a tag, the member:
    
    * Cannot be the tag itself (trivial loop prevention)
    * Cannot already be a member (database unique constraint would catch this)
    * Cannot already include the receiver as a member, either directly or
      indirectly (loop prevention).
    * Must be a valid member of the tag, by calling *Tag.valid_member?
    * Cannot be the Admin subject tag, if receiver is subject tag.
    
    I think the add member code for preventing loops would be subject to
    race conditions, so this uses a serializable transaction for it.  I'm
    not positive even that will prevent all loops, but it should make
    creating loops very difficult.  We do have a configurable recursion
    limit in all recursive queries, so loops are not catastrophic.
    
    This adds the same validation we have on project names to tag names.
    It also removes any references to the tag when deleting it:
    
    * Removes memberships in tag itself
    * Remove memberships of current tag in other tags
    * Removes any access control entries directly on the tag itself
    
    Just as with access control entries, when showing tag membership,
    tokens are hidden. This may only affect the Admin subject tag, as
    that is the tag that tokens will be added to by default.
    
    This uses the same routing tree and views to manage all tag types,
    which significantly DRYs things up.

--- lib/access_control_model_tag.rb
+++ lib/access_control_model_tag.rb
@@ -20,4 +20,86 @@ module AccessControlModelTag
   def add_member(member_id)
     applied_dataset.insert(:tag_id => id, applied_column => member_id)
   end
+
+  def member_ids
+    applied_dataset.where(tag_id: id).select_map(applied_column)
+  end
+
+  def add_members(member_ids)
+    applied_dataset.import([:tag_id, applied_column], member_ids.map { [id, _1] })
+  end
+
+  def remove_members(member_ids)
+    applied_dataset.where(:tag_id => id, applied_column => member_ids).delete
+  end
+
+  def currently_included_in
+    DB[:tag]
+      .with_recursive(:tag,
+        DB[applied_table].where(applied_column => id).select(:tag_id, 0),
+        DB[applied_table].join(:tag, tag_id: applied_column)
+          .select(Sequel[applied_table][:tag_id], Sequel[:level] + 1)
+          .where { level < Config.recursive_tag_limit },
+        args: [:tag_id, :level])
+      .select_map(:tag_id)
+  end
+
+  def check_members_to_add(to_add)
+    issues = []
+
+    current_members = member_ids
+
+    # Do not allow tag to include itself (prevent simple recursion)
+    if to_add.include?(id)
+      to_add.delete(id)
+      issues << "cannot include tag in itself"
+    end
+
+    # Remove current members so there are no duplicates
+    size = to_add.size
+    to_add -= current_members
+    if to_add.size != size
+      issues << "#{size - to_add.size} members already in tag"
+    end
+
+    # Check that current tag is not included already in one of the tags
+    # being added directly or indirectly (prevent complex recursion)
+    size = to_add.size
+    to_add -= currently_included_in
+    if to_add.size != size
+      issues << "#{size - to_add.size} members already include tag directly or indirectly"
+    end
+
+    proposed_additions = {}
+    to_add.each { proposed_additions[_1] = nil }
+    UBID.resolve_map(proposed_additions)
+
+    to_add = []
+    # Only allow valid members into the tag
+    proposed_additions.each_value do
+      if is_a?(SubjectTag) && _1.is_a?(SubjectTag) && _1.name == "Admin"
+        issues << "cannot include Admin subject tag in another tag"
+        next
+      end
+      to_add << _1 if _1 && self.class.valid_member?(project_id, _1)
+    end
+    if proposed_additions.size != to_add.size
+      issues << "#{proposed_additions.size - to_add.size} members not valid"
+    end
+
+    to_add.map!(&:id)
+    to_add.uniq!
+    [to_add, issues]
+  end
+
+  def before_destroy
+    applied_dataset.where(tag_id: id).or(applied_column => id).delete
+    AccessControlEntry.where(applied_column => id).destroy
+    super
+  end
+
+  def validate
+    validates_format(%r{\A[a-z0-9](?:[a-z0-9\-]{0,61}[a-z0-9])?\z}i, :name, message: "must only include ASCII letters, numbers, and dashes, and must start and end with an ASCII letter or number")
+    super
+  end
 end
