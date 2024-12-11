15cafb83b5920adce7368d7cd5619c1dbf4a8e30
Enes Cakir <enes@ubicloud.com>
2024-12-11 23:09:36 +0300

    Add vm restart semaphore
    Several customers have already requested a method to force restart their
    VMs. Previously, we handled this through support tickets. However, we
    can easily integrate this functionality into the UI.

--- model/vm.rb
+++ model/vm.rb
@@ -25,6 +25,7 @@ class Vm < Sequel::Model
   include SemaphoreMethods
   include HealthMonitorMethods
   semaphore :destroy, :start_after_host_reboot, :prevent_destroy, :update_firewall_rules, :checkup, :update_spdk_dependency, :waiting_for_capacity, :lb_expiry_started
+  semaphore :restart
 
   include Authorization::HyperTagMethods
 
