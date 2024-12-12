63279b6c36a632e644ef3836d3097979fb171a87
Enes Cakir <enes@ubicloud.com>
2024-12-12 16:27:20 +0300

    Use cores from vm_size option when calculating total active runner cores
    We start to keep core values at vm_size option. So no need to calculate
    it here anymore.

--- model/github_installation.rb
+++ model/github_installation.rb
@@ -10,16 +10,14 @@ class GithubInstallation < Sequel::Model
   include ResourceMethods
 
   def total_active_runner_cores
-    runner_labels = runners_dataset.left_join(:strand, id: :id).exclude(Sequel[:strand][:label] => "start").exclude(Sequel[:strand][:label] => "wait_concurrency_limit").select_map(Sequel[:github_runner][:label])
-    label_record_data_set = runner_labels.map { |label| Github.runner_labels[label] }
-    label_record_data_set.sum do |label_record|
-      vcpu = Validation.validate_vm_size(label_record["vm_size"], label_record["arch"]).vcpus
-      if label_record["arch"] == "arm64"
-        vcpu
-      else
-        vcpu / 2
+    runners_dataset
+      .left_join(:strand, id: :id)
+      .exclude(Sequel[:strand][:label] => ["start", "wait_concurrency_limit"])
+      .select_map(Sequel[:github_runner][:label])
+      .sum do
+        label = Github.runner_labels[_1]
+        Validation.validate_vm_size(label["vm_size"], label["arch"]).cores
       end
-    end
   end
 end
 
