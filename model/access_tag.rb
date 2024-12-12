710c2b454fc9a11e9d2437862298e87aae0bdcac
Jeremy Evans <jeremy@ubicloud.com>
2024-12-12 12:48:32 -0800

    Fix destruction of tokens when there is still an applied tag for them
    Could potentially fix deletion of other things that have access
    tags and applied tags.

--- model/access_tag.rb
+++ model/access_tag.rb
@@ -6,6 +6,12 @@ class AccessTag < Sequel::Model
   many_to_one :project
 
   include ResourceMethods
+
+  def before_destroy
+    # XXX: Remove when dropping applied_tag table
+    DB[:applied_tag].where(access_tag_id: id).delete
+    super
+  end
 end
 
 # Table: access_tag
