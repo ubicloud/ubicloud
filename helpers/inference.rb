878b163d523edd3a5e2e8e5a228ceeaa1ed85a5f
Benjamin Satzger <ben@ubicloud.com>
2024-12-16 17:30:04 +0100

    Introduce inference helper methods for web
    Add `inference_endpoint_ds`, which gets a dataset of relevant inference
    endpoints for a project.
    
    Add `inference_token_ds`, which gets a dataset of relevant inference
    tokens for a project.

---
+++ helpers/inference.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Clover
+  def inference_endpoint_ds
+    dataset_private = dataset_authorize(@project.inference_endpoints_dataset, "InferenceEndpoint:view")
+    dataset_public = InferenceEndpoint.where(is_public: true)
+
+    dataset = dataset_private.union(dataset_public)
+    dataset = dataset.where(visible: true)
+    dataset = dataset.order(:model_name)
+    dataset.eager(:load_balancer)
+  end
+
+  def inference_token_ds
+    dataset = dataset_authorize(@project.api_keys_dataset.where(used_for: "inference_endpoint"), "InferenceToken:view")
+    dataset = dataset.where(is_valid: true)
+    dataset.order(:created_at)
+  end
+end
