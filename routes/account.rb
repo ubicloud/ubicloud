8a464e0f7a7e99ab6e9fba1b5487c7312a1bf6d9
Enes Cakir <enes@ubicloud.com>
2024-12-26 15:12:48 +0300

    Allow existing users to connect their social accounts
    In the previous commit, we prevented existing users from connecting
    their social accounts without logging in first for security reasons.
    
    This PR adds a new tab to the "My Account" page that allows existing
    users to connect and disconnect their social accounts.
    
    If the email of the current account does not match the social account,
    we do not allow the operation.
    
    We also do not allow disconnecting the last login method. However, we
    can allow disconnecting the last omniauth provider if the user has a
    password. The `has_password?` method is not yet public in Rodauth, so I
    created a PR for it.

--- routes/account.rb
+++ routes/account.rb
@@ -2,8 +2,37 @@
 
 class Clover
   hash_branch("account") do |r|
-    r.get web? do
-      r.redirect "/account/change-password"
+    r.web do
+      r.get true do
+        r.redirect "/account/multifactor-manage"
+      end
+
+      r.on "login-method" do
+        r.get true do
+          @identities = current_account.identities.to_h { [_1.provider, _1.uid] }
+
+          view "account/login_method"
+        end
+
+        r.post "disconnect" do
+          provider, uid = typecast_params.nonempty_str(["provider", "uid"])
+          identities = current_account.identities
+          unless identities.length > 1
+            # YYYY: We can allow to disconnect the last omniauth provider if the user has a password
+            # But need https://github.com/jeremyevans/rodauth/pull/461 to be merged first.
+            flash[:error] = "You must have at least one login method"
+            r.redirect "/account/login-method"
+          end
+          if (identity = identities.find { _1.provider == provider && _1.uid == uid })
+            identity.destroy
+            flash[:notice] = "Your account has been disconnected from #{provider.capitalize}"
+          else
+            flash[:error] = "Your account already has been disconnected from #{provider.capitalize}"
+          end
+
+          r.redirect "/account/login-method"
+        end
+      end
     end
   end
 end
