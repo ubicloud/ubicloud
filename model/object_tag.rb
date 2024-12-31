ad87089cbb135ad9fcb8f453e8e1991256b6d772
Jeremy Evans <jeremy@ubicloud.com>
2024-12-31 13:14:08 -0800

    Support Inference{Endpoint,Token} as ObjectTag members
    Inference{Endpoint,Token} were added after the authorization
    refactoring was started.  This adds support for them as members
    of ObjectTag.

--- model/object_tag.rb
+++ model/object_tag.rb
@@ -8,12 +8,14 @@ class ObjectTag < Sequel::Model
 
   def self.valid_member?(project_id, object)
     case object
-    when ObjectTag, SubjectTag, ActionTag
+    when ObjectTag, SubjectTag, ActionTag, InferenceEndpoint
       object.project_id == project_id
     when Vm, PrivateSubnet, PostgresResource, Firewall, LoadBalancer
       !AccessTag.where(project_id:, hyper_tag_id: object.id).empty?
     when Project
       object.id == project_id
+    when ApiKey
+      object.owner_table == "project" && object.owner_id == project_id
     end
   end
 end
