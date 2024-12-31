b71af30d92e81f5ee45dc9cbb4d7e9ae2545ffdd
Benjamin Satzger <ben@ubicloud.com>
2024-12-31 14:15:07 +0100

    Playground: filter inference endpoints based on tags
    In previous commits we introduced tags describing inference endpoints.
    This commit adds a filter to the inference playground to consider only
    endpoints with a specific tag.

--- routes/project/inference_playground.rb
+++ routes/project/inference_playground.rb
@@ -6,7 +6,7 @@ class Clover
       next unless @project.get_ff_inference_ui
 
       r.get true do
-        @inference_endpoints = Serializers::InferenceEndpoint.serialize(inference_endpoint_ds.reject { _1.model_type == :embedding })
+        @inference_endpoints = Serializers::InferenceEndpoint.serialize(inference_endpoint_ds.where(Sequel.pg_jsonb_op(:tags).get_text("capability") => "Text Generation"))
         @inference_tokens = Serializers::InferenceToken.serialize(inference_token_ds.all)
         view "inference/endpoint/playground"
       end
