6d6d208d4e1299a58ab6cc2b4f8c89dbcbbbdeff
Furkan Sahin <furkan@ubicloud.com>
2025-01-07 10:34:22 +0100

    Selectively run Load Balancer health probes for each stack
    We had introduced stack option at the backend but we forgot to update
    the health probes accordingly. This PR fixes that.

--- prog/vnet/load_balancer_health_probes.rb
+++ prog/vnet/load_balancer_health_probes.rb
@@ -8,20 +8,24 @@ class Prog::Vnet::LoadBalancerHealthProbes < Prog::Base
   end
 
   label def health_probe
-    response_code = begin
-      cmd = if load_balancer.health_check_protocol == "tcp"
-        "sudo ip netns exec #{vm.inhost_name} nc -z -w #{load_balancer.health_check_timeout} #{vm.nics.first.private_ipv4.network} #{load_balancer.dst_port} && echo 200 || echo 400"
-      else
-        "sudo ip netns exec #{vm.inhost_name} curl --insecure --resolve #{load_balancer.hostname}:#{load_balancer.dst_port}:#{vm.nics.first.private_ipv4.network} --max-time #{load_balancer.health_check_timeout} --silent --output /dev/null --write-out '%{http_code}' #{load_balancer.health_check_protocol}://#{load_balancer.hostname}:#{load_balancer.dst_port}#{load_balancer.health_check_endpoint}"
-      end
+    address_ipv4 = load_balancer.ipv4_enabled? ? vm.nics.first.private_ipv4.network : nil
+    address_ipv6 = load_balancer.ipv6_enabled? ? vm.ephemeral_net6.nth(2) : nil
 
-      vm.vm_host.sshable.cmd(cmd)
-    rescue
-      "500"
+    response_codes = [address_ipv4, address_ipv6].compact.map do |address|
+      cmd = if load_balancer.health_check_protocol == "tcp"
+        "sudo ip netns exec #{vm.inhost_name} nc -z -w #{load_balancer.health_check_timeout} #{address} #{load_balancer.dst_port} && echo 200 || echo 400"
+      else
+        "sudo ip netns exec #{vm.inhost_name} curl --insecure --resolve #{load_balancer.hostname}:#{load_balancer.dst_port}:#{(address.version == 6) ? "[#{address}]" : address} --max-time #{load_balancer.health_check_timeout} --silent --output /dev/null --write-out '%{http_code}' #{load_balancer.health_check_protocol}://#{load_balancer.hostname}:#{load_balancer.dst_port}#{load_balancer.health_check_endpoint}"
+      end
+      begin
+        vm.vm_host.sshable.cmd(cmd).strip
+      rescue
+        "500"
+      end
     end
 
     vm_state, vm_state_counter = load_balancer.load_balancers_vms_dataset.where(vm_id: vm.id).get([:state, :state_counter])
-    threshold, health_check = (response_code.to_i == 200) ?
+    threshold, health_check = response_codes.include?("200") ?
       [load_balancer.health_check_up_threshold, "up"] :
       [load_balancer.health_check_down_threshold, "down"]
     counter = (vm_state == health_check) ? vm_state_counter + 1 : 1
