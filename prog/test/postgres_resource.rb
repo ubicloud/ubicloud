3d5538ef5bf0a5d38aa8363dd63a1f077247d977
shikharbhardwaj <8502456+shikharbhardwaj@users.noreply.github.com>
2025-01-10 17:40:07 +0530

    Add E2E tests for PostgreSQL

---
+++ prog/test/postgres_resource.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require_relative "../../lib/util"
+
+class Prog::Test::PostgresResource < Prog::Test::Base
+  semaphore :destroy
+
+  def self.assemble
+    postgres_service_project = Project.create(name: "Postgres-Service-Project") { _1.id = Config.postgres_service_project_id }
+    postgres_test_project = Project.create(name: "Postgres-Test-Project")
+
+    Strand.create_with_id(
+      prog: "Test::PostgresResource",
+      label: "start",
+      stack: [{
+        "postgres_service_project_id" => postgres_service_project.id,
+        "postgres_test_project_id" => postgres_test_project.id
+      }]
+    )
+  end
+
+  label def start
+    st = Prog::Postgres::PostgresResourceNexus.assemble(
+      project_id: frame["postgres_test_project_id"],
+      location: "hetzner-fsn1",
+      name: "postgres-test-standard",
+      target_vm_size: "standard-2",
+      target_storage_size_gib: 128
+    )
+
+    update_stack({"postgres_resource_id" => st.id})
+    hop_wait_postgres_resource
+  end
+
+  label def wait_postgres_resource
+    if postgres_resource.strand.label == "wait" &&
+        postgres_server.run_query("SELECT 1") == "1"
+      hop_test_postgres
+    else
+      nap 10
+    end
+  end
+
+  label def test_postgres
+    unless postgres_server.run_query(test_queries_sql) == "DROP TABLE\nCREATE TABLE\nINSERT 0 10\n4159.90\n415.99\n4.1"
+      update_stack({"fail_message" => "Failed to run test queries"})
+    end
+
+    hop_destroy_postgres
+  end
+
+  label def destroy_postgres
+    postgres_resource.incr_destroy
+    hop_destroy
+  end
+
+  label def destroy
+    postgres_service_project.destroy
+    postgres_test_project.destroy
+
+    fail_test(frame["fail_message"]) if frame["fail_message"]
+
+    pop "Postgres tests are finished!"
+  end
+
+  label def failed
+    nap 15
+  end
+
+  def postgres_test_project
+    @postgres_test_project ||= Project[frame["postgres_test_project_id"]]
+  end
+
+  def postgres_service_project
+    @postgres_service_project ||= Project[frame["postgres_service_project_id"]]
+  end
+
+  def postgres_resource
+    @postgres_resource ||= PostgresResource[frame["postgres_resource_id"]]
+  end
+
+  def postgres_server
+    @postgres_server ||= postgres_resource.representative_server
+  end
+
+  def test_queries_sql
+    File.read("./prog/test/testdata/order_analytics_queries.sql")
+  end
+end
---
+++ spec/prog/test/postgres_resource_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require_relative "../../model/spec_helper"
+
+RSpec.describe Prog::Test::PostgresResource do
+  subject(:pgr_test) { described_class.new(described_class.assemble) }
+
+  let(:postgres_service_project_id) { "546a1ed8-53e5-86d2-966c-fb782d2ae3ab" }
+  let(:working_representative_server) { instance_double(PostgresServer, run_query: "DROP TABLE\nCREATE TABLE\nINSERT 0 10\n4159.90\n415.99\n4.1") }
+  let(:faulty_representative_server) { instance_double(PostgresServer, run_query: "") }
+
+  describe ".assemble" do
+    it "creates a strand and service projects" do
+      expect(Config).to receive(:postgres_service_project_id).and_return(postgres_service_project_id)
+      st = described_class.assemble
+      expect(st).to be_a Strand
+      expect(st.label).to eq("start")
+    end
+  end
+
+  describe "#start" do
+    it "hops to wait_postgres_resource" do
+      expect(Prog::Postgres::PostgresResourceNexus).to receive(:assemble).and_return(instance_double(Strand, id: "1234"))
+      expect { pgr_test.start }.to hop("wait_postgres_resource")
+    end
+  end
+
+  describe "#wait_postgres_resource" do
+    it "hops to test_basic_connectivity if the postgres resource is ready" do
+      expect(pgr_test).to receive(:postgres_resource).exactly(2).and_return(instance_double(Prog::Postgres::PostgresResourceNexus, strand: instance_double(Strand, label: "wait"), representative_server: instance_double(PostgresServer, run_query: "1")))
+      expect { pgr_test.wait_postgres_resource }.to hop("test_postgres")
+    end
+
+    it "naps for 10 seconds if the postgres resource is not ready" do
+      expect(pgr_test).to receive(:postgres_resource).exactly(2).and_return(instance_double(Prog::Postgres::PostgresResourceNexus, strand: instance_double(Strand, label: "wait"), representative_server: instance_double(PostgresServer, run_query: "")))
+      expect { pgr_test.wait_postgres_resource }.to nap(10)
+    end
+  end
+
+  describe "#test_postgres" do
+    it "fails if the basic connectivity test fails" do
+      expect(pgr_test).to receive(:postgres_server).and_return(faulty_representative_server)
+      expect { pgr_test.test_postgres }.to hop("destroy_postgres")
+    end
+
+    it "hops to test_table_create if the basic connectivity test passes" do
+      expect(pgr_test).to receive(:postgres_server).and_return(working_representative_server)
+      expect { pgr_test.test_postgres }.to hop("destroy_postgres")
+    end
+  end
+
+  describe "#destroy_postgres" do
+    it "increments the destroy count and hops to destroy" do
+      postgres_resource = instance_double(Prog::Postgres::PostgresResourceNexus, incr_destroy: nil)
+      expect(PostgresResource).to receive(:[]).and_return(postgres_resource)
+      expect(postgres_resource).to receive(:incr_destroy)
+      expect(pgr_test).to receive(:frame).and_return({})
+      expect { pgr_test.destroy_postgres }.to hop("destroy")
+    end
+  end
+
+  describe "#destroy" do
+    it "increments the destroy count and exits if no failure happened" do
+      expect(Project).to receive(:[]).exactly(2).and_return(instance_double(Project, destroy: nil))
+      expect(pgr_test).to receive(:frame).exactly(4).and_return({})
+      expect { pgr_test.destroy }.to exit({"msg" => "Postgres tests are finished!"})
+    end
+
+    it "increments the destroy count and hops to failed if a failure happened" do
+      expect(Project).to receive(:[]).exactly(2).and_return(instance_double(Project, destroy: nil))
+      expect(pgr_test).to receive(:frame).exactly(4).and_return({"fail_message" => "Test failed"})
+      expect { pgr_test.destroy }.to hop("failed")
+    end
+  end
+
+  describe "#failed" do
+    it "naps" do
+      expect { pgr_test.failed }.to nap(15)
+    end
+  end
+
+  describe ".postgres_server" do
+    it "returns the representative server" do
+      postgres_resource = instance_double(Prog::Postgres::PostgresResourceNexus, representative_server: working_representative_server)
+      expect(pgr_test).to receive(:postgres_resource).and_return(postgres_resource)
+      expect(pgr_test.postgres_server).to eq(working_representative_server)
+    end
+  end
+end
