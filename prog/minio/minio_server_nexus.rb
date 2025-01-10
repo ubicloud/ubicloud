1808d472b1c78bdd80931014af82a3538e1a6379
Furkan Sahin <furkan@ubicloud.com>
2025-01-10 16:21:07 +0100

    Start using encryption for MinIO drives
    At the time when we first introduced no-raid VM setup for MinIO servers,
    we did not have encryption support. Now, we do. So, we are starting to
    use the encryption for MinIO servers, too.

--- prog/minio/minio_server_nexus.rb
+++ prog/minio/minio_server_nexus.rb
@@ -26,7 +26,7 @@ class Prog::Minio::MinioServerNexus < Prog::Base
         size: minio_pool.vm_size,
         storage_volumes: [
           {encrypted: true, size_gib: 30}
-        ] + Array.new(minio_pool.per_server_drive_count) { {encrypted: false, size_gib: (minio_pool.per_server_storage_size / minio_pool.per_server_drive_count).floor} },
+        ] + Array.new(minio_pool.per_server_drive_count) { {encrypted: true, size_gib: (minio_pool.per_server_storage_size / minio_pool.per_server_drive_count).floor} },
         boot_image: "ubuntu-jammy",
         enable_ip4: true,
         private_subnet_id: minio_pool.cluster.private_subnet.id,
