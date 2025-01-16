aa834125473abea47ec8cbadd7ee62709e7c04f6
Eren Başak <eren@ubicloud.com>
2025-01-16 20:05:42 +0300

    Introduce Kubernetes Nodepools
    This change adds the concept of Kubernetes Nodepools.
    
    Each kubernetes nodepool is a set of VMs that act as the nodes that run
    the actual application workloads.
    
    A cluster have multiple nodepools with different size and
    characteristics. The logic in this PR is also implemented this way.
    However, product-wise, we may hide this feature for now and default to a
    single nodepool.
    
    A kubernetes cluster without a nodepool is kinda useless at this point.
    
    Nodepools should be created separately from the cluster. However, a
    nodepool nexus strand would wait until its cluster is stable.
    
    Co-authored-by: Eren Başak <eren@ubicloud.com>
    Co-authored-by: mohi-kalantari <mohi.kalantari1@gmail.com>

---
+++ prog/kubernetes/kubernetes_nodepool_nexus.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class Prog::Kubernetes::KubernetesNodepoolNexus < Prog::Base
+  subject_is :kubernetes_nodepool
+
+  def self.assemble(name:, node_count:, kubernetes_cluster_id:)
+    DB.transaction do
+      unless KubernetesCluster[kubernetes_cluster_id]
+        fail "No existing cluster"
+      end
+
+      kn = KubernetesNodepool.create(
+        name: name,
+        node_count: node_count,
+        kubernetes_cluster_id: kubernetes_cluster_id
+      )
+
+      Strand.create(prog: "Kubernetes::KubernetesNodepoolNexus", label: "start") { _1.id = kn.id }
+    end
+  end
+
+  def before_run
+    when_destroy_set? do
+      if strand.label != "destroy"
+        hop_destroy
+      end
+    end
+  end
+
+  label def start
+    nap 30 unless kubernetes_nodepool.kubernetes_cluster.strand.label == "wait"
+    register_deadline("wait", 120 * 60)
+    hop_bootstrap_worker_vms
+  end
+
+  label def bootstrap_worker_vms
+    hop_wait if kubernetes_nodepool.vms.count >= kubernetes_nodepool.node_count
+    push Prog::Kubernetes::ProvisionKubernetesNode, {"nodepool_id" => kubernetes_nodepool.id, "subject_id" => kubernetes_nodepool.kubernetes_cluster.id}
+  end
+
+  label def wait
+    nap 30
+  end
+
+  label def destroy
+    kubernetes_nodepool.vms.each(&:incr_destroy)
+    kubernetes_nodepool.vms.each { kubernetes_nodepool.remove_vm(_1) }
+    kubernetes_nodepool.destroy
+    pop "kubernetes nodepool is deleted"
+  end
+end
---
+++ spec/prog/kubernetes/kubernetes_nodepool_nexus_spec.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+require_relative "../../model/spec_helper"
+
+RSpec.describe Prog::Kubernetes::KubernetesNodepoolNexus do
+  subject(:nx) { described_class.new(Strand.new(id: "8148ebdf-66b8-8ed0-9c2f-8cfe93f5aa77")) }
+
+  let(:project) { Project.create(name: "default") }
+  let(:subnet) { PrivateSubnet.create(net6: "0::0", net4: "127.0.0.1", name: "x", location: "x", project_id: project.id) }
+
+  let(:kc) {
+    kc = KubernetesCluster.create(
+      name: "k8scluster",
+      version: "v1.32",
+      cp_node_count: 3,
+      private_subnet_id: subnet.id,
+      location: "hetzner-fsn1",
+      project_id: project.id
+    )
+
+    lb = LoadBalancer.create(private_subnet_id: subnet.id, name: "somelb", src_port: 123, dst_port: 456, health_check_endpoint: "/foo", project_id: project.id)
+    kc.add_cp_vm(create_vm)
+    kc.add_cp_vm(create_vm)
+    kc.update(api_server_lb_id: lb.id)
+    kc
+  }
+
+  let(:kn) { KubernetesNodepool.create(name: "k8stest-np", node_count: 2, kubernetes_cluster_id: kc.id) }
+
+  before do
+    allow(nx).to receive(:kubernetes_nodepool).and_return(kn)
+  end
+
+  describe ".assemble" do
+    it "validates input" do
+      expect {
+        described_class.assemble(name: "name", node_count: 2, kubernetes_cluster_id: SecureRandom.uuid)
+      }.to raise_error RuntimeError, "No existing cluster"
+    end
+
+    it "creates a kubernetes nodepool" do
+      st = described_class.assemble(name: "k8stest-np", node_count: 2, kubernetes_cluster_id: kc.id)
+
+      expect(st.subject.name).to eq "k8stest-np"
+      expect(st.subject.ubid).to start_with("kn")
+      expect(st.subject.kubernetes_cluster_id).to eq kc.id
+      expect(st.subject.node_count).to eq 2
+      expect(st.label).to eq "start"
+    end
+  end
+
+  describe "#before_run" do
+    it "hops to destroy" do
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect { nx.before_run }.to hop("destroy")
+    end
+
+    it "does not hop to destroy if already in the destroy state" do
+      expect(nx).to receive(:when_destroy_set?).and_yield
+      expect(nx.strand).to receive(:label).and_return("destroy")
+      expect { nx.before_run }.not_to hop("destroy")
+    end
+  end
+
+  describe "#start" do
+    it "naps if the kubernetes cluster is not ready" do
+      expect(kn.kubernetes_cluster).to receive(:strand).and_return(Strand.new(label: "not-wait"))
+      expect { nx.start }.to nap(30)
+    end
+
+    it "registers a deadline and hops if the cluster is ready" do
+      expect(kn.kubernetes_cluster).to receive(:strand).and_return(Strand.new(label: "wait"))
+      expect(nx).to receive(:register_deadline)
+      expect { nx.start }.to hop("bootstrap_worker_vms")
+    end
+  end
+
+  describe "#bootstrap_worker_vms" do
+    it "hops wait if the target number of vms is reached" do
+      expect(kn).to receive(:vms).and_return [1, 2]
+      expect { nx.bootstrap_worker_vms }.to hop("wait")
+    end
+
+    it "pushes ProvisionKubernetesNode prog to create VMs" do
+      expect(nx).to receive(:push).with(Prog::Kubernetes::ProvisionKubernetesNode, {"nodepool_id" => kn.id, "subject_id" => kn.kubernetes_cluster.id})
+      nx.bootstrap_worker_vms
+    end
+  end
+
+  describe "#wait" do
+    it "just naps for 30 seconds for now" do
+      expect { nx.wait }.to nap(30)
+    end
+  end
+
+  describe "#destroy" do
+    it "destroys the nodepool and its vms" do
+      vms = [create_vm, create_vm]
+      expect(kn).to receive(:vms).and_return(vms).twice
+
+      expect(vms).to all(receive(:incr_destroy))
+      expect(kn).to receive(:remove_vm).with(vms[0])
+      expect(kn).to receive(:remove_vm).with(vms[1])
+      expect(kn).to receive(:destroy)
+      expect { nx.destroy }.to exit({"msg" => "kubernetes nodepool is deleted"})
+    end
+  end
+end
