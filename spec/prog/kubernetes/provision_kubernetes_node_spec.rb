77842ee39df612e42cba79868da8e7c5e3cc1968
Eren Başak <eren@ubicloud.com>
2025-01-16 09:53:36 +0300

    Introduce Ubicni for establishing intra-cluster network connectivity
    Ubicni is our CNI plugin for kubernetes that allows intra-cluster network
    communication within a kubernetes cluster on top of Ubicloud network stack.
    
    Co-authored-by: Eren Başak <eren@ubicloud.com>
    Co-authored-by: mohi-kalantari <mohi.kalantari1@gmail.com>

--- spec/prog/kubernetes/provision_kubernetes_node_spec.rb
+++ spec/prog/kubernetes/provision_kubernetes_node_spec.rb
@@ -165,7 +165,7 @@ RSpec.describe Prog::Kubernetes::ProvisionKubernetesNode do
 
     it "pops if the init_cluster script is successful" do
       expect(prog.vm.sshable).to receive(:cmd).with("common/bin/daemonizer --check init_kubernetes_cluster").and_return("Succeeded")
-      expect { prog.init_cluster }.to exit({vm_id: prog.vm.id})
+      expect { prog.init_cluster }.to hop("install_cni")
     end
 
     it "naps forever if the daemonizer check returns something unknown" do
@@ -202,12 +202,28 @@ RSpec.describe Prog::Kubernetes::ProvisionKubernetesNode do
 
     it "pops if the join_control_plane script is successful" do
       expect(prog.vm.sshable).to receive(:cmd).with("common/bin/daemonizer --check join_control_plane").and_return("Succeeded")
-      expect { prog.join_control_plane }.to exit({vm_id: prog.vm.id})
+      expect { prog.join_control_plane }.to hop("install_cni")
     end
 
     it "naps forever if the daemonizer check returns something unknown" do
       expect(prog.vm.sshable).to receive(:cmd).with("common/bin/daemonizer --check join_control_plane").and_return("Unknown")
       expect { prog.join_control_plane }.to nap(65536)
+    end
+  end
+
+  describe "#install_cni" do
+    it "configures ubicni" do
+      sshable = instance_double(Sshable)
+      expect(prog.vm).to receive(:sshable).and_return(sshable)
+      expect(prog.vm).to receive(:ephemeral_net6).and_return(NetAddr::IPv6Net.parse("fe80::/64"))
+      allow(prog.vm).to receive(:nics).and_return([instance_double(Nic, private_ipv4: "10.0.0.37", private_ipv6: "0::1")])
+
+      expect(sshable).to receive(:cmd).with("sudo tee /opt/cni/bin/ubicni", stdin: /exec .\/kubernetes\/bin\/ubicni/)
+      expect(sshable).to receive(:cmd).with("sudo tee /etc/cni/net.d/ubicni-config.json", stdin: /"type": "ubicni"/)
+      expect(sshable).to receive(:cmd).with("sudo chmod +x /opt/cni/bin/ubicni")
+      expect(sshable).to receive(:cmd).with("sudo iptables -t nat -A POSTROUTING -s 10.0.0.37 -o ens3 -j MASQUERADE")
+
+      expect { prog.install_cni }.to exit({vm_id: prog.vm.id})
     end
   end
 end
