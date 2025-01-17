0f73f75053912b7f3bdc8559acf174b26280ced2
Enes Cakir <enes@ubicloud.com>
2025-01-17 23:15:44 +0300

    Deduplicate the client in GitHub runner E2E rspecs

--- spec/prog/test/github_runner_spec.rb
+++ spec/prog/test/github_runner_spec.rb
@@ -5,10 +5,13 @@ require_relative "../../model/spec_helper"
 RSpec.describe Prog::Test::GithubRunner do
   subject(:gr_test) { described_class.new(described_class.assemble(12345, ["github_runner_ubuntu_2204"])) }
 
+  let(:client) { instance_double(Octokit::Client) }
+
   before do
     expect(Config).to receive(:github_runner_service_project_id).and_return("fabd95f8-d002-8ed2-9f4c-00625eb7f574")
     expect(Config).to receive(:vm_pool_project_id).and_return("c3fd495f-9888-82d2-8100-7fae94e87e27")
     expect(Config).to receive(:e2e_github_installation_id).and_return(123456).at_least(:once)
+    allow(Github).to receive(:installation_client).with(Config.e2e_github_installation_id).and_return(client)
   end
 
   describe "#start" do
@@ -68,8 +71,6 @@ RSpec.describe Prog::Test::GithubRunner do
 
   describe "#trigger_test_runs" do
     it "triggers test runs" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       allow(ENV).to receive(:[]).and_call_original
       expect(ENV).to receive(:[]).with("GITHUB_RUN_ID").and_return("12345")
       expect(client).to receive(:workflow_dispatch).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", "main", {inputs: {triggered_by: "12345"}}).and_return(true)
@@ -78,8 +79,6 @@ RSpec.describe Prog::Test::GithubRunner do
     end
 
     it "can not triggers test runs" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       allow(ENV).to receive(:[]).and_call_original
       expect(ENV).to receive(:[]).with("GITHUB_RUN_ID").and_return("12345")
       expect(client).to receive(:workflow_dispatch).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", "main", {inputs: {triggered_by: "12345"}}).and_return(false)
@@ -89,50 +88,33 @@ RSpec.describe Prog::Test::GithubRunner do
 
   describe "#check_test_runs" do
     it "check test runs completed" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: "success", created_at: Time.now}]})
-      expect(gr_test).to receive(:frame).and_return({"created_at" => Time.new(2023, 1, 1).to_s, "test_cases" => ["github_runner_ubuntu_2204"]}).at_least(:once)
       expect { gr_test.check_test_runs }.to hop("clean_resources")
     end
 
     it "check test runs in progress with nil" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: nil, created_at: Time.now}]})
-      expect(gr_test).to receive(:frame).and_return({"created_at" => Time.new(2023, 1, 1).to_s, "test_cases" => ["github_runner_ubuntu_2204"]}).at_least(:once)
       expect { gr_test.check_test_runs }.to nap(15)
     end
 
     it "check test runs in progress state" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: "in_progress", created_at: Time.now}]})
-      expect(gr_test).to receive(:frame).and_return({"created_at" => Time.new(2023, 1, 1).to_s, "test_cases" => ["github_runner_ubuntu_2204"]}).at_least(:once)
       expect { gr_test.check_test_runs }.to nap(15)
     end
 
     it "check test runs failed" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: "failure", created_at: Time.now}]})
-      expect(gr_test).to receive(:frame).and_return({"created_at" => Time.new(2023, 1, 1).to_s, "test_cases" => ["github_runner_ubuntu_2204"]}).at_least(:once)
       expect { gr_test.check_test_runs }.to hop("clean_resources")
     end
 
     it "check test runs created before" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client)
-      expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: "failure", created_at: Time.new(2023, 1, 1)}]})
-      expect(gr_test).to receive(:frame).and_return({"created_at" => Time.now.to_s, "test_cases" => ["github_runner_ubuntu_2204"]}).at_least(:once)
+      expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{conclusion: "failure", created_at: Time.now - 2 * 60 * 60}]})
       expect { gr_test.check_test_runs }.to hop("clean_resources")
     end
   end
 
   describe "#clean_resources" do
     it "not clean with github exists" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client).at_least(:twice)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{id: 10}]})
       expect(client).to receive(:cancel_workflow_run).with("ubicloud/github-e2e-test-workflows", 10)
       expect(GithubRunner).to receive(:any?).and_return(true)
@@ -140,8 +122,6 @@ RSpec.describe Prog::Test::GithubRunner do
     end
 
     it "cleans resources and hop finish" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client).at_least(:twice)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{id: 10}]})
       expect(client).to receive(:cancel_workflow_run).with("ubicloud/github-e2e-test-workflows", 10)
       expect(GithubRunner).to receive(:any?).and_return(false)
@@ -151,8 +131,6 @@ RSpec.describe Prog::Test::GithubRunner do
     end
 
     it "cleans resources and hop failed" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client).at_least(:twice)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{id: 10}]})
       expect(client).to receive(:cancel_workflow_run).with("ubicloud/github-e2e-test-workflows", 10)
       expect(GithubRunner).to receive(:any?).and_return(false)
@@ -163,8 +141,6 @@ RSpec.describe Prog::Test::GithubRunner do
     end
 
     it "cleans resources already cancelled" do
-      client = instance_double(Octokit::Client)
-      expect(gr_test).to receive(:client).and_return(client).at_least(:twice)
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{id: 10}]})
       expect(client).to receive(:cancel_workflow_run).and_raise(StandardError)
       expect(GithubRunner).to receive(:any?).and_return(true)
@@ -181,14 +157,6 @@ RSpec.describe Prog::Test::GithubRunner do
   describe "failed" do
     it "finish" do
       expect { gr_test.failed }.to nap(15)
-    end
-  end
-
-  describe "get_client" do
-    it "get_client" do
-      expect(Github).to receive(:installation_client).with(Config.e2e_github_installation_id).and_return(Octokit::Client)
-
-      gr_test.client
     end
   end
 end
