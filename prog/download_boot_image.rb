7822b0680f7aa382b542d8e3690b913abed5b569
Benjamin Satzger <ben@ubicloud.com>
2025-01-18 16:15:56 +0100

    Introduce AI model Llama-3.1-8B-Instruct
    Add AI model image and billing information for Llama-3.1-8B-Instruct.
    For more info about the model, see
    https://huggingface.co/meta-llama/Llama-3.1-8B-Instruct.
    
    Job producing the model image:
    https://github.com/ubicloud/ai-images/actions/runs/12843738017

--- prog/download_boot_image.rb
+++ prog/download_boot_image.rb
@@ -94,6 +94,7 @@ class Prog::DownloadBootImage < Prog::Base
       ["postgres17-lantern-ubuntu-2204", "x64", "20250103.1.0"] => "a95b2e5d03291783dc1753228d7a87949257a06c7b1eca2c94502ab21ffdecdb",
       ["ai-ubuntu-2404-nvidia", "x64", "20250118.1.0"] => "b2aadc3c0a222ae79a534fc159ffc432e3839ac16e1dfc3050a6fc4691ba3f6d",
       ["ai-model-gemma-2-2b-it", "x64", "20240918.1.0"] => "b726ead6d5f48fb8e6de7efb48fb22367c9a7c155cfee71a3a7e5527be5df08e",
+      ["ai-model-llama-3-1-8b-it", "x64", "20250118.1.0"] => "7296f70a861c364f59c38b816e1210152ebafbec85ce797888c16b4d48a15e8f",
       ["ai-model-llama-3-1-405b-it", "x64", "20241029.1.0"] => "54680b8b18ed501956a78c63b94576b7200fbd8fbe025eccb90152d13478882e",
       ["ai-model-llama-3-2-3b-it", "x64", "20241028.1.0"] => "023d2a285564f3b75413c537b1242e4c2acfea3d3f88912c595da80354516fba",
       ["ai-model-llama-guard-3-1b", "x64", "20241028.1.0"] => "ef85abe990d26ded8e231fd05195c580b517b6555fe8f1dc9463012ce3e93f3f",
