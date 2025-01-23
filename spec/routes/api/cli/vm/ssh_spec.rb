0233aa09ca350bd400cf4268988497ad7b1f39e7
Jeremy Evans <jeremy@ubicloud.com>
2025-01-23 15:12:37 -0800

    Add support for vm ssh CLI command
    This supports automatically connecting to the requested VM via SSH.
    It takes options for which address family to use, and to override the
    username used when connecting.
    
    This passes information on the command the client should execute
    via the headers understood by bin/ubi.  This includes support for
    letting the client know which part of ARGV should be used in the
    SSH connection, supporting commands such as:
    
      vm ssh location vm-name remote-command remote-command-arg
    
    Note that users must provide both the location and vm name or
    ubid when connecting.  This is because a principle of UbiCli is
    that it does not access the database or use the models, it only
    makes internal requests to Clover's api, and the api URLs require
    the location and either the name or ubid.

---
+++ spec/routes/api/cli/vm/ssh_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+RSpec.describe Clover, "cli vm ssh" do
+  before do
+    @vm = create_vm(project_id: @project.id, ephemeral_net6: "128:1234::0/64")
+    subnet = @project.default_private_subnet(@vm.location)
+    nic = Prog::Vnet::NicNexus.assemble(subnet.id, name: "test-nic").subject
+    nic.update(vm_id: @vm.id)
+  end
+
+  after do
+    @socket&.close
+  end
+
+  it "provides headers to connect to vm" do
+    expect(cli_exec(["vm", "ssh", @vm.display_location, @vm.name])).to eq %w[ssh ubi@128:1234::2 0]
+  end
+
+  it "IPv4 address is used by default if available" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    expect(cli_exec(["vm", "ssh", @vm.display_location, @vm.name])).to eq %w[ssh ubi@128.0.0.1 0]
+  end
+
+  it "uses IPv4 address if available and connection is made via IPv4" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    @socket = UDPSocket.new(Socket::AF_INET)
+    expect(cli_exec(["vm", "ssh", @vm.display_location, @vm.name], env: {"puma.socket" => @socket})).to eq %w[ssh ubi@128.0.0.1 0]
+  end
+
+  it "uses IPv6 address if connection is made via IPv6" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    @socket = UDPSocket.new(Socket::AF_INET6)
+    expect(cli_exec(["vm", "ssh", @vm.display_location, @vm.name], env: {"puma.socket" => @socket})).to eq %w[ssh ubi@128:1234::2 0]
+  end
+
+  it "-4 option fails if VM has no IPv4 address" do
+    expect(cli(["vm", "ssh", "-4", @vm.display_location, @vm.name], status: 400)).to eq "No valid IPv4 address for requested VM"
+  end
+
+  it "-4 option uses IPv4 even if connection is made via IPv6" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    @socket = UDPSocket.new(Socket::AF_INET6)
+    expect(cli_exec(["vm", "ssh", "-4", @vm.display_location, @vm.name], env: {"puma.socket" => @socket})).to eq %w[ssh ubi@128.0.0.1 0]
+  end
+
+  it "-6 option uses IPv6 even if connection is made via IPv4" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    @socket = UDPSocket.new(Socket::AF_INET)
+    expect(cli_exec(["vm", "ssh", "-6", @vm.display_location, @vm.name], env: {"puma.socket" => @socket})).to eq %w[ssh ubi@128:1234::2 0]
+  end
+
+  it "-u option overrides user to connect with" do
+    expect(cli_exec(["vm", "ssh", "-ufoo", @vm.display_location, @vm.name])).to eq %w[ssh foo@128:1234::2 0]
+  end
+
+  it "returns the number of additional arguments in argv that should be used for the command" do
+    expect(cli_exec(["vm", "ssh", @vm.display_location, @vm.name, "id"])).to eq %w[ssh ubi@128:1234::2 1]
+  end
+
+  it "handles multiple options" do
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    expect(cli_exec(["vm", "ssh", "-6u", "foo", @vm.display_location, @vm.name])).to eq %w[ssh foo@128:1234::2 0]
+  end
+
+  it "handles invalid location or name" do
+    expect(cli(["vm", "ssh", "-4", @vm.display_location, "foo"], status: 404)).to eq "Error: unexpected response status: 404\nDetails: Sorry, we couldn’t find the resource you’re looking for."
+    expect(cli(["vm", "ssh", "-4", "foo", @vm.name], status: 404)).to eq "Error: unexpected response status: 404\nDetails: Sorry, we couldn’t find the resource you’re looking for."
+  end
+end
