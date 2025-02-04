4fd62508a8a6fffff2dfc0a094b189a25f253261
Hadi Moshayedi <hadi@ubicloud.com>
2025-02-03 19:21:42 -0800

    Clover: Increase SPDK hugepages.
    In the Rhizome side we increase number of hugepages reserved for SPDK.
    This PR updates the Clover side to reflect that.

--- prog/storage/setup_spdk.rb
+++ prog/storage/setup_spdk.rb
@@ -36,7 +36,7 @@ class Prog::Storage::SetupSpdk < Prog::Base
       allocation_weight: 0,
       vm_host_id: vm_host.id,
       cpu_count: vm_host.spdk_cpu_count,
-      hugepages: 2
+      hugepages: 3
     ) { _1.id = SpdkInstallation.generate_uuid }
 
     hop_install_spdk
