3b45f05e43d8412eac1ca15e582437fe48483025
Jeremy Evans <jeremy@ubicloud.com>
2025-02-03 17:39:01 -0800

    Add query parameterization analysis
    This allows you to analyze all queries issued during testing, and see which
    ones were parameterized, which ones were not parameterized, and possible
    queries where parameterization was missed.
    
    To see this analysis, run "rake check_query_parameterization".  This will
    run the specs logging all queries, then run a script that analyzes the
    logged queries.  You can view the analysis in the created
    sql_query_parameterization_analysis.txt.
    
    The first line in this analysis has summary data:
    
    ```
    Summary: Missed: 11|11, Parameterized: 1034|71461, Not Parameterized: 94|33003
    ```
    
    `Missed: 11|11` means 11 total and 11 unique queries were issued that you would
    expect to be parameterized but were not (missed parameterization).
    
    `Parameterized: 1034|71461` means there were 71461 total and 1034 unique
    parameterized queries.
    
    `Not Parameterized: 94:33003` means there were 33003 total and 94 unique
    queries that were not parameterized and not expected to be parameterized.
    
    The rest of the file lists the unique queries for each of the three types.
    It can be useful to review all sections to see the queries in use, in
    case any appear odd and worthy of further review.
    
    By default, Sequel::Model does not parameterize model lookups and deletes
    for scalar primary keys, for performance reasons.  Those would show up as
    missed parameterization.  This disables that optimization when running
    the query parameterization check.
    
    Disabling the optimization broke one of the specs, because
    SequelExtensions#delete did not handle the case where Model#delete called
    Dataset#delete.  Fix it by adding another caller check.  Also, only call
    caller once instead of twice, probably making this faster than it was before.
    
    Additionally, I found that logging SQL in any capacity broke about 8 specs
    that mocked Time.now and expected a specific number of Time.now calls.
    Such assertions on the number of Time.now calls in the specs are questionable,
    but work around the issue in this particular case by setting Logger::Time to
    an object where now returns a static value instead of calling Time.now.
    
    While here, fix the rodaauth typo in SequelExtensions#delete.

--- model.rb
+++ model.rb
@@ -39,14 +39,44 @@ if (level = Config.database_logger_level) || Config.test?
   DB.loggers << LOGGER
 end
 
+if ENV["CHECK_LOGGED_SQL"]
+  require "logger"
+  File.unlink("sql.log")
+  f = File.open("sql.log", "ab")
+
+  # Remove optimization that does not use parameterization
+  def (Sequel::Model).reset_fast_pk_lookup_sql = nil
+
+  # Hack to make specs pass that mock Time.now and depend
+  # on certain number of Time.now calls
+  time = Time.now
+  def time.now
+    self
+  end
+  Logger.const_set(:Time, time)
+
+  sql_logger = Logger.new(f, level: :INFO)
+  sql_logger.formatter = proc do |sev, _, _, msg|
+    "#{sev} -- : #{msg}\0"
+  end
+
+  DB.loggers << sql_logger
+end
+
 module SequelExtensions
   def delete(force: false, &)
     # Do not error if this is a plain dataset that does not respond to destroy
     return super(&) unless respond_to?(:destroy)
 
-    rodaauth_in_callstack = !caller.grep(/rodauth/).empty?
-    destroy_in_callstack = !caller.grep(/sequel\/model\/base.*_destroy_delete/).empty?
-    unless rodaauth_in_callstack || destroy_in_callstack || force
+    caller_lines = caller
+    rodauth_in_callstack = !caller_lines.grep(/rodauth/).empty?
+    destroy_in_callstack = !caller_lines.grep(/sequel\/model\/base.*_destroy_delete/).empty?
+
+    # This can happen when fast instance deletes are disabled (when CHECK_LOGGED_SQL
+    # environment variable is set)
+    callee_in_callstack = !caller_lines.grep(/#{Regexp.escape(__FILE__)}.*delete/).empty?
+
+    unless rodauth_in_callstack || destroy_in_callstack || callee_in_callstack || force
       raise "Calling delete is discouraged as it skips hooks such as before_destroy, which " \
             "we use to archive records. Use destroy instead. If you know what you are doing " \
             "and still want to use delete, you can pass force: true to trigger delete."
