73c50d78278c647da74d2bd8bc25c888d4dc0380
Jeremy Evans <jeremy@ubicloud.com>
2025-02-05 15:57:12 -0800

    Use headers for cli vm list command by default
    Ruby's optparse is a little weird here, setting the `:"no-headers"`
    value to `false` in this case.

--- cli-commands/vm/list.rb
+++ cli-commands/vm/list.rb
@@ -2,7 +2,7 @@
 
 UbiRodish.on("vm", "list") do
   options("ubi vm list [options]", key: :vm_list) do
-    on("-h", "--headers", "show headers")
+    on("-N", "--no-headers", "do not show headers")
     on("-i", "--id", "show id")
     on("-n", "--name", "show name")
     on("-l", "--location", "show location")
@@ -15,10 +15,11 @@ UbiRodish.on("vm", "list") do
   run do |opts|
     get(project_path("vm")) do |data|
       keys = fields
+      headers = true
       if (opts = opts[:vm_list])
         keys = keys.select { opts[_1.to_sym] }
         keys = fields if keys.empty?
-        headers = opts[:headers]
+        headers = false if opts[:"no-headers"] == false
       end
 
       format_rows(keys, data["items"], headers:)
--- spec/routes/api/cli/vm/list_spec.rb
+++ spec/routes/api/cli/vm/list_spec.rb
@@ -11,39 +11,39 @@ RSpec.describe Clover, "cli vm list" do
   end
 
   it "shows list of vms" do
-    expect(cli(%w[vm list])).to eq "eu-central-h1 test-vm #{@vm.ubid} 128.0.0.1 128:1234::2\n"
+    expect(cli(%w[vm list -N])).to eq "eu-central-h1 test-vm #{@vm.ubid} 128.0.0.1 128:1234::2\n"
   end
 
   it "-i option includes VM ubid" do
-    expect(cli(%w[vm list -i])).to eq "#{@vm.ubid}\n"
+    expect(cli(%w[vm list -Ni])).to eq "#{@vm.ubid}\n"
   end
 
   it "-n option includes VM name" do
-    expect(cli(%w[vm list -n])).to eq "test-vm\n"
+    expect(cli(%w[vm list -Nn])).to eq "test-vm\n"
   end
 
   it "-l option includes VM location" do
-    expect(cli(%w[vm list -l])).to eq "eu-central-h1\n"
+    expect(cli(%w[vm list -Nl])).to eq "eu-central-h1\n"
   end
 
   it "-4 option includes VM IPv4 address" do
-    expect(cli(%w[vm list -4])).to eq "128.0.0.1\n"
+    expect(cli(%w[vm list -N4])).to eq "128.0.0.1\n"
   end
 
   it "-6 option includes VM IPv6 address" do
-    expect(cli(%w[vm list -6])).to eq "128:1234::2\n"
+    expect(cli(%w[vm list -N6])).to eq "128:1234::2\n"
   end
 
-  it "-h option includes headers" do
-    expect(cli(%w[vm list -h])).to eq <<~END
+  it "headers are shown by default" do
+    expect(cli(%w[vm list])).to eq <<~END
       location      name    #{id_headr} ip4       ip6        
       eu-central-h1 test-vm #{@vm.ubid} 128.0.0.1 128:1234::2
     END
   end
 
   it "handles multiple options" do
-    expect(cli(%w[vm list -inl])).to eq "eu-central-h1 test-vm #{@vm.ubid}\n"
-    expect(cli(%w[vm list -hinl])).to eq <<~END
+    expect(cli(%w[vm list -Ninl])).to eq "eu-central-h1 test-vm #{@vm.ubid}\n"
+    expect(cli(%w[vm list -inl])).to eq <<~END
       location      name    #{id_headr}
       eu-central-h1 test-vm #{@vm.ubid}
     END
