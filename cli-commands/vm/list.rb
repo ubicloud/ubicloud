91e4a7e78c7baecd598beeabdbecf87d7a448778
Jeremy Evans <jeremy@ubicloud.com>
2025-02-05 16:08:04 -0800

    Make cli vm list use -f/--fields option to specify fields
    Remove the other options to specify fields
    
    This makes it simpler to expand the list of fields later, and
    allows the user to specify the order of fields.

--- cli-commands/vm/list.rb
+++ cli-commands/vm/list.rb
@@ -1,24 +1,33 @@
 # frozen_string_literal: true
 
 UbiRodish.on("vm", "list") do
+  fields = %w[location name id ip4 ip6].freeze.each(&:freeze)
+
   options("ubi vm list [options]", key: :vm_list) do
     on("-N", "--no-headers", "do not show headers")
-    on("-i", "--id", "show id")
-    on("-n", "--name", "show name")
-    on("-l", "--location", "show location")
-    on("-4", "--ip4", "show IPv4 address")
-    on("-6", "--ip6", "show IPv6 address")
+    on("-f", "--fields=fields", "show specific fields (default: #{fields.join(",")})")
   end
-
-  fields = %w[location name id ip4 ip6].freeze.each(&:freeze)
 
   run do |opts|
     get(project_path("vm")) do |data|
       keys = fields
       headers = true
       if (opts = opts[:vm_list])
-        keys = keys.select { opts[_1.to_sym] }
-        keys = fields if keys.empty?
+        if opts[:fields]
+          keys = opts[:fields].split(",")
+          if keys.empty?
+            raise Rodish::CommandFailure, "no fields given in vm list -f option"
+          end
+          unless keys.size == keys.uniq.size
+            raise Rodish::CommandFailure, "duplicate field(s) in vm list -f option"
+          end
+
+          invalid_keys = keys - fields
+          unless invalid_keys.empty?
+            raise Rodish::CommandFailure, "invalid field(s) given vm list -f option: #{invalid_keys.join(",")}"
+          end
+        end
+
         headers = false if opts[:"no-headers"] == false
       end
 
