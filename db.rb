b44e3468d9bc97fb259d9e8fb05b9f831a6714af
Jeremy Evans <jeremy@ubicloud.com>
2025-02-06 22:54:53 -0800

    Add array casting in additional cases
    Reenable the automatic conversion of string value lists to text arrays only
    in development and when running frozen tests, and do it for all non-empty
    arrays, instead of the default of only arrays of 2 or more elements.
    
    I do not feel comfortable enough to turn this on in production, since it will
    not catch places in the tests where we are only using empty arrays, but where
    the arrays will be non-empty in production.  I'm not sure we have that case,
    but since we don't have the tooling to ensure that, enabling this in production
    seems too risky.  Doing it only in the frozen tests is sufficient for now, since
    it will still result in the query parameterization log having consistent SQL.
    I'm also enabling it in development, because after enough time, if there are any
    issues that are not caught by the tests, they should occur in a developer's
    development environment, and once reported, they can be easily fixed.
    
    Update the Sequel version to 5.90 as that includes the changes this needs.

--- db.rb
+++ db.rb
@@ -9,7 +9,11 @@ db_ca_bundle_filename = File.join(Dir.pwd, "var", "ca_bundles", "db_ca_bundle.cr
 Util.safe_write_to_file(db_ca_bundle_filename, Config.clover_database_root_certs)
 max_connections = Config.db_pool - 1
 max_connections = 1 if ENV["SHARED_CONNECTION"] == "1"
-DB = Sequel.connect(Config.clover_database_url, max_connections:, pool_timeout: Config.database_timeout).tap do |db|
+if Config.development? || (Config.test? && ENV["CLOVER_FREEZE"] == "1")
+  treat_string_list_as_text_array = true
+  pg_auto_parameterize_min_array_size = 1
+end
+DB = Sequel.connect(Config.clover_database_url, max_connections:, pool_timeout: Config.database_timeout, treat_string_list_as_text_array:, pg_auto_parameterize_min_array_size:).tap do |db|
   # Replace dangerous (for cidrs) Ruby IPAddr type that is otherwise
   # used by sequel_pg.  Has come up more than once in the bug tracker:
   #
