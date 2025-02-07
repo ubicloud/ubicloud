490e8f6e59da203e262d38fbc8a1ce28835009bd
Jeremy Evans <jeremy@ubicloud.com>
2025-02-07 14:13:51 -0800

    Add cli vm list -l option to specify location

--- cli-commands/vm/list.rb
+++ cli-commands/vm/list.rb
@@ -4,12 +4,24 @@ UbiRodish.on("vm", "list") do
   fields = %w[location name id ip4 ip6].freeze.each(&:freeze)
 
   options("ubi vm list [options]", key: :vm_list) do
-    on("-N", "--no-headers", "do not show headers")
     on("-f", "--fields=fields", "show specific fields (default: #{fields.join(",")})")
+    on("-l", "--location=location", "only show VMs in given location")
+    on("-N", "--no-headers", "do not show headers")
   end
 
   run do |opts|
-    get(project_path("vm")) do |data|
+    vm_opts = opts[:vm_list]
+    path = if vm_opts && (location = vm_opts[:location])
+      if LocationNameConverter.to_internal_name(location)
+        "location/#{location}/vm"
+      else
+        raise Rodish::CommandFailure, "invalid location provided in vm list -l option"
+      end
+    else
+      "vm"
+    end
+
+    get(project_path(path)) do |data|
       keys = fields
       headers = true
       if (opts = opts[:vm_list])
