04831a06360fe232a46cfc12dbc2af30b5d99b1d
Jeremy Evans <jeremy@ubicloud.com>
2025-02-10 10:56:36 -0800

    Support cli pg create command

---
+++ cli-commands/pg.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+UbiRodish.on("pg") do
+  # :nocov:
+  unless Config.production? || ENV["FORCE_AUTOLOAD"] == "1"
+    autoload_post_subcommand_dir("cli-commands/pg/post")
+  end
+  # :nocov:
+
+  args(2...)
+
+  run do |(pg_ref, *argv), opts, command|
+    @location, @pg_name, extra = pg_ref.split("/", 3)
+    raise Rodish::CommandFailure, "invalid pg reference, should be in location/(pg-name|_pg-ubid) format" if extra
+    command.run(self, opts, argv)
+  end
+end
