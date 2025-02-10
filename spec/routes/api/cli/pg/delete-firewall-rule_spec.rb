f878c10c78d06ad7ee787e29b0dff9d58ca574d5
Jeremy Evans <jeremy@ubicloud.com>
2025-02-10 15:27:45 -0800

    Add cli pg delete-firewall-rule command

---
+++ spec/routes/api/cli/pg/delete-firewall-rule_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+RSpec.describe Clover, "cli pg delete-firewall-rule" do
+  before do
+    expect(Config).to receive(:postgres_service_project_id).and_return(@project.id).at_least(:once)
+  end
+
+  it "deletes the specified firewall rule for the database" do
+    cli(%w[pg eu-central-h1/test-pg create])
+    pg = PostgresResource.first
+    fwr = pg.firewall_rules_dataset.first
+    expect(cli(%w[pg eu-central-h1/test-pg delete-firewall-rule a/b], status: 400)).to eq "invalid firewall rule id format"
+    expect(pg.firewall_rules_dataset).not_to be_empty
+    expect(cli(%W[pg eu-central-h1/test-pg delete-firewall-rule #{fwr.ubid}])).to eq "Firewall rule, if it exists, has been scheduled for deletion"
+    expect(pg.firewall_rules_dataset).to be_empty
+  end
+end
