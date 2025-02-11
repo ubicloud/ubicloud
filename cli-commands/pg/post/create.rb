798488f0abb096e17cf42314a576eb44f6e31bc1
Jeremy Evans <code@jeremyevans.net>
2025-02-10 17:36:03 -0800

    Fix typo in cli pg create -f option description
    Co-authored-by: Daniel Farina <daniel@ubicloud.com>

--- cli-commands/pg/post/create.rb
+++ cli-commands/pg/post/create.rb
@@ -2,7 +2,7 @@
 
 UbiRodish.on("pg").run_on("create") do
   options("ubi pg location/pg_name create [options]", key: :pg_create) do
-    on("-f", "--flavor=type", "flavor (standard, paradedb, latern)")
+    on("-f", "--flavor=type", "flavor (standard, paradedb, lantern)")
     on("-h", "--ha_type=type", "replication type (none, async, sync)")
     on("-s", "--size=size", "server size (standard-{2,4,8,16,30,60})")
     on("-S", "--storage_size=size", "storage size GB (64, 128, 256)")
