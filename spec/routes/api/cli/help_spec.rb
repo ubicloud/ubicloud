38604b3b6c407c5ae67de391412b68d8404dc154
Jeremy Evans <jeremy@ubicloud.com>
2025-02-13 12:00:00 -0800

    Add cli help command
    The help command shows usage for the top command or named subcommand:
    
    ```
    $ bin/ubi help
    Usage: ubi [options] [subcommand [subcommand_options] ...]
    
    Options:
            --version                    show program version
            --help                       show program help
            --confirm=confirmation       confirmation value (not for direct use)
    
    Subcommands: help pg vm
    
    $ bin/ubi help help
    Usage: ubi help [options] [command [subcommand ...]]
    
    Options:
        -r, --recursive                  also show documentation for all subcommands of command
        -u, --usage                      only show usage
    
    $ bin/ubi help vm ssh
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    ```
    
    As shown, you can use the -r and/or -u options:
    
    ```
    $ bin/ubi help -u help
    Usage: ubi help [options] [command [subcommand ...]]
    
    $ bin/ubi help -r vm |head -n 3
    Usage: ubi vm subcommand [...]
    
    Subcommands: list
    
    $ bin/ubi help -r vm |tail -n 3
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    
    $ bin/ubi help -ru vm |head -n 3
    Usage: ubi vm subcommand [...]
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] subcommand [...]
    Usage: ubi vm list [options]
    
    $ bin/ubi help -ru vm |tail -n 3
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] sftp [sftp-opts])
    Usage: ubi vm location/(vm-name|_vm-ubid) show [options]
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    ```
    
    Using -ru is useful for seeing cases where usage is not consistent.
    Those cases will be fixed in a subsequent commit.

---
+++ spec/routes/api/cli/help_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require_relative "spec_helper"
+
+RSpec.describe Clover, "cli help" do
+  it "shows help for top level command with no arguments" do
+    expect(cli(%w[help])).to eq <<~OUTPUT
+      Usage: ubi [options] [subcommand [subcommand_options] ...]
+
+      Options:
+              --version                    show program version
+              --help                       show program help
+              --confirm=confirmation       confirmation value (not for direct use)
+
+      Subcommands: help pg vm
+    OUTPUT
+  end
+
+  it "shows help for specific command if given" do
+    expect(cli(%w[help help])).to eq <<~OUTPUT
+      Usage: ubi help [options] [command [subcommand ...]]
+
+      Options:
+          -r, --recursive                  also show documentation for all subcommands of command
+          -u, --usage                      only show usage
+    OUTPUT
+  end
+
+  it "shows help for specific subcommand if given" do
+    expect(cli(%w[help vm ssh])).to eq <<~OUTPUT
+      Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
+    OUTPUT
+  end
+
+  it "shows only usage if the -u flag is given" do
+    expect(cli(%w[help -u help])).to eq <<~OUTPUT
+      Usage: ubi help [options] [command [subcommand ...]]
+    OUTPUT
+  end
+
+  it "shows help for all subcommands of command if -r is given" do
+    expect(cli(%w[help -r vm])).to include <<~OUTPUT
+      Usage: ubi vm location/vm_name create [options] public_key
+
+      Options:
+          -6, --ipv6-only                  do not enable IPv4
+    OUTPUT
+  end
+
+  it "shows usage for all subcommands of command if -ru is given" do
+    expect(cli(%w[help -ru vm])).to include <<~OUTPUT
+      Usage: ubi vm list [options]
+      Usage: ubi vm location/vm_name create [options] public_key
+    OUTPUT
+  end
+
+  it "shows error and help for top level if top level command invalid" do
+    expect(cli(%w[help foo], status: 400)).to eq <<~OUTPUT
+      invalid command: foo
+
+      Usage: ubi [options] [subcommand [subcommand_options] ...]
+
+      Options:
+              --version                    show program version
+              --help                       show program help
+              --confirm=confirmation       confirmation value (not for direct use)
+
+      Subcommands: help pg vm
+    OUTPUT
+  end
+
+  it "shows error and help for nested command if there is a partial match" do
+    expect(cli(%w[help vm ssh foo], status: 400)).to eq <<~OUTPUT
+      invalid command: vm ssh foo
+
+      Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
+    OUTPUT
+  end
+end
