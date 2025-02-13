8c6a6ec401216275dfab10bc8aac31c45b36b5ff
Jeremy Evans <jeremy@ubicloud.com>
2025-02-13 12:00:00 -0800

    Add cli help command
    The help command shows usage for the top command or named subcommand:
    
    ```
    $ bin/ubi help
    Usage: ubi [options] [subcommand [subcommand_options] ...]
    
    Options:
            --version                    show program version
            --help                       show program help
            --confirm=confirmation       confirmation value (not for direct use)
    
    Subcommands: help pg vm
    
    $ bin/ubi help help
    Usage: ubi help [options] [command [subcommand ...]]
    
    Options:
        -r, --recursive                  also show documentation for all subcommands of command
        -u, --usage                      only show usage
    
    $ bin/ubi help vm ssh
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    ```
    
    As shown, you can use the -r and/or -u options:
    
    ```
    $ bin/ubi help -u help
    Usage: ubi help [options] [command [subcommand ...]]
    
    $ bin/ubi help -r vm |head -n 3
    Usage: ubi vm subcommand [...]
    
    Subcommands: list
    
    $ bin/ubi help -r vm |tail -n 3
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    
    $ bin/ubi help -ru vm |head -n 3
    Usage: ubi vm subcommand [...]
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] subcommand [...]
    Usage: ubi vm list [options]
    
    $ bin/ubi help -ru vm |tail -n 3
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] sftp [sftp-opts])
    Usage: ubi vm location/(vm-name|_vm-ubid) show [options]
    Usage: ubi vm location-name/(vm-name|_vm-ubid) [options] ssh [ssh-options --] [ssh-args]
    ```
    
    Using -ru is useful for seeing cases where usage is not consistent.
    Those cases will be fixed in a subsequent commit.

---
+++ cli-commands/help.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+UbiRodish.on("help") do
+  options("ubi help [options] [command [subcommand ...]]") do
+    on("-r", "--recursive", "also show documentation for all subcommands of command")
+    on("-u", "--usage", "only show usage")
+  end
+
+  args(0..)
+
+  run do |argv, opts|
+    orig_command = command = UbiRodish.command
+
+    argv.each do |arg|
+      break unless (command = command.subcommand(arg) || command.post_subcommand(arg))
+      orig_command = command
+    end
+
+    usage = opts[:usage]
+    if command
+      if opts[:recursive]
+        body = []
+        command.each_subcommand do |_, cmd|
+          if usage
+            cmd.option_parsers.each do |op|
+              body << op.banner << "\n"
+            end
+          else
+            body << cmd.options_text << "\n\n"
+          end
+        end
+        response(body)
+      elsif usage
+        body = []
+        command.option_parsers.each do |op|
+          body << op.banner << "\n"
+        end
+        response(body)
+      else
+        response(command.options_text)
+      end
+    else
+      orig_command.raise_failure("invalid command: #{argv.join(" ")}")
+    end
+  end
+end
