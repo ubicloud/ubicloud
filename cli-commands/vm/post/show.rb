df69ccdb1ef771f02e658d53a28eae152cc83f51
Jeremy Evans <jeremy@ubicloud.com>
2025-02-14 10:09:08 -0800

    Rely on options hash always being set when using :key option

--- cli-commands/vm/post/show.rb
+++ cli-commands/vm/post/show.rb
@@ -13,15 +13,10 @@ UbiRodish.on("vm").run_on("show") do
 
   run do |opts|
     get(project_path("location/#{@location}/vm/#{@name}")) do |data|
-      keys = fields
-      firewall_keys = firewall_fields
-      firewall_rule_keys = firewall_rule_fields
-
-      if (opts = opts[:vm_show])
-        keys = check_fields(opts[:fields], fields, "vm show -f option")
-        firewall_keys = check_fields(opts[:"firewall-fields"], firewall_fields, "vm show -w option")
-        firewall_rule_keys = check_fields(opts[:"rule-fields"], firewall_rule_fields, "vm show -r option")
-      end
+      opts = opts[:vm_show]
+      keys = check_fields(opts[:fields], fields, "vm show -f option")
+      firewall_keys = check_fields(opts[:"firewall-fields"], firewall_fields, "vm show -w option")
+      firewall_rule_keys = check_fields(opts[:"rule-fields"], firewall_rule_fields, "vm show -r option")
 
       body = []
 
