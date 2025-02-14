f9161bfc7f3fb9f2702c91ca4c88b8d2b52fc7e8
Jeremy Evans <jeremy@ubicloud.com>
2025-02-13 19:55:50 -0800

    Add a golden files approach to testing the cli
    Each golden file filename is in the format:
    
    * status-cmd line.txt
    * status[confirm_prompt:prompt]-cmd line.txt
    * status[command_execute:command]-cmd line.txt
    
    Command line is mangled with `_` -> `/`, and then `pg/dump` -> `pg_dump`.
    
    It was substantial work to set up the necessary mocking of ids to
    get consistent output.
    
    If the golden files spec fails, it tells you were you can see the
    differences, and it doesn't delete the recorded files from
    spec/routes/api/cli/spec-output-files, so if you are updating and
    the changes are expected, you can copy the files to
    spec/routes/api/cli/golden-files and commit them.
    
    This handles all cli commands, and most commands tests both the success
    and failure cases. The pg failover success case is not tested as that
    needs special setup.

---
+++ spec/routes/api/cli/golden_files_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require_relative "spec_helper"
+require "open3"
+
+RSpec.describe Clover, "cli" do
+  it "produces expected output for commands" do
+    golden_file_dir = "spec/routes/api/cli/golden-files"
+    output_dir = "spec/routes/api/cli/spec-output-files"
+    diff_file = "cli-golden-files.diff"
+    Dir.mkdir(output_dir) unless File.directory?(output_dir)
+
+    postgres_project = Project.create(name: "postgres")
+    expect(Config).to receive(:postgres_service_project_id).and_return(postgres_project.id).at_least(:once)
+    expect(Vm).to receive(:generate_ubid).and_return(UBID.parse("vmdzyppz6j166jh5e9t2dwrfas"))
+    expect(PrivateSubnet).to receive(:generate_ubid).and_return(UBID.parse("psfzm9e26xky5m9ggetw4dpqe2"))
+    expect(Firewall).to receive(:generate_uuid).and_return("24242d92-217b-85fc-b891-7046af3c1150")
+    expect(FirewallRule).to receive(:generate_uuid).and_return("51e5bc7d-245b-8df8-bf91-7c5d150cb160", "3b367895-7f18-89f8-a295-ff247e9d5192")
+    expect(PostgresResource).to receive(:generate_uuid).and_return("dd0375a6-1c66-82d0-a5e8-af1e8527a8a2")
+    expect(PostgresFirewallRule).to receive(:generate_uuid).and_return("5a601238-b56e-8ecf-bbca-9e3e680812b8")
+    expect(PostgresMetricDestination).to receive(:generate_uuid).and_return("1682bcf3-37f8-81da-a1e4-7ac9f25d9200")
+    cli(%w[vm eu-central-h1/test-vm create a])
+    @vm = Vm.first
+    add_ipv4_to_vm(@vm, "128.0.0.1")
+    @vm.nics.first.update(private_ipv4: "10.67.141.133/32", private_ipv6: "fda0:d79a:93e7:d4fd:1c2::0/80")
+
+    expect(Config).to receive(:postgres_service_hostname).and_return("pg.example.com").at_least(:once)
+    @dns_zone = DnsZone.new
+    expect(Vm).to receive(:generate_ubid).and_return(UBID.parse("vma9rnygexga6jns6x3yj9a6b2"))
+    expect(Prog::Postgres::PostgresResourceNexus).to receive(:dns_zone).and_return(@dns_zone).at_least(:once)
+    expect(PrivateSubnet).to receive(:generate_ubid).and_return(UBID.parse("psnqtahcasrj1hn16kh1ygekmn"))
+    expect(Firewall).to receive(:generate_uuid).and_return("30a3eec9-afb5-81fc-bbb5-8691d252ef03")
+    expect(FirewallRule).to receive(:generate_uuid).and_return("305d838d-a3cd-85f8-aa08-9a66e71a5877", "5aa5b086-37bd-81f8-8d03-dd4b0e09a436", "20c360fa-bc06-8df8-b067-33f4a1ebdbbd", "2b628450-25bd-8df8-8b42-fb5cc5d01ad1", "da42e2ef-b5f1-8df8-966d-1387afb1b2f4")
+
+    cli(%w[pg eu-central-h1/test-pg create])
+    cli(%w[pg eu-central-h1/test-pg reset-superuser-password bar456FOO123])
+    cli(%w[pg eu-central-h1/test-pg add-metric-destination foo bar https://baz.example.com])
+
+    expect(Vm).to receive(:generate_ubid).and_return(UBID.parse("vmz7b0dxt40t4g7rnmag9hct7c")).at_least(:once)
+    expect(PrivateSubnet).to receive(:generate_ubid).and_return(UBID.parse("ps9a8v5tm1020qn73f0c7db0x7")).at_least(:once)
+    fw_uuids = %w[2b4ae5cf-1aac-8dfc-bc80-c87e3e381e10 f5e6cb31-e580-81fc-88d6-a379f13494bf].cycle
+    expect(Firewall).to receive(:generate_uuid).and_invoke(-> { fw_uuids.next }).at_least(:once)
+    fwr_uuids = %w[c81cb3d1-81bc-89f8-96c8-6b4e8d4375bd e3fdf2f2-2603-85f8-b0f9-3fc2c28636cd 99785615-0cc7-8df8-a937-4f1d26b620c8 168054f0-e069-89f8-b12a-e4b010cf47b5 6d590d38-5f88-89f8-b6d8-0a079e1c61b6].cycle
+    expect(FirewallRule).to receive(:generate_uuid).and_invoke(-> { fwr_uuids.next }).at_least(:once)
+    expect(PostgresResource).to receive(:generate_uuid).and_return("97eb0a77-7869-86d0-9dcb-a46416ddc5c9").at_least(:once)
+    expect(PostgresFirewallRule).to receive(:generate_uuid).and_return("6d674a31-e1c1-8ecf-b5ac-363abb5b9185").at_least(:once)
+    expect(PostgresMetricDestination).to receive(:generate_uuid).and_return("bc563e43-9b83-89da-b3ac-d38acc87fd63").at_least(:once)
+
+    Dir["#{golden_file_dir}/*.txt"].each do |f|
+      f = File.basename(f)
+      cmd = f.delete_suffix(".txt")
+      status, cmd = cmd.split("-", 2)
+      cmd.tr!("_", "/")
+      cmd.gsub!("pg/dump", "pg_dump")
+      kws = {}
+      if (kw = status[/\[(\w+:\w+)\]/, 1])
+        status.sub!(/\[(\w+:\w+)\]/, "")
+        kw, kw_value = kw.split(":", 2)
+        kw = kw.to_sym
+        kws[kw] = kw_value
+      end
+      kws[:status] = Integer(status)
+      body = DB.transaction(savepoint: true, rollback: :always) do
+        cli(cmd.split, **kws)
+      end
+      File.write(File.join(output_dir, f), body)
+    end
+
+    diff, = Open3.capture2e("diff", "-u", golden_file_dir, output_dir)
+    output_matches_golden_files = diff.empty?
+
+    if diff.empty?
+      File.delete(diff_file) if File.file?(diff_file)
+    else
+      File.write(diff_file, diff)
+    end
+
+    expect(output_matches_golden_files).to be_truthy, "differences are in #{diff_file}"
+
+    # Only clear output directory on success
+    Dir["#{output_dir}/*.txt"].each do |f|
+      File.delete(f)
+    end
+    Dir.rmdir(output_dir)
+  end
+end
