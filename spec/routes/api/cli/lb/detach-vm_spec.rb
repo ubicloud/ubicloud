939d85bae886bb9bb46c352406df518f7e6a88bc
Jeremy Evans <jeremy@ubicloud.com>
2025-02-19 12:29:35 -0800

    Add cli lb detach-vm command

---
+++ spec/routes/api/cli/lb/detach-vm_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+RSpec.describe Clover, "cli lb attach-vm" do
+  before do
+    cli(%w[vm eu-central-h1/test-vm create a])
+    @vm = Vm.first
+    cli(%w[ps eu-central-h1/test-ps create])
+    @ps = PrivateSubnet.first
+    cli(%W[lb eu-central-h1/test-lb create #{@ps.ubid} 12345 54321])
+    cli(%W[lb eu-central-h1/test-lb attach-vm #{@vm.ubid}])
+    @lb = LoadBalancer.first
+  end
+
+  it "detaches VM from load balancer" do
+    expect(@lb.load_balancers_vms_dataset.select_map(:state)).to eq ["down"]
+    expect(cli(%W[lb eu-central-h1/test-lb detach-vm #{@vm.ubid}])).to eq "Detached VM with id #{@vm.ubid} from load balancer with id #{@lb.ubid}"
+    expect(@lb.load_balancers_vms_dataset.select_map(:state)).to eq ["detaching"]
+  end
+end
