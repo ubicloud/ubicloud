a0c1e4698131a2e8370fd7a50f498c2c62cbb01a
Jeremy Evans <jeremy@ubicloud.com>
2025-02-19 12:29:35 -0800

    Add cli lb detach-vm command

---
+++ cli-commands/lb/post/detach-vm.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+UbiCli.on("lb").run_on("detach-vm") do
+  options("ubi lb location/(lb-name|_lb-ubid) detach-vm vm-id")
+
+  args 1
+
+  run do |vm_id|
+    post(lb_path("/detach-vm"), "vm_id" => vm_id) do |data|
+      ["Detached VM with id #{vm_id} from load balancer with id #{data["id"]}"]
+    end
+  end
+end
