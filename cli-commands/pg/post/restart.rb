babcd5dbdc22c0245284849fd058b52305b83378
Jeremy Evans <jeremy@ubicloud.com>
2025-02-21 11:39:04 -0800

    Add cli pg restart command
    This was not previously supported in the api, so add api support
    so the cli can use it.

---
+++ cli-commands/pg/post/restart.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+UbiCli.on("pg").run_on("restart") do
+  options("ubi pg location/(pg-name|_pg-ubid) restart")
+
+  run do
+    post(pg_path("/restart")) do |data|
+      ["Scheduled restart of PostgreSQL database with id #{data["id"]}"]
+    end
+  end
+end
