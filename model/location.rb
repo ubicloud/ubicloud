22f325481ebe0e629da60c72777987709878b26b
Furkan Sahin <furkan@ubicloud.com>
2025-02-28 15:28:24 +0100

    Make location references a foreign key, create indexes - Mig 1
    This commit adds the location foreign_keys to all of the models that
    uses location. This way, we will not be matching the text values but
    make use of the database relationship.
    We are refilling the location_ids based on the location column of the
    entities.
    
    We are also adding a new index on project_id, location_id, name because we
    already had an index on project_id, location, name and it will be
    dropped once the location column is dropped.

--- model/location.rb
+++ model/location.rb
@@ -18,3 +18,13 @@ end
 #  location_pkey | PRIMARY KEY btree (id)
 # Foreign key constraints:
 #  location_provider_fkey | (provider) REFERENCES provider(name)
+# Referenced By:
+#  firewall           | firewall_location_id_fkey           | (location_id) REFERENCES location(id)
+#  inference_endpoint | inference_endpoint_location_id_fkey | (location_id) REFERENCES location(id)
+#  kubernetes_cluster | kubernetes_cluster_location_id_fkey | (location_id) REFERENCES location(id)
+#  minio_cluster      | minio_cluster_location_id_fkey      | (location_id) REFERENCES location(id)
+#  postgres_resource  | postgres_resource_location_id_fkey  | (location_id) REFERENCES location(id)
+#  private_subnet     | private_subnet_location_id_fkey     | (location_id) REFERENCES location(id)
+#  vm                 | vm_location_id_fkey                 | (location_id) REFERENCES location(id)
+#  vm_host            | vm_host_location_id_fkey            | (location_id) REFERENCES location(id)
+#  vm_pool            | vm_pool_location_id_fkey            | (location_id) REFERENCES location(id)
