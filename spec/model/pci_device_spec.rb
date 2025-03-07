63be63c68c17e99eab371d572563f612e2295e67
Benjamin Satzger <ben@ubicloud.com>
2025-03-07 14:27:45 +0100

    Get name of PCI device
    This commit adds new methods to get the name of known PCI devices.
    
    For NVIDIA devices, the mapping of pci ids to names can be found at
    https://download.nvidia.com/XFree86/Linux-x86_64/535.98/README/supportedchips.html

--- spec/model/pci_device_spec.rb
+++ spec/model/pci_device_spec.rb
@@ -17,4 +17,10 @@ RSpec.describe PciDevice do
     d = described_class.new(device_class: "0403")
     expect(d.is_gpu).to be_falsy
   end
+
+  it "returns correct names" do
+    expect(described_class.new(device_class: "0302", vendor: "10de", device: "20b5").name).to equal("NVIDIA A100 80GB PCIe")
+    expect(described_class.new(device_class: "0300", vendor: "10de", device: "27b0").name).to equal("NVIDIA RTX 4000 SFF Ada Generation")
+    expect(described_class.new(device_class: "0302", vendor: "10de", device: "????").name).to equal("PCI device")
+  end
 end
