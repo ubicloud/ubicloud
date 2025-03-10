3d704dfee0381e255c42f23f1be15667882e777f
Jeremy Evans <jeremy@ubicloud.com>
2025-03-10 09:07:11 -0700

    Remove unnecessary join table for MinioCluster servers associations
    Having the associated model's table as a join table is almost always
    wrong unless the table contains self-referential foreign keys.
    
    While here, use the :order option instead of an association block,
    and drop unnecessary option.

--- model/minio/minio_cluster.rb
+++ model/minio/minio_cluster.rb
@@ -4,14 +4,10 @@ require_relative "../../model"
 
 class MinioCluster < Sequel::Model
   many_to_one :project
-  one_to_many :pools, key: :cluster_id, class: :MinioPool do |ds|
-    ds.order(:start_index)
-  end
-  many_through_many :servers, [[:minio_pool, :cluster_id, :id], [:minio_server, :minio_pool_id, :id]], class: :MinioServer do |ds|
-    ds.order(:index)
-  end
+  one_to_many :pools, key: :cluster_id, class: :MinioPool, order: :start_index
+  many_to_many :servers, join_table: :minio_pool, left_key: :cluster_id, right_key: :id, right_primary_key: :minio_pool_id, class: :MinioServer, order: :index
   one_to_one :strand, key: :id
-  many_to_one :private_subnet, key: :private_subnet_id
+  many_to_one :private_subnet
 
   include ResourceMethods
   include SemaphoreMethods
