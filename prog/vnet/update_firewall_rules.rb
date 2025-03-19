4fb981266fe30df0509f22c017db5b50868c91d2
Furkan Sahin <furkan@ubicloud.com>
2025-03-19 17:29:16 +0100

    Update firewall rules in AWS
    Since we do not have access to the host and nftables there, AWS
    firewalls need to be handled with the use of APIs. Therefore, we are
    introducing new states to modify the records in the security group.

--- prog/vnet/update_firewall_rules.rb
+++ prog/vnet/update_firewall_rules.rb
@@ -10,6 +10,10 @@ class Prog::Vnet::UpdateFirewallRules < Prog::Base
   end
 
   label def update_firewall_rules
+    if vm.location.provider == "aws"
+      hop_update_aws_firewall_rules
+    end
+
     rules = vm.firewalls.map(&:firewall_rules).flatten
     allowed_ingress_ip4_port_set, allowed_ingress_ip4_lb_dest_set = consolidate_rules(rules.select { !_1.ip6? && _1.port_range })
     allowed_ingress_ip6_port_set, allowed_ingress_ip6_lb_dest_set = consolidate_rules(rules.select { _1.ip6? && _1.port_range })
@@ -174,6 +178,86 @@ table inet fw_table {
 TEMPLATE
 
     pop "firewall rule is added"
+  end
+
+  label def update_aws_firewall_rules
+    rules = vm.firewalls.flat_map(&:firewall_rules)
+    permissions = rules.select(&:port_range).map! do |rule|
+      perm = {
+        ip_protocol: "tcp",
+        from_port: rule.port_range.begin,
+        to_port: rule.port_range.end - 1
+      }
+      if rule.ip6?
+        perm[:ipv_6_ranges] = [{cidr_ipv_6: rule.cidr.to_s}]
+      else
+        perm[:ip_ranges] = [{cidr_ip: rule.cidr.to_s}]
+      end
+      perm
+    end
+
+    permissions.each do |perm|
+      aws_client.authorize_security_group_ingress({
+        group_id: vm.private_subnets.first.private_subnet_aws_resource.security_group_id,
+        ip_permissions: [perm]
+      })
+    rescue Aws::EC2::Errors::InvalidPermissionDuplicate
+      next
+    end
+
+    hop_remove_aws_old_rules
+  end
+
+  label def remove_aws_old_rules
+    rules = vm.firewalls.map(&:firewall_rules).flatten
+    ip4_rules = rules.select { !_1.ip6? && _1.port_range }
+    ip6_rules = rules.select { _1.ip6? && _1.port_range }
+
+    # Fetch existing security group rules
+    security_group = aws_client.describe_security_groups({
+      group_ids: [vm.private_subnets.first.private_subnet_aws_resource.security_group_id]
+    }).security_groups.first
+
+    # Remove existing rules that aren't in our current rules list
+    permissions_to_revoke = security_group.ip_permissions.select do |permission|
+      permission.ip_protocol == "tcp" && # Skip if not TCP for now
+        (ip4_rules.none? { |r|
+          r.cidr.to_s == permission.ip_ranges.first&.cidr_ip &&
+            r.port_range.begin == permission.from_port &&
+            r.port_range.end - 1 == permission.to_port
+        } &&
+        ip6_rules.none? { |r|
+          r.cidr.to_s == permission.ipv_6_ranges.first&.cidr_ipv_6 &&
+            r.port_range.begin == permission.from_port &&
+            r.port_range.end - 1 == permission.to_port
+        })
+    end.map! do |permission|
+      perm = {
+        ip_protocol: "tcp",
+        from_port: permission.from_port,
+        to_port: permission.to_port
+      }
+      perm[:ip_ranges] = permission.ip_ranges if permission.ip_ranges.any?
+      perm[:ipv_6_ranges] = permission.ipv_6_ranges if permission.ipv_6_ranges.any?
+      perm
+    end
+
+    if permissions_to_revoke.any?
+      permissions_to_revoke.each do |perm|
+        aws_client.revoke_security_group_ingress({
+          group_id: vm.private_subnets.first.private_subnet_aws_resource.security_group_id,
+          ip_permissions: [perm]
+        })
+      rescue Aws::EC2::Errors::InvalidPermissionNotFound
+        next
+      end
+    end
+
+    pop "firewall rule is added"
+  end
+
+  def aws_client
+    @aws_client ||= vm.location.location_credential.client
   end
 
   def generate_globally_blocked_lists
