a7f78e879b2e6601ecdb7c642647ad3f07efe750
Enes Cakir <enes@ubicloud.com>
2025-03-24 21:15:41 +0300

    Fix a test on the main
    I merged https://github.com/ubicloud/ubicloud/pull/3023 without
    rebasing.
    
    This caused a failure in the test suite on the main branch.
    
    We made significant changes to the location logic.

--- spec/prog/test/github_runner_spec.rb
+++ spec/prog/test/github_runner_spec.rb
@@ -102,7 +102,7 @@ RSpec.describe Prog::Test::GithubRunner do
     it "waits vm pools to be destroyed" do
       expect(client).to receive(:workflow_runs).with("ubicloud/github-e2e-test-workflows", "test_2204.yml", {branch: "main"}).and_return({workflow_runs: [{id: 10}]})
       expect(client).to receive(:cancel_workflow_run).with("ubicloud/github-e2e-test-workflows", 10)
-      pool = Prog::Vm::VmPool.assemble(size: 1, vm_size: "standard-2", location: "hetzner-fsn1", boot_image: "github-ubuntu-2204", storage_size_gib: 86, storage_encrypted: true,
+      pool = Prog::Vm::VmPool.assemble(size: 1, vm_size: "standard-2", location_id: Location::HETZNER_FSN1_ID, boot_image: "github-ubuntu-2204", storage_size_gib: 86, storage_encrypted: true,
         storage_skip_sync: false, arch: "x64").subject
       expect(VmPool).to receive(:[]).and_return(pool)
       expect { gr_test.clean_resources }.to nap(15)
