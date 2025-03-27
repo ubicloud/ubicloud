cb1bb2d1a0a92fd3f0f23a3085e55e05550b1a7c
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-03-27 16:25:13 +0100

    Disallow access to pods GUA ips from internet
    So far pods had both ULA and GUA ipv6 ips and the GUA ip was accessible from
    the internet. with this change, we add nft rules to each vm to only allow
    connections to be made from inside the pod and disallow all new connections
    coming to the GUA ip address.
    
    This change is critical to not expose pods to the internet.

--- prog/kubernetes/provision_kubernetes_node.rb
+++ prog/kubernetes/provision_kubernetes_node.rb
@@ -70,6 +70,27 @@ class Prog::Kubernetes::ProvisionKubernetesNode < Prog::Base
     nap 5 unless vm.strand.label == "wait"
 
     vm.sshable.cmd "sudo iptables-nft -t nat -A POSTROUTING -s #{vm.nics.first.private_ipv4} -o ens3 -j MASQUERADE"
+    vm.sshable.cmd("sudo nft --file -", stdin: <<TEMPLATE)
+table ip6 pod_access;
+delete table ip6 pod_access;
+table ip6 pod_access {
+  chain ingress_egress_control {
+    type filter hook forward priority filter; policy drop;
+    # allow access to the vm itself in order to not break the normal functionality of Clover and SSH
+    ip6 daddr #{vm.ephemeral_net6.nth(2)} ct state established,related,new counter accept
+    ip6 saddr #{vm.ephemeral_net6.nth(2)} ct state established,related,new counter accept
+
+    # not allow new connections from internet but allow new connections from inside
+    ip6 daddr #{vm.ephemeral_net6} ct state established,related counter accept
+    ip6 saddr #{vm.ephemeral_net6} ct state established,related,new counter accept
+
+    # used for internal private ipv6 communication between pods
+    ip6 saddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
+    ip6 daddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
+  }
+}
+TEMPLATE
+    vm.ephemeral_net6
     vm.sshable.cmd "sudo systemctl enable --now kubelet"
 
     bud Prog::BootstrapRhizome, {"target_folder" => "kubernetes", "subject_id" => vm.id, "user" => "ubi"}
