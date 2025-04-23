3a2741e80ab70e71c3e3aafad153cab87209cb67
Jeremy Evans <jeremy@ubicloud.com>
2025-04-23 11:14:03 -0700

    Log related response body when Excon::Error::HTTPStatus is raised

--- routes/runtime/github.rb
+++ routes/runtime/github.rb
@@ -8,7 +8,8 @@ class Clover
 
     begin
       repository.setup_blob_storage unless repository.access_key
-    rescue Excon::Error::HTTPStatus
+    rescue Excon::Error::HTTPStatus => ex
+      Clog.emit("Unable to setup blob storage") { {failed_blob_storage_setup: {ubid: runner.ubid, repository_ubid: repository.ubid, response: ex.response.body}} }
       fail CloverError.new(400, "InvalidRequest", "unable to setup blob storage")
     end
 
