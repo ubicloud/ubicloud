af9a6faa001c670171e5f8150a4a4aba65ff4126
Jeremy Evans <jeremy@ubicloud.com>
2025-04-24 13:41:49 -0700

    Allow API access to get/create/delete inference api keys

--- routes/project/inference_api_key.rb
+++ routes/project/inference_api_key.rb
@@ -2,30 +2,39 @@
 
 class Clover
   hash_branch(:project_prefix, "inference-api-key") do |r|
-    r.web do
-      r.get true do
+    r.is do
+      r.get do
         @inference_api_keys = Serializers::InferenceApiKey.serialize(inference_api_key_ds.all)
-        @remaining_free_quota = FreeQuota.remaining_free_quota("inference-tokens", @project.id)
-        @free_quota_unit = "inference tokens"
-        @has_valid_payment_method = @project.has_valid_payment_method?
-        view "inference/api_key/index"
+
+        if web?
+          @remaining_free_quota = FreeQuota.remaining_free_quota("inference-tokens", @project.id)
+          @free_quota_unit = "inference tokens"
+          @has_valid_payment_method = @project.has_valid_payment_method?
+          view "inference/api_key/index"
+        else
+          {items: @inference_api_keys.map { it.slice(:id, :key) }}
+        end
       end
 
-      r.post true do
+      r.post do
         authorize("InferenceApiKey:create", @project.id)
         iak = DB.transaction { ApiKey.create_inference_api_key(@project) }
-        flash["notice"] = "Created Inference API Key with id #{iak.ubid}. It may take a few minutes to sync."
-        r.redirect "#{@project.path}/inference-api-key"
-      end
-
-      r.delete String do |ubid|
-        if (iak = inference_api_key_ds.with_pk(UBID.to_uuid(ubid)))
-          authorize("InferenceApiKey:delete", iak.id)
-          iak.destroy
-          flash["notice"] = "Inference API Key deleted successfully"
+        if web?
+          flash["notice"] = "Created Inference API Key with id #{iak.ubid}. It may take a few minutes to sync."
+          r.redirect "#{@project.path}/inference-api-key"
+        else
+          {id: iak.ubid, key: iak.key}
         end
-        204
       end
+    end
+
+    r.delete String do |ubid|
+      if (iak = inference_api_key_ds.with_pk(UBID.to_uuid(ubid)))
+        authorize("InferenceApiKey:delete", iak.id)
+        iak.destroy
+        flash["notice"] = "Inference API Key deleted successfully" if web?
+      end
+      204
     end
   end
 end
