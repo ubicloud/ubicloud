045837597c060ddd5c73fd7634a77f25ea9771c5
Jeremy Evans <jeremy@ubicloud.com>
2025-04-24 16:16:52 -0700

    Add cli ai api-key commands
    The following commands are supported:
    
    * ai api-key list
    * ai api-key create
    * ai api-key api-key-id show
    * ai api-key api-key-id destroy
    
    To get the golden files spec passing, add ApiKey.random_key, so it can
    be overridden to provide a consistent key for the specs.

---
+++ cli-commands/ai/api-key/post/show.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+UbiCli.on("ai", "api-key").run_on("show") do
+  desc "Show details for an inference api key"
+
+  banner "ubi ai api-key api-key-id show"
+
+  run do
+    iak = @sdk_object
+    body = []
+    body << "id: " << iak.id << "\n"
+    body << "key: " << iak.key << "\n"
+    response(body)
+  end
+end
