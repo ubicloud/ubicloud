6a2009638fdb6f9d69205be904d243efb5dc26d5
Jeremy Evans <jeremy@ubicloud.com>
2025-04-25 15:45:36 -0700

    Support cloud-hypervisor 45.0 in Prog::DownloadCloudHypervisor

--- prog/download_cloud_hypervisor.rb
+++ prog/download_cloud_hypervisor.rb
@@ -38,14 +38,18 @@ class Prog::DownloadCloudHypervisor < Prog::Base
     nap 15
   end
 
-  def sha_256(bin)
-    hashes = {
-      ["ch-bin", "x64", "35.1"] => "e8426b0733248ed559bea64eb04d732ce8a471edc94807b5e2ecfdfc57136ab4",
-      ["ch-remote", "x64", "35.1"] => "337bd88183f6886f1c7b533499826587360f23168eac5aabf38e6d6b977c93b0",
-      ["ch-bin", "arm64", "35.1"] => "071a0b4918565ce81671ecd36d65b87351c85ea9ca0fbf73d4a67ec810efe606",
-      ["ch-remote", "arm64", "35.1"] => "355cdb1e2af7653a15912c66f7c76c922ca788fd33d77f6f75846ff41278e249"
-    }
+  HASHES = {
+    ["ch-bin", "x64", "35.1"] => "e8426b0733248ed559bea64eb04d732ce8a471edc94807b5e2ecfdfc57136ab4",
+    ["ch-remote", "x64", "35.1"] => "337bd88183f6886f1c7b533499826587360f23168eac5aabf38e6d6b977c93b0",
+    ["ch-bin", "arm64", "35.1"] => "071a0b4918565ce81671ecd36d65b87351c85ea9ca0fbf73d4a67ec810efe606",
+    ["ch-remote", "arm64", "35.1"] => "355cdb1e2af7653a15912c66f7c76c922ca788fd33d77f6f75846ff41278e249",
+    ["ch-bin", "x64", "45.0"] => "362d42eb464e2980d7b41109a214f8b1518b4e1f8e7d8c227b67c19d4581c250",
+    ["ch-remote", "x64", "45.0"] => "11a050087d279f9b5860ddbf2545fda43edf93f9b266440d0981932ee379c6ec",
+    ["ch-bin", "arm64", "45.0"] => "3a8073379d098817d54f7c0ab25a7734b88b070a98e5d820ab39e244b35b5e5e",
+    ["ch-remote", "arm64", "45.0"] => "a4b736ce82f5e2fc4a92796a9a443f243ef69f4970dad1e1772bd841c76c3301"
+  }
 
-    hashes[[bin, vm_host.arch, version]]
+  def sha_256(bin)
+    HASHES[[bin, vm_host.arch, version]]
   end
 end
