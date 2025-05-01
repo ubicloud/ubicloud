4e2b96c0da1ecd8034e48120951b5d545a4014db
Jeremy Evans <jeremy@ubicloud.com>
2025-05-01 15:12:08 -0700

    Fix the specs to avoid defining methods on Object
    Defining methods inside RSpec.configure blocks still defines them on
    Object.  Use config.include with a module to define these only in the
    specs and not globally.
    
    For matcher methods, define them in RSpec::Matchers::DSL::Matcher.
    Change rubocop config to allow this inside of the RSpec.configure
    blocks (technically, any constant inside any block).
    
    Add an Object.method_added hook that raises an exception if defining
    a method on object, other than Nokogiri, which is defined by the
    nokogiri gem. This will result in the specs raising an error without
    executing anything if you attempt to define a method on Object.

--- spec/routes/api/spec_helper.rb
+++ spec/routes/api/spec_helper.rb
@@ -3,21 +3,23 @@
 require_relative "../spec_helper"
 
 RSpec.configure do |config|
-  def login_api
-    @use_pat = true
-  end
-
-  def project_with_default_policy(account, name: "project-1")
-    project = account.create_project_with_default_policy(name)
-
-    if @use_pat
-      @pat = account.api_keys.first || ApiKey.create_personal_access_token(account, project:)
-      header "Authorization", "Bearer pat-#{@pat.ubid}-#{@pat.key}"
-      SubjectTag.first(project_id: project.id, name: "Admin").add_subject(@pat.id)
+  config.include(Module.new do
+    def login_api
+      @use_pat = true
     end
 
-    project
-  end
+    def project_with_default_policy(account, name: "project-1")
+      project = account.create_project_with_default_policy(name)
+
+      if @use_pat
+        @pat = account.api_keys.first || ApiKey.create_personal_access_token(account, project:)
+        header "Authorization", "Bearer pat-#{@pat.ubid}-#{@pat.key}"
+        SubjectTag.first(project_id: project.id, name: "Admin").add_subject(@pat.id)
+      end
+
+      project
+    end
+  end)
 
   config.define_derived_metadata(file_path: %r{\A\./spec/routes/api/}) do |metadata|
     metadata[:clover_api] = true
--- spec/routes/spec_helper.rb
+++ spec/routes/spec_helper.rb
@@ -11,45 +11,28 @@ TEST_USER_PASSWORD = "Secret@Password123"
 TEST_LOCATION = "eu-central-h1"
 
 RSpec.configure do |config|
-  include Rack::Test::Methods
+  config.include Rack::Test::Methods
 
-  def app
-    Clover.app
-  end
+  class RSpec::Matchers::DSL::Matcher
+    def self.error_response_matcher(expected_state, expected_message, expected_details, nested_error)
+      message_path = nested_error ? ["error", "message"] : ["message"]
+      details_path = nested_error ? ["error", "details"] : ["details"]
 
-  def last_response
-    lr = super
-
-    if lr && !lr.instance_variable_get(:@body_checked)
-      lr.instance_variable_set(:@body_checked, true)
-      if (match = lr.body.match(/(?<=(.{50}\W))(et[a-z0-9]{24})(?=(\W.{50}))/m))
-        unless match[1].match?(/otp_raw_secret|csrf/)
-          raise "response body contains TYPE_ETC ubid: #{match.captures.inspect}"
-        end
+      match do |response|
+        return false if response.body.empty?
+        parsed_body = JSON.parse(response.body)
+        response.status == expected_state &&
+          (expected_message.nil? || parsed_body.dig(*message_path) == expected_message) &&
+          (expected_details.nil? || parsed_body.dig(*details_path) == expected_details)
       end
-    end
 
-    lr
-  end
-
-  def error_response_matcher(expected_state, expected_message, expected_details, nested_error)
-    message_path = nested_error ? ["error", "message"] : ["message"]
-    details_path = nested_error ? ["error", "details"] : ["details"]
-
-    match do |response|
-      return false if response.body.empty?
-      parsed_body = JSON.parse(response.body)
-      response.status == expected_state &&
-        (expected_message.nil? || parsed_body.dig(*message_path) == expected_message) &&
-        (expected_details.nil? || parsed_body.dig(*details_path) == expected_details)
-    end
-
-    failure_message do |response|
-      parsed_body = response.body.empty? ? {} : JSON.parse(response.body)
-      <<~MESSAGE
-        #{"expected: ".rjust(16)}#{expected_state}#{expected_message && " - #{expected_message}"}#{expected_details && " - #{expected_details}"}
-        #{"got: ".rjust(16)}#{response.status}#{expected_message && " - #{parsed_body.dig(*message_path)}"}#{expected_details && " - #{parsed_body.dig(*details_path)}"}
-      MESSAGE
+      failure_message do |response|
+        parsed_body = response.body.empty? ? {} : JSON.parse(response.body)
+        <<~MESSAGE
+          #{"expected: ".rjust(16)}#{expected_state}#{expected_message && " - #{expected_message}"}#{expected_details && " - #{expected_details}"}
+          #{"got: ".rjust(16)}#{response.status}#{expected_message && " - #{parsed_body.dig(*message_path)}"}#{expected_details && " - #{parsed_body.dig(*details_path)}"}
+        MESSAGE
+      end
     end
   end
 
@@ -61,40 +44,61 @@ RSpec.configure do |config|
     error_response_matcher(expected_state, expected_message, expected_details, false)
   end
 
-  def create_account(email = TEST_USER_EMAIL, password = TEST_USER_PASSWORD, with_project: true, enable_otp: false, enable_webauthn: false)
-    hash = Argon2::Password.new({
-      t_cost: 1,
-      m_cost: 5,
-      secret: Config.clover_session_secret
-    }).create(password)
-
-    account = Account.create_with_id(email: email, status_id: 2)
-    DB[:account_password_hashes].insert(id: account.id, password_hash: hash)
-    if enable_otp
-      DB[:account_otp_keys].insert(id: account.id, key: "oth555fnbrrfbi3nu2gksjxh63n2xofh")
-    end
-    if enable_webauthn
-      DB[:account_webauthn_keys].insert(account_id: account.id, webauthn_id: "mKH7k5", public_key: "public-key", sign_count: 1, name: "test_key")
+  config.include(Module.new do
+    def app
+      Clover.app
     end
 
-    account.create_project_with_default_policy("Default") if with_project
-    account
-  end
+    def last_response
+      lr = super
 
-  def create_private_location(project:)
-    loc = Location.create(
-      name: "us-east-1",
-      display_name: "aws-us-east-1",
-      ui_name: "aws-us-east-1",
-      visible: true,
-      provider: "aws",
-      project_id: project.id
-    )
+      if lr && !lr.instance_variable_get(:@body_checked)
+        lr.instance_variable_set(:@body_checked, true)
+        if (match = lr.body.match(/(?<=(.{50}\W))(et[a-z0-9]{24})(?=(\W.{50}))/m))
+          unless match[1].match?(/otp_raw_secret|csrf/)
+            raise "response body contains TYPE_ETC ubid: #{match.captures.inspect}"
+          end
+        end
+      end
 
-    LocationCredential.create(
-      access_key: "access-key-id",
-      secret_key: "secret-access-key"
-    ) { it.id = loc.id }
-    loc
-  end
+      lr
+    end
+
+    def create_account(email = TEST_USER_EMAIL, password = TEST_USER_PASSWORD, with_project: true, enable_otp: false, enable_webauthn: false)
+      hash = Argon2::Password.new({
+        t_cost: 1,
+        m_cost: 5,
+        secret: Config.clover_session_secret
+      }).create(password)
+
+      account = Account.create_with_id(email: email, status_id: 2)
+      DB[:account_password_hashes].insert(id: account.id, password_hash: hash)
+      if enable_otp
+        DB[:account_otp_keys].insert(id: account.id, key: "oth555fnbrrfbi3nu2gksjxh63n2xofh")
+      end
+      if enable_webauthn
+        DB[:account_webauthn_keys].insert(account_id: account.id, webauthn_id: "mKH7k5", public_key: "public-key", sign_count: 1, name: "test_key")
+      end
+
+      account.create_project_with_default_policy("Default") if with_project
+      account
+    end
+
+    def create_private_location(project:)
+      loc = Location.create(
+        name: "us-east-1",
+        display_name: "aws-us-east-1",
+        ui_name: "aws-us-east-1",
+        visible: true,
+        provider: "aws",
+        project_id: project.id
+      )
+
+      LocationCredential.create(
+        access_key: "access-key-id",
+        secret_key: "secret-access-key"
+      ) { it.id = loc.id }
+      loc
+    end
+  end)
 end
