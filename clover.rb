5bb0de97854402232ab473d296ddc58790ca9c29
Enes Cakir <enes@ubicloud.com>
2025-05-08 00:11:48 +0300

    Add GitHub organization picture to the installation dropdown
    I think adding the GitHub profile picture to the installation dropdown
    makes it more human friendly. It helps people feel friendlier.
    
    "http://github.com/enescakir.png" redirected to
    "https://avatars.githubusercontent.com/u/993199".

--- clover.rb
+++ clover.rb
@@ -119,7 +119,7 @@ class Clover < Roda
   plugin :content_security_policy do |csp|
     csp.default_src :none
     csp.style_src :self, "https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css"
-    csp.img_src :self, "data: image/svg+xml"
+    csp.img_src :self, "data: image/svg+xml", "https://github.com", "https://avatars.githubusercontent.com"
     csp.form_action :self, "https://checkout.stripe.com", "https://github.com/login/oauth/authorize", "https://accounts.google.com/o/oauth2/auth"
     csp.script_src :self, "https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js", "https://cdn.jsdelivr.net/npm/dompurify@3.0.5/dist/purify.min.js", "https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js", "https://challenges.cloudflare.com/turnstile/v0/api.js", "https://cdn.jsdelivr.net/npm/marked@15.0.5/marked.min.js", "https://cdn.jsdelivr.net/npm/echarts@5.6.0/dist/echarts.min.js"
     csp.frame_src :self, "https://challenges.cloudflare.com"
