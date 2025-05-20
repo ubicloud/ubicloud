45ea1341ee4eb2cfe8bb5c90f218ab9571fd9fb6
Jeremy Evans <jeremy@ubicloud.com>
2025-05-20 14:08:19 -0700

    Use symbol proc instead of literal blocks in a few places

--- model/assigned_vm_address.rb
+++ model/assigned_vm_address.rb
@@ -5,7 +5,7 @@ require_relative "../model"
 class AssignedVmAddress < Sequel::Model
   one_to_one :vm, key: :id, primary_key: :dst_vm_id
   many_to_one :address, key: :address_id
-  one_to_one :active_billing_record, class: :BillingRecord, key: :resource_id do |ds| ds.active end
+  one_to_one :active_billing_record, class: :BillingRecord, key: :resource_id, &:active
 
   include ResourceMethods
 end
--- model/vm_host.rb
+++ model/vm_host.rb
@@ -391,11 +391,11 @@ class VmHost < Sequel::Model
   end
 
   def available_storage_gib
-    storage_devices.sum { it.available_storage_gib }
+    storage_devices.sum(&:available_storage_gib)
   end
 
   def total_storage_gib
-    storage_devices.sum { it.total_storage_gib }
+    storage_devices.sum(&:total_storage_gib)
   end
 
   def render_arch(arm64:, x64:)
