07ea750d65a200a028571c487d8162502161796d
Jeremy Evans <jeremy@ubicloud.com>
2025-05-29 09:55:14 -0700

    Change return 404 to next in postgres ca-certificates route
    return 404 only works as an implementation detail (the hash_branch
    call at the top currently defines a method), not by design. next
    should be used instead.

--- routes/project/location/postgres.rb
+++ routes/project/location/postgres.rb
@@ -371,7 +371,7 @@ class Clover
       r.get "ca-certificates" do
         authorize("Postgres:view", pg.id)
 
-        return 404 unless (certs = pg.ca_certificates)
+        next unless (certs = pg.ca_certificates)
 
         response.headers["content-disposition"] = "attachment; filename=\"#{pg.name}.pem\""
         response.headers["content-type"] = "application/x-pem-file"
