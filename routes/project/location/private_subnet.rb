2412d87b9706ae8961f25339606190087cced12e
Jeremy Evans <jeremy@ubicloud.com>
2025-05-29 10:06:56 -0700

    Only expose private subnet creation route inside location branch to api
    The web creation route is outside the location branch. This makes
    the code consistent with other routes inside the location branch.

--- routes/project/location/private_subnet.rb
+++ routes/project/location/private_subnet.rb
@@ -8,7 +8,7 @@ class Clover
 
     r.on PRIVATE_SUBNET_NAME_OR_UBID do |ps_name, ps_id|
       if ps_name
-        r.post true do
+        r.post api? do
           check_visible_location
           private_subnet_post(ps_name)
         end
