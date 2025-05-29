88ea886a8a8df5822e0786b23a0c29ace90b147f
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2025-05-30 02:50:40 +0300

    Add port and database name to connection string
    This is to be more explicit about the information in the connection string.
    With the addition of pgBouncer, we need to specify the port anyway, which
    leaves only the database name to be optional. For the sake of consistency,
    I decided to include it in the connection string as well.

--- spec/routes/api/cli/pg/pg_dumpall_spec.rb
+++ spec/routes/api/cli/pg/pg_dumpall_spec.rb
@@ -13,25 +13,25 @@ RSpec.describe Clover, "cli pg pg_dumpall" do
       target_storage_size_gib: 64
     ).subject
     @ref = [@pg.display_location, @pg.name].join("/")
-    @conn_string = URI("postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com?channel_binding=require")
+    @conn_string = URI("postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require")
     expect(Config).to receive(:postgres_service_hostname).and_return("pg.example.com").at_least(:once)
     @dns_zone = DnsZone.new
     expect(Prog::Postgres::PostgresResourceNexus).to receive(:dns_zone).and_return(@dns_zone).at_least(:once)
   end
 
   it "connects to database via pg_dumpall" do
-    expect(cli_exec(["pg", @ref, "pg_dumpall"])).to eq %W[pg_dumpall -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com?channel_binding=require]
+    expect(cli_exec(["pg", @ref, "pg_dumpall"])).to eq %W[pg_dumpall -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
   end
 
   it "supports pg_dumpall options" do
-    expect(cli_exec(["pg", @ref, "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com?channel_binding=require]
+    expect(cli_exec(["pg", @ref, "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
   end
 
   it "supports -U option for user name" do
-    expect(cli_exec(["pg", @ref, "-Ufoo", "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://foo@test-pg.#{@pg.ubid}.pg.example.com?channel_binding=require]
+    expect(cli_exec(["pg", @ref, "-Ufoo", "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://foo@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
   end
 
   it "supports -d option for database name" do
-    expect(cli_exec(["pg", @ref, "-dfoo", "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com/foo?channel_binding=require]
+    expect(cli_exec(["pg", @ref, "-dfoo", "pg_dumpall", "-a"])).to eq %W[pg_dumpall -a -dpostgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/foo?channel_binding=require]
   end
 end
