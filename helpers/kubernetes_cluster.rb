8cf23437d91c6744763e879499e72f37fa06aab1
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-06-06 12:37:33 +0200

    Add api support to kubernetes routes
    Prior to this commit, we only supported web requests but from now on,
    in order to support CLI, we will support api requests as well.

--- helpers/kubernetes_cluster.rb
+++ helpers/kubernetes_cluster.rb
@@ -25,15 +25,24 @@ class Clover
       )
       audit_log(kc, "create")
 
-      flash["notice"] = "'#{name}' will be ready in a few minutes"
-      request.redirect "#{@project.path}#{kc.path}"
+      if api?
+        Serializers::KubernetesCluster.serialize(kc, {detailed: true})
+      else
+        flash["notice"] = "'#{name}' will be ready in a few minutes"
+        request.redirect "#{@project.path}#{kc.path}"
+      end
     end
   end
 
   def kubernetes_cluster_list
     dataset = dataset_authorize(@project.kubernetes_clusters_dataset, "KubernetesCluster:view")
-    @kcs = Serializers::KubernetesCluster.serialize(dataset.all)
-    view "kubernetes-cluster/index"
+    if api?
+      paginated_result(dataset, Serializers::KubernetesCluster)
+    else
+      @kcs = Serializers::KubernetesCluster.serialize(dataset.all)
+      view "kubernetes-cluster/index"
+    end
+
   end
 
   def generate_kubernetes_cluster_options
