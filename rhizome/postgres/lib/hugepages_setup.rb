2ce898b677ed7cb9118cdc4d625c3d7e3cdfb6ba
Daniel Farina <daniel@ubicloud.com>
2025-06-13 15:23:32 -0700

    fixup! Configure hugepages for Postgres

--- rhizome/postgres/lib/hugepages_setup.rb
+++ rhizome/postgres/lib/hugepages_setup.rb
@@ -7,49 +7,38 @@ class HugepagesSetup
     @version, @cluster = instance.split("-", 2)
   end
 
-  def hugepage_size_kib
-    r('awk \'/Hugepagesize/ {printf "%.2f\n", $2}\' /proc/meminfo').to_f
-  end
-
   def get_postgres_param(param)
-    r "sudo -u postgres /usr/lib/postgresql/#{@version}/bin/postgres -D /dat/#{@version}/data -c config_file=/etc/postgresql/#{@version}/#{@cluster}/postgresql.conf -C #{param.shellescape}"
+    Integer(r("sudo -u postgres /usr/lib/postgresql/#{@version}/bin/postgres -D /dat/#{@version}/data -c config_file=/etc/postgresql/#{@version}/#{@cluster}/postgresql.conf -C #{param.shellescape}"), 10)
   end
 
-  def shared_buffers_kib
-    shared_buffers_blocks = get_postgres_param("shared_buffers").to_f
-    block_size_bytes = get_postgres_param("block_size").to_f
-
-    shared_buffers_blocks * block_size_bytes / 1024
+  def hugepage_info
+    meminfo = File.read("/proc/meminfo")
+    hugepages_count = Integer(meminfo[/^HugePages_Total:\s*(\d+)/, 1], 10)
+    hugepage_size_kib = Integer(meminfo[/^Hugepagesize:\s*(\d+)\s*kB/, 1], 10)
+    [hugepages_count, hugepage_size_kib]
   end
 
   def stop_postgres_cluster
     r "sudo pg_ctlcluster stop #{@version} #{@cluster}", expect: [0, 2] # 2 is "not running"
   end
 
-  def hugepages_count
-    @hugepages_count ||= begin
-      shared_memory_size_kib = 1024 * get_postgres_param("shared_memory_size").to_f
-      (shared_memory_size_kib / hugepage_size_kib).ceil
-    end
-  end
-
   def setup_postgres_hugepages
-    shared_memory_size_kib = 1024 * get_postgres_param("shared_memory_size").to_f
+    hugepages_count, hugepage_size_kib = hugepage_info
+
     hugepages_kib = hugepages_count * hugepage_size_kib
-    update_postgres_hugepages_conf(hugepages_kib.ceil)
+    update_postgres_hugepages_conf(hugepages_kib)
 
-    # Get the updated shared_memory_size from the postgres config, and remove
-    # the overhead from shared_buffers.
-    new_shared_memory_size_kib = 1024 * get_postgres_param("shared_memory_size").to_f
-    additional_shared_memory_kib = new_shared_memory_size_kib - shared_memory_size_kib
+    shmem_and_overhead_kib = 1024 * get_postgres_param("shared_memory_size")
+    overhead = shmem_and_overhead_kib - hugepages_kib
 
-    reduced_shared_buffers_kib = shared_buffers_kib - additional_shared_memory_kib
-    update_postgres_hugepages_conf(reduced_shared_buffers_kib.floor)
-  end
+    target_kib = hugepages_kib - overhead
 
-  def setup_system_hugepages
-    r "echo 'vm.nr_hugepages = #{hugepages_count}' | sudo tee /etc/sysctl.d/10-hugepages.conf"
-    r "sudo sysctl --system"
+    # Floor division to nearest multiple of block_size: (a / b) * b
+    block_size_bytes = get_postgres_param("block_size")
+    block_size_kib = block_size_bytes / 1024
+    final_shared_buffers_kib = (target_kib / block_size_kib) * block_size_kib
+
+    update_postgres_hugepages_conf(final_shared_buffers_kib)
   end
 
   def update_postgres_hugepages_conf(shared_buffers_kib)
@@ -69,12 +58,9 @@ CONF
   end
 
   def setup
-    # If postgres is already running, we don't change the hugepages
-    # configuration.
     unless postgres_running?
       stop_postgres_cluster
       setup_postgres_hugepages
-      setup_system_hugepages
     end
   end
 end
--- rhizome/postgres/spec/hugepages_setup_spec.rb
+++ rhizome/postgres/spec/hugepages_setup_spec.rb
@@ -6,31 +6,33 @@ RSpec.describe HugepagesSetup do
   let(:hugepages_setup) { described_class.new("17-main") }
 
   describe "#setup_postgres_hugepages" do
-    it "calculates hugepages and updates postgres config" do
-      expect(hugepages_setup).to receive(:get_postgres_param).with("shared_memory_size").at_least(:once).and_return("1076", "1130")
-      expect(hugepages_setup).to receive(:shared_buffers_kib).and_return(1024 * 1024)
-      expect(hugepages_setup).to receive(:hugepages_count).and_return(538)
-      expect(hugepages_setup).to receive(:hugepage_size_kib).and_return(2048.0)
-      expect(hugepages_setup).to receive(:update_postgres_hugepages_conf).with(538 * 2048)
-      expect(hugepages_setup).to receive(:update_postgres_hugepages_conf).with(1024 * 1024 - (1130 - 1076) * 1024)
+    it "calculates hugepages via overhead subtraction and rounding" do
+      # Mock hugepage info: 512 x 2MB hugepages = 1024MB total hugepage space
+      expect(hugepages_setup).to receive(:hugepage_info).and_return([512, 2048])
+
+      # shared_memory_size increases due to simulated overhead:
+      # 1024MB -> 1061MB (37MB overhead)
+      expect(hugepages_setup).to receive(:get_postgres_param)
+        .with("shared_memory_size").and_return(1061)
+
+      # Block size for rounding shared_buffers
+      expect(hugepages_setup).to receive(:get_postgres_param)
+        .with("block_size").and_return(8192)  # 8KB blocks
+
+      # First call: set shared_buffers to total hugepage space
+      # (1024MB = 1,048,576 KiB)
+      expect(hugepages_setup).to receive(:update_postgres_hugepages_conf)
+        .with(1_048_576)
+
+      # Second call: back off by overhead and round down to block boundary
+      # overhead = (1061 - 1024) * 1024 = 37,888 KiB
+      # target = 1,048,576 - 37,888 = 1,010,688 KiB
+      expected_shared_buffers = 1_010_688
+      expect(hugepages_setup).to receive(:update_postgres_hugepages_conf)
+        .with(expected_shared_buffers)
+      expect(expected_shared_buffers & 0b111).to eq(0)  # Divisible by 8
 
       expect { hugepages_setup.setup_postgres_hugepages }.not_to raise_error
-    end
-  end
-
-  describe "#setup" do
-    it "sets up hugepages if postgres is not running" do
-      expect(hugepages_setup).to receive(:postgres_running?).and_return(false)
-      expect(hugepages_setup).to receive(:stop_postgres_cluster)
-      expect(hugepages_setup).to receive(:setup_postgres_hugepages)
-      expect(hugepages_setup).to receive(:setup_system_hugepages)
-      expect { hugepages_setup.setup }.not_to raise_error
-    end
-
-    it "does not set up hugepages if postgres is running" do
-      expect(hugepages_setup).to receive(:postgres_running?).and_return(true)
-      expect(hugepages_setup).not_to receive(:stop_postgres_cluster)
-      expect { hugepages_setup.setup }.not_to raise_error
     end
   end
 end
