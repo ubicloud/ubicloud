7845c051efa389381ecd9f1733b09f756471b6ff
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-06-20 15:44:13 +0200

    Add Serializers for Kubernetes models

---
+++ serializers/kubernetes_cluster.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Serializers::KubernetesCluster < Serializers::Base
+  def self.serialize_internal(kc, options = {})
+    base = {
+      id: kc.ubid,
+      name: kc.name,
+      location: kc.location.display_name,
+      display_state: kc.display_state,
+      cp_node_count: kc.cp_node_count,
+      node_size: kc.target_node_size,
+      version: kc.version
+    }
+    if options[:detailed]
+      base[:cp_vms] = Serializers::Vm.serialize(kc.cp_vms_dataset.all)
+      base[:nodepools] = Serializers::KubernetesNodepool.serialize(kc.nodepools_dataset.all, {detailed: true})
+    end
+    base
+  end
+end
