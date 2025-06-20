25a57b46a744dcdaf14b46f29ac3ef6ae5260b3b
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-06-20 16:55:28 +0200

    Add create command to KubernetesCluster CLI

--- cli-commands/kc.rb
+++ cli-commands/kc.rb
@@ -2,6 +2,7 @@
 
 UbiCli.base("kc") do
   banner "ubi kc command [...]"
+  post_banner "ubi kc (location/kc-name | kc-id) post-command [...]"
 end
 
 Unreloader.record_dependency(__FILE__, "cli-commands/kc")
