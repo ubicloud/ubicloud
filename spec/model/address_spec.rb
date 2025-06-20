415154ad4ed132976e51e35dc693897af23004c4
Jeremy Evans <jeremy@ubicloud.com>
2025-06-20 15:10:40 -0700

    Fix ipv4_address table population in Address#populate_ipv4_addresses
    Change
    
    id != routed_to_host_id
    
    to
    
    vm_host.sshable.host != cidr.network.to_s
    
    We do not consistently use the VmHost id as the primary Address id
    (it is not done at all for Hetzner hosts), so the next simplest way
    to check is check the sshable address.
    
    Existing installations that previously ran
    Address#populate_ipv4_addresses can be fixed using:
    
    DB[:ipv4_address].where(ip: Sshable.where(id: VmHost.select(:id)).select(Sequel.cast(:host, :inet))).delete

--- spec/model/address_spec.rb
+++ spec/model/address_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe Address do
     expect(DB[:ipv4_address]).to be_empty
   end
 
+  it "does not populate host sshable address" do
+    Prog::Vm::HostNexus.assemble("1.1.1.1").subject
+    expect(described_class.count).to eq 1
+    expect(Sshable.count).to eq 1
+    expect(described_class.get(:cidr).network.to_s).to eq Sshable.get(:host)
+    expect(DB[:ipv4_address]).to be_empty
+  end
+
   it "populates ipv4_address table with addresses in cidr without first and last, when using leaseweb" do
     vm_host = Prog::Vm::HostNexus.assemble("1.2.3.4", provider_name: HostProvider::LEASEWEB_PROVIDER_NAME, server_identifier: "1").subject
     described_class.create(cidr: "0.0.0.0/30", vm_host:)
