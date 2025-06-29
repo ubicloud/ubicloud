1b5fbeeaddff6b905fbe3d6dd53037c5c9a076a2
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2025-06-29 04:35:52 -0400

    Migration to add arch column to pg_aws_ami

--- model/pg_aws_ami.rb
+++ model/pg_aws_ami.rb
@@ -12,6 +12,7 @@ end
 #  aws_location_name | text |
 #  aws_ami_id        | text |
 #  pg_version        | text |
+#  arch              | text | NOT NULL
 # Indexes:
-#  pg_aws_ami_pkey                               | PRIMARY KEY btree (id)
-#  pg_aws_ami_aws_location_name_pg_version_index | UNIQUE btree (aws_location_name, pg_version)
+#  pg_aws_ami_pkey                                    | PRIMARY KEY btree (id)
+#  pg_aws_ami_aws_location_name_pg_version_arch_index | UNIQUE btree (aws_location_name, pg_version, arch)
