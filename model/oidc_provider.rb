b16c01d2bea521b14cdc90dbb6ef0fe8b2b8b92f
Jeremy Evans <jeremy@ubicloud.com>
2025-06-30 16:54:15 -0700

    Add OidcProvider.register for registering with an OIDC provider

--- model/oidc_provider.rb
+++ model/oidc_provider.rb
@@ -1,12 +1,44 @@
 # frozen_string_literal: true
 
 require_relative "../model"
+require "excon"
 
 class OidcProvider < Sequel::Model
+  # Register a new OIDC Provider. Currently, you must restart the application after
+  # adding the provider (or require clover.rb to be reloaded).
+  def self.register(display_name, url)
+    oidc_provider = new_with_id(display_name:)
+
+    uri = URI(url)
+    uri.path = "/.well-known/openid-configuration"
+    response = Excon.get(uri.to_s,
+      headers: {"Accept" => "application/json"},
+      expects: 200)
+
+    info = JSON.parse(response.body)
+    response = Excon.post(info["registration_endpoint"],
+      headers: {"Accept" => "application/json", "Content-Type" => "application/json"},
+      body: {
+        client_name: "Ubicloud",
+        redirect_uris: [oidc_provider.callback_url],
+        scopes: "openid email"
+      }.to_json)
+
+    info = JSON.parse(response.body)
+    raise "Unable to register with oidc provider: #{response.status} #{info.inspect}" unless response.status == 201
+
+    uri.path = ""
+    oidc_provider.update(url: uri, client_id: info["client_id"], client_secret: info["client_secret"])
+  end
+
   plugin ResourceMethods
 
   plugin :column_encryption do |enc|
     enc.column :client_secret
+  end
+
+  def callback_url
+    "#{Config.base_url}/auth/#{ubid}/callback"
   end
 end
 
