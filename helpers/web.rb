b0b173d76cac6854f1e37c4b5011a3ff01017046
Jeremy Evans <jeremy@ubicloud.com>
2025-07-08 15:26:05 -0700

    Fix GitHub captialization in omniauth code
    The omniauth_provider_name method previously used provider.capitalize,
    as that's what the code it replaced used, but while that works fine for
    google, it doesn't work well for github. Use the name in
    omniauth_providers, which has the correct capitalization.

--- helpers/web.rb
+++ helpers/web.rb
@@ -85,11 +85,8 @@ class Clover < Roda
   end
 
   def omniauth_provider_name(provider)
-    if omniauth_providers.map { |provider,| provider.to_s }.include?(provider)
-      provider.capitalize
-    else
-      OidcProvider.name_for_ubid(provider) || provider
-    end
+    omniauth_providers.each { |sym, name| return name if sym.name == provider }
+    OidcProvider.name_for_ubid(provider) || provider
   end
 
   def omniauth_providers
