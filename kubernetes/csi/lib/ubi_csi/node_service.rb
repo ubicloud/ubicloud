6eaf3ddbee343d32f5b3ba0b8420d64dead1b7f0
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-07-09 12:33:40 +0200

    Log all the commands run in CSI and improve logging

--- kubernetes/csi/lib/ubi_csi/node_service.rb
+++ kubernetes/csi/lib/ubi_csi/node_service.rb
@@ -15,7 +15,7 @@ module Csi
       LOGGER = Logger.new($stdout)
 
       def log_with_id(id, message)
-        LOGGER.info("[req_id=#{id}] #{message}")
+        LOGGER.info("[req_id=#{id}] [CSI NodeService] #{message}")
       end
 
       def node_get_capabilities(req, _call)
@@ -52,12 +52,15 @@ module Csi
         resp
       end
 
-      def run_cmd(*cmd)
+      def run_cmd(*cmd, req_id: nil)
+        unless req_id.nil?
+          log_with_id(req_id, "Running command: #{cmd}")
+        end
         Open3.capture2e(*cmd)
       end
 
-      def is_mounted?(path)
-        _, status = run_cmd("mountpoint", "-q", path)
+      def is_mounted?(path, req_id: nil)
+        _, status = run_cmd("mountpoint", "-q", path, req_id:)
         status == 0
       end
 
@@ -77,18 +80,19 @@ module Csi
         staging_path = req.staging_target_path
         size_bytes = req.volume_context["size_bytes"].to_i
         backing_file = File.join(VOLUME_BASE_PATH, "#{volume_id}.img")
+        log_with_id(req_id, "Creating backing file's directory #{File.dirname(backing_file)}")
         FileUtils.mkdir_p(File.dirname(backing_file))
 
         begin
           unless File.exist?(backing_file)
-            output, ok = run_cmd("fallocate", "-l", size_bytes.to_s, backing_file)
+            output, ok = run_cmd("fallocate", "-l", size_bytes.to_s, backing_file, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_stage_volume: failed to fallocate: #{output}")
+              log_with_id(req_id, "gRPC error in node_stage_volume: failed to fallocate: #{output}")
               raise GRPC::ResourceExhausted, "Failed to allocate backing file: #{output}"
             end
-            output, ok = run_cmd("fallocate", "--punch-hole", "--keep-size", "-o", "0", "-l", size_bytes.to_s, backing_file)
+            output, ok = run_cmd("fallocate", "--punch-hole", "--keep-size", "-o", "0", "-l", size_bytes.to_s, backing_file, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_stage_volume: failed to punchhole: #{output}")
+              log_with_id(req_id, "gRPC error in node_stage_volume: failed to punchhole: #{output}")
               raise GRPC::ResourceExhausted, "Failed to punch hole in backing file: #{output}"
             end
           end
@@ -97,7 +101,7 @@ module Csi
           is_new_loop_device = loop_device.nil?
           if is_new_loop_device
             log_with_id(req_id, "Setting up new loop device for: #{backing_file}")
-            output, ok = run_cmd("losetup", "--find", "--show", backing_file)
+            output, ok = run_cmd("losetup", "--find", "--show", backing_file, req_id:)
             loop_device = output.strip
             unless ok && !loop_device.empty?
               raise GRPC::Internal, "Failed to setup loop device: #{output}"
@@ -108,26 +112,26 @@ module Csi
 
           if req.volume_capability&.mount && is_new_loop_device
             fs_type = req.volume_capability.mount.fs_type || "ext4"
-            output, ok = run_cmd("mkfs.#{fs_type}", loop_device)
+            output, ok = run_cmd("mkfs.#{fs_type}", loop_device, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_stage_volume: failed to format device: #{output}")
+              log_with_id(req_id, "gRPC error in node_stage_volume: failed to format device: #{output}")
               raise GRPC::Internal, "Failed to format device #{loop_device} with #{fs_type}: #{output}"
             end
           end
-          unless is_mounted?(staging_path)
+          unless is_mounted?(staging_path, req_id:)
             FileUtils.mkdir_p(staging_path)
-            output, ok = run_cmd("mount", loop_device, staging_path)
+            output, ok = run_cmd("mount", loop_device, staging_path, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_stage_volume: failed to mount loop device: #{output}")
+              log_with_id(req_id, "gRPC error in node_stage_volume: failed to mount loop device: #{output}")
               raise GRPC::Internal, "Failed to mount #{loop_device} to #{staging_path}: #{output}"
             end
           end
           # If block, do nothing else
         rescue GRPC::BadStatus => e
-          log_with_id(req_id, "[CSI NodeService] gRPC error in node_stage_volume: #{e.class} - #{e.message}")
+          log_with_id(req_id, "gRPC error in node_stage_volume: #{e.class} - #{e.message}")
           raise e
         rescue => e
-          log_with_id(req_id, "[CSI NodeService] Internal error in node_stage_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+          log_with_id(req_id, "Internal error in node_stage_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
           raise GRPC::Internal, "NodeStageVolume error: #{e.message}"
         end
         resp = NodeStageVolumeResponse.new
@@ -141,19 +145,19 @@ module Csi
         staging_path = req.staging_target_path
         begin
           if is_mounted?(staging_path)
-            output, ok = run_cmd("umount", "-q", staging_path)
+            output, ok = run_cmd("umount", "-q", staging_path, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_unstage_volume: failed to umount device: #{output}")
+              log_with_id(req_id, "gRPC error in node_unstage_volume: failed to umount device: #{output}")
               raise GRPC::Internal, "Failed to unmount #{staging_path}: #{output}"
             end
           else
             log_with_id(req_id, "#{staging_path} is not mounted, skipping umount")
           end
         rescue GRPC::BadStatus => e
-          log_with_id(req_id, "[CSI NodeService] gRPC error in node_unstage_volume: #{e.class} - #{e.message}")
+          log_with_id(req_id, "gRPC error in node_unstage_volume: #{e.class} - #{e.message}")
           raise e
         rescue => e
-          log_with_id(req_id, "[CSI NodeService] Internal error in node_unstage_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+          log_with_id(req_id, "Internal error in node_unstage_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
           raise GRPC::Internal, "NodeUnstageVolume error: #{e.message}"
         end
         resp = NodeUnstageVolumeResponse.new
@@ -168,16 +172,16 @@ module Csi
         target_path = req.target_path
         begin
           FileUtils.mkdir_p(target_path)
-          output, ok = run_cmd("mount", "--bind", staging_path, target_path)
+          output, ok = run_cmd("mount", "--bind", staging_path, target_path, req_id:)
           unless ok
-            log_with_id(req_id, "[CSI NodeService] gRPC error in node_publish_volume: failed to bind mount device: #{output}")
+            log_with_id(req_id, "gRPC error in node_publish_volume: failed to bind mount device: #{output}")
             raise GRPC::Internal, "Failed to bind mount #{staging_path} to #{target_path}: #{output}"
           end
         rescue GRPC::BadStatus => e
-          log_with_id(req_id, "[CSI NodeService] gRPC error in node_publish_volume: #{e.class} - #{e.message}")
+          log_with_id(req_id, "gRPC error in node_publish_volume: #{e.class} - #{e.message}")
           raise e
         rescue => e
-          log_with_id(req_id, "[CSI NodeService] Internal error in node_publish_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+          log_with_id(req_id, "Internal error in node_publish_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
           raise GRPC::Internal, "NodePublishVolume error: #{e.message}"
         end
         resp = NodePublishVolumeResponse.new
@@ -191,19 +195,19 @@ module Csi
         target_path = req.target_path
         begin
           if is_mounted?(target_path)
-            output, ok = run_cmd("umount", "-q", target_path)
+            output, ok = run_cmd("umount", "-q", target_path, req_id:)
             unless ok
-              log_with_id(req_id, "[CSI NodeService] gRPC error in node_unpublish_volume: failed to umount device: #{output}")
+              log_with_id(req_id, "gRPC error in node_unpublish_volume: failed to umount device: #{output}")
               raise GRPC::Internal, "Failed to unmount #{target_path}: #{output}"
             end
           else
             log_with_id(req_id, "#{target_path} is not mounted, skipping umount")
           end
         rescue GRPC::BadStatus => e
-          log_with_id(req_id, "[CSI NodeService] gRPC error in node_unpublish_volume: #{e.class} - #{e.message}")
+          log_with_id(req_id, "gRPC error in node_unpublish_volume: #{e.class} - #{e.message}")
           raise e
         rescue => e
-          log_with_id(req_id, "[CSI NodeService] Internal error in node_unpublish_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+          log_with_id(req_id, "Internal error in node_unpublish_volume: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
           raise GRPC::Internal, "NodeUnpublishVolume error: #{e.message}"
         end
         resp = NodeUnpublishVolumeResponse.new
