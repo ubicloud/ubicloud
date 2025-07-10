d1e8b835ad5c13e935a25fede0f3eb7f0b4db758
mohi-kalantari <mohi.kalantari1@gmail.com>
2025-07-10 13:40:18 +0200

    Add KubernetesClient to CSI
    With the help of this client, the commands we need in order to
    implement the data copy and migration is added. During the process
    we need to create/update/get/list PVs, get nodes and
    get/create/delete/update PVCs.
    
    Dockerfile is updated to install kubectl on the docker images. Note
    that we won't use any ruby gems for interacting with kubernetes since
    most are not maintained well and the best they do is assembling a
    REST client and giving you back the whole response which kubectl does
    with no effort.
    
    Also kubectl will use the credentials avaialbe in pod and won't be
    affected by token rotations as it would read the token on every command.
    
    ClusterRole of node_plugin is also updated to give enough access to
    perform the commands.

---
+++ kubernetes/csi/lib/ubi_csi/kubernetes_client.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require "open3"
+require "yaml"
+require_relative "errors"
+
+module Csi
+  class KubernetesClient
+    def initialize(req_id: nil)
+      @req_id = req_id
+      @logger = Logger.new($stdout) if req_id.nil?
+    end
+
+    def run_cmd(*cmd, **options)
+      Open3.capture2e(*cmd, **options)
+    end
+
+    def run_kubectl(*args, stdin_data: nil)
+      cmd = [
+        "kubectl",
+        *args
+      ]
+      @logger&.info("[req_id=#{@req_id}] [Kubernetes Client] #{cmd.join(" ")}")
+      output, status = run_cmd(*cmd, stdin_data:)
+      unless status.success?
+        if output.strip.end_with?("not found")
+          raise ObjectNotFoundError, output
+        end
+        raise "Command failed: #{cmd.join(" ")}\nOutput: #{output}"
+      end
+      output
+    end
+
+    def get_node(name)
+      YAML.safe_load(run_kubectl("get", "node", name, "-oyaml"))
+    end
+
+    def get_node_ip(name)
+      node_yaml = get_node(name)
+      node_yaml.dig("status", "addresses", 0, "address")
+    end
+
+    def get_pv(name)
+      YAML.safe_load(run_kubectl("get", "pv", name, "-oyaml"))
+    end
+
+    def extract_node_from_pv(pv)
+      pv.dig("spec", "nodeAffinity", "required", "nodeSelectorTerms", 0, "matchExpressions", 0, "values", 0)
+    end
+
+    def create_pv(yaml_data)
+      run_kubectl("create", "-f", "-", stdin_data: YAML.dump(yaml_data))
+    end
+
+    def update_pv(yaml_data)
+      run_kubectl("apply", "-f", "-", stdin_data: YAML.dump(yaml_data))
+    end
+
+    def get_pvc(namespace, name)
+      YAML.safe_load(run_kubectl("-n", namespace, "get", "pvc", name, "-oyaml"))
+    end
+
+    def create_pvc(yaml_data)
+      run_kubectl("create", "-f", "-", stdin_data: YAML.dump(yaml_data))
+    end
+
+    def update_pvc(yaml_data)
+      run_kubectl("apply", "-f", "-", stdin_data: YAML.dump(yaml_data))
+    end
+
+    def delete_pvc(namespace, name)
+      run_kubectl("-n", namespace, "delete", "pvc", name, "--wait=false", "--ignore-not-found=true")
+    end
+
+    def node_schedulable?(name)
+      !get_node(name)["spec"]&.fetch("unschedulable", false)
+    end
+
+    def find_pv_by_volume_id(volume_id)
+      pv_list = YAML.safe_load(run_kubectl("get", "pv", "-oyaml"))
+      pv_list["items"].find { |pv| pv.dig("spec", "csi", "volumeHandle") == volume_id }
+    end
+  end
+end
