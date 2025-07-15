21f20061d076d06809f63329c83c0cc9227e0476
Jeremy Evans <jeremy@ubicloud.com>
2025-07-15 13:24:46 -0700

    Add more plugins to CloverAdmin
    This adds the render, exception_page (in development), public, flash,
    and sessions plugin.
    
    This adds a views/admin and public/admin directories to hold the
    templates and public files for the admin site.
    
    For the session secret, to avoid using the same session secret as
    Clover itself, this uses an HMAC with the Clover session secret as
    the key.

--- clover_admin.rb
+++ clover_admin.rb
@@ -3,9 +3,44 @@
 require_relative "model"
 
 require "roda"
+require "tilt"
+require "tilt/erubi"
+require "openssl"
 
 class CloverAdmin < Roda
+  # :nocov:
+  plugin :exception_page if Config.development?
+  default_fixed_locals = if Config.production? || ENV["CLOVER_FREEZE"] == "1"
+    "()"
+  # :nocov:
+  else
+    "(_no_kw: nil)"
+  end
+
+  plugin :render, views: "views/admin", escape: true, assume_fixed_locals: true, template_opts: {
+    chain_appends: !defined?(SimpleCov),
+    freeze: true,
+    skip_compiled_encoding_detection: true,
+    scope_class: self,
+    default_fixed_locals:,
+    extract_fixed_locals: true
+  }
+
+  plugin :public
+  plugin :flash
+
+  plugin :sessions,
+    key: "_CloverAdmin.session",
+    cookie_options: {secure: !(Config.development? || Config.test?)},
+    secret: OpenSSL::HMAC.digest("SHA512", Config.clover_session_secret, "admin-site")
+
   route do |r|
-    r.env["HTTP_HOST"]
+    r.public
+
+    # :nocov:
+    r.exception_page_assets if Config.development?
+    # :nocov:
+
+    view(content: r.env["HTTP_HOST"])
   end
 end
--- spec/routes/web/admin/admin_spec.rb
+++ spec/routes/web/admin/admin_spec.rb
@@ -5,6 +5,7 @@ require_relative "spec_helper"
 RSpec.describe CloverAdmin do
   it "dispatches requests from Clover to CloverAdmin if host starts with admin." do
     visit "/"
-    expect(page.body).to eq "admin.ubicloud.com"
+    expect(page.title).to eq "Ubicloud Admin"
+    expect(page).to have_content "admin.ubicloud.com"
   end
 end
