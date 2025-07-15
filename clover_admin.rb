7bdfb81cb7fcd330c62cd291271e2d8d7e256bb0
Jeremy Evans <jeremy@ubicloud.com>
2025-07-15 15:40:49 -0700

    Support searching by ubid and navigating to associated objects in the admin site
    For each object, this shows the object contents (other than redacted columns),
    as well as links to all associated objects.
    
    This adds the following plugins: typecast_params, symbol_matchers, not_found,
    route_csrf, and forme_route_csrf.  Forme will be used to build the forms on
    the admin site. Currently, there is only a single form, and it only does a
    GET request and not a POST request, so the route_csrf code is not yet needed,
    but it will be needed as soon as authentication is added.

--- clover_admin.rb
+++ clover_admin.rb
@@ -34,6 +34,34 @@ class CloverAdmin < Roda
     cookie_options: {secure: !(Config.development? || Config.test?)},
     secret: OpenSSL::HMAC.digest("SHA512", Config.clover_session_secret, "admin-site")
 
+  plugin :typecast_params_sized_integers, sizes: [64], default_size: 64
+  plugin :typecast_params do
+    handle_type(:ubid) do
+      it if /\A[a-tv-z0-9]{26}\z/.match?(it)
+    end
+  end
+
+  plugin :symbol_matchers
+  symbol_matcher(:ubid, /([a-tv-z0-9]{26})/)
+
+  plugin :not_found do
+    @page_title = "File Not Found"
+    view(content: "")
+  end
+
+  plugin :route_csrf do |token|
+    # :nocov:
+    # Cover after we have a form submitting a POST request
+    flash.now["error"] = "An invalid security token submitted with this request, please try again"
+    @page_title = "Invalid Security Token"
+    view(content: "")
+    # :nocov:
+  end
+
+  plugin :forme_route_csrf
+  Forme.register_config(:clover_admin, base: :default, labeler: :explicit)
+  Forme.default_config = :clover_admin
+
   route do |r|
     r.public
 
@@ -41,6 +69,27 @@ class CloverAdmin < Roda
     r.exception_page_assets if Config.development?
     # :nocov:
 
-    view(content: r.env["HTTP_HOST"])
+    r.get "model", /([A-Z][a-zA-Z]+)/, :ubid do |model_name, ubid|
+      begin
+        @klass = Object.const_get(model_name)
+      rescue NameError
+        next
+      end
+
+      next unless @klass.is_a?(Class) && @klass < ResourceMethods::InstanceMethods
+      next unless (@obj = @klass[ubid])
+
+      view("object")
+    end
+
+    r.root do
+      if (ubid = typecast_params.ubid("ubid")) && (klass = UBID.class_for_ubid(ubid))
+        r.redirect("/model/#{klass.name}/#{ubid}")
+      elsif typecast_params.nonempty_str("ubid")
+        flash.now["error"] = "Invalid ubid provided"
+      end
+
+      view("index")
+    end
   end
 end
--- spec/routes/web/admin/admin_spec.rb
+++ spec/routes/web/admin/admin_spec.rb
@@ -3,9 +3,61 @@
 require_relative "spec_helper"
 
 RSpec.describe CloverAdmin do
-  it "dispatches requests from Clover to CloverAdmin if host starts with admin." do
+  def object_data
+    page.all(".object-table tbody tr").map { it.all("td").map(&:text) }.to_h.transform_keys(&:to_sym).except(:created_at)
+  end
+
+  it "allows searching by ubid and navigating to related objects" do
+    account = create_account
+
     visit "/"
     expect(page.title).to eq "Ubicloud Admin"
-    expect(page).to have_content "admin.ubicloud.com"
+
+    fill_in "UBID", with: account.ubid
+    click_button "Show Object"
+    expect(page.title).to eq "Ubicloud Admin - Account #{account.ubid}"
+    expect(object_data).to eq(email: "user@example.com", name: "", status_id: "2", suspended_at: "")
+
+    project = account.projects.first
+    click_link project.ubid
+    expect(page.title).to eq "Ubicloud Admin - Project #{project.ubid}"
+    expect(object_data).to eq(billable: "true", billing_info_id: "", credit: "0.0", discount: "0", feature_flags: "{}", name: "Default", reputation: "new", visible: "true")
+
+    subject_tag = project.subject_tags.first
+    click_link subject_tag.ubid
+    expect(page.title).to eq "Ubicloud Admin - SubjectTag #{subject_tag.ubid}"
+    expect(object_data).to eq(name: "Admin", project_id: project.ubid)
+
+    # Column Link
+    click_link project.ubid
+    expect(page.title).to eq "Ubicloud Admin - Project #{project.ubid}"
+  end
+
+  it "handles request for invalid ubid" do
+    visit "/"
+    fill_in "UBID", with: "foo"
+    click_button "Show Object"
+    expect(page.title).to eq "Ubicloud Admin"
+    expect(page).to have_flash_error("Invalid ubid provided")
+
+    fill_in "UBID", with: "ts1cyaqvp5ha6j5jt8ypbyagw9"
+    click_button "Show Object"
+    expect(page.title).to eq "Ubicloud Admin - File Not Found"
+  end
+
+  it "handles request for invalid model or missing object" do
+    visit "/model/Foo/ts1cyaqvp5ha6j5jt8ypbyagw9"
+    expect(page.title).to eq "Ubicloud Admin - File Not Found"
+
+    visit "/model/ArchivedRecord/ts1cyaqvp5ha6j5jt8ypbyagw9"
+    expect(page.title).to eq "Ubicloud Admin - File Not Found"
+
+    visit "/model/SubjectTag/ts1cyaqvp5ha6j5jt8ypbyagw9"
+    expect(page.title).to eq "Ubicloud Admin - File Not Found"
+  end
+
+  it "handles 404 page" do
+    visit "/invalid-page"
+    expect(page.title).to eq "Ubicloud Admin - File Not Found"
   end
 end
