dfdd0f12c2275d0dafafa07389edb5c285c79a17
Daniel Farina <daniel@ubicloud.com>
2025-07-23 14:09:16 -0700

    Require 100% branch coverage in Kubernetes CSI
    We're almost there anyway, may as well not let it slip, and make it
    efficient to figure out if a patch introduces untested branches, as
    all caused by new code if the previous state was 100%.

--- kubernetes/csi/spec/spec_helper.rb
+++ kubernetes/csi/spec/spec_helper.rb
@@ -28,8 +28,8 @@ unless ENV["COVERAGE"] == "false"
     }
 
     # Set minimum coverage threshold
-    minimum_coverage 80
-    minimum_coverage_by_file 70
+    minimum_coverage 100
+    minimum_coverage_by_file 100
 
     # Track branches for more detailed coverage
     enable_coverage :branch
