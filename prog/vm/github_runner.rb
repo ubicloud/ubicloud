4c6b54fa3d99cb3139d83a97cafccbba635c5f0f
Enes Cakir <enes@ubicloud.com>
2025-07-28 18:30:10 +0300

    Decrease the initial nap for wait_vm from 13s to 11s
    Our VM provisioning times have improved over time, and we have bunch of
    VMs that are provisioned in less than 12 seconds. Therefore, we can
    reduce the initial nap for wait_vm from 13 seconds to 11 seconds.

--- prog/vm/github_runner.rb
+++ prog/vm/github_runner.rb
@@ -202,8 +202,8 @@ class Prog::Vm::GithubRunner < Prog::Base
 
   label def wait_vm
     # If the vm is not allocated yet, we know that the vm provisioning will take
-    # definitely more than 13 seconds.
-    nap 13 unless vm.allocated_at
+    # definitely more than 11 seconds.
+    nap 11 unless vm.allocated_at
     nap 1 unless vm.provisioned_at
     register_deadline("wait", 10 * 60)
     hop_setup_environment
