5d0c7e99f0a1c5dc0bca98659abed078c4fbb830
Jeremy Evans <jeremy@ubicloud.com>
2025-07-31 15:15:08 -0700

    Use handle_validation_failure in postgres create route
    Also, treat unrecognized flavor as standard flavor, instead of
    if trying to handle it as an error.  This is only on the create
    view, actual submissions will raise an error for invalid flavor.
    You can only get the invalid flavor by manually changing the URL,
    so there doesn't seem to be a need to make it an error, and
    treating it as an error complicates the logic.

--- routes/project/postgres.rb
+++ routes/project/postgres.rb
@@ -8,22 +8,13 @@ class Clover
 
     r.web do
       r.post true do
+        handle_validation_failure("postgres/create")
         check_visible_location
         postgres_post(typecast_params.nonempty_str("name"))
       end
 
       r.get "create" do
         authorize("Postgres:create", @project.id)
-
-        flavor = typecast_params.nonempty_str("flavor", PostgresResource::Flavor::STANDARD)
-        Validation.validate_postgres_flavor(flavor)
-
-        @flavor = flavor
-        @prices = fetch_location_based_prices("PostgresVCpu", "PostgresStorage")
-        @has_valid_payment_method = @project.has_valid_payment_method?
-        @enabled_postgres_sizes = Option::VmSizes.select { @project.quota_available?("PostgresVCpu", it.vcpus) }.map(&:name)
-        @option_tree, @option_parents = generate_postgres_options(flavor: @flavor)
-
         view "postgres/create"
       end
     end
