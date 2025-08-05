ddc2269cf918a616ddbf9a58139400f7e5e06596
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2025-08-05 15:00:01 +0200

    Add migration for EC2 DNS name

--- model/aws_instance.rb
+++ model/aws_instance.rb
@@ -10,8 +10,9 @@ end
 
 # Table: aws_instance
 # Columns:
-#  id          | uuid | PRIMARY KEY
-#  instance_id | text |
-#  az_id       | text |
+#  id            | uuid | PRIMARY KEY
+#  instance_id   | text |
+#  az_id         | text |
+#  ipv4_dns_name | text |
 # Indexes:
 #  aws_instance_pkey | PRIMARY KEY btree (id)
