9b008c2071c199baf523d1fd2f4b58c8c04acead
Jeremy Evans <jeremy@ubicloud.com>
2025-08-07 14:37:15 -0700

    Remove serializer usage for inference api key route
    This removes it for both the web and api routes. Generally, we would
    use the serializer for the api route, but in this case, it's simpler
    not do.
    
    Use ApiKey#path in the template while here.

--- routes/project/inference_api_key.rb
+++ routes/project/inference_api_key.rb
@@ -4,7 +4,7 @@ class Clover
   hash_branch(:project_prefix, "inference-api-key") do |r|
     r.is do
       r.get do
-        @inference_api_keys = Serializers::InferenceApiKey.serialize(inference_api_key_ds.all)
+        @inference_api_keys = inference_api_key_ds.all
 
         if web?
           @remaining_free_quota = FreeQuota.remaining_free_quota("inference-tokens", @project.id)
@@ -12,7 +12,7 @@ class Clover
           @has_valid_payment_method = @project.has_valid_payment_method?
           view "inference/api_key/index"
         else
-          {items: @inference_api_keys.map { it.slice(:id, :key) }}
+          {items: @inference_api_keys.map { {id: it.ubid, key: it.key} }}
         end
       end
 
