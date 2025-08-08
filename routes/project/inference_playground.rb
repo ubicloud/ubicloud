24dc56c2d1ee26347e29272caeceb3a24505dce8
Jeremy Evans <jeremy@ubicloud.com>
2025-08-08 12:29:12 -0700

    Stop using serializers in the inference playground route
    Remove the InferenceEndpoint, InferenceRouterModel, and InferenceApiKey
    serializers as all are no longer used.

--- routes/project/inference_playground.rb
+++ routes/project/inference_playground.rb
@@ -3,10 +3,11 @@
 class Clover
   hash_branch(:project_prefix, "inference-playground") do |r|
     r.get web? do
-      inference_endpoints = Serializers::InferenceEndpoint.serialize(inference_endpoint_ds.eager(:location).where(Sequel.pg_jsonb_op(:tags).get_text("capability") => "Text Generation"))
-      inference_router_models = Serializers::InferenceRouterModel.serialize(inference_router_model_ds.where(Sequel.pg_jsonb_op(:tags).get_text("capability") => "Text Generation").all)
-      @inference_models = inference_router_models + inference_endpoints
-      @inference_api_keys = Serializers::InferenceApiKey.serialize(inference_api_key_ds.all)
+      @inference_models = [inference_router_model_ds, inference_endpoint_ds.eager(:location)].flat_map do |ds|
+        ds.where(Sequel.pg_jsonb_op(:tags).get_text("capability") => "Text Generation").all
+      end
+
+      @inference_api_keys = inference_api_key_ds.all
       @remaining_free_quota = FreeQuota.remaining_free_quota("inference-tokens", @project.id)
       @free_quota_unit = "inference tokens"
       @has_valid_payment_method = @project.has_valid_payment_method?
