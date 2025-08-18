0ead92f94d2522b27af2081bdee96db4d26e795a
Jeremy Evans <jeremy@ubicloud.com>
2025-08-18 14:05:48 -0700

    Support creating postgres firewall rules with descriptions in the cli/sdk

--- cli-commands/pg/post/add-firewall-rule.rb
+++ cli-commands/pg/post/add-firewall-rule.rb
@@ -3,12 +3,14 @@
 UbiCli.on("pg").run_on("add-firewall-rule") do
   desc "Add a PostgreSQL firewall rule"
 
-  banner "ubi pg (location/pg-name | pg-id) add-firewall-rule cidr"
+  options("ubi pg (location/pg-name | pg-id) add-firewall-rule [options] cidr", key: :pg_fw_rule) do
+    on("-d", "--description=desc", "description of rule")
+  end
 
   args 1
 
-  run do |cidr|
-    data = sdk_object.add_firewall_rule(cidr)
-    response("Firewall rule added to PostgreSQL database.\n  rule id: #{data[:id]}, cidr: #{data[:cidr]}")
+  run do |cidr, opts|
+    data = sdk_object.add_firewall_rule(cidr, description: opts[:pg_fw_rule][:description])
+    response("Firewall rule added to PostgreSQL database.\n  rule id: #{data[:id]}\n  cidr: #{data[:cidr]}\n  description: #{data[:description].inspect}")
   end
 end
--- spec/routes/api/cli/pg/add-firewall-rule_spec.rb
+++ spec/routes/api/cli/pg/add-firewall-rule_spec.rb
@@ -13,7 +13,22 @@ RSpec.describe Clover, "cli pg add-firewall-rule" do
     expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0]
     expect(cli(%w[pg eu-central-h1/test-pg add-firewall-rule 1.2.3.0/24])).to eq <<~END
       Firewall rule added to PostgreSQL database.
-        rule id: #{pg.firewall_rules_dataset.first(cidr: "1.2.3.0/24").ubid}, cidr: 1.2.3.0/24
+        rule id: #{pg.firewall_rules_dataset.first(cidr: "1.2.3.0/24").ubid}
+        cidr: 1.2.3.0/24
+        description: ""
+    END
+    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 1.2.3.0/24]
+  end
+
+  it "adds a firewall rule with a description to the database" do
+    cli(%w[pg eu-central-h1/test-pg create -s standard-2 -S 64])
+    pg = PostgresResource.first
+    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0]
+    expect(cli(%w[pg eu-central-h1/test-pg add-firewall-rule -d] << "Example description" << "1.2.3.0/24")).to eq <<~END
+      Firewall rule added to PostgreSQL database.
+        rule id: #{pg.firewall_rules_dataset.first(cidr: "1.2.3.0/24").ubid}
+        cidr: 1.2.3.0/24
+        description: "Example description"
     END
     expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 1.2.3.0/24]
   end
