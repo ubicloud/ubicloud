f87d30724f5185333f6e000f67ed22c198ddfefa
Jeremy Evans <jeremy@ubicloud.com>
2025-08-20 15:54:27 -0700

    Allow fw detach-subnet cli to accept ps-name for ps-id

--- cli-commands/fw/post/detach-subnet.rb
+++ cli-commands/fw/post/detach-subnet.rb
@@ -3,12 +3,12 @@
 UbiCli.on("fw").run_on("detach-subnet") do
   desc "Detch a private subnet from a firewall"
 
-  banner "ubi fw (location/fw-name | fw-id) detach-subnet ps-id"
+  banner "ubi fw (location/fw-name | fw-id) detach-subnet (ps-name | ps-id)"
 
   args 1
 
   run do |subnet_id|
-    id = sdk_object.detach_subnet(subnet_id).id
-    response("Detached private subnet with id #{subnet_id} from firewall with id #{id}")
+    id = sdk_object.detach_subnet(convert_name_to_id(sdk.private_subnet, subnet_id)).id
+    response("Detached private subnet #{subnet_id} from firewall with id #{id}")
   end
 end
