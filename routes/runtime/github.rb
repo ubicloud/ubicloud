d05eafd7f073527a192610d8320e12dfe649f844
Burak Velioglu <velioglub@ubicloud.com>
2025-08-20 16:37:58 +0300

    Setup blob storage only when adding cache entries
    In the current logic, blob storage bucket is destroyed if
    it exists but has no cache entries. Looking at the logs
    we noticed some buckets repeatedly getting deleted. This
    happens because users only request cache entries without
    ever adding new ones.
    
    To fix this, blob storage bucket will now only be created
    when adding a new cache entry. Subsequent get requests will
    still work since the blob storage will already exist once
    an entry is added.

--- routes/runtime/github.rb
+++ routes/runtime/github.rb
@@ -6,13 +6,6 @@ class Clover
       fail CloverError.new(400, "InvalidRequest", "invalid JWT format or claim in Authorization header")
     end
 
-    begin
-      repository.setup_blob_storage unless repository.access_key
-    rescue Excon::Error::HTTPStatus => ex
-      Clog.emit("Unable to setup blob storage", {failed_blob_storage_setup: {ubid: runner.ubid, repository_ubid: repository.ubid, response: ex.response.body}})
-      fail CloverError.new(400, "InvalidRequest", "unable to setup blob storage")
-    end
-
     # getCacheEntry
     r.get "cache" do
       keys, version = typecast_params.nonempty_str!(%w[keys version])
@@ -96,6 +89,13 @@ class Clover
 
         # reserveCache
         r.post do
+          begin
+            repository.setup_blob_storage unless repository.access_key
+          rescue Excon::Error::HTTPStatus => ex
+            Clog.emit("Unable to setup blob storage", {failed_blob_storage_setup: {ubid: runner.ubid, repository_ubid: repository.ubid, response: ex.response.body}})
+            fail CloverError.new(400, "InvalidRequest", "unable to setup blob storage")
+          end
+
           key, version = typecast_params.nonempty_str!(%w[key version])
           size = typecast_params.pos_int("cacheSize")
 
