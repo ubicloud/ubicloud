84292ef53f92f118945352d60c7f7e54c6093381
Jeremy Evans <jeremy@ubicloud.com>
2025-08-20 16:14:41 -0700

    Allow lb attach-vm cli to accept vm-name for vm-id

--- cli-commands/lb/post/attach-vm.rb
+++ cli-commands/lb/post/attach-vm.rb
@@ -3,12 +3,12 @@
 UbiCli.on("lb").run_on("attach-vm") do
   desc "Attach a virtual machine to a load balancer"
 
-  banner "ubi lb (location/lb-name | lb-id) attach-vm vm-id"
+  banner "ubi lb (location/lb-name | lb-id) attach-vm (vm-name | vm-id)"
 
   args 1
 
   run do |vm_id|
-    id = sdk_object.attach_vm(vm_id).id
-    response("Attached VM with id #{vm_id} to load balancer with id #{id}")
+    id = sdk_object.attach_vm(convert_name_to_id(sdk.vm, vm_id)).id
+    response("Attached VM #{vm_id} to load balancer with id #{id}")
   end
 end
