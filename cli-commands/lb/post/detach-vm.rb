9fc1ada5fd5ca3db5a3fcaa23241e00ddc3149c8
Jeremy Evans <jeremy@ubicloud.com>
2025-08-20 16:18:38 -0700

    Allow lb detach-vm cli to accept vm-name for vm-id

--- cli-commands/lb/post/detach-vm.rb
+++ cli-commands/lb/post/detach-vm.rb
@@ -3,12 +3,12 @@
 UbiCli.on("lb").run_on("detach-vm") do
   desc "Detach a virtual machine from a load balancer"
 
-  banner "ubi lb (location/lb-name | lb-id) detach-vm vm-id"
+  banner "ubi lb (location/lb-name | lb-id) detach-vm (vm-name | vm-id)"
 
   args 1
 
   run do |vm_id|
-    id = sdk_object.detach_vm(vm_id).id
-    response("Detached VM with id #{vm_id} from load balancer with id #{id}")
+    id = sdk_object.detach_vm(convert_name_to_id(sdk.vm, vm_id)).id
+    response("Detached VM #{vm_id} from load balancer with id #{id}")
   end
 end
