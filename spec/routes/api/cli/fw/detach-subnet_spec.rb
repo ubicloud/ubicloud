7a7dabe911ebf8b8c7a0bdfa7b93410d1aec2e0e
Jeremy Evans <jeremy@ubicloud.com>
2025-08-20 15:54:27 -0700

    Allow fw detach-subnet cli to accept ps-name for ps-id

--- spec/routes/api/cli/fw/detach-subnet_spec.rb
+++ spec/routes/api/cli/fw/detach-subnet_spec.rb
@@ -9,9 +9,15 @@ RSpec.describe Clover, "cli fw attach-subnet" do
     @fw = Firewall.first
   end
 
-  it "detaches firewall from subnet" do
+  it "detaches firewall from subnet by id" do
     expect(@fw.private_subnets.map(&:ubid)).to eq [@ps.ubid]
-    expect(cli(%W[fw eu-central-h1/#{@fw.name} detach-subnet #{@ps.ubid}])).to eq "Detached private subnet with id #{@ps.ubid} from firewall with id #{@fw.ubid}\n"
+    expect(cli(%W[fw eu-central-h1/#{@fw.name} detach-subnet #{@ps.ubid}])).to eq "Detached private subnet #{@ps.ubid} from firewall with id #{@fw.ubid}\n"
+    expect(@fw.reload.private_subnets).to be_empty
+  end
+
+  it "detaches firewall from subnet by name" do
+    expect(@fw.private_subnets.map(&:ubid)).to eq [@ps.ubid]
+    expect(cli(%W[fw eu-central-h1/#{@fw.name} detach-subnet test-ps])).to eq "Detached private subnet test-ps from firewall with id #{@fw.ubid}\n"
     expect(@fw.reload.private_subnets).to be_empty
   end
 end
