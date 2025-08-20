ffc6e0f981825d5eded8308f81f9edfe713b6071
Jeremy Evans <jeremy@ubicloud.com>
2025-08-20 15:05:21 -0700

    Allow fw attach-subnet cli to accept ps-name for ps-id
    As the private subnet needs to be in the same location, there is
    no need to specify the location on the command line.

--- cli-commands/fw/post/attach-subnet.rb
+++ cli-commands/fw/post/attach-subnet.rb
@@ -3,12 +3,12 @@
 UbiCli.on("fw").run_on("attach-subnet") do
   desc "Attach a private subnet to a firewall"
 
-  banner "ubi fw (location/fw-name | fw-id) attach-subnet ps-id"
+  banner "ubi fw (location/fw-name | fw-id) attach-subnet (ps-name | ps-id)"
 
   args 1
 
   run do |subnet_id|
-    id = sdk_object.attach_subnet(subnet_id).id
-    response("Attached private subnet with id #{subnet_id} to firewall with id #{id}")
+    id = sdk_object.attach_subnet(convert_name_to_id(sdk.private_subnet, subnet_id)).id
+    response("Attached private subnet #{subnet_id} to firewall with id #{id}")
   end
 end
