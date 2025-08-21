4644bba65047c788abc31fdc6b3c80a801e5ead6
Jeremy Evans <jeremy@ubicloud.com>
2025-08-21 07:21:59 -0700

    Allow ps create cli -f option to accept firewall name

--- cli-commands/ps/post/create.rb
+++ cli-commands/ps/post/create.rb
@@ -4,11 +4,14 @@ UbiCli.on("ps").run_on("create") do
   desc "Create a private subnet"
 
   options("ubi ps location/ps-name create [options]", key: :ps_create) do
-    on("-f", "--firewall-id=id", "add to given firewall")
+    on("-f", "--firewall-id=fw-id-or-name", "add to given firewall")
   end
 
   run do |opts|
     params = underscore_keys(opts[:ps_create])
+    if params[:firewall_id]
+      params[:firewall_id] = convert_name_to_id(sdk.firewall, params[:firewall_id])
+    end
     id = sdk.private_subnet.create(location: @location, name: @name, **params).id
     response("Private subnet created with id: #{id}")
   end
