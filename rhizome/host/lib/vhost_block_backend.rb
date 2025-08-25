2e8ed5175fdc70931ea446f8cb4b1cfd4a15dd14
Hadi Moshayedi <hadi@ubicloud.com>
2025-08-25 14:42:11 -0700

    Rhizome: Support Ubiblk v0.2.0.
    Ubiblk v0.2.0 made several performance improvements:
    
    - Reduced number of syscalls per IO,
    - Improved flush performance,
    - Added cpu pinning,
    - Added write-through mode.
    
    We'll keep v0.1-7 as supported and the default version while v0.2.0 is
    being tested.

--- rhizome/host/lib/vhost_block_backend.rb
+++ rhizome/host/lib/vhost_block_backend.rb
@@ -13,6 +13,10 @@ class VhostBlockBackend
       "114119bc78609db3795bcd426a386eb97a623ba78e9177de6b375b9616927ca6"
     elsif @version == "v0.1-7" && Arch.arm64?
       "aa92b91130de6e086332c4ad8dcb76e233624055532d5494db0a987883adee0f"
+    elsif @version == "v0.2.0" && Arch.x64?
+      "68638b779a227424cbe81674de4b26122ea27a337f1c6d81db895a1ffa3a917a"
+    elsif @version == "v0.2.0" && Arch.arm64?
+      "6b15ca96cd3134524a639d3c913b43b794423a8dddc897c1604a5085625d44e3"
     else
       fail "Unsupported version: #{@version}, #{Arch.sym}"
     end
