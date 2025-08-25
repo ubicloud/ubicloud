82730ae6fa92820272992e33985b7c8b1c6e11cc
Jeremy Evans <jeremy@ubicloud.com>
2025-08-25 13:27:53 -0700

    Render error template if route did not use handle_validation_failure
    This removes the old, now untested fallback code that submitted
    an internal request to the presumed referrer. Instead, a simple
    error page is shown, similar to the one used for 404 and 500
    responses.
    
    Add a test so the entire error handler is tested.

--- helpers/web.rb
+++ helpers/web.rb
@@ -56,10 +56,9 @@ class Clover < Roda
   end
 
   def redirect_back_with_inputs(error)
-    # :nocov:
+    flash.sweep
+
     if (template = @validation_failure_template)
-      # :nocov:
-      flash.sweep
       @validation_failure_block&.call
 
       request.on do
@@ -86,46 +85,16 @@ class Clover < Roda
       }
     end
 
-    # :nocov:
-    # This code path is deprecated and will be removed after all routes have been updated
-    # to use handle_validation_failure.
-
-    if Config.test?
+    if Config.test? && !ENV["SHOW_WEB_ERROR_PAGE"]
       # Raise error in the tests if we get here. If this error is raised, the route
       # should be fixed to call handle_validation_failure.
       raise "Request failure without handle_validation_failure: #{request.request_method} #{request.path_info}"
     end
 
-    referrer = flash["referrer"] || env["HTTP_REFERER"]
-    uri = begin
-      Kernel.URI(referrer)
-    rescue URI::InvalidURIError, ArgumentError
-      nil
-    end
-
-    request.redirect "/" unless uri
-
-    flash["old"] = redirect_back_with_inputs_params
-
-    if uri && env["REQUEST_METHOD"] != "GET"
-      # Force flash rotation, so flash works correctly for internal redirects
-      _roda_after_40__flash(nil)
-
-      rack_response = Clover.call(env.merge("REQUEST_METHOD" => "GET", "PATH_INFO" => uri.path, "rack.input" => StringIO.new("".b), "rack.request.form_input" => nil, "rack.request.form_hash" => nil))
-      flash.discard
-      flash["referrer"] = referrer
-      env.delete("roda.session.serialized")
-      rack_response[0] = response.status || 400
-      request.halt rack_response
-    else
-      request.redirect referrer
+    request.on do
+      view "error"
     end
   end
-
-  def redirect_back_with_inputs_params
-    request.params
-  end
-  # :nocov:
 
   def redirect_default_project_dashboard
     if (project = current_account.projects_dataset.order(:created_at, :name).first)
