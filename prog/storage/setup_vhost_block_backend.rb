e4bebfcf5d046d4842219c19f76191246f3b5190
Hadi Moshayedi <hadi@ubicloud.com>
2025-08-25 14:45:48 -0700

    Clover: Support Ubiblk v0.2.0.
    Ubiblk v0.2.0 made several performance improvements:
    
    - Reduced number of syscalls per IO,
    - Improved flush performance by avoiding unnecessary metadata flushes,
    - Added cpu pinning,
    - Added write-through mode.
    
    We'll keep v0.1-7 as supported and the default version while v0.2.0 is
    being tested.

--- prog/storage/setup_vhost_block_backend.rb
+++ prog/storage/setup_vhost_block_backend.rb
@@ -5,7 +5,9 @@ class Prog::Storage::SetupVhostBlockBackend < Prog::Base
 
   SUPPORTED_VHOST_BLOCK_BACKEND_VERSIONS = [
     ["v0.1-7", "x64"],
-    ["v0.1-7", "arm64"]
+    ["v0.1-7", "arm64"],
+    ["v0.2.0", "x64"],
+    ["v0.2.0", "arm64"]
   ].freeze.each(&:freeze)
 
   def self.assemble(vm_host_id, version, allocation_weight: 0)
