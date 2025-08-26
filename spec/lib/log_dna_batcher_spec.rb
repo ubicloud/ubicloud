db28dbef7a85cc9253cba0a34ff860f14dbf2a30
Jeremy Evans <jeremy@ubicloud.com>
2025-08-25 17:10:34 -0700

    Fix leaked threads in LogDnaBatcher spec
    This requires some modifications and additions to the specs to
    keep 100% coverage.

--- spec/lib/log_dna_batcher_spec.rb
+++ spec/lib/log_dna_batcher_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe LogDnaBatcher do
     let(:log_dna_batcher) { described_class.new("dummy-key") }
     let(:http) { log_dna_batcher.ensure_connection }
 
+    after { log_dna_batcher.stop }
+
     describe "#send_batch" do
       it "prints failure message when response is not success" do
         expect(http).to receive(:request).and_return(Net::HTTPBadRequest.new(nil, 400, "Bad Request"))
@@ -56,6 +58,8 @@ RSpec.describe LogDnaBatcher do
 
         log_dna_batcher.ensure_connection
         log_dna_batcher.ensure_connection
+      ensure
+        log_dna_batcher.stop
       end
     end
   end
@@ -64,6 +68,8 @@ RSpec.describe LogDnaBatcher do
     let(:log_dna_batcher) { described_class.new("dummy-key", base_url: "https://localhost/logdna/ingest", flush_interval: 10000, max_batch_size: 100) }
     let(:http) { log_dna_batcher.ensure_connection }
 
+    after { log_dna_batcher.stop }
+
     before do
       allow(http).to receive(:request).and_return(Net::HTTPSuccess.new(nil, nil, nil))
     end
@@ -71,6 +77,21 @@ RSpec.describe LogDnaBatcher do
     it "sends batch when input queue is closed" do
       expect(log_dna_batcher).to receive(:send_batch).exactly(:once)
       log_dna_batcher.stop
+    end
+
+    it "adds to batch if not over the limit" do
+      expected = false
+      q = Queue.new
+      log_dna_batcher.instance_variable_set(:@max_batch_size, 2)
+      log_dna_batcher.define_singleton_method(:send_batch) do |batch|
+        raise unless expected
+        q.push(batch.dup)
+        super(batch)
+      end
+      log_dna_batcher.log("test log")
+      expected = true
+      log_dna_batcher.log("test log 2")
+      expect(q.pop.map { it[:line] }).to eq ["test log", "test log 2"]
     end
 
     it "sends the batch when flush interval is exceeded" do
@@ -106,10 +127,16 @@ RSpec.describe LogDnaBatcher do
         m.call(*args)
       end
 
-      expect(log_dna_batcher).to receive(:send_batch).at_least(:once).and_wrap_original do |m, *args|
+      first_time = true
+      expect(log_dna_batcher).to receive(:send_batch).at_least(:once).and_wrap_original do |m, batch|
         # Reset max batch size to a high value to avoid immediate re-send(s)
         log_dna_batcher.instance_variable_set(:@max_batch_size, 100)
         q.push(true)
+        m.call(batch)
+        if first_time
+          first_time = false
+          batch << "test log"
+        end
       end
 
       log_dna_batcher.log("test log")
@@ -119,25 +146,33 @@ RSpec.describe LogDnaBatcher do
     end
 
     it "does not send the batch until one of the pre-conditions are satisfied" do
-      expect(log_dna_batcher).not_to receive(:send_batch)
+      called = false
+      log_dna_batcher.define_singleton_method(:send_batch) do
+        called = true
+        super(it)
+      end
       log_dna_batcher.log("test log")
+      expect(called).to be false
     end
 
     it "logs error in case of an exception during batch processing" do
       q = Queue.new
 
       expect(log_dna_batcher.instance_variable_get(:@input_queue)).to receive(:closed?).at_least(:once).and_wrap_original do |m, *args|
-        q.push(true)
         m.call(*args)
         true
       end
 
-      log_dna_batcher.log("test log")
-
       expect(log_dna_batcher.instance_variable_get(:@input_queue)).to receive(:empty?).and_raise(StandardError, "Unexpected error")
       expect(log_dna_batcher).to receive(:puts).with("Error in processor: Unexpected error").ordered
       expect(log_dna_batcher).to receive(:puts).with(anything).ordered
-      expect(log_dna_batcher).to receive(:exit).with(1)
+      expect(log_dna_batcher).to receive(:exit) do |status|
+        expect(status).to eq 1
+        q.push(true)
+        raise StopIteration
+      end
+
+      log_dna_batcher.log("test log")
 
       expect(q.pop(timeout: 5)).to be true
       expect(q.empty?).to be(true)
@@ -149,6 +184,8 @@ RSpec.describe LogDnaBatcher do
       log_dna_batcher = described_class.new("dummy-key", base_url: "https://localhost/logdna/ingest?param=test")
 
       expect { log_dna_batcher.close_connection }.not_to raise_error
+    ensure
+      log_dna_batcher.stop
     end
   end
 end
