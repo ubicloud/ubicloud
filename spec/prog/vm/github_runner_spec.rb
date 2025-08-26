af4133ed10c43222e4b8a2f1790834e5079e7211
Enes Cakir <enes@ubicloud.com>
2025-08-26 15:34:12 +0300

    Decrease the initial nap for wait_vm from 11s to 10s
    Recently, we moved the runneradmin user creation to the image
    generation. It reduced booting duration by ~1-2 seconds.
    
    https://github.com/ubicloud/runner-images/pull/44
    
    We have bunch of VMs that are provisioned in less than 11 seconds.
    Therefore, we can reduce the initial nap for wait_vm from 11 seconds to
    10 seconds.

--- spec/prog/vm/github_runner_spec.rb
+++ spec/prog/vm/github_runner_spec.rb
@@ -417,9 +417,9 @@ RSpec.describe Prog::Vm::GithubRunner do
   end
 
   describe "#wait_vm" do
-    it "naps 11 seconds if vm is not allocated yet" do
+    it "naps 10 seconds if vm is not allocated yet" do
       vm.update(allocated_at: nil)
-      expect { nx.wait_vm }.to nap(11)
+      expect { nx.wait_vm }.to nap(10)
     end
 
     it "naps a second if vm is allocated but not provisioned yet" do
