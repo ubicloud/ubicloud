fb6731aa59f90abe038c6ff8545f3a5e9c8435b0
Jeremy Evans <jeremy@ubicloud.com>
2025-09-03 14:35:26 -0700

    Update to Puma 7
    Use silence_fork_callback_warning to avoid the warning when
    using before_fork without workers.
    
    Puma changed from on_booted to after_booted event in 7, so update
    to that.

--- puma_config.rb
+++ puma_config.rb
@@ -5,13 +5,11 @@ environment ENV["RACK_ENV"] || "development"
 port ENV["PORT"] || "3000"
 threads 15, 15
 enable_keep_alives false
+silence_fork_callback_warning
+silence_single_worker_warning
+preload_app!
 
-if @config.options[:workers] > 0
-  silence_single_worker_warning
-  preload_app!
-
-  before_fork do
-    Sequel::DATABASES.each(&:disconnect)
-  end
+before_fork do
+  Sequel::DATABASES.each(&:disconnect)
 end
 # :nocov:
