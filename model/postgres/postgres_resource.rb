1ea9e96fc0b63a7aa0e7cec7178a71a24035028a
Jeremy Evans <jeremy@ubicloud.com>
2025-09-03 10:31:50 -0700

    Add PostgresResource#incr_restart
    This adds a restart semaphore to all servers. This method is added
    to allow the same incr_restart method to work with both Vm and
    PostgresResource.

--- model/postgres/postgres_resource.rb
+++ model/postgres/postgres_resource.rb
@@ -137,6 +137,10 @@ class PostgresResource < Sequel::Model
     servers.any? { it.taking_over? }
   end
 
+  def incr_restart
+    Semaphore.incr(servers_dataset.select(:id), "restart")
+  end
+
   module HaType
     NONE = "none"
     ASYNC = "async"
