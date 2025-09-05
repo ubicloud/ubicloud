0efff421920cc12516a86a2f4bf808363a28eb93
Hadi Moshayedi <hadi@ubicloud.com>
2025-09-05 12:00:12 -0700

    Use vring_workers as num_queues of Ubiblk backed volumes.
    Ubiblk creates one vring_worker thread for each queue. SPDK doesn't
    support multithreaded vhost backends, so we keep num_queues of SPDK
    volumes as 1 regardless of the value of `vring_workers`.

--- model/vm_storage_volume.rb
+++ model/vm_storage_volume.rb
@@ -33,7 +33,7 @@ class VmStorageVolume < Sequel::Model
 
   def num_queues
     @num_queues ||= if vhost_block_backend
-      [vm.vcpus / 2, 1].max
+      vring_workers || [vm.vcpus / 2, 1].max
     else
       # SPDK volumes
       1
