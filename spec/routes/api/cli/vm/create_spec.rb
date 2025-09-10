e765514f7e8298dc2785e7fd412ee59c27d0dc0b
Jeremy Evans <jeremy@ubicloud.com>
2025-09-10 16:14:46 -0700

    Support creating a VM using a registered SSH public key in the api/sdk/cli
    Since a registered SSH public key name can never be a valid SSH public key,
    as an SSH public key must include a space, and a registered SSH public key
    name cannot include a space, if a public key name without a space is given,
    assume it can be a registered SSH public key name, and look up the public
    key.
    
    This required removing the public key validity checks from the cli. To
    still provide nice error messages, recognize invalid public key error
    messages, and provide a better explanation when displaying the error.

--- spec/routes/api/cli/vm/create_spec.rb
+++ spec/routes/api/cli/vm/create_spec.rb
@@ -24,6 +24,28 @@ RSpec.describe Clover, "cli vm create" do
     expect(body).to eq "VM created with id: #{vm.ubid}\n"
   end
 
+  it "creates vm registered SSH public key" do
+    expect(Vm.count).to eq 0
+    expect(PrivateSubnet.count).to eq 0
+    cli(%w[sk spk create] << "a a")
+    body = cli(%w[vm eu-central-h1/test-vm create spk])
+    expect(Vm.count).to eq 1
+    expect(PrivateSubnet.count).to eq 1
+    vm = Vm.first
+    expect(vm).to be_a Vm
+    ps = PrivateSubnet.first
+    expect(ps).to be_a PrivateSubnet
+    expect(vm.name).to eq "test-vm"
+    expect(vm.public_key).to eq "a a"
+    expect(vm.display_location).to eq "eu-central-h1"
+    expect(vm.display_size).to eq "standard-2"
+    expect(vm.boot_image).to eq Config.default_boot_image_name
+    expect(vm.ip4_enabled).to be true
+    expect(vm.strand.stack[0]["storage_volumes"][0]["size_gib"]).to eq 40
+    expect(vm.nics.first.private_subnet_id).to eq ps.id
+    expect(body).to eq "VM created with id: #{vm.ubid}\n"
+  end
+
   it "creates vm with all options, specifying private subnet by id" do
     expect(Vm.count).to eq 0
     ps = PrivateSubnet.create(project_id: @project.id, name: "test-ps", location_id: Location[name: "hetzner-hel1"].id, net6: "fe80::/64", net4: "192.168.0.0/24")
