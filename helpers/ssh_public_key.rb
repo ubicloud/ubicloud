956f58ef94d8519dd53bea6fde4a06def5fa24d2
Jeremy Evans <jeremy@ubicloud.com>
2025-09-10 15:24:35 -0700

    Add support for managing SSH public keys via the api
    No direct specs for this, it will be tested via cli specs in a
    coming commit.

--- helpers/ssh_public_key.rb
+++ helpers/ssh_public_key.rb
@@ -7,14 +7,25 @@ class Clover
       flash.now["error"] = "Error #{create ? "registering" : "updating"} SSH public key"
     end
 
-    @ssh_public_key.name, @ssh_public_key.public_key = typecast_params.nonempty_str(%w[name public_key])
+    name, public_key = typecast_params.nonempty_str(%w[name public_key])
+    if create || web?
+      @ssh_public_key.name = name
+      @ssh_public_key.public_key = public_key
+    else
+      @ssh_public_key.name = name if name
+      @ssh_public_key.public_key = public_key if public_key
+    end
 
     DB.transaction do
       @ssh_public_key.save_changes
       audit_log(@ssh_public_key, create ? "create" : "update")
     end
 
-    flash["notice"] = "SSH public key with name #{@ssh_public_key.name} #{create ? "registered" : "updated"}"
-    request.redirect "#{@project.path}/ssh-public-key"
+    if api?
+      Serializers::SshPublicKey.serialize(@ssh_public_key, detailed: true)
+    else
+      flash["notice"] = "SSH public key with name #{@ssh_public_key.name} #{create ? "registered" : "updated"}"
+      request.redirect "#{@project.path}/ssh-public-key"
+    end
   end
 end
