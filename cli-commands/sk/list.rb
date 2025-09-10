8df4b7a10bf2e6d637f4b6758424113b217ce038
Jeremy Evans <jeremy@ubicloud.com>
2025-09-10 15:26:30 -0700

    Add support for managing SSH public keys to the CLI

---
+++ cli-commands/sk/list.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+UbiCli.on("sk", "list") do
+  desc "List SSH public keys"
+
+  key = :ssh_public_key_list
+
+  options("ubi sk (sk-name | sk-id) list [options]", key:) do
+    on("-N", "--no-headers", "do not show headers")
+  end
+
+  run do |opts|
+    opts = opts[key]
+    items = sdk.ssh_public_key.list
+    response(format_rows(%i[id name], items, headers: opts[:"no-headers"] != false))
+  end
+end
