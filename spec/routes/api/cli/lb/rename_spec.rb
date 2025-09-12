a566ba4b1d040921339ecbb980604ca48f57cf35
Jeremy Evans <jeremy@ubicloud.com>
2025-09-11 17:07:37 -0700

    Rewrite dns records and refresh cert when renaming load balancer
    Since this is a potentially dangerous operation, move the rename
    form in the Web UI to the Danger Zone.

--- spec/routes/api/cli/lb/rename_spec.rb
+++ spec/routes/api/cli/lb/rename_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe Clover, "cli lb rename" do
   it "renames load balancer" do
     cli(%w[ps eu-central-h1/test-ps create])
     cli(%W[lb eu-central-h1/test-lb create #{PrivateSubnet.first.ubid} 12345 54321])
+    lb = LoadBalancer.first
+    expect(lb.semaphores_dataset.all).to eq []
     expect(cli(%w[lb eu-central-h1/test-lb rename new-name])).to eq "Load balancer renamed to new-name\n"
     expect(LoadBalancer.first.name).to eq "new-name"
+    expect(lb.semaphores_dataset.select_order_map(:name)).to eq %w[refresh_cert rewrite_dns_records]
   end
 end
