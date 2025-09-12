788f5cc6728d64c8597f8d4eb96fd784332e3a8e
Jeremy Evans <jeremy@ubicloud.com>
2025-09-12 14:56:18 -0700

    Allow disconnecting from postgres private subnets in the api/cli/sdk
    Similar to connecting, this uses a separate option. Rather than copying
    the connect route code to disconnect and modify it, since the majority
    of the route code is the same, this combines the private subnet connect
    and disconnect routes using a private_subnet_connection_action helper
    method.
    
    In order to tell the difference between postgres and private subnet
    ids, we need to keep in ubid format so we can check the first two
    characters. Add ubid typecast param and symbol matcher to support that.

--- spec/routes/api/cli/ps/disconnect_spec.rb
+++ spec/routes/api/cli/ps/disconnect_spec.rb
@@ -21,4 +21,21 @@ RSpec.describe Clover, "cli ps connect" do
     expect(cli(%W[ps eu-central-h1/test-ps disconnect eu-central-h1/test-ps2])).to eq "Disconnected private subnet eu-central-h1/test-ps2 from #{@ps1.ubid}\n"
     expect(ConnectedSubnet.count).to eq 0
   end
+
+  it "disconnects requested postgres private subnet to this subnet by postgres resource id" do
+    expect(Config).to receive(:postgres_service_project_id).and_return(@project.id).at_least(:once)
+    cli(%w[pg eu-central-h1/test-pg create -s standard-2 -S 64 -C test-ps])
+    expect(ConnectedSubnet.count).to eq 2
+    pg = PostgresResource.first
+    expect(cli(%W[ps eu-central-h1/test-ps disconnect -P #{pg.ubid}])).to eq "Disconnected PostgreSQL database private subnet #{pg.ubid} from #{@ps1.ubid}\n"
+    expect(ConnectedSubnet.count).to eq 1
+  end
+
+  it "disconnects requested postgres private subnet to this subnet by postgres resource name" do
+    expect(Config).to receive(:postgres_service_project_id).and_return(@project.id).at_least(:once)
+    cli(%w[pg eu-central-h1/test-pg create -s standard-2 -S 64 -C test-ps])
+    expect(ConnectedSubnet.count).to eq 2
+    expect(cli(%W[ps eu-central-h1/test-ps disconnect -P test-pg])).to eq "Disconnected PostgreSQL database private subnet test-pg from #{@ps1.ubid}\n"
+    expect(ConnectedSubnet.count).to eq 1
+  end
 end
