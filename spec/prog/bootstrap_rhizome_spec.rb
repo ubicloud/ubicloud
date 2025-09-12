7694c4495c0a4d0874da152930b0f3126c921c6f
Eren Başak <eren@ubicloud.com>
2025-09-12 09:11:30 +0300

    Check bundle, not bundler in bootstrap_rhizome
    Co-authored-by: Jeremy Evans <code@jeremyevans.net>

--- spec/prog/bootstrap_rhizome_spec.rb
+++ spec/prog/bootstrap_rhizome_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Prog::BootstrapRhizome do
 set -ueo pipefail
 sudo apt-get update
 sudo apt-get -y install ruby-bundler
-sudo which bundler
+sudo which bundle
 sudo userdel -rf rhizome || true
 sudo adduser --disabled-password --gecos '' rhizome
 echo 'rhizome ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/98-rhizome
