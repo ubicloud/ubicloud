978577e5f42d02822b72bb9fb55392479284ec3e
Hadi Moshayedi <hadi@ubicloud.com>
2025-09-16 09:56:57 -0700

    Update the default ubiblk version to v0.2.0.
    Now that we have tested ubiblk v0.2.0 in prod for a week, we can change
    the default version from v0.1-7 to v0.2.0.

--- config.rb
+++ config.rb
@@ -134,7 +134,7 @@ module Config
   override :spdk_version, "v23.09-ubi-0.3"
 
   # Vhost Block Backend
-  override :vhost_block_backend_version, "v0.1-7"
+  override :vhost_block_backend_version, "v0.2.0"
 
   # Boot Images
   override :default_boot_image_name, "ubuntu-jammy", string
