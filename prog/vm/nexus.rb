09fd2a326158b243070b31168d50f678a98842a3
Enes Cakir <enes@ubicloud.com>
2025-09-17 10:29:23 +0300

    Round VM and runner provisioning times in logs
    Durations in logs currently have 8 digits of precision, which is
    unnecessary and makes logs harder to read. This change rounds durations
    to 3 digits of precision, which is sufficient for our needs.

--- prog/vm/nexus.rb
+++ prog/vm/nexus.rb
@@ -366,7 +366,7 @@ class Prog::Vm::Nexus < Prog::Base
   label def create_billing_record
     vm.update(display_state: "running", provisioned_at: Time.now)
 
-    Clog.emit("vm provisioned") { [vm, {provision: {vm_ubid: vm.ubid, vm_host_ubid: host&.ubid, duration: Time.now - vm.allocated_at}}] }
+    Clog.emit("vm provisioned") { [vm, {provision: {vm_ubid: vm.ubid, vm_host_ubid: host&.ubid, duration: (Time.now - vm.allocated_at).round(3)}}] }
 
     project = vm.project
     hop_wait unless project.billable
