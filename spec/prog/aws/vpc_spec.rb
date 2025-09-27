969f25aa5c646c56011a08fdf41cc3aa65798214
Enes Cakir <enes@ubicloud.com>
2025-09-27 10:59:06 +0300

    Fix Vnet::SubnetNexus#destroy to not violate strand leases
    Previously, Vnet::SubnetNexus#destroy ran:
    
    ```ruby
    strand.children.select { it.prog == "Aws::Vpc" }.each { it.destroy }
    ```
    
    This is not safe as there could be an existing lease for Aws::Vpc
    children.
    
    @jeremy fixed a similar issue at 773562ebc.
    
    Fix this by private subnet increments the destroy semaphore instead of
    destroying directly. Aws::Vpc will pop out if the destroy semaphore is
    incremented.
    
    I didn’t add semaphore :destroy to Aws::Vpc, since one of its subjects
    is :private_subnet, which already has a destroy semaphore.

--- spec/prog/aws/vpc_spec.rb
+++ spec/prog/aws/vpc_spec.rb
@@ -24,7 +24,13 @@ RSpec.describe Prog::Aws::Vpc do
 
   before do
     allow(nx).to receive(:private_subnet).and_return(ps)
-    expect(Aws::EC2::Client).to receive(:new).with(access_key_id: "test-access-key", secret_access_key: "test-secret-key", region: "us-west-2").and_return(client)
+    allow(Aws::EC2::Client).to receive(:new).with(access_key_id: "test-access-key", secret_access_key: "test-secret-key", region: "us-west-2").and_return(client)
+  end
+
+  it "exits if destroy is set" do
+    expect(nx.before_run).to be_nil
+    expect(nx).to receive(:when_destroy_set?).and_yield
+    expect { nx.before_run }.to exit({"msg" => "exiting early due to destroy semaphore"})
   end
 
   describe "#create_vpc" do
