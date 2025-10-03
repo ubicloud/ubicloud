26d2b0551ee3dfadea42f7f3d7b69b4ecfb7637f
Jeremy Evans <jeremy@ubicloud.com>
2025-10-03 13:59:01 -0700

    Add support for managing VM init scripts to the API
    No direct specs for this, it will be tested via CLI specs in a
    coming commit.

--- helpers/vm_init_script.rb
+++ helpers/vm_init_script.rb
@@ -7,14 +7,25 @@ class Clover
       flash.now["error"] = "Error #{create ? "registering" : "updating"} virtual machine init script"
     end
 
-    @vm_init_script.name, @vm_init_script.script = typecast_params.nonempty_str(%w[name script])
+    name, script = typecast_params.nonempty_str(%w[name script])
+    if create || web?
+      @vm_init_script.name = name
+      @vm_init_script.script = script
+    else
+      @vm_init_script.name = name if name
+      @vm_init_script.script = script if script
+    end
 
     DB.transaction do
       @vm_init_script.save_changes
       audit_log(@vm_init_script, create ? "create" : "update")
     end
 
-    flash["notice"] = "Virtual machine init script with name #{@vm_init_script.name} #{create ? "registered" : "updated"}"
-    request.redirect "#{@project.path}/vm-init-script"
+    if api?
+      Serializers::VmInitScript.serialize(@vm_init_script, detailed: true)
+    else
+      flash["notice"] = "Virtual machine init script with name #{@vm_init_script.name} #{create ? "registered" : "updated"}"
+      request.redirect "#{@project.path}/vm-init-script"
+    end
   end
 end
--- routes/project/vm_init_script.rb
+++ routes/project/vm_init_script.rb
@@ -2,44 +2,55 @@
 
 class Clover
   hash_branch(:project_prefix, "vm-init-script") do |r|
-    r.web do
-      authorize("Project:edit", @project.id)
+    authorize("Project:edit", @project.id)
 
-      r.is do
-        r.get do
+    r.is do
+      r.get do
+        if api?
+          {items: Serializers::VmInitScript.serialize(@project.vm_init_scripts)}
+        else
           view "vm-init-script/index"
         end
-
-        r.post do
-          @vm_init_script = VmInitScript.new(project_id: @project.id)
-          vm_init_script_save
-        end
       end
 
-      r.is :ubid_uuid do |uuid|
-        next unless (@vm_init_script = @project.vm_init_scripts_dataset.with_pk(uuid))
+      r.post do
+        @vm_init_script = VmInitScript.new(project_id: @project.id)
+        vm_init_script_save
+      end
+    end
 
-        r.get do
+    r.get web?, "register" do
+      @vm_init_script = VmInitScript.new
+      view "vm-init-script/register"
+    end
+
+    r.is VM_INIT_SCRIPT_NAME_OR_UBID do |name, id|
+      @vm_init_script = if name
+        @project.vm_init_scripts_dataset.first(name:)
+      else
+        @project.vm_init_scripts_dataset.with_pk(id)
+      end
+      check_found_object(@vm_init_script)
+
+      r.get do
+        if api?
+          Serializers::VmInitScript.serialize(@vm_init_script, detailed: true)
+        else
           view "vm-init-script/register"
         end
-
-        r.post do
-          vm_init_script_save
-        end
-
-        r.delete do
-          DB.transaction do
-            @vm_init_script.destroy
-            audit_log(@vm_init_script, "destroy")
-          end
-
-          204
-        end
       end
 
-      r.get "register" do
-        @vm_init_script = VmInitScript.new
-        view "vm-init-script/register"
+      r.post do
+        vm_init_script_save
+      end
+
+      r.delete do
+        DB.transaction do
+          @vm_init_script.destroy
+          audit_log(@vm_init_script, "destroy")
+        end
+
+        204
       end
     end
   end
