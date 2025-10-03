da54dbba9feec1f1d331de83ffe3b7646e370bbc
Jeremy Evans <jeremy@ubicloud.com>
2025-10-03 12:42:10 -0700

    Add migration for vm_init_script table
    This adds a foreign key in the vm table for the init script, and
    a string column for the args to the init script. This allows easy
    separation of per-VM data from multiple VM data
    
    Unlike SSH public keys, We will not track per-VM init scripts, all
    VM init scripts will need to be registered with the project to be
    used (we can support auto registration on VM creation if we want).

--- model/vm.rb
+++ model/vm.rb
@@ -309,12 +309,15 @@ end
 #  cpu_percent_limit       | integer                  |
 #  cpu_burst_percent_limit | integer                  |
 #  location_id             | uuid                     | NOT NULL
+#  init_script_id          | uuid                     |
+#  init_script_args        | text                     |
 # Indexes:
 #  vm_pkey                             | PRIMARY KEY btree (id)
 #  vm_ephemeral_net6_key               | UNIQUE btree (ephemeral_net6)
 #  vm_project_id_location_id_name_uidx | UNIQUE btree (project_id, location_id, name)
 #  vm_pool_id_index                    | btree (pool_id) WHERE pool_id IS NOT NULL
 # Foreign key constraints:
+#  vm_init_script_id_fkey   | (init_script_id) REFERENCES vm_init_script(id)
 #  vm_location_id_fkey      | (location_id) REFERENCES location(id)
 #  vm_pool_id_fkey          | (pool_id) REFERENCES vm_pool(id)
 #  vm_project_id_fkey       | (project_id) REFERENCES project(id)
