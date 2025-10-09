8226258a55422d37ad50382ec023e08141da13ef
Furkan Sahin <furkan@ubicloud.com>
2025-10-09 14:28:32 +0200

    Add cert-enabled to the lb show cli/sdk

--- cli-commands/lb/post/show.rb
+++ cli-commands/lb/post/show.rb
@@ -3,7 +3,7 @@
 UbiCli.on("lb").run_on("show") do
   desc "Show details for a load balancer"
 
-  fields = %w[id name state location hostname algorithm stack health-check-endpoint health-check-protocol src-port dst-port subnet vms].freeze.each(&:freeze)
+  fields = %w[id name state location hostname algorithm stack health-check-endpoint health-check-protocol src-port dst-port subnet vms cert-enabled].freeze.each(&:freeze)
 
   options("ubi lb (location/lb-name | lb-id) show [options]", key: :lb_show) do
     on("-f", "--fields=fields", "show specific fields (comma separated)")
