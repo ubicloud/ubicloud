17ca2741b54dc20772ef9fd0c02f9e2f2fe16648
Jeremy Evans <jeremy@ubicloud.com>
2025-10-09 15:01:12 -0700

    Use eager loading to avoid N+1 query issues in a bunch of places

--- routes/project/inference_endpoint.rb
+++ routes/project/inference_endpoint.rb
@@ -3,7 +3,8 @@
 class Clover
   hash_branch(:project_prefix, "inference-endpoint") do |r|
     r.get web? do
-      @inference_models = inference_endpoint_ds.eager(:location).all + inference_router_model_ds.all
+      @inference_models = inference_endpoint_ds.eager(:location, load_balancer: :private_subnet).all +
+        inference_router_model_ds.eager(inference_router: {load_balancer: :private_subnet}).all
       @remaining_free_quota = FreeQuota.remaining_free_quota("inference-tokens", @project.id)
       @free_quota_unit = "inference tokens"
       @has_valid_payment_method = @project.has_valid_payment_method?
