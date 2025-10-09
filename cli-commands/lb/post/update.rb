14cc43793609e33fac651caf3ee225a2a5aff311
Furkan Sahin <furkan@ubicloud.com>
2025-10-09 14:09:18 +0200

    Integrate the new cert-enabled to the lb update endpoint
    Unfortunately this is not a backward compatible change because the lb
    update command gets a vm array at the very end. If we include a nullable
    argument here, it is ambigious if that boolean is a vm name or not.

--- cli-commands/lb/post/update.rb
+++ cli-commands/lb/post/update.rb
@@ -3,15 +3,16 @@
 UbiCli.on("lb").run_on("update") do
   desc "Update a load balancer"
 
-  banner "ubi lb (location/lb-name | lb-id) update algorithm src-port dst-port health-check-endpoint [(vm-name | vm-id) [...]]"
+  banner "ubi lb (location/lb-name | lb-id) update algorithm src-port dst-port health-check-endpoint cert-enabled [(vm-name | vm-id) [...]]"
 
   args(4...)
 
   run do |argv, _, cmd|
-    algorithm, src_port, dst_port, health_check_endpoint, *vms = argv
+    algorithm, src_port, dst_port, health_check_endpoint, cert_enabled, *vms = argv
     src_port = need_integer_arg(src_port, "src-port", cmd)
     dst_port = need_integer_arg(dst_port, "dst-port", cmd)
-    id = sdk_object.update(algorithm:, src_port:, dst_port:, health_check_endpoint:, vms: vms.map { convert_name_to_id(sdk.vm, it) }).id
+    cert_enabled = need_boolean_arg(cert_enabled, "cert-enabled", cmd)
+    id = sdk_object.update(algorithm:, src_port:, dst_port:, health_check_endpoint:, cert_enabled:, vms: vms.map { convert_name_to_id(sdk.vm, it) }).id
     response("Updated load balancer with id #{id}")
   end
 end
--- routes/project/location/load_balancer.rb
+++ routes/project/location/load_balancer.rb
@@ -80,6 +80,8 @@ class Clover
           algorithm, health_check_endpoint = typecast_params.nonempty_str!(%w[algorithm health_check_endpoint])
           src_port, dst_port = typecast_params.pos_int!(%w[src_port dst_port])
           vm_ids = typecast_params.array(:ubid_uuid, "vms")
+          cert_enabled = typecast_params.bool!("cert_enabled")
+          cert_enablement_changed = lb.cert_enabled != cert_enabled
 
           DB.transaction do
             lb.update(algorithm:, health_check_endpoint:)
@@ -107,6 +109,9 @@ class Clover
             end
 
             lb.incr_update_load_balancer
+            if cert_enablement_changed
+              cert_enabled ? lb.enable_cert_server : lb.disable_cert_server
+            end
             audit_log(lb, "update")
           end
           Serializers::LoadBalancer.serialize(lb.reload, {detailed: true})
