cef19fea556b8c551a0092cc414ef748f253ec68
Jeremy Evans <jeremy@ubicloud.com>
2025-10-22 16:29:00 -0700

    Make postgres POST firewall-rule route insert 2 FirewallRules
    Instead of inserting 1 PostgresFirewallRule. This is a little
    awkward, since we can return only one rule. Return the 5432 rule
    in this case.
    
    Use the same description for both created FirewallRules.

--- spec/routes/api/cli/pg/add-firewall-rule_spec.rb
+++ spec/routes/api/cli/pg/add-firewall-rule_spec.rb
@@ -10,26 +10,26 @@ RSpec.describe Clover, "cli pg add-firewall-rule" do
   it "adds a firewall rule to the database" do
     cli(%w[pg eu-central-h1/test-pg create -s standard-2 -S 64])
     pg = PostgresResource.first
-    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 ::/0]
+    expect(pg.pg_firewall_rules.map { it.cidr.to_s }.uniq).to eq %w[0.0.0.0/0 ::/0]
     expect(cli(%w[pg eu-central-h1/test-pg add-firewall-rule 1.2.3.0/24])).to eq <<~END
       Firewall rule added to PostgreSQL database.
-        rule id: #{pg.firewall_rules_dataset.first(cidr: "1.2.3.0/24").ubid}
+        rule id: #{pg.pg_firewall_rules.find { it.cidr.to_s == "1.2.3.0/24" && it.port_range.begin == 5432 }.ubid}
         cidr: 1.2.3.0/24
         description: ""
     END
-    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 1.2.3.0/24 ::/0]
+    expect(pg.pg_firewall_rules.map { it.cidr.to_s }.uniq).to eq %w[0.0.0.0/0 1.2.3.0/24 ::/0]
   end
 
-  it "adds a firewall rule with a description to the database" do
+  it "ignores description when adding rule" do
     cli(%w[pg eu-central-h1/test-pg create -s standard-2 -S 64])
     pg = PostgresResource.first
-    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 ::/0]
+    expect(pg.pg_firewall_rules.map { it.cidr.to_s }.uniq).to eq %w[0.0.0.0/0 ::/0]
     expect(cli(%w[pg eu-central-h1/test-pg add-firewall-rule -d] << "Example description" << "1.2.3.0/24")).to eq <<~END
       Firewall rule added to PostgreSQL database.
-        rule id: #{pg.firewall_rules_dataset.first(cidr: "1.2.3.0/24").ubid}
+        rule id: #{pg.pg_firewall_rules.find { it.cidr.to_s == "1.2.3.0/24" && it.port_range.begin == 5432 }.ubid}
         cidr: 1.2.3.0/24
         description: "Example description"
     END
-    expect(pg.firewall_rules_dataset.select_order_map(:cidr).map(&:to_s)).to eq %w[0.0.0.0/0 1.2.3.0/24 ::/0]
+    expect(pg.pg_firewall_rules.map { it.cidr.to_s }.uniq).to eq %w[0.0.0.0/0 1.2.3.0/24 ::/0]
   end
 end
