5e7a9ea90d4f79b950134b0cb666e420ad43ab92
Jeremy Evans <jeremy@ubicloud.com>
2025-10-23 13:34:58 -0700

    Support firewall rule description in CLI and SDK

--- cli-commands/fw/post/add-rule.rb
+++ cli-commands/fw/post/add-rule.rb
@@ -6,13 +6,15 @@ UbiCli.on("fw").run_on("add-rule") do
   options("ubi fw (location/fw-name | fw-id) add-rule [options] cidr", key: :fw_add_rule) do
     on("-s", "--start-port=port", Integer, "starting (or only) port to allow (default: 0)")
     on("-e", "--end-port=port", Integer, "ending port to allow (default: 65535)")
+    on("-d", "--description=desc", "description of rule")
   end
 
   args 1
 
   run do |cidr, opts|
-    start_port, end_port = opts[:fw_add_rule].values_at(:"start-port", :"end-port")
-    id = sdk_object.add_rule(cidr, start_port:, end_port:)[:id]
+    opts = opts[:fw_add_rule]
+    start_port, end_port = opts.values_at(:"start-port", :"end-port")
+    id = sdk_object.add_rule(cidr, start_port:, end_port:, description: opts[:description])[:id]
     response("Added firewall rule with id: #{id}")
   end
 end
