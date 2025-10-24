536a8439ca417d1c1314d5c206c21d36e373454c
Enes Cakir <enes@ubicloud.com>
2025-10-24 16:30:27 +0300

    Do not check authorization for renaming if it's passed as nil explicitly
    We don't need to check authorization for all cases such as renaming
    accounts.

--- helpers/general.rb
+++ helpers/general.rb
@@ -14,7 +14,7 @@ class Clover < Roda
     def rename(object, perm:, serializer:, template_prefix:)
       post "rename" do
         scope.instance_exec do
-          authorize(perm, object)
+          authorize(perm, object) if perm
           handle_validation_failure("#{template_prefix}/show") { @page = "settings" }
           name = typecast_body_params.nonempty_str!("name")
 
