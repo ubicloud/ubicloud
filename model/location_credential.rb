555551170077b49353e8706768715109b6107f51
Philip Dubé <philip.dube@clickhouse.com>
2025-11-10 13:42:25 +0000

    Add 2 columns for IAM auth
    `aws_instance` stores `iam_auth` of role system was already creating for vm,
    this can then be used by timeline for wal-g to use IAM auth on vm
    
    `location_credential` can have `assume_role` set, overriding use of access keys,
    so that sdk can authenticate with IAM auth. Actual permission setup is outside clover

--- model/location_credential.rb
+++ model/location_credential.rb
@@ -20,10 +20,13 @@ end
 
 # Table: location_credential
 # Columns:
-#  access_key | text | NOT NULL
-#  secret_key | text | NOT NULL
-#  id         | uuid | PRIMARY KEY
+#  access_key  | text |
+#  secret_key  | text |
+#  id          | uuid | PRIMARY KEY
+#  assume_role | text |
 # Indexes:
 #  location_credential_pkey | PRIMARY KEY btree (id)
+# Check constraints:
+#  location_credential_single_auth_mechanism | (access_key IS NOT NULL AND secret_key IS NOT NULL AND assume_role IS NULL OR access_key IS NULL AND secret_key IS NULL AND assume_role IS NOT NULL)
 # Foreign key constraints:
 #  location_credential_id_fkey | (id) REFERENCES location(id)
