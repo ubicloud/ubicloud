1ac6fa1109a2af748179c0890c285f51a53ee5bb
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2025-11-12 05:10:43 +0100

    Create provider specific subnet nexus progs
    This is just a dummy commit to add two new progs. These two progs and their
    tests are exact copies of Prog::Aws::Vpc and Prog::Vnet::SubnetNexus. They are
    not being used at the moment. I'll add actual logic changes in the next commit.
    By adding these two files now, I can avoid a huge diff in the next commit, thus
    making the review significantly easier.

---
+++ prog/vnet/metal/subnet_nexus.rb
@@ -0,0 +1,279 @@
+# frozen_string_literal: true
+
+class Prog::Vnet::SubnetNexus < Prog::Base
+  subject_is :private_subnet
+
+  def self.assemble(project_id, name: nil, location_id: Location::HETZNER_FSN1_ID, ipv6_range: nil, ipv4_range: nil, allow_only_ssh: false, firewall_id: nil, firewall_name: nil)
+    unless (project = Project[project_id])
+      fail "No existing project"
+    end
+
+    unless (location = Location[location_id])
+      fail "No existing location"
+    end
+    if allow_only_ssh && firewall_id
+      fail "Cannot specify both allow_only_ssh and firewall_id"
+    end
+
+    ubid = PrivateSubnet.generate_ubid
+    id = ubid.to_uuid
+    name ||= PrivateSubnet.ubid_to_name(ubid)
+
+    Validation.validate_name(name)
+
+    ipv6_range ||= random_private_ipv6(location, project).to_s
+    ipv4_range ||= random_private_ipv4(location, project, location.aws? ? PrivateSubnet::DEFAULT_AWS_SUBNET_PREFIX_LEN : PrivateSubnet::DEFAULT_SUBNET_PREFIX_LEN).to_s
+    DB.transaction do
+      ps = PrivateSubnet.create_with_id(id, name:, location_id: location.id, net6: ipv6_range, net4: ipv4_range, state: "waiting", project_id:)
+      firewall_dataset = project.firewalls_dataset.where(location_id:)
+
+      if firewall_id
+        unless (firewall = firewall_dataset.first(Sequel[:firewall][:id] => firewall_id))
+          fail "Firewall with id #{firewall_id} and location #{location.name} does not exist"
+        end
+      else
+        port_range = allow_only_ssh ? 22..22 : 0..65535
+
+        unless firewall_name
+          firewall_name = "#{name[0, 55]}-default"
+          # As is typical when checking before inserting, there is a race condition here with
+          # a user concurrently manually creating a firewall with the same name.  However,
+          # the worst case scenario is a bogus error message, and the user could try creating
+          # the private subnet again.
+          unless firewall_dataset.where(Sequel[:firewall][:name] => firewall_name).empty?
+            firewall_name = "#{name[0, 47]}-default-#{Array.new(7) { UBID.from_base32(rand(32)) }.join}"
+          end
+        end
+
+        firewall = Firewall.create(name: firewall_name, location_id: location.id, project_id:)
+        DB.ignore_duplicate_queries do
+          ["0.0.0.0/0", "::/0"].each { |cidr| FirewallRule.create(firewall_id: firewall.id, cidr: cidr, port_range: Sequel.pg_range(port_range)) }
+        end
+      end
+      firewall.associate_with_private_subnet(ps, apply_firewalls: false)
+
+      Strand.create_with_id(id, prog: "Vnet::SubnetNexus", label: "start")
+    end
+  end
+
+  def before_run
+    when_destroy_set? do
+      if strand.label != "destroy"
+        register_deadline(nil, 10 * 60)
+        hop_destroy
+      end
+    end
+  end
+
+  label def start
+    if private_subnet.location.aws?
+      PrivateSubnetAwsResource.create_with_id(private_subnet.id) unless private_subnet.private_subnet_aws_resource
+      bud Prog::Aws::Vpc, {"subject_id" => private_subnet.id}, :create_vpc
+      hop_wait_vpc_created
+    else
+      hop_wait
+    end
+  end
+
+  label def wait_vpc_created
+    reap(:wait, nap: 2)
+  end
+
+  label def wait
+    if private_subnet.location.aws?
+      check_firewall_update
+      private_subnet.semaphores.each(&:destroy)
+      nap 60 * 60 * 24 * 365
+    end
+
+    when_refresh_keys_set? do
+      private_subnet.update(state: "refreshing_keys")
+      hop_refresh_keys
+    end
+
+    when_add_new_nic_set? do
+      private_subnet.update(state: "adding_new_nic")
+      hop_add_new_nic
+    end
+
+    check_firewall_update
+
+    if private_subnet.last_rekey_at < Time.now - 60 * 60 * 24
+      private_subnet.incr_refresh_keys
+    end
+
+    nap 10 * 60
+  end
+
+  def check_firewall_update
+    when_update_firewall_rules_set? do
+      private_subnet.vms.each(&:incr_update_firewall_rules)
+      decr_update_firewall_rules
+    end
+  end
+
+  def gen_encryption_key
+    "0x" + SecureRandom.bytes(36).unpack1("H*")
+  end
+
+  def gen_spi
+    "0x" + SecureRandom.bytes(4).unpack1("H*")
+  end
+
+  def gen_reqid
+    SecureRandom.random_number(100000) + 1
+  end
+
+  label def add_new_nic
+    register_deadline("wait", 3 * 60)
+    nics_snap = nics_to_rekey
+    nap 10 if nics_snap.any? { |nic| nic.lock_set? }
+    nics_snap.each do |nic|
+      nic.update(encryption_key: gen_encryption_key, rekey_payload: {spi4: gen_spi, spi6: gen_spi, reqid: gen_reqid})
+      nic.incr_start_rekey
+      nic.incr_lock
+      private_subnet.create_tunnels(nics_snap, nic)
+    end
+
+    decr_add_new_nic
+    hop_wait_inbound_setup
+  end
+
+  label def refresh_keys
+    decr_refresh_keys
+    nics = active_nics
+    nap 10 if nics.any? { |nic| nic.lock_set? }
+    nics.each do |nic|
+      nic.update(encryption_key: gen_encryption_key, rekey_payload: {spi4: gen_spi, spi6: gen_spi, reqid: gen_reqid})
+      nic.incr_start_rekey
+      nic.incr_lock
+    end
+
+    hop_wait_inbound_setup
+  end
+
+  label def wait_inbound_setup
+    nics = rekeying_nics
+    if nics.all? { |nic| nic.strand.label == "wait_rekey_outbound_trigger" }
+      nics.each(&:incr_trigger_outbound_update)
+      hop_wait_outbound_setup
+    end
+
+    nap 5
+  end
+
+  label def wait_outbound_setup
+    nics = rekeying_nics
+    if nics.all? { |nic| nic.strand.label == "wait_rekey_old_state_drop_trigger" }
+      nics.each(&:incr_old_state_drop_trigger)
+      hop_wait_old_state_drop
+    end
+
+    nap 5
+  end
+
+  label def wait_old_state_drop
+    nics = rekeying_nics
+    if nics.all? { |nic| nic.strand.label == "wait" }
+      private_subnet.update(state: "waiting", last_rekey_at: Time.now)
+      nics.each do |nic|
+        nic.update(encryption_key: nil, rekey_payload: nil)
+        nic.unlock
+      end
+      hop_wait
+    end
+
+    nap 5
+  end
+
+  label def destroy
+    if private_subnet.nics.any? { |n| !n.vm_id.nil? }
+      register_deadline(nil, 10 * 60, allow_extension: true) if private_subnet.nics.any? { |n| n.vm&.prevent_destroy_set? }
+
+      Clog.emit("Cannot destroy subnet with active nics, first clean up the attached resources") { private_subnet }
+
+      nap 5
+    end
+
+    decr_destroy
+    if private_subnet.location.aws?
+      private_subnet.nics.map(&:incr_destroy)
+      private_subnet.firewalls.map(&:destroy)
+      Semaphore.incr(strand.children_dataset.where(prog: "Aws::Vpc").select(:id), "destroy")
+      bud Prog::Aws::Vpc, {"subject_id" => private_subnet.id}, :destroy
+      hop_wait_aws_vpc_destroyed
+    end
+    private_subnet.firewalls.map { it.disassociate_from_private_subnet(private_subnet, apply_firewalls: false) }
+
+    private_subnet.connected_subnets.each do |subnet|
+      private_subnet.disconnect_subnet(subnet)
+    end
+
+    if private_subnet.nics.empty? && private_subnet.load_balancers.empty?
+      private_subnet.destroy
+      pop "subnet destroyed"
+    else
+      private_subnet.nics.map { |n| n.incr_destroy }
+      private_subnet.load_balancers.map { |lb| lb.incr_destroy }
+      nap rand(5..10)
+    end
+  end
+
+  label def wait_aws_vpc_destroyed
+    reap(nap: 10) do
+      nap 5 unless private_subnet.nics.empty?
+      private_subnet.private_subnet_aws_resource.destroy
+      private_subnet.destroy
+      pop "vpc destroyed"
+    end
+  end
+
+  def self.random_private_ipv6(location, project)
+    network_address = NetAddr::IPv6.new((SecureRandom.bytes(7) + 0xfd.chr).unpack1("Q<") << 64)
+    network_mask = NetAddr::Mask128.new(64)
+    selected_addr = NetAddr::IPv6Net.new(network_address, network_mask)
+
+    selected_addr = random_private_ipv6(location, project) if project.private_subnets_dataset[net6: selected_addr.to_s, location_id: location.id]
+
+    selected_addr
+  end
+
+  def self.random_private_ipv4(location, project, cidr_size = 26)
+    raise ArgumentError, "CIDR size must be between 0 and 32" unless cidr_size.between?(0, 32)
+
+    private_range = PrivateSubnet.random_subnet
+    addr = NetAddr::IPv4Net.parse(private_range)
+
+    selected_addr = if addr.netmask.prefix_len < cidr_size
+      addr.nth_subnet(cidr_size, SecureRandom.random_number(2**(cidr_size - addr.netmask.prefix_len) - 1).to_i + 1)
+    else
+      random_private_ipv4(location, project, cidr_size)
+    end
+
+    selected_addr = random_private_ipv4(location, project, cidr_size) if PrivateSubnet::BANNED_IPV4_SUBNETS.any? { it.rel(selected_addr) } || project.private_subnets_dataset[net4: selected_addr.to_s, location_id: location.id]
+
+    selected_addr
+  end
+
+  def active_nics
+    nics_with_strand_label("wait").all
+  end
+
+  def nics_to_rekey
+    nics_with_strand_label(%w[wait wait_setup]).all
+  end
+
+  def rekeying_nics
+    all_connected_nics.eager(:strand).exclude(rekey_payload: nil).all
+  end
+
+  private
+
+  def all_connected_nics
+    private_subnet.find_all_connected_nics
+  end
+
+  def nics_with_strand_label(label)
+    all_connected_nics.join(:strand, {id: :id, label:}).select_all(:nic)
+  end
+end
---
+++ spec/prog/vnet/aws/vpc_nexus_spec.rb
@@ -0,0 +1,227 @@
+# frozen_string_literal: true
+
+RSpec.describe Prog::Aws::Vpc do
+  subject(:nx) {
+    described_class.new(st)
+  }
+
+  let(:st) {
+    Strand.create(prog: "Aws::Vpc", stack: [{"subject_id" => ps.id}], label: "create_vpc")
+  }
+
+  let(:ps) {
+    prj = Project.create(name: "test-prj")
+    loc = Location.create(name: "us-west-2", provider: "aws", project_id: prj.id, display_name: "aws-us-west-2", ui_name: "AWS US East 1", visible: true)
+    LocationCredential.create_with_id(loc.id, access_key: "test-access-key", secret_key: "test-secret-key")
+    ps = Prog::Vnet::SubnetNexus.assemble(prj.id, name: "test-ps", location_id: loc.id).subject
+    PrivateSubnetAwsResource.create_with_id(ps.id)
+    ps
+  }
+
+  let(:client) {
+    Aws::EC2::Client.new(stub_responses: true)
+  }
+
+  before do
+    allow(nx).to receive(:private_subnet).and_return(ps)
+    allow(Aws::EC2::Client).to receive(:new).with(access_key_id: "test-access-key", secret_access_key: "test-secret-key", region: "us-west-2").and_return(client)
+  end
+
+  it "exits if destroy is set" do
+    expect(nx.before_run).to be_nil
+    expect(nx).to receive(:when_destroy_set?).and_yield
+    expect { nx.before_run }.to exit({"msg" => "exiting early due to destroy semaphore"})
+  end
+
+  describe "#create_vpc" do
+    it "creates a vpc" do
+      client.stub_responses(:describe_vpcs, vpcs: [])
+      expect(client).to receive(:create_vpc).with({cidr_block: ps.net4.to_s, amazon_provided_ipv_6_cidr_block: true, tag_specifications: Util.aws_tag_specifications("vpc", ps.name)}).and_return(instance_double(Aws::EC2::Types::CreateVpcResult, vpc: instance_double(Aws::EC2::Types::Vpc, vpc_id: "vpc-0123456789abcdefg")))
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(vpc_id: "vpc-0123456789abcdefg")
+      expect { nx.create_vpc }.to hop("wait_vpc_created")
+    end
+
+    it "reuses existing vpc" do
+      client.stub_responses(:describe_vpcs, vpcs: [{vpc_id: "vpc-existing"}])
+      expect(client).not_to receive(:create_vpc)
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(vpc_id: "vpc-existing")
+      expect { nx.create_vpc }.to hop("wait_vpc_created")
+    end
+  end
+
+  describe "#wait_vpc_created" do
+    before do
+      client.stub_responses(:modify_vpc_attribute)
+      client.stub_responses(:create_security_group, group_id: "sg-0123456789abcdefg")
+      client.stub_responses(:authorize_security_group_ingress)
+    end
+
+    it "checks if vpc is available, if not naps" do
+      client.stub_responses(:describe_vpcs, vpcs: [{state: "pending", vpc_id: "vpc-0123456789abcdefg"}])
+      expect { nx.wait_vpc_created }.to nap(1)
+    end
+
+    it "creates a security group and authorizes ingress" do
+      client.stub_responses(:describe_vpcs, vpcs: [{state: "available", vpc_id: "vpc-0123456789abcdefg"}])
+
+      expect(client).to receive(:describe_vpcs).with({filters: [{name: "vpc-id", values: ["vpc-0123456789abcdefg"]}]}).and_call_original
+      expect(client).to receive(:create_security_group).with({group_name: "aws-us-west-2-#{ps.ubid}", description: "Security group for aws-us-west-2-#{ps.ubid}", vpc_id: "vpc-0123456789abcdefg", tag_specifications: Util.aws_tag_specifications("security-group", ps.name)}).and_call_original
+      ps.firewalls.map { it.firewall_rules.map { |fw| fw.destroy } }
+      FirewallRule.create(firewall_id: ps.firewalls.first.id, cidr: "0.0.0.1/32", port_range: 22..80)
+      ps.reload
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:authorize_security_group_ingress).with({group_id: "sg-0123456789abcdefg", ip_permissions: [{ip_protocol: "tcp", from_port: 22, to_port: 80, ip_ranges: [{cidr_ip: "0.0.0.1/32"}]}]}).and_call_original
+      expect(client).to receive(:authorize_security_group_ingress).with({group_id: "sg-0123456789abcdefg", ip_permissions: [{ip_protocol: "tcp", from_port: 22, to_port: 22, ip_ranges: [{cidr_ip: "0.0.0.0/0"}]}]}).and_call_original
+      expect { nx.wait_vpc_created }.to hop("create_route_table")
+    end
+
+    it "does not create a security group if it already exists" do
+      client.stub_responses(:describe_vpcs, vpcs: [{state: "available", vpc_id: "vpc-0123456789abcdefg"}])
+      client.stub_responses(:create_security_group, Aws::EC2::Errors::InvalidGroupDuplicate.new(nil, nil))
+      client.stub_responses(:describe_security_groups, security_groups: [{group_id: "sg-0123456789abcdefg"}])
+
+      ps.firewalls.map { it.firewall_rules.map { |fw| fw.destroy } }
+      FirewallRule.create(firewall_id: ps.firewalls.first.id, cidr: "0.0.0.1/32", port_range: 22..80)
+      FirewallRule.create(firewall_id: ps.firewalls.first.id, cidr: "::/32", port_range: 22..80)
+      ps.reload
+      expect { nx.wait_vpc_created }.to hop("create_route_table")
+    end
+
+    it "skips security group ingress rule if it already exists" do
+      client.stub_responses(:describe_vpcs, vpcs: [{state: "available", vpc_id: "vpc-0123456789abcdefg"}])
+      client.stub_responses(:authorize_security_group_ingress, Aws::EC2::Errors::InvalidPermissionDuplicate.new(nil, nil), Aws::EC2::Errors::InvalidPermissionDuplicate.new(nil, nil))
+
+      ps.firewalls.map { it.firewall_rules.map { |fw| fw.destroy } }
+      FirewallRule.create(firewall_id: ps.firewalls.first.id, cidr: "0.0.0.1/32", port_range: 22..80)
+      ps.reload
+      expect { nx.wait_vpc_created }.to hop("create_route_table")
+    end
+  end
+
+  describe "#create_route_table" do
+    it "creates a route table with new internet gateway" do
+      client.stub_responses(:describe_route_tables, route_tables: [{route_table_id: "rtb-0123456789abcdefg", associations: []}])
+      client.stub_responses(:describe_internet_gateways, internet_gateways: [])
+      client.stub_responses(:create_internet_gateway, internet_gateway: {internet_gateway_id: "igw-0123456789abcdefg"})
+      client.stub_responses(:create_route)
+      client.stub_responses(:attach_internet_gateway)
+      client.stub_responses(:associate_route_table)
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(internet_gateway_id: "igw-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(route_table_id: "rtb-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:attach_internet_gateway).with({internet_gateway_id: "igw-0123456789abcdefg", vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_ipv_6_cidr_block: "::/0", gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_cidr_block: "0.0.0.0/0", gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect { nx.create_route_table }.to exit({"msg" => "subnet created"})
+    end
+
+    it "reuses existing internet gateway and attaches if needed" do
+      client.stub_responses(:describe_route_tables, route_tables: [{route_table_id: "rtb-0123456789abcdefg", associations: []}])
+      client.stub_responses(:describe_internet_gateways, internet_gateways: [{internet_gateway_id: "igw-existing", attachments: []}])
+      client.stub_responses(:create_route)
+      client.stub_responses(:attach_internet_gateway)
+      client.stub_responses(:associate_route_table)
+      expect(client).not_to receive(:create_internet_gateway)
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(internet_gateway_id: "igw-existing")
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(route_table_id: "rtb-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:attach_internet_gateway).with({internet_gateway_id: "igw-existing", vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_ipv_6_cidr_block: "::/0", gateway_id: "igw-existing"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_cidr_block: "0.0.0.0/0", gateway_id: "igw-existing"}).and_call_original
+      expect { nx.create_route_table }.to exit({"msg" => "subnet created"})
+    end
+
+    it "reuses existing internet gateway and skips attachment if already attached" do
+      client.stub_responses(:describe_route_tables, route_tables: [{route_table_id: "rtb-0123456789abcdefg", associations: []}])
+      client.stub_responses(:describe_internet_gateways, internet_gateways: [{internet_gateway_id: "igw-existing", attachments: [{vpc_id: "vpc-0123456789abcdefg"}]}])
+      client.stub_responses(:create_route)
+      client.stub_responses(:associate_route_table)
+      expect(client).not_to receive(:create_internet_gateway)
+      expect(client).not_to receive(:attach_internet_gateway)
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(internet_gateway_id: "igw-existing")
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(route_table_id: "rtb-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_ipv_6_cidr_block: "::/0", gateway_id: "igw-existing"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_cidr_block: "0.0.0.0/0", gateway_id: "igw-existing"}).and_call_original
+      expect { nx.create_route_table }.to exit({"msg" => "subnet created"})
+    end
+
+    it "omits if the route already exists" do
+      client.stub_responses(:describe_route_tables, route_tables: [{route_table_id: "rtb-0123456789abcdefg", associations: []}])
+      client.stub_responses(:describe_internet_gateways, internet_gateways: [])
+      client.stub_responses(:create_internet_gateway, internet_gateway: {internet_gateway_id: "igw-0123456789abcdefg"})
+      client.stub_responses(:create_route, Aws::EC2::Errors::RouteAlreadyExists.new(nil, nil))
+      client.stub_responses(:attach_internet_gateway)
+      expect(client).to receive(:describe_route_tables).with({filters: [{name: "vpc-id", values: ["vpc-0123456789abcdefg"]}]}).and_call_original
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(internet_gateway_id: "igw-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(route_table_id: "rtb-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:attach_internet_gateway).with({internet_gateway_id: "igw-0123456789abcdefg", vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_ipv_6_cidr_block: "::/0", gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect { nx.create_route_table }.to exit({"msg" => "subnet created"})
+    end
+
+    it "skips route table association if already associated" do
+      client.stub_responses(:describe_route_tables, route_tables: [{route_table_id: "rtb-0123456789abcdefg", associations: [{subnet_id: "subnet-existing"}]}])
+      client.stub_responses(:describe_internet_gateways, internet_gateways: [])
+      client.stub_responses(:create_internet_gateway, internet_gateway: {internet_gateway_id: "igw-0123456789abcdefg"})
+      client.stub_responses(:create_route)
+      client.stub_responses(:attach_internet_gateway)
+      expect(client).not_to receive(:associate_route_table)
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(internet_gateway_id: "igw-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:update).with(route_table_id: "rtb-0123456789abcdefg")
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg").at_least(:once)
+      expect(client).to receive(:attach_internet_gateway).with({internet_gateway_id: "igw-0123456789abcdefg", vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_ipv_6_cidr_block: "::/0", gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:create_route).with({route_table_id: "rtb-0123456789abcdefg", destination_cidr_block: "0.0.0.0/0", gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect { nx.create_route_table }.to exit({"msg" => "subnet created"})
+    end
+  end
+
+  describe "#destroy" do
+    before do
+      ps.private_subnet_aws_resource.update(security_group_id: "sg-0123456789abcdefg", internet_gateway_id: "igw-0123456789abcdefg")
+      client.stub_responses(:describe_subnets, subnets: [{state: "available"}])
+    end
+
+    it "deletes the security group and hops to delete_internet_gateway" do
+      client.stub_responses(:delete_security_group)
+      expect(client).to receive(:delete_security_group).with({group_id: "sg-0123456789abcdefg"}).and_call_original
+      expect { nx.destroy }.to hop("delete_internet_gateway")
+    end
+
+    it "hops to delete_internet_gateway if security group is not found" do
+      client.stub_responses(:delete_security_group, Aws::EC2::Errors::InvalidGroupNotFound.new(nil, nil))
+      expect { nx.destroy }.to hop("delete_internet_gateway")
+    end
+
+    it "deletes the internet gateway and hops to delete_vpc" do
+      client.stub_responses(:delete_internet_gateway)
+      client.stub_responses(:detach_internet_gateway)
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg")
+      expect(client).to receive(:delete_internet_gateway).with({internet_gateway_id: "igw-0123456789abcdefg"}).and_call_original
+      expect(client).to receive(:detach_internet_gateway).with({internet_gateway_id: "igw-0123456789abcdefg", vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect { nx.delete_internet_gateway }.to hop("delete_vpc")
+    end
+
+    it "hops to delete_vpc if internet gateway is not found" do
+      client.stub_responses(:delete_internet_gateway, Aws::EC2::Errors::InvalidInternetGatewayIDNotFound.new(nil, nil))
+      client.stub_responses(:detach_internet_gateway)
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg")
+      expect { nx.delete_internet_gateway }.to hop("delete_vpc")
+    end
+
+    it "deletes the vpc" do
+      client.stub_responses(:delete_vpc)
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg")
+      expect(client).to receive(:delete_vpc).with({vpc_id: "vpc-0123456789abcdefg"}).and_call_original
+      expect { nx.delete_vpc }.to exit({"msg" => "vpc destroyed"})
+    end
+
+    it "pops if vpc is not found" do
+      client.stub_responses(:delete_vpc, Aws::EC2::Errors::InvalidVpcIDNotFound.new(nil, nil))
+      expect(ps.private_subnet_aws_resource).to receive(:vpc_id).and_return("vpc-0123456789abcdefg")
+      expect { nx.delete_vpc }.to exit({"msg" => "vpc destroyed"})
+    end
+  end
+end
