fe0fb70133af5d3eb4d0959c37308d6ea21b5954
Enes Cakir <enes@ubicloud.com>
2025-11-18 15:42:14 +0300

    Move failed deliveries logic to RedeliverGithubFailures prog
    This helpers are only used in this prog, so it's better to keep them in
    that prog.

--- prog/redeliver_github_failures.rb
+++ prog/redeliver_github_failures.rb
@@ -4,11 +4,45 @@ class Prog::RedeliverGithubFailures < Prog::Base
   label def wait
     current_frame = strand.stack.first
     last_check_time = Time.parse(current_frame["last_check_at"])
-    Github.redeliver_failed_deliveries(last_check_time)
+    redeliver_failed_deliveries(last_check_time)
     current_frame["last_check_at"] = Time.now
     strand.modified!(:stack)
     strand.save_changes
 
     nap 2 * 60
+  end
+
+  def client
+    @client ||= Github.app_client
+  end
+
+  def redeliver_failed_deliveries(*)
+    failed = failed_deliveries(*).each do |delivery|
+      Clog.emit("redelivering failed delivery") { {delivery: delivery.to_h} }
+      client.post("/app/hook/deliveries/#{delivery[:id]}/attempts")
+    end.count
+    Clog.emit("redelivered failed deliveries") { {deliveries: {failed:}} }
+  end
+
+  def failed_deliveries(since, max_page = 50)
+    all_deliveries = client.get("/app/hook/deliveries?per_page=100")
+    page = 1
+    while (next_url = client.last_response.rels[:next]&.href) && (since < all_deliveries.last[:delivered_at])
+      if page >= max_page
+        Clog.emit("failed deliveries page limit reached") { {deliveries: {max_page:, since:}} }
+        break
+      end
+      page += 1
+      all_deliveries += client.get(next_url)
+    end
+
+    Clog.emit("fetched deliveries") { {deliveries: {total: all_deliveries.count, page:, since:}} }
+
+    all_deliveries
+      .reject { it[:delivered_at] < since }
+      .group_by { it[:guid] }
+      .values
+      .reject { |group| group.any? { it[:status] == "OK" } }
+      .map { |group| group.max_by { it[:delivered_at] } }
   end
 end
--- spec/prog/redeliver_github_failures_spec.rb
+++ spec/prog/redeliver_github_failures_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../model/spec_helper"
+require "octokit"
 
 RSpec.describe Prog::RedeliverGithubFailures do
   subject(:rgf) {
@@ -10,11 +11,63 @@ RSpec.describe Prog::RedeliverGithubFailures do
   describe "#wait" do
     it "redelivers failed deliveries and naps" do
       expect(Time).to receive(:now).and_return("2023-10-19 23:27:47 +0000").at_least(:once)
-      expect(Github).to receive(:redeliver_failed_deliveries).with(Time.utc(2023, 10, 19, 22, 27, 47))
+      expect(rgf).to receive(:redeliver_failed_deliveries).with(Time.utc(2023, 10, 19, 22, 27, 47))
       expect(rgf.strand).to receive(:save_changes)
       expect {
         expect { rgf.wait }.to nap(2 * 60)
       }.to change { rgf.strand.stack.first["last_check_at"] }.from("2023-10-19 22:27:47 +0000").to("2023-10-19 23:27:47 +0000")
+    end
+  end
+
+  describe "failed deliveries" do
+    let(:time) { Time.now }
+    let(:app_client) { instance_double(Octokit::Client) }
+
+    before do
+      allow(Github).to receive(:app_client).and_return(app_client)
+    end
+
+    it "fetches failed deliveries" do
+      expect(app_client).to receive(:get).with("/app/hook/deliveries?per_page=100").and_return([
+        {guid: "1", status: "Fail", delivered_at: time + 5},
+        {guid: "2", status: "Fail", delivered_at: time + 4},
+        {guid: "3", status: "OK", delivered_at: time + 3}
+      ])
+      next_url = "/app/hook/deliveries?per_page=100&cursor=next_page"
+      expect(app_client).to receive(:last_response).and_return(instance_double(Sawyer::Response, rels: {next: instance_double(Sawyer::Relation, href: next_url)}))
+      expect(app_client).to receive(:last_response).and_return(instance_double(Sawyer::Response, rels: {next: nil}))
+      expect(app_client).to receive(:get).with(next_url).and_return([
+        {guid: "2", status: "OK", delivered_at: time + 2},
+        {guid: "4", status: "Fail", delivered_at: time + 2},
+        {guid: "4", status: "Fail", delivered_at: time + 1},
+        {guid: "5", status: "Fail", delivered_at: time - 2},
+        {guid: "6", status: "OK", delivered_at: time - 3}
+      ])
+
+      failed_deliveries = rgf.failed_deliveries(time)
+      expect(failed_deliveries).to eq([
+        {guid: "1", status: "Fail", delivered_at: time + 5},
+        {guid: "4", status: "Fail", delivered_at: time + 2}
+      ])
+    end
+
+    it "fetches failed deliveries with max page" do
+      expect(app_client).to receive(:get).with("/app/hook/deliveries?per_page=100").and_return([
+        {guid: "3", status: "Fail", delivered_at: time + 3}
+      ])
+      expect(app_client).to receive(:last_response).and_return(instance_double(Sawyer::Response, rels: {next: instance_double(Sawyer::Relation, href: "next_url")}))
+      expect(Clog).to receive(:emit).with("failed deliveries page limit reached").and_call_original
+      expect(Clog).to receive(:emit).with("fetched deliveries").and_call_original
+      failed_deliveries = rgf.failed_deliveries(time, 1)
+      expect(failed_deliveries).to eq([{guid: "3", status: "Fail", delivered_at: time + 3}])
+    end
+
+    it "redelivers failed deliveries" do
+      expect(rgf).to receive(:failed_deliveries).with(time).and_return([{id: "1"}, {id: "2"}])
+      expect(app_client).to receive(:post).with("/app/hook/deliveries/1/attempts")
+      expect(app_client).to receive(:post).with("/app/hook/deliveries/2/attempts")
+
+      rgf.redeliver_failed_deliveries(time)
     end
   end
 end
