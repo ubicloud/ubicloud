f2669b6d0b89cc467e449e15278c0d519434a341
Furkan Sahin <furkan@ubicloud.com>
2025-11-19 13:52:31 +0100

    Individualize nexus progs according to AWS or Metal usage
    We are cleaning up the parts of the Nexus.rb files to tailor them
    according to their specific usage. Metal/Nexus.rb is cleaned up from the
    AWS related states and vice versa.

--- prog/vm/metal/nexus.rb
+++ prog/vm/metal/nexus.rb
@@ -1,186 +1,12 @@
 # frozen_string_literal: true
 
-require "netaddr"
 require "json"
 require "shellwords"
-require "openssl"
-require "base64"
 
 class Prog::Vm::Metal::Nexus < Prog::Base
   DEFAULT_SIZE = "standard-2"
 
   subject_is :vm
-
-  def self.assemble(public_key, project_id, name: nil, size: DEFAULT_SIZE,
-    unix_user: "ubi", location_id: Location::HETZNER_FSN1_ID, boot_image: Config.default_boot_image_name,
-    private_subnet_id: nil, nic_id: nil, storage_volumes: nil, boot_disk_index: 0,
-    enable_ip4: false, pool_id: nil, arch: "x64", swap_size_bytes: nil,
-    distinct_storage_devices: false, force_host_id: nil, exclude_host_ids: [], gpu_count: 0, gpu_device: nil,
-    hugepages: true, hypervisor: nil, ch_version: nil, firmware_version: nil, new_private_subnet_name: nil,
-    exclude_availability_zones: [], availability_zone: nil, alternative_families: [],
-    allow_private_subnet_in_other_project: false, init_script: nil)
-
-    unless (project = Project[project_id])
-      fail "No existing project"
-    end
-    if exclude_host_ids.include?(force_host_id)
-      fail "Cannot force and exclude the same host"
-    end
-
-    unless (location = Location[location_id])
-      fail "No existing location"
-    end
-
-    vm_size = Validation.validate_vm_size(size, arch)
-    Validation.validate_billing_rate("VmVCpu", vm_size.family, location.name)
-
-    storage_volumes ||= [{
-      size_gib: vm_size.storage_size_options.first,
-      encrypted: true
-    }]
-
-    # allow missing fields to make testing during development more convenient.
-    storage_volumes.each_with_index do |volume, disk_index|
-      volume[:size_gib] ||= vm_size.storage_size_options.first
-      volume[:skip_sync] ||= false
-      volume[:max_read_mbytes_per_sec] ||= vm_size.io_limits.max_read_mbytes_per_sec
-      volume[:max_write_mbytes_per_sec] ||= vm_size.io_limits.max_write_mbytes_per_sec
-      volume[:vring_workers] ||= vm_size.vring_workers
-      volume[:encrypted] = true if !volume.has_key? :encrypted
-      volume[:boot] = disk_index == boot_disk_index
-
-      if volume[:read_only]
-        volume[:size_gib] = 0
-        volume[:encrypted] = false
-        volume[:skip_sync] = true
-        volume[:boot] = false
-      end
-    end
-
-    Validation.validate_storage_volumes(storage_volumes, boot_disk_index)
-
-    ubid = Vm.generate_ubid
-    name ||= Vm.ubid_to_name(ubid)
-
-    Validation.validate_name(name)
-    Validation.validate_os_user_name(unix_user)
-
-    DB.transaction do
-      # Here the logic is the following;
-      # - If the user provided nic_id, that nic has to exist and we fetch private_subnet
-      # from the reference of nic. We just assume it and not even check the validity of the
-      # private_subnet_id.
-      # - If the user did not provide nic_id but the private_subnet_id, that private_subnet
-      # must exist, otherwise we fail.
-      # - If the user did not provide nic_id but the private_subnet_id and that subnet exists
-      # then we create a nic on that subnet.
-      # - If the user provided neither nic_id nor private_subnet_id, that's OK, we create both.
-      nic = nil
-      subnet = if nic_id
-        nic = Nic[nic_id]
-        raise("Given nic doesn't exist with the id #{nic_id}") unless nic
-        raise("Given nic is assigned to a VM already") if nic.vm_id
-        raise("Given nic is created in a different location") if nic.private_subnet.location_id != location.id
-        raise("Given nic is not available in the given project") unless project.private_subnets.any? { |ps| ps.id == nic.private_subnet_id }
-
-        nic.private_subnet
-      end
-
-      unless nic
-        subnet = if private_subnet_id
-          subnet = PrivateSubnet[private_subnet_id]
-          raise "Given subnet doesn't exist with the id #{private_subnet_id}" unless subnet
-          unless allow_private_subnet_in_other_project
-            raise "Given subnet is not available in the given project" unless project.private_subnets.any? { |ps| ps.id == subnet.id }
-          end
-
-          subnet
-        elsif new_private_subnet_name
-          Prog::Vnet::SubnetNexus.assemble(project_id, name: new_private_subnet_name, location_id:).subject
-        else
-          project.default_private_subnet(location)
-        end
-        nic = Prog::Vnet::NicNexus.assemble(subnet.id, name: "#{name}-nic", exclude_availability_zones: exclude_availability_zones, availability_zone: availability_zone).subject
-      end
-
-      vm = Vm.create(
-        public_key: public_key,
-        unix_user: unix_user,
-        name: name,
-        family: vm_size.family,
-        cores: 0, # this will be updated after allocation is complete based on the host's topology
-        vcpus: vm_size.vcpus,
-        cpu_percent_limit: vm_size.cpu_percent_limit,
-        cpu_burst_percent_limit: vm_size.cpu_burst_percent_limit,
-        memory_gib: vm_size.memory_gib,
-        location_id: location.id,
-        boot_image: boot_image,
-        ip4_enabled: enable_ip4,
-        pool_id: pool_id,
-        arch: arch,
-        project_id:
-      ) { it.id = ubid.to_uuid }
-      nic.update(vm_id: vm.id)
-
-      if init_script && !init_script.empty?
-        VmInitScript.create_with_id(vm, script: init_script)
-      end
-
-      if vm_size.family == "standard-gpu"
-        gpu_count = 1
-        gpu_device = "27b0"
-      end
-
-      label = if location.aws?
-        disk_index = 0
-        storage_volumes.each do |volume|
-          max_disk_size = (vm_size.family == "i8g") ? 3750.0 : 1900.0
-          disk_count = (volume[:size_gib] / max_disk_size).ceil
-
-          disk_count.times do
-            VmStorageVolume.create(
-              vm_id: vm.id,
-              size_gib: volume[:size_gib] / disk_count,
-              boot: volume[:boot],
-              use_bdev_ubi: false,
-              disk_index:
-            )
-
-            disk_index += 1
-          end
-        end
-        "start_aws"
-      else
-        "start"
-      end
-
-      Strand.create(
-        prog: "Vm::Nexus",
-        label:,
-        stack: [{
-          "storage_volumes" => storage_volumes.map { |v| v.transform_keys(&:to_s) },
-          "swap_size_bytes" => swap_size_bytes,
-          "distinct_storage_devices" => distinct_storage_devices,
-          "force_host_id" => force_host_id,
-          "exclude_host_ids" => exclude_host_ids,
-          "gpu_count" => gpu_count,
-          "gpu_device" => gpu_device,
-          "hugepages" => hugepages,
-          "hypervisor" => hypervisor,
-          "ch_version" => ch_version,
-          "firmware_version" => firmware_version,
-          "alternative_families" => alternative_families
-        }]
-      ) { it.id = vm.id }
-    end
-  end
-
-  def self.assemble_with_sshable(*, sshable_unix_user: "rhizome", **kwargs)
-    ssh_key = SshKey.generate
-    st = assemble(ssh_key.public_key, *, **kwargs)
-    Sshable.create_with_id(st, unix_user: sshable_unix_user, host: "temp_#{st.id}", raw_private_key_1: ssh_key.keypair)
-    st
-  end
 
   def vm_name
     @vm_name ||= vm.inhost_name
@@ -211,23 +37,13 @@ class Prog::Vm::Metal::Nexus < Prog::Base
 
   def before_run
     when_destroy_set? do
-      unless ["destroy", "wait_aws_vm_destroyed", "remove_vm_from_load_balancer", "wait_vm_removal_from_load_balancer", "destroy_slice"].include? strand.label
+      unless ["destroy", "remove_vm_from_load_balancer", "wait_vm_removal_from_load_balancer", "destroy_slice"].include? strand.label
         vm.active_billing_records.each(&:finalize)
         vm.assigned_vm_address&.active_billing_record&.finalize
         register_deadline(nil, 5 * 60)
         hop_destroy
       end
     end
-  end
-
-  label def start_aws
-    nap 1 unless vm.nics.all? { |nic| nic.strand.label == "wait" }
-    bud Prog::Aws::Instance, {"subject_id" => vm.id, "alternative_families" => frame["alternative_families"]}, :start
-    hop_wait_aws_vm_started
-  end
-
-  label def wait_aws_vm_started
-    reap(:wait_sshable, nap: 3)
   end
 
   label def start
@@ -379,7 +195,7 @@ class Prog::Vm::Metal::Nexus < Prog::Base
   label def create_billing_record
     vm.update(display_state: "running", provisioned_at: Time.now)
 
-    Clog.emit("vm provisioned") { [vm, {provision: {vm_ubid: vm.ubid, vm_host_ubid: host&.ubid, instance_id: vm.aws_instance&.instance_id, duration: (Time.now - vm.allocated_at).round(3)}}] }
+    Clog.emit("vm provisioned") { [vm, {provision: {vm_ubid: vm.ubid, vm_host_ubid: host.ubid, duration: (Time.now - vm.allocated_at).round(3)}}] }
 
     project = vm.project
     strand.stack[-1]["create_billing_record_done"] = true
@@ -394,39 +210,37 @@ class Prog::Vm::Metal::Nexus < Prog::Base
       amount: vm.vcpus
     )
 
-    unless vm.location.aws?
-      vm.storage_volumes.each do |vol|
-        BillingRecord.create(
-          project_id: project.id,
-          resource_id: vm.id,
-          resource_name: "Disk ##{vol["disk_index"]} of #{vm.name}",
-          billing_rate_id: BillingRate.from_resource_properties("VmStorage", vm.family, vm.location.name)["id"],
-          amount: vol["size_gib"]
-        )
-      end
+    vm.storage_volumes.each do |vol|
+      BillingRecord.create(
+        project_id: project.id,
+        resource_id: vm.id,
+        resource_name: "Disk ##{vol["disk_index"]} of #{vm.name}",
+        billing_rate_id: BillingRate.from_resource_properties("VmStorage", vm.family, vm.location.name)["id"],
+        amount: vol["size_gib"]
+      )
+    end
 
-      if vm.ip4_enabled
-        BillingRecord.create(
-          project_id: project.id,
-          resource_id: vm.id,
-          resource_name: vm.assigned_vm_address.ip,
-          billing_rate_id: BillingRate.from_resource_properties("IPAddress", "IPv4", vm.location.name)["id"],
-          amount: 1
-        )
-      end
+    if vm.ip4_enabled
+      BillingRecord.create(
+        project_id: project.id,
+        resource_id: vm.id,
+        resource_name: vm.assigned_vm_address.ip,
+        billing_rate_id: BillingRate.from_resource_properties("IPAddress", "IPv4", vm.location.name)["id"],
+        amount: 1
+      )
+    end
 
-      if vm.pci_devices.any? { |dev| dev.is_gpu }
-        gpu_count = vm.pci_devices.count { |dev| dev.is_gpu }
-        gpu = vm.pci_devices.find { |dev| dev.is_gpu }
+    if vm.pci_devices.any? { |dev| dev.is_gpu }
+      gpu_count = vm.pci_devices.count { |dev| dev.is_gpu }
+      gpu = vm.pci_devices.find { |dev| dev.is_gpu }
 
-        BillingRecord.create(
-          project_id: project.id,
-          resource_id: vm.id,
-          resource_name: "GPUs of #{vm.name}",
-          billing_rate_id: BillingRate.from_resource_properties("Gpu", gpu.device, vm.location.name)["id"],
-          amount: gpu_count
-        )
-      end
+      BillingRecord.create(
+        project_id: project.id,
+        resource_id: vm.id,
+        resource_name: "GPUs of #{vm.name}",
+        billing_rate_id: BillingRate.from_resource_properties("Gpu", gpu.device, vm.location.name)["id"],
+        amount: gpu_count
+      )
     end
 
     hop_wait
@@ -539,11 +353,6 @@ class Prog::Vm::Metal::Nexus < Prog::Base
     end
 
     vm.update(display_state: "deleting")
-    if vm.location.aws?
-      Semaphore.incr(strand.children_dataset.where(prog: "Aws::Instance").select(:id), "destroy")
-      bud Prog::Aws::Instance, {"subject_id" => vm.id}, :destroy
-      hop_wait_aws_vm_destroyed
-    end
 
     unless host.nil?
       begin
@@ -591,13 +400,6 @@ class Prog::Vm::Metal::Nexus < Prog::Base
     hop_remove_vm_from_load_balancer if vm.load_balancer
 
     hop_destroy_slice
-  end
-
-  label def wait_aws_vm_destroyed
-    reap(nap: 10) do
-      final_clean_up
-      pop "vm deleted"
-    end
   end
 
   label def remove_vm_from_load_balancer
