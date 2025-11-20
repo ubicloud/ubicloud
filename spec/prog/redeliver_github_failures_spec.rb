bca736392c82c93437b97b51ca1f20896ae5d587
Enes Cakir <enes@ubicloud.com>
2025-11-20 14:38:43 +0300

    Redeliver failed GitHub webhook deliveries concurrently
    GitHub can occasionally fail to deliver webhooks to us for various
    reasons (e.g., network issues, brief downtime). To avoid missing
    important events, we periodically check GitHub’s API for failed
    deliveries and try to redeliver them every 2 minutes.
    
    When GitHub or we have an incident, this process can’t finish within 120
    seconds and ends up triggering apoptosis.
    
    Since the “list deliveries” endpoint doesn’t support filtering, we have
    to paginate through all deliveries until the last check time to find the
    failed ones. There’s no way to make that part more concurrent.
    
    However, once we identify the failed deliveries, we can redeliver them
    concurrently. I refactored the code so that it buds child strands, each
    handling 25 failed deliveries.
    
    Right now it takes ~70s to redeliver 250 failed deliveries. Because the
    listing phase is constant, I expect the new design to drop the
    redelivery time to ~10s for the same workload (since delivering 25
    events currently takes that much).

--- spec/prog/redeliver_github_failures_spec.rb
+++ spec/prog/redeliver_github_failures_spec.rb
@@ -5,7 +5,7 @@ require "octokit"
 
 RSpec.describe Prog::RedeliverGithubFailures do
   subject(:prog) {
-    described_class.new(Strand.create(prog: "RedeliverGithubFailures", label: "wait", stack: [{"last_check_at" => "2023-10-19 22:27:47 +0000"}]))
+    described_class.new(Strand.create(prog: "RedeliverGithubFailures", label: "wait", stack: [{"last_check_at" => "2023-10-19 22:27:47 UTC"}]))
   }
 
   let(:time) { Time.utc(2023, 10, 19, 22, 27, 47) }
@@ -16,18 +16,37 @@ RSpec.describe Prog::RedeliverGithubFailures do
   end
 
   describe "#wait" do
-    it "redelivers failed deliveries and naps" do
-      expect(Time).to receive(:now).and_return("2023-10-19 23:27:47 +0000").at_least(:once)
-      expect(prog).to receive(:failed_deliveries).with(time).and_return([{guid: "1", id: "11", status: "Fail", delivered_at: time + 5}])
-      expect(Clog).to receive(:emit).with("redelivered failed deliveries").and_wrap_original do |&blk|
-        expect(blk.call).to eq(deliveries: {failed: 1})
-      end
-      expect(Clog).to receive(:emit).with("redelivering failed delivery").and_wrap_original do |&blk|
-        expect(blk.call).to eq(delivery: {delivered_at: time + 5, guid: "1", id: "11", status: "Fail"})
-      end
+    it "naps if last check was less than 2 minutes ago" do
+      expect(Time).to receive(:now).and_return(time + 50).at_least(:once)
+      expect { prog.wait }.to nap(71)
+    end
+
+    it "buds redelivery children and hops" do
+      expect(Time).to receive(:now).and_return(time + 60 * 60).at_least(:once)
+      expect(prog).to receive(:failed_deliveries).with(time).and_return(
+        Array.new(26).map.with_index(1) { |_, i| {guid: i, id: i, status: "Fail", delivered_at: time + 5} }
+      )
+      expect { prog.wait }.to hop("wait_redelivers")
+        .and change { prog.strand.stack.first["last_check_at"] }.from("2023-10-19 22:27:47 UTC").to("2023-10-19 23:27:47 UTC")
+        .and change(Strand, :count).by(2)
+      expect(prog.strand.children.count).to eq(2)
+      expect(prog.strand.children.last.stack.first["delivery_ids"]).to eq([26])
+    end
+  end
+
+  describe "#wait_redelivers" do
+    it "registers deadline and reaps wait" do
+      expect(prog).to receive(:register_deadline).with("wait", 10 * 60)
+      expect { prog.wait_redelivers }.to hop("wait")
+    end
+  end
+
+  describe "#redeliver" do
+    it "redelivers the given delivery ids" do
+      expect(prog).to receive(:frame).and_return({"delivery_ids" => ["11", "21"]}).at_least(:once)
       expect(app_client).to receive(:post).with("/app/hook/deliveries/11/attempts")
-      expect { prog.wait }.to nap(2 * 60)
-        .and change { prog.strand.stack.first["last_check_at"] }.from("2023-10-19 22:27:47 +0000").to("2023-10-19 23:27:47 +0000")
+      expect(app_client).to receive(:post).with("/app/hook/deliveries/21/attempts")
+      expect { prog.redeliver }.to exit({"msg" => "redelivered failures"})
     end
   end
 
@@ -60,12 +79,12 @@ RSpec.describe Prog::RedeliverGithubFailures do
 
     it "fetches failed deliveries with max page" do
       expect(app_client).to receive(:get).with("/app/hook/deliveries?per_page=100").and_return([
+        {guid: "2", id: "21", status: "OK", delivered_at: time + 3},
         {guid: "3", id: "31", status: "Fail", delivered_at: time + 3}
       ])
       expect(app_client).to receive(:last_response).and_return(instance_double(Sawyer::Response, rels: {next: instance_double(Sawyer::Relation, href: "next_url")}))
-      expect(Clog).to receive(:emit).with("failed deliveries page limit reached").and_call_original
-      expect(Clog).to receive(:emit).with("fetched deliveries").and_wrap_original do |&blk|
-        expect(blk.call).to eq(deliveries: {page: 1, since: time, total: 1})
+      expect(Clog).to receive(:emit).with("fetched github deliveries").and_wrap_original do |&blk|
+        expect(blk.call).to eq(fetched_github_deliveries: {total: 2, failed: 1, status: {"Fail" => 1}, page: 1, since: time})
       end
 
       expect(prog.failed_deliveries(time, 1)).to eq([{guid: "3", id: "31", status: "Fail", delivered_at: time + 3}])
