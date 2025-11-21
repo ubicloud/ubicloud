664f0fc82caa97a5b43b7ea4bed7cb6ba3e5e015
Enes Cakir <enes@ubicloud.com>
2025-11-21 11:53:47 +0000

    Show deprecation warning for GPU runners

--- prog/vm/github_runner.rb
+++ prog/vm/github_runner.rb
@@ -327,7 +327,12 @@ class Prog::Vm::GithubRunner < Prog::Base
       COMMAND
     end
 
-    if !github_runner.label.include?("ubuntu")
+    if github_runner.label.include?("gpu")
+      message = "The GPU runners will be deprecated on December 31, 2025. All jobs using these runners should be migrated to other runner types."
+      command += <<~COMMAND
+        echo "::warning::#{message}" | sudo -u runner tee /home/runner/actions-runner/.ubicloud_complete_message
+      COMMAND
+    elsif !github_runner.label.include?("ubuntu")
       message = "The default operating system for this runner has been changed to Ubuntu 24.04 on November 23, 2025. You can continue using Ubuntu 22.04 by explicitly specifying it. If you have any questions, feel free to reach out at support@ubicloud.com"
       command += <<~COMMAND
         echo "::notice::#{message}" | sudo -u runner tee /home/runner/actions-runner/.ubicloud_complete_message
