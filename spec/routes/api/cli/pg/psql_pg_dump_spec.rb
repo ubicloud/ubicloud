07dd4321443c28193568c1136326fed662813a94
Jeremy Evans <jeremy@ubicloud.com>
2025-11-24 13:10:40 -0800

    Make API/SDK/CLI use channel_binding=require for PostgreSQL connection strings
    channel_binding=require was replaced with sslmode=require in
    0976ce107ea1bae8b83530dfcdd82a63ea4d802f, in order to allow pgBouncer
    to work. However, as the connection strings provided use port 5432
    and not 6432, they would not use pgBouncer anyway.

--- spec/routes/api/cli/pg/psql_pg_dump_spec.rb
+++ spec/routes/api/cli/pg/psql_pg_dump_spec.rb
@@ -14,29 +14,29 @@ require_relative "../spec_helper"
         target_storage_size_gib: 64
       ).subject
       @ref = [@pg.display_location, @pg.name].join("/")
-      @conn_string = URI("postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?sslmode=require")
+      @conn_string = URI("postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require")
       expect(Config).to receive(:postgres_service_hostname).and_return("pg.example.com").at_least(:once)
       DnsZone.create(project_id: @project.id, name: "pg.example.com")
     end
 
     it "connects to database via #{cmd}" do
-      expect(cli_exec(["pg", @ref, cmd])).to eq %W[#{cmd} -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?sslmode=require]
+      expect(cli_exec(["pg", @ref, cmd])).to eq %W[#{cmd} -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
     end
 
     it "puts password in ubi-pgpassword if client is 1.1.0+" do
-      expect(cli_exec(["pg", @ref, cmd], env: {"HTTP_X_UBI_VERSION" => "1.1.0"}, command_pgpassword: @pg.superuser_password)).to eq %W[#{cmd} -- postgres://postgres@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?sslmode=require]
+      expect(cli_exec(["pg", @ref, cmd], env: {"HTTP_X_UBI_VERSION" => "1.1.0"}, command_pgpassword: @pg.superuser_password)).to eq %W[#{cmd} -- postgres://postgres@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
     end
 
     it "supports #{cmd} options" do
-      expect(cli_exec(["pg", @ref, cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?sslmode=require]
+      expect(cli_exec(["pg", @ref, cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
     end
 
     it "supports -U option for user name" do
-      expect(cli_exec(["pg", @ref, "-Ufoo", cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://foo@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?sslmode=require]
+      expect(cli_exec(["pg", @ref, "-Ufoo", cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://foo@test-pg.#{@pg.ubid}.pg.example.com:5432/postgres?channel_binding=require]
     end
 
     it "supports -d option for database name" do
-      expect(cli_exec(["pg", @ref, "-dfoo", cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/foo?sslmode=require]
+      expect(cli_exec(["pg", @ref, "-dfoo", cmd, "-a"])).to eq %W[#{cmd} -a -- postgres://postgres:#{@pg.superuser_password}@test-pg.#{@pg.ubid}.pg.example.com:5432/foo?channel_binding=require]
     end
   end
 end
