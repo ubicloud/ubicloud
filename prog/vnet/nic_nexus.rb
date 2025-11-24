ubicloud/ubicloud

commit ec3f78682ee9c8e67a617fdc13c0c2585a1b1493

Start using individual NicNexus progs for aws or metal

prog/vnet/nic_nexus.rb
@@ -13,17 +13,16 @@ class Prog::Vnet::NicNexus < Prog::Base
     name ||= Nic.ubid_to_name(ubid)
 
     ipv6_addr ||= subnet.random_private_ipv6.to_s
-    ipv4_addr ||= random_private_ipv4(subnet) || subnet.random_private_ipv4
-    ipv4_addr = ipv4_addr.to_s
 
     DB.transaction do
-      Nic.create_with_id(id, private_ipv6: ipv6_addr, private_ipv4: ipv4_addr, mac: gen_mac, name:, private_subnet_id:)
-      label = if subnet.location.aws?
-        "create_aws_nic"
+      prog, ipv4_addr, mac = if subnet.location.aws?
+        ["Vnet::Aws::NicNexus", (ipv4_addr || subnet.random_private_ipv4.nth_subnet(32, 4)).to_s, nil]
       else
-        "wait_allocation"
+        ["Vnet::Metal::NicNexus", (ipv4_addr || subnet.random_private_ipv4).to_s, gen_mac]
       end
-      Strand.create_with_id(id, prog: "Vnet::NicNexus", label:, stack: [{"exclude_availability_zones" => exclude_availability_zones, "availability_zone" => availability_zone}])
+
+      Nic.create_with_id(id, private_ipv6: ipv6_addr, private_ipv4: ipv4_addr, mac:, name:, private_subnet_id:)
+      Strand.create_with_id(id, prog:, label: "start", stack: [{"exclude_availability_zones" => exclude_availability_zones, "availability_zone" => availability_zone, "ipv4_addr" => ipv4_addr}])
     end
   end
 
@@ -155,12 +154,5 @@ class Prog::Vnet::NicNexus < Prog::Base
     ([rand(256) & 0xFE | 0x02] + Array.new(5) { rand(256) }).map {
       "%0.2X" % it
     }.join(":").downcase
-  end
-
-  def self.random_private_ipv4(private_subnet)
-    random_subnet = private_subnet.random_private_ipv4
-    return random_subnet unless private_subnet.location.aws?
-
-    random_subnet.nth_subnet(32, 4)
   end
 end

spec/prog/vnet/nic_nexus_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe Prog::Vnet::NicNexus do
         mac: "7a:7b:7b:7b:7b:7b",
         private_subnet_id: "57afa8a7-2357-4012-9632-07fbe13a3133",
         name: "demonic").and_return(nic)
-      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::NicNexus", label: "wait_allocation", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil}]).and_return(Strand.new)
+      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::Metal::NicNexus", label: "start", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil, "ipv4_addr" => "10.0.0.12/32"}]).and_return(Strand.new)
       described_class.assemble(ps.id, ipv6_addr: "fd10:9b0b:6b4b:8fbb::/128", name: "demonic")
     end
 
@@ -50,7 +50,7 @@ RSpec.describe Prog::Vnet::NicNexus do
         mac: "00:11:22:33:44:55",
         private_subnet_id: "57afa8a7-2357-4012-9632-07fbe13a3133",
         name: "demonic").and_return(nic)
-      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::NicNexus", label: "wait_allocation", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil}]).and_return(Strand.new)
+      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::Metal::NicNexus", label: "start", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil, "ipv4_addr" => "10.0.0.12/32"}]).and_return(Strand.new)
       described_class.assemble(ps.id, ipv4_addr: "10.0.0.12/32", name: "demonic")
     end
 
@@ -61,15 +61,14 @@ RSpec.describe Prog::Vnet::NicNexus do
       expect(ps).to receive(:random_private_ipv4).and_return(NetAddr::IPv4Net.parse("10.0.0.0/26"))
       id = "0a9a166c-e7e7-4447-ab29-7ea442b5bb0e"
       expect(Nic).to receive(:generate_ubid).and_return(UBID.from_uuidish(id))
-      expect(described_class).to receive(:gen_mac).and_return("00:11:22:33:44:55")
       nic = instance_double(Nic, private_subnet: ps, id:)
       expect(Nic).to receive(:create_with_id).with(id,
         private_ipv6: "fd10:9b0b:6b4b:8fbb::/128",
         private_ipv4: "10.0.0.4/32",
-        mac: "00:11:22:33:44:55",
+        mac: nil,
         private_subnet_id: "57afa8a7-2357-4012-9632-07fbe13a3133",
         name: "demonic").and_return(nic)
-      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::NicNexus", label: "create_aws_nic", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil}]).and_return(Strand.new)
+      expect(Strand).to receive(:create_with_id).with(id, prog: "Vnet::Aws::NicNexus", label: "start", stack: [{"exclude_availability_zones" => [], "availability_zone" => nil, "ipv4_addr" => "10.0.0.4/32"}]).and_return(Strand.new)
       described_class.assemble(ps.id, name: "demonic")
     end
   end

commit 5d1e4bc6a8877a6b8179243839eb4bd7cf8b718a

Fix Nic provisioning for old AWS subnets
We broke this with the commit ec3f786. Only old clusters are impacted

prog/vnet/nic_nexus.rb
@@ -16,7 +16,7 @@ class Prog::Vnet::NicNexus < Prog::Base
 
     DB.transaction do
       prog, ipv4_addr, mac, state = if subnet.location.aws?
-        ["Vnet::Aws::NicNexus", (ipv4_addr || subnet.random_private_ipv4.nth_subnet(32, 4)).to_s, nil, "active"]
+        ["Vnet::Aws::NicNexus", (ipv4_addr || subnet.random_private_ipv4.nth_subnet(32, 4) || subnet.random_private_ipv4).to_s, nil, "active"]
       else
         ["Vnet::Metal::NicNexus", (ipv4_addr || subnet.random_private_ipv4).to_s, gen_mac, "initializing"]
       end