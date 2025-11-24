dc23324307e797ff301cdd738a8acd8af25be89a
Jeremy Evans <jeremy@ubicloud.com>
2025-11-24 11:06:21 -0800

    Revert "Add VmInitScript.populate_encrypted_column for backfilling encrypted column"
    This reverts commit 6f0ba5483966088a5bb7e86de44b7d417c658d9e.

--- spec/model/vm_init_script_spec.rb
+++ spec/model/vm_init_script_spec.rb
@@ -9,15 +9,4 @@ RSpec.describe VmInitScript do
     vm.init_script = "a" * 2000
     expect(vm.valid?).to be true
   end
-
-  it ".populate_encrypted_column populates the encrypted column for existing entries without values" do
-    project_id = Project.create(name: "test").id
-    vm1 = Prog::Vm::Nexus.assemble("a a", project_id)
-    vm2 = Prog::Vm::Nexus.assemble("a a", project_id)
-    vmis_id = described_class.insert(id: vm1.id, script: "a")
-    vmis2 = described_class.create_with_id(vm2, script: "b", init_script: "c")
-    described_class.populate_encrypted_column
-    expect(described_class[vmis_id].init_script).to eq "a"
-    expect(vmis2.reload.init_script).to eq "c"
-  end
 end
