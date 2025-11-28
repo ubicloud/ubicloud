c7986858625ea3bb84b62a54c142b629e1e4e78a
Philip Dubé <philip.dube@clickhouse.com>
2025-11-28 04:16:31 +0000

    Add protocol to firewall rules
    Opens up UDP firewall rules, which matters for DNS port 53

---
+++ migrate/20251130_add_firewall_protocol.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    alter_table(:firewall_rule) do
+      drop_constraint :firewall_rule_cidr_port_range_firewall_id_key
+      add_column :protocol, :text, null: false, default: "tcp"
+      add_unique_constraint [:cidr, :port_range, :firewall_id, :protocol], name: :firewall_rule_unique
+      add_constraint :valid_protocol, protocol: %w[tcp udp]
+    end
+  end
+
+  down do
+    alter_table(:firewall_rule) do
+      drop_column :protocol
+      drop_constraint :firewall_rule_unique
+      drop_constraint :valid_protocol
+      add_unique_constraint [:cidr, :port_range, :firewall_id], name: :firewall_rule_cidr_port_range_firewall_id_key
+    end
+  end
+end
