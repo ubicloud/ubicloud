34ea83e49c9156a4acf22beb20a7c6d2f2c1b9f3
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-12-02 00:09:43 +0530

    aws: more i8g sizes, plus i8ge, i7i, & i7ie

--- helpers/postgres.rb
+++ helpers/postgres.rb
@@ -127,7 +127,7 @@ class Clover
 
     options.add_option(name: "family", values: Option::POSTGRES_FAMILY_OPTIONS.keys, parent: "location") do |flavor, location, family|
       if location.aws?
-        ["m8gd", "i8g"].include?(family) || (Option::AWS_FAMILY_OPTIONS.include?(family) && @project.send(:"get_ff_enable_#{family}"))
+        ["m8gd", "i8g", "i8ge", "i7i", "i7ie"].include?(family) || (Option::AWS_FAMILY_OPTIONS.include?(family) && @project.send(:"get_ff_enable_#{family}"))
       else
         family == "standard" || family == "burstable"
       end
