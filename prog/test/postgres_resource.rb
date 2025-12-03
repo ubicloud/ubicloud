a751beb0b0d2cf2be004c1b6bde11046d631c4a3
Enes Cakir <enes@ubicloud.com>
2025-12-03 17:07:00 +0300

    Add E2E test for no-HA PG on AWS
    Since we have an E2E test for runner on AWS, it’s straightforward to add
    a test for no-HA PG on AWS as well.
    
    I didn’t split the metal and AWS tests into separate files because, for
    both runners and no-HA PG, the only difference is the arguments passed
    to nexus assemble. The overall test logic remains the same.

--- prog/test/postgres_resource.rb
+++ prog/test/postgres_resource.rb
@@ -5,12 +5,13 @@ require_relative "../../lib/util"
 class Prog::Test::PostgresResource < Prog::Test::Base
   semaphore :destroy
 
-  def self.assemble
+  def self.assemble(provider: "metal")
     postgres_test_project = Project.create(name: "Postgres-Test-Project")
     postgres_service_project = Project[Config.postgres_service_project_id] ||
       Project.create_with_id(Config.postgres_service_project_id || Project.generate_uuid, name: "Postgres-Service-Project")
 
     frame = {
+      "provider" => provider,
       "postgres_service_project_id" => postgres_service_project.id,
       "postgres_test_project_id" => postgres_test_project.id
     }
@@ -23,12 +24,22 @@ class Prog::Test::PostgresResource < Prog::Test::Base
   end
 
   label def start
+    location_id, target_vm_size, target_storage_size_gib = if frame["provider"] == "aws"
+      location = Location[provider: "aws", project_id: nil, name: "us-west-2"]
+      LocationCredential.create_with_id(location.id, access_key: Config.e2e_aws_access_key, secret_key: Config.e2e_aws_secret_key)
+      family = "m8gd"
+      vcpus = 2
+      [location.id, Option.aws_instance_type_name(family, vcpus), Option::AWS_STORAGE_SIZE_OPTIONS[family][vcpus].first.to_i]
+    else
+      [Location::HETZNER_FSN1_ID, "standard-2", 128]
+    end
+
     st = Prog::Postgres::PostgresResourceNexus.assemble(
       project_id: frame["postgres_test_project_id"],
-      location_id: Location::HETZNER_FSN1_ID,
+      location_id:,
       name: "postgres-test-standard",
-      target_vm_size: "standard-2",
-      target_storage_size_gib: 128
+      target_vm_size:,
+      target_storage_size_gib:
     )
 
     update_stack({"postgres_resource_id" => st.id})
@@ -59,6 +70,11 @@ class Prog::Test::PostgresResource < Prog::Test::Base
 
   label def wait_resources_destroyed
     nap 5 if postgres_resource
+    if PrivateSubnet[project_id: frame["postgres_test_project_id"]]
+      Clog.emit("Waiting for private subnet to be destroyed")
+      nap 5
+    end
+
     hop_destroy
   end
 
--- spec/prog/test/postgres_resource_spec.rb
+++ spec/prog/test/postgres_resource_spec.rb
@@ -5,13 +5,17 @@ require_relative "../../model/spec_helper"
 RSpec.describe Prog::Test::PostgresResource do
   subject(:pgr_test) { described_class.new(described_class.assemble) }
 
-  let(:postgres_service_project_id) { "546a1ed8-53e5-86d2-966c-fb782d2ae3ab" }
+  let(:test_project) { Project.create(name: "test-project") }
+  let(:service_project) { Project.create(name: "service-project") }
   let(:working_representative_server) { instance_double(PostgresServer, run_query: "DROP TABLE\nCREATE TABLE\nINSERT 0 10\n4159.90\n415.99\n4.1") }
   let(:faulty_representative_server) { instance_double(PostgresServer, run_query: "") }
 
+  before do
+    allow(Config).to receive(:postgres_service_project_id).and_return(service_project.id)
+  end
+
   describe ".assemble" do
     it "creates a strand and service projects" do
-      expect(Config).to receive(:postgres_service_project_id).exactly(2).and_return(postgres_service_project_id)
       st = described_class.assemble
       expect(st).to be_a Strand
       expect(st.label).to eq("start")
@@ -19,9 +23,18 @@ RSpec.describe Prog::Test::PostgresResource do
   end
 
   describe "#start" do
-    it "hops to wait_postgres_resource" do
-      expect(Prog::Postgres::PostgresResourceNexus).to receive(:assemble).and_return(instance_double(Strand, id: "1234"))
+    it "creates resource on metal and hops to wait_postgres_resource" do
+      expect(pgr_test).to receive(:frame).and_return({"provider" => "metal", "postgres_test_project_id" => test_project.id}).at_least(:once)
       expect { pgr_test.start }.to hop("wait_postgres_resource")
+    end
+
+    it "creates resource on aws and hops to wait_postgres_resource" do
+      expect(Config).to receive(:e2e_aws_access_key).and_return("access_key")
+      expect(Config).to receive(:e2e_aws_secret_key).and_return("secret_key")
+      expect(pgr_test).to receive(:frame).and_return({"provider" => "aws", "postgres_test_project_id" => test_project.id}).at_least(:once)
+      expect { pgr_test.start }.to hop("wait_postgres_resource")
+      location = Location[provider: "aws", project_id: nil, name: "us-west-2"]
+      expect(LocationCredential[location.id].access_key).to eq("access_key")
     end
   end
 
@@ -62,6 +75,12 @@ RSpec.describe Prog::Test::PostgresResource do
   describe "#wait_resources_destroyed" do
     it "naps if the postgres resource isn't deleted yet" do
       expect(pgr_test).to receive(:postgres_resource).and_return(instance_double(PostgresResource))
+      expect { pgr_test.wait_resources_destroyed }.to nap(5)
+    end
+
+    it "naps if the private subnet isn't deleted yet" do
+      expect(pgr_test).to receive(:postgres_resource).and_return(nil)
+      expect(PrivateSubnet).to receive(:[]).and_return(instance_double(PrivateSubnet))
       expect { pgr_test.wait_resources_destroyed }.to nap(5)
     end
 
