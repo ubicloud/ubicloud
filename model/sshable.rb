91cbf5fb3638a63152ae4608363cef79d9933d9c
Jeremy Evans <jeremy@ubicloud.com>
2025-12-03 14:29:56 -0800

    Use NetSsh::WarnUnsafe::Sshable in Sshable model
    Have the specs actually testing #cmd implementation use
    _skip_command_checking to prevent the default checking.

--- model/sshable.rb
+++ model/sshable.rb
@@ -4,6 +4,8 @@ require_relative "../model"
 require_relative "../lib/net_ssh"
 
 class Sshable < Sequel::Model
+  prepend NetSsh::WarnUnsafe::Sshable
+
   # We need to unrestrict primary key so Sshable.new(...).save_changes works
   # in sshable_spec.rb.
   unrestrict_primary_key
