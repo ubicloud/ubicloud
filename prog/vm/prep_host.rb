be6ad938a54fc8a3288847fae7c214720c9bb597
Jeremy Evans <jeremy@ubicloud.com>
2025-12-04 14:27:55 -0800

    Avoid interpolated command string in Prog::Vm::PrepHost

--- prog/vm/prep_host.rb
+++ prog/vm/prep_host.rb
@@ -4,7 +4,7 @@ class Prog::Vm::PrepHost < Prog::Base
   subject_is :sshable, :vm_host
 
   label def start
-    sshable.cmd("sudo host/bin/prep_host.rb #{vm_host.ubid.shellescape} #{Config.rack_env.shellescape}")
+    sshable.cmd("sudo host/bin/prep_host.rb :ubid :rack_env", ubid: vm_host.ubid, rack_env: Config.rack_env)
     pop "host prepared"
   end
 end
