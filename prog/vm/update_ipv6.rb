e20bcd1ad73887d6a2e29ad3a0a28a89386c49b4
Jeremy Evans <jeremy@ubicloud.com>
2025-12-04 14:31:30 -0800

    Avoid interpolated command string in Prog::Vm::UpdateIpv6

--- prog/vm/update_ipv6.rb
+++ prog/vm/update_ipv6.rb
@@ -9,11 +9,15 @@ class Prog::Vm::UpdateIpv6 < Prog::Base
     @vm_host ||= VmHost[vm.vm_host_id]
   end
 
+  def inhost_name
+    @inhost_name ||= vm.inhost_name
+  end
+
   label def start
-    vm_host.sshable.cmd("sudo systemctl stop #{vm.inhost_name}.service")
-    vm_host.sshable.cmd("sudo systemctl stop #{vm.inhost_name}-metadata-endpoint.service") if vm.load_balancer&.cert_enabled
-    vm_host.sshable.cmd("sudo systemctl stop #{vm.inhost_name}-dnsmasq.service")
-    vm_host.sshable.cmd("sudo ip netns del #{vm.inhost_name}")
+    vm_host.sshable.cmd("sudo systemctl stop :inhost_name.service", inhost_name:)
+    vm_host.sshable.cmd("sudo systemctl stop :inhost_name-metadata-endpoint.service", inhost_name:) if vm.load_balancer&.cert_enabled
+    vm_host.sshable.cmd("sudo systemctl stop :inhost_name-dnsmasq.service", inhost_name:)
+    vm_host.sshable.cmd("sudo ip netns del :inhost_name", inhost_name:)
     hop_rewrite_persisted
   end
 
@@ -23,7 +27,7 @@ class Prog::Vm::UpdateIpv6 < Prog::Base
     )
 
     write_params_json
-    vm_host.sshable.cmd("sudo host/bin/setup-vm reassign-ip6 #{vm.inhost_name}", stdin: JSON.generate({storage: vm.storage_secrets}))
+    vm_host.sshable.cmd("sudo host/bin/setup-vm reassign-ip6 :inhost_name", inhost_name:, stdin: JSON.generate({storage: vm.storage_secrets}))
     hop_start_vm
   end
 
@@ -31,16 +35,16 @@ class Prog::Vm::UpdateIpv6 < Prog::Base
     nic = vm.nics.first
     addr = nic.private_subnet.net4.nth(1).to_s + nic.private_subnet.net4.netmask.to_s
 
-    vm_host.sshable.cmd("sudo ip -n #{vm.inhost_name.shellescape} addr replace #{addr} dev #{nic.ubid_to_tap_name}")
-    vm_host.sshable.cmd("sudo systemctl start #{vm.inhost_name}-metadata-endpoint.service") if vm.load_balancer&.cert_enabled
+    vm_host.sshable.cmd("sudo ip -n :inhost_name addr replace :addr dev :tap_name", inhost_name:, addr:, tap_name: nic.ubid_to_tap_name)
+    vm_host.sshable.cmd("sudo systemctl start :inhost_name-metadata-endpoint.service", inhost_name:) if vm.load_balancer&.cert_enabled
     vm.incr_update_firewall_rules
     vm.private_subnets.first.incr_refresh_keys
     pop "VM #{vm.name} updated"
   end
 
   def write_params_json
-    vm_host.sshable.cmd("sudo rm /vm/#{vm.inhost_name}/prep.json")
+    vm_host.sshable.cmd("sudo rm /vm/:inhost_name/prep.json", inhost_name:)
 
-    vm_host.sshable.cmd("sudo -u #{vm.inhost_name} tee /vm/#{vm.inhost_name}/prep.json", stdin: vm.params_json(**vm.strand.stack.first.slice("swap_size_bytes", "hugepages", "hypervisor", "ch_version", "firmware_version").transform_keys!(&:to_sym)))
+    vm_host.sshable.cmd("sudo -u :inhost_name tee /vm/:inhost_name/prep.json", inhost_name:, stdin: vm.params_json(**vm.strand.stack.first.slice("swap_size_bytes", "hugepages", "hypervisor", "ch_version", "firmware_version").transform_keys!(&:to_sym)))
   end
 end
