1fa3f3d46148611349c4d041c01aa09536216676
Jeremy Evans <jeremy@ubicloud.com>
2025-12-05 11:14:45 -0800

    Avoid interpolated command string in Prog::SetupHugepages
    This is interpolated inside a shell command. While none of the
    substituted values need shell escaping, to keep with secure
    practices, ensure the substitution takes place outside an
    existing shell quote.

--- spec/prog/setup_hugepages_spec.rb
+++ spec/prog/setup_hugepages_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Prog::SetupHugepages do
       vm_host = instance_double(VmHost)
       allow(vm_host).to receive(:total_mem_gib).and_return(64)
       sshable = Sshable.new
-      expect(sshable).to receive(:_cmd).with(/sudo sed.*default_hugepagesz=1G.*hugepagesz=1G.*hugepages=59.*grub/)
+      expect(sshable).to receive(:_cmd).with("sudo sed -i '/^GRUB_CMDLINE_LINUX=\"/ s/\"$/ hugetlb_free_vmemmap=on default_hugepagesz='1G' hugepagesz='1G' hugepages='59'&/' /etc/default/grub")
       expect(sshable).to receive(:_cmd).with("sudo update-grub")
       expect(sh).to receive(:sshable).and_return(sshable).at_least(:once)
       expect(sh).to receive(:vm_host).and_return(vm_host).at_least(:once)
