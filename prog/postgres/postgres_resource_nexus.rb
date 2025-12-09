abdf713f428f5d04c935f837d4d59c923b690b25
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2025-12-09 16:02:48 +0100

    Use existing value in textarea inputs for update forms

--- prog/postgres/postgres_resource_nexus.rb
+++ prog/postgres/postgres_resource_nexus.rb
@@ -52,9 +52,7 @@ class Prog::Postgres::PostgresResourceNexus < Prog::Base
       postgres_resource = PostgresResource.create(
         project_id:, location_id: location.id, name:,
         target_vm_size:, target_storage_size_gib:,
-        superuser_password:, ha_type:, target_version:, flavor:, parent_id:,
-        tags:, restore_target:, hostname_version: "v2", user_config:,
-        pgbouncer_user_config:
+        superuser_password:, ha_type:, target_version:, flavor:, parent_id:, tags:, restore_target:, hostname_version: "v2", user_config:, pgbouncer_user_config:
       )
 
       PostgresInitScript.create_with_id(postgres_resource, init_script:) if init_script && !init_script.empty?
