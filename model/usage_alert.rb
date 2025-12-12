9adeacfc3af6c98bbad270307f2ca2d0b458a3c4
Enes Cakir <enes@ubicloud.com>
2025-12-12 19:42:42 +0300

    Pass current cost to usage alerts instead of recalculating it
    Calculating the current cost is an expensive operation. Since we already
    compute it once, we should pass this value directly to the trigger
    method rather than recalculating it for each usage alert.

--- model/usage_alert.rb
+++ model/usage_alert.rb
@@ -8,16 +8,16 @@ class UsageAlert < Sequel::Model
 
   plugin ResourceMethods
 
-  def trigger
-    send_email
+  def trigger(current_cost)
+    send_email(current_cost)
     update(last_triggered_at: Time.now)
   end
 
-  def send_email
+  def send_email(current_cost)
     Util.send_email(user.email, "Usage alert is triggered for project #{project.name}",
       greeting: "Hello #{user.name},",
       body: ["The usage alert, #{name}, you set for project #{project.name} (id: #{project.ubid}) has been triggered.",
-        "Current cost: $#{project.current_invoice.content["cost"].to_f.round(2)}",
+        "Current cost: $#{current_cost.to_f.round(2)}",
         "Please note that this alert is only for informational purposes and no action is taken automatically."],
       button_title: "See usage",
       button_link: "#{Config.base_url}#{project.path}/billing")
