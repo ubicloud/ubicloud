c3644b3b075f9d204b5463b0f736ccc24d7299e8
Enes Cakir <enes@ubicloud.com>
2025-12-12 19:25:18 +0300

    Calculate current usage for each project once in CheckUsageAlerts
    I noticed a few apoptoses caused by this prog in production, and there
    are some low-hanging optimizations we can apply.
    
    The current usage calculation is an expensive operation. Since a project
    can have multiple usage alerts, we should compute the usage only once
    per project rather than repeating the calculation for each alert.
    
    I checked production data, and around 30% of projects with usage alerts
    have more than one alert configured.

--- prog/check_usage_alerts.rb
+++ prog/check_usage_alerts.rb
@@ -5,9 +5,11 @@ class Prog::CheckUsageAlerts < Prog::Base
     begin_time = Date.new(Time.now.year, Time.now.month, 1).to_time
 
     alerts = UsageAlert.eager(:project).where { last_triggered_at < begin_time }.all
-    alerts.each do |alert|
-      cost = alert.project.current_invoice.content["cost"]
-      alert.trigger if cost > alert.limit
+    alerts.group_by(&:project).each do |project, project_alerts|
+      cost = project.current_invoice.content["cost"]
+      project_alerts.each do |alert|
+        alert.trigger if cost > alert.limit
+      end
     end
 
     nap 5 * 60
