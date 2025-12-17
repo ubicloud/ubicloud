319916847029a76d46a24f33c3e20600f605e5fa
Jeremy Evans <jeremy@ubicloud.com>
2025-12-17 12:12:32 -0800

    Use ProviderDispatcher in PostgresServer
    This adds add_provider_configs and refresh_walg_blob_storage_credentials
    methods so they can have separate aws/metal implementations.
    
    This adds an additional_labels local variable because coverage
    testing was invalidly flagging the line as not covered.
    
    This modifies the mocking in the specs so the specs continue to pass.

---
+++ model/postgres/metal/postgres_server.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class PostgresServer < Sequel::Model
+  module Metal
+    private
+
+    def metal_add_provider_configs(configs)
+      # nothing
+    end
+
+    def metal_refresh_walg_blob_storage_credentials
+      vm.sshable.cmd("sudo tee /usr/lib/ssl/certs/blob_storage_ca.crt > /dev/null", stdin: timeline.blob_storage.root_certs)
+    end
+
+    def metal_storage_device_paths
+      [vm.vm_storage_volumes.find { it.boot == false }.device_path]
+    end
+
+    def metal_attach_s3_policy_if_needed
+      # nothing
+    end
+  end
+end
