1b2d5a16ec3df97666da0ffa1d16d743c2b2f977
Jeremy Evans <jeremy@ubicloud.com>
2025-12-17 12:08:42 -0800

    Use ProviderDispatcher in PrivateSubnet
    This doesn't use the method dispatching feature, but it allows
    moving the methods that only are used in the metal implementation
    to a metal-separate file.

--- model/private_subnet.rb
+++ model/private_subnet.rb
@@ -54,6 +54,7 @@ class PrivateSubnet < Sequel::Model
   end
 
   plugin ResourceMethods
+  plugin ProviderDispatcher, __FILE__
 
   def self.ubid_to_name(ubid)
     ubid.to_s[0..7]
@@ -121,44 +122,6 @@ class PrivateSubnet < Sequel::Model
     addr
   end
 
-  def connect_subnet(subnet)
-    ConnectedSubnet.create(subnet_hash(subnet))
-    nics.each do |nic|
-      create_tunnels(subnet.nics, nic)
-    end
-    subnet.incr_refresh_keys
-  end
-
-  def disconnect_subnet(subnet)
-    nics(eager: {src_ipsec_tunnels: [:src_nic, :dst_nic]}, dst_ipsec_tunnels: [:src_nic, :dst_nic]).each do |nic|
-      (nic.src_ipsec_tunnels + nic.dst_ipsec_tunnels).each do |tunnel|
-        tunnel.destroy if tunnel.src_nic.private_subnet_id == subnet.id || tunnel.dst_nic.private_subnet_id == subnet.id
-      end
-    end
-    ConnectedSubnet.where(subnet_hash(subnet)).destroy
-    subnet.incr_refresh_keys
-    incr_refresh_keys
-  end
-
-  def create_tunnels(nics, src_nic)
-    nics.each do |dst_nic|
-      next if src_nic == dst_nic
-
-      IpsecTunnel.create(src_nic_id: src_nic.id, dst_nic_id: dst_nic.id) unless IpsecTunnel[src_nic_id: src_nic.id, dst_nic_id: dst_nic.id]
-      IpsecTunnel.create(src_nic_id: dst_nic.id, dst_nic_id: src_nic.id) unless IpsecTunnel[src_nic_id: dst_nic.id, dst_nic_id: src_nic.id]
-    end
-  end
-
-  def find_all_connected_nics
-    Nic
-      .where(private_subnet_id: DB[:subnet].exclude(:is_cycle).select(:id))
-      .with_recursive(:subnet,
-        this.select(:id),
-        DB[:connected_subnet]
-          .join(:subnet, {id: [:subnet_id_1, :subnet_id_2]})
-          .select(Sequel.case({subnet_id_1: :subnet_id_2}, :subnet_id_1, Sequel[:subnet][:id])),
-        cycle: {columns: :id})
-  end
   def incr_destroy_if_only_used_internally(ubid:, vm_ids:)
     firewalls_dataset = self.firewalls_dataset
 
