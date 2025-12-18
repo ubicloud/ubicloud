a02a8728cd04c94bb18d70b7e424373d1855e2a3
shikharbhardwaj <8502456+shikharbhardwaj@users.noreply.github.com>
2025-12-18 17:08:10 +0100

    Update PG image to optimize postgres_exporter
    In its default configuration, postgres_exporter outputs a number of
    timeseries whose cardinality scales with the count of logical databases
    on the instance. This produces two bad effects:
    
    1. The metrics scrape size is unbounded, as it scales with however many
       databases the user creates. This leads to a backlog while exporting
       metrics and dropped metrics data on an instance with large (1k+) DBs.
    2. More seriously, on low spec instances (standard-2), this can cause
       the exporter process to consume a significant amount of the available
       resources, which can potentially starve other processes disrupting
       other key operations like archiving.
    
    In this change, we disable a lot of the collectors enabled by default.
    Additionally, to ensure we still have visibility into some key metrics,
    we introduce a set of custom queries that produce instance-aggregated
    metrics.

--- config.rb
+++ config.rb
@@ -184,7 +184,7 @@ module Config
   override :github_gpu_ubuntu_2204_version, "20251208.1.0", string
   override :github_ubuntu_2204_aws_ami_version, "ami-06f02994ea7801a2d", string
   override :github_ubuntu_2404_aws_ami_version, "ami-0b44ef7a91252a724", string
-  override :postgres_ubuntu_2204_version, "20251216.1.0", string
+  override :postgres_ubuntu_2204_version, "20251218.1.0", string
   override :postgres16_ubuntu_2204_version, "20250425.1.1", string
   override :postgres17_ubuntu_2204_version, "20250425.1.1", string
   override :postgres_paradedb_ubuntu_2204_version, "20250803.1.0", string
