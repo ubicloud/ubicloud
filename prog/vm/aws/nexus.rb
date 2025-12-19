bae7b151c65549ded285ea47bde2be1e2c6ceebe
Kunal Gupta <39487888+iamKunalGupta@users.noreply.github.com>
2025-12-20 01:58:17 +0530

    Ignore missing role on aws vm cleanup

--- prog/vm/aws/nexus.rb
+++ prog/vm/aws/nexus.rb
@@ -336,9 +336,11 @@ class Prog::Vm::Aws::Nexus < Prog::Base
     end
 
     if Config.aws_postgres_iam_access
-      iam_client.list_attached_role_policies(role_name:).attached_policies.each do |policy|
-        ignore_invalid_entity do
-          iam_client.detach_role_policy(role_name:, policy_arn: policy.policy_arn)
+      ignore_invalid_entity do
+        iam_client.list_attached_role_policies(role_name:).attached_policies.each do |policy|
+          ignore_invalid_entity do
+            iam_client.detach_role_policy(role_name:, policy_arn: policy.policy_arn)
+          end
         end
       end
     end
