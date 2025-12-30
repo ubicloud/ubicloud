e2bbddcd81c413a03872a150c37e3ffca9df2e45
Daniel Farina <daniel@ubicloud.com>
2025-12-29 18:48:57 -0800

    Demock project_spec
    Some assocations have been democked. Some pre/post invariants have been
    tightened up with `change` constructions to more quickly guide the
    reader on pre and post invariants.

--- spec/model/project_spec.rb
+++ spec/model/project_spec.rb
@@ -93,18 +93,19 @@ RSpec.describe Project do
     it "deletes github installations" do
       SubjectTag.create(project_id: project.id, name: "test").add_subject(project.id)
       AccessControlEntry.new_with_id(project_id: project.id, subject_id: project.id).save_changes(validate: false)
-      expect(project).to receive(:github_installations).and_return([instance_double(GithubInstallation)])
-      expect(Prog::Github::DestroyGithubInstallation).to receive(:assemble)
-      expect(project).to receive(:update).with(visible: false)
-      project.soft_delete
-      expect(SubjectTag.all).to be_empty
-      expect(AccessControlEntry.all).to be_empty
+      github_installation = GithubInstallation.create(installation_id: 123, name: "test-install", project_id: project.id, type: "User")
+
+      expect { project.soft_delete }
+        .to change { Strand.where(prog: "Github::DestroyGithubInstallation", stack: Sequel.pg_jsonb([{"subject_id" => github_installation.id}])).count }.from(0).to(1)
+        .and change(SubjectTag, :count).from(1).to(0)
+        .and change(AccessControlEntry, :count).from(1).to(0)
+        .and change { project.reload.visible }.from(true).to(false)
     end
   end
 
   describe ".active?" do
     it "returns false if it's soft deleted" do
-      expect(project).to receive(:visible).and_return(false)
+      project.update(visible: false)
       expect(project.active?).to be false
     end
 
@@ -158,40 +159,43 @@ RSpec.describe Project do
 
   it "calculates current resource usage" do
     expect(project.current_resource_usage("VmVCpu")).to eq 0
-    vm1 = Prog::Vm::Nexus.assemble("a a", project.id).subject
-    expect(project.current_resource_usage("VmVCpu")).to eq 2
-    Prog::Vm::Nexus.assemble("a a", project.id, size: "standard-4")
-    expect(project.current_resource_usage("VmVCpu")).to eq 6
+    vm1 = nil
+    expect {
+      vm1 = Prog::Vm::Nexus.assemble("a a", project.id).subject
+    }.to change { project.current_resource_usage("VmVCpu") }.from(0).to(2)
+    expect {
+      Prog::Vm::Nexus.assemble("a a", project.id, size: "standard-4")
+    }.to change { project.current_resource_usage("VmVCpu") }.from(2).to(6)
 
-    expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 0
     gi = GithubInstallation.create(installation_id: 1, name: "a", project_id: project.id, type: "a")
     gr = gi.add_runner(label: "ubicloud", repository_name: "a/a")
     gr.update(vm_id: vm1.id)
     expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 0
     grst = Strand.new(id: gr.id, label: "start", prog: "Prog::Github::GithubRunnerNexus")
-    expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 0
-    grst.update(label: "wait_vm")
-    expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 2
+    expect { grst.update(label: "wait_vm") }.to change { project.current_resource_usage("GithubRunnerVCpu") }.from(0).to(2)
     gr2 = gi.add_runner(label: "ubicloud-standard-60", repository_name: "a/a")
     grst2 = Strand.new(id: gr2.id, label: "wait_concurrency_limit", prog: "Prog::Github::GithubRunnerNexus")
     expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 2
-    grst2.update(label: "wait_vm")
-    expect(project.current_resource_usage("GithubRunnerVCpu")).to eq 62
+    expect { grst2.update(label: "wait_vm") }.to change { project.current_resource_usage("GithubRunnerVCpu") }.from(2).to(62)
 
     expect(Config).to receive(:postgres_service_project_id).and_return(project.id).at_least(:once)
     expect(project.current_resource_usage("PostgresVCpu")).to eq 0
-    Prog::Postgres::PostgresResourceNexus.assemble(project_id: project.id, location_id: Location::HETZNER_FSN1_ID, name: "a", target_vm_size: "standard-2", target_storage_size_gib: 64)
-    expect(project.current_resource_usage("PostgresVCpu")).to eq 2
-    Prog::Postgres::PostgresResourceNexus.assemble(project_id: project.id, location_id: Location::HETZNER_FSN1_ID, name: "b", target_vm_size: "standard-4", target_storage_size_gib: 128)
-    expect(project.current_resource_usage("PostgresVCpu")).to eq 6
+    expect {
+      Prog::Postgres::PostgresResourceNexus.assemble(project_id: project.id, location_id: Location::HETZNER_FSN1_ID, name: "a", target_vm_size: "standard-2", target_storage_size_gib: 64)
+    }.to change { project.current_resource_usage("PostgresVCpu") }.from(0).to(2)
+    expect {
+      Prog::Postgres::PostgresResourceNexus.assemble(project_id: project.id, location_id: Location::HETZNER_FSN1_ID, name: "b", target_vm_size: "standard-4", target_storage_size_gib: 128)
+    }.to change { project.current_resource_usage("PostgresVCpu") }.from(2).to(6)
 
     expect(Config).to receive(:kubernetes_service_project_id).and_return(project.id).at_least(:once)
     expect(project.current_resource_usage("KubernetesVCpu")).to eq 0
-    cluster = Prog::Kubernetes::KubernetesClusterNexus.assemble(name: "a", project_id: project.id, location_id: Location::HETZNER_FSN1_ID, cp_node_count: 1).subject
-    Prog::Kubernetes::KubernetesNodeNexus.assemble(project.id, sshable_unix_user: "ubi", name: "cp-node", location_id: cluster.location.id, size: cluster.target_node_size, storage_volumes: [{encrypted: true, size_gib: cluster.target_node_storage_size_gib}], boot_image: "kubernetes-#{cluster.version.tr(".", "_")}", private_subnet_id: cluster.private_subnet_id, enable_ip4: true, kubernetes_cluster_id: cluster.id).subject
-    expect(project.current_resource_usage("KubernetesVCpu")).to eq 2
-    Prog::Kubernetes::KubernetesNodepoolNexus.assemble(name: "a-np", node_count: 3, kubernetes_cluster_id: cluster.id, target_node_size: "standard-4").subject
-    expect(project.current_resource_usage("KubernetesVCpu")).to eq 14
+    cluster = nil
+    expect {
+      cluster = Prog::Kubernetes::KubernetesClusterNexus.assemble(name: "a", project_id: project.id, location_id: Location::HETZNER_FSN1_ID, cp_node_count: 1).subject
+    }.to change { project.current_resource_usage("KubernetesVCpu") }.from(0).to(2)
+    expect {
+      Prog::Kubernetes::KubernetesNodepoolNexus.assemble(name: "a-np", node_count: 3, kubernetes_cluster_id: cluster.id, target_node_size: "standard-4").subject
+    }.to change { project.current_resource_usage("KubernetesVCpu") }.from(2).to(14)
 
     expect { project.current_resource_usage("UnknownResource") }.to raise_error(RuntimeError)
   end
@@ -216,8 +220,10 @@ RSpec.describe Project do
   end
 
   it "checks if quota is available" do
-    expect(project).to receive(:current_resource_usage).and_return(10).twice
-    expect(project).to receive(:effective_quota_value).and_return(20).twice
+    Prog::Vm::Nexus.assemble("a a", project.id, size: "standard-4")
+    Prog::Vm::Nexus.assemble("a a", project.id, size: "standard-4")
+    Prog::Vm::Nexus.assemble("a a", project.id, size: "standard-2")
+    project.add_quota(quota_id: ProjectQuota.default_quotas["VmVCpu"]["id"], value: 20)
     expect(project.quota_available?("VmVCpu", 5)).to be true
     expect(project.quota_available?("VmVCpu", 20)).to be false
   end
