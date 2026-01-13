1355885395ff2bb827a4ab13001469c063b441fd
mohi-kalantari <mohi.kalantari1@gmail.com>
2026-01-13 10:21:15 +0100

    Implement etcd backups with MinIO upload
    Introduces the KubernetesEtcdBackup strand and associated logic to
    perform hourly etcd backups and upload them to a MinIO bucket.
    
    - Model: Add methods to KubernetesEtcdBackup model to check backup
    status, setup_bucket, and retrieve blob storage credentials.
    - Nexus: Create `Prog::Kubernetes::EtcdBackupNexus` to manage the
    backup lifecycle:
        - Sets up blob storage user and policy.
        - Creates the backup bucket.
        - Triggers the backup job periodically.
        - Passes MinIO credentials to the backup script.
    - Rhizome: Add backup-etcd script to:
        - Install `mc` (MinIO Client) idempotently.
        - Clean up old backups to prevent disk accumulation.
        - Capture etcd snapshot using `etcdctl` via `crictl`.
        - Upload the backup to the specified MinIO bucket.
        - Clean up the local backup file after upload.

--- model/kubernetes/kubernetes_etcd_backup.rb
+++ model/kubernetes/kubernetes_etcd_backup.rb
@@ -3,9 +3,63 @@
 require_relative "../../model"
 
 class KubernetesEtcdBackup < Sequel::Model
-  many_to_one :kubernetes_cluster
+  one_to_one :strand, key: :id
+  many_to_one :location, read_only: true
+  many_to_one :kubernetes_cluster, read_only: true
 
   plugin ResourceMethods, encrypted_columns: :secret_key
+  plugin SemaphoreMethods, :destroy
+
+  BACKUP_BUCKET_EXPIRATION_DAYS = 7
+
+  def need_backup?
+    return false unless blob_storage
+    return false if kubernetes_cluster.functional_nodes.empty?
+
+    sshable = kubernetes_cluster.sshable
+    case sshable.d_check("backup_etcd")
+    when "Failed", "NotStarted"
+      true
+    when "Succeeded"
+      latest_backup_started_at.nil? || latest_backup_started_at < Time.now - 60 * 60
+    else
+      false
+    end
+  end
+
+  def next_backup_time
+    return Time.now + 86400 unless blob_storage
+    return Time.now unless latest_backup_started_at
+
+    latest_backup_started_at + 60 * 60
+  end
+
+  def blob_storage
+    return @blob_storage if defined?(@blob_storage)
+    @blob_storage = MinioCluster[project_id: Config.minio_service_project_id, location_id: location.id]
+  end
+
+  def blob_storage_policy
+    {Version: "2012-10-17", Statement: [{Effect: "Allow", Action: ["s3:*"], Resource: ["arn:aws:s3:::#{ubid}*"]}]}
+  end
+
+  def blob_storage_endpoint
+    @blob_storage_endpoint ||= blob_storage.url || blob_storage.ip4_urls.sample
+  end
+
+  def blob_storage_client
+    @blob_storage_client ||= Minio::Client.new(
+      endpoint: blob_storage_endpoint,
+      access_key:,
+      secret_key:,
+      ssl_ca_data: blob_storage.root_certs
+    )
+  end
+
+  def setup_bucket
+    blob_storage_client.create_bucket(ubid)
+    blob_storage_client.set_lifecycle_policy(ubid, ubid, BACKUP_BUCKET_EXPIRATION_DAYS)
+  end
 end
 
 # Table: kubernetes_etcd_backup
---
+++ prog/kubernetes/etcd_backup_nexus.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+class Prog::Kubernetes::EtcdBackupNexus < Prog::Base
+  subject_is :kubernetes_etcd_backup
+
+  def self.assemble(kubernetes_cluster_id)
+    unless (kc = KubernetesCluster[kubernetes_cluster_id])
+      fail "KubernetesCluster does not exist"
+    end
+
+    DB.transaction do
+      keb = KubernetesEtcdBackup.create(
+        kubernetes_cluster_id: kc.id,
+        access_key: SecureRandom.hex(16),
+        secret_key: SecureRandom.hex(32),
+        location_id: kc.location.id
+      )
+      Strand.create_with_id(keb, prog: "Kubernetes::EtcdBackupNexus", label: "setup_blob_storage")
+    end
+  end
+
+  def kubernetes_cluster
+    @kubernetes_cluster ||= kubernetes_etcd_backup.kubernetes_cluster
+  end
+
+  label def setup_blob_storage
+    admin_client.admin_add_user(kubernetes_etcd_backup.access_key, kubernetes_etcd_backup.secret_key)
+    admin_client.admin_policy_add(kubernetes_etcd_backup.ubid, kubernetes_etcd_backup.blob_storage_policy)
+    admin_client.admin_policy_set(kubernetes_etcd_backup.ubid, kubernetes_etcd_backup.access_key)
+
+    hop_setup_bucket
+  end
+
+  label def setup_bucket
+    kubernetes_etcd_backup.setup_bucket
+
+    hop_wait
+  end
+
+  label def wait
+    if kubernetes_etcd_backup.need_backup?
+      hop_run_backup
+    end
+
+    nap (kubernetes_etcd_backup.next_backup_time - Time.now + 1).clamp(1, 3601)
+  end
+
+  label def run_backup
+    nap 20 * 60 unless kubernetes_cluster.strand.label == "wait"
+
+    kubernetes_etcd_backup.update(latest_backup_started_at: Time.now)
+
+    sshable = kubernetes_cluster.sshable
+    creds = {
+      "access_key" => kubernetes_etcd_backup.access_key,
+      "secret_key" => kubernetes_etcd_backup.secret_key,
+      "endpoint" => kubernetes_etcd_backup.blob_storage_endpoint,
+      "bucket" => kubernetes_etcd_backup.ubid,
+      "root_certs" => kubernetes_etcd_backup.blob_storage.root_certs
+    }
+    sshable.d_run("backup_etcd", "kubernetes/bin/backup-etcd", stdin: JSON.generate(creds), log: false)
+
+    hop_wait
+  end
+
+  label def destroy
+    register_deadline(nil, 5 * 60)
+    decr_destroy
+    admin_client.admin_remove_user(kubernetes_etcd_backup.access_key)
+    admin_client.admin_policy_remove(kubernetes_etcd_backup.ubid)
+
+    kubernetes_etcd_backup.destroy
+    pop "kubernetes etcd backup is deleted"
+  end
+
+  def admin_client
+    @admin_client ||= Minio::Client.new(
+      endpoint: kubernetes_etcd_backup.blob_storage_endpoint,
+      access_key: kubernetes_etcd_backup.blob_storage.admin_user,
+      secret_key: kubernetes_etcd_backup.blob_storage.admin_password,
+      ssl_ca_data: kubernetes_etcd_backup.blob_storage.root_certs
+    )
+  end
+end
