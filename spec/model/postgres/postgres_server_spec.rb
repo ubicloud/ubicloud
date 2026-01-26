29f385d096f5d3901abefcabd107d6c6a319fe3c
Furkan Sahin <furkan@ubicloud.com>
2026-01-26 13:17:00 +0000

    Checks if standby caught up even when primary is offline

--- spec/model/postgres/postgres_server_spec.rb
+++ spec/model/postgres/postgres_server_spec.rb
@@ -355,6 +355,39 @@ RSpec.describe PostgresServer do
       postgres_server.update(representative_at: nil)
       expect(postgres_server.lsn_caught_up).to be(true)
     end
+
+    it "falls back to caught_up_to_archive? when parent server is unreachable" do
+      expect(resource.parent.representative_server).to receive(:current_lsn).and_raise(StandardError)
+      expect(postgres_server).to receive(:caught_up_to_archive?).and_return(true)
+      expect(postgres_server.lsn_caught_up).to be(true)
+    end
+  end
+
+  describe "caught_up_to_archive?" do
+    it "returns false if WAL receiver is streaming" do
+      expect(postgres_server).to receive(:_run_query).with("SELECT status FROM pg_stat_wal_receiver").and_return("streaming")
+      expect(postgres_server.caught_up_to_archive?).to be(false)
+    end
+
+    it "returns true if receive_lsn is empty (never connected to primary)" do
+      expect(postgres_server).to receive(:_run_query).with("SELECT status FROM pg_stat_wal_receiver").and_return("")
+      expect(postgres_server).to receive(:_run_query).with("SELECT pg_last_wal_receive_lsn()").and_return("")
+      expect(postgres_server.caught_up_to_archive?).to be(true)
+    end
+
+    it "returns true if replay has caught up to received" do
+      expect(postgres_server).to receive(:_run_query).with("SELECT status FROM pg_stat_wal_receiver").and_return("")
+      expect(postgres_server).to receive(:_run_query).with("SELECT pg_last_wal_receive_lsn()").and_return("F/F")
+      expect(postgres_server).to receive(:_run_query).with("SELECT pg_last_wal_replay_lsn()").and_return("F/F")
+      expect(postgres_server.caught_up_to_archive?).to be(true)
+    end
+
+    it "returns false if replay is behind received" do
+      expect(postgres_server).to receive(:_run_query).with("SELECT status FROM pg_stat_wal_receiver").and_return("")
+      expect(postgres_server).to receive(:_run_query).with("SELECT pg_last_wal_receive_lsn()").and_return("F/F")
+      expect(postgres_server).to receive(:_run_query).with("SELECT pg_last_wal_replay_lsn()").and_return("1/00000000")
+      expect(postgres_server.caught_up_to_archive?).to be(false)
+    end
   end
 
   it "initiates a new health monitor session" do
