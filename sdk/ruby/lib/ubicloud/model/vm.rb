858c760a1f219c7dfc721c7c3c54949d2ad1d998
Jeremy Evans <jeremy@ubicloud.com>
2026-01-27 12:34:52 -0800

    Add SDK support for Vm start/stop actions

--- sdk/ruby/lib/ubicloud/model/vm.rb
+++ sdk/ruby/lib/ubicloud/model/vm.rb
@@ -23,5 +23,17 @@ module Ubicloud
     def restart
       merge_into_values(adapter.post(_path("/restart")))
     end
+
+    # Schedule a start of the virtual machine. Returns self.
+    def start
+      merge_into_values(adapter.post(_path("/start")))
+    end
+
+    # Schedule a stop of the virtual machine. Returns self.
+    # Note that stopped virtual machines still accrue billing charges.
+    # To avoid billing charges, destroy the virtual machine.
+    def stop
+      merge_into_values(adapter.post(_path("/stop")))
+    end
   end
 end
