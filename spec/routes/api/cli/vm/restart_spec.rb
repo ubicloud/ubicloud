d5b06eb915070b548eba73c82434fc40b6adc1e6
Jeremy Evans <jeremy@ubicloud.com>
2026-01-27 12:44:13 -0800

    Disallow Vm restart/start/stop for AWS VMs
    These semaphores are not supported for AWS VMs, so we should raise
    an error instead of silently accepting and ignoring them.

--- spec/routes/api/cli/vm/restart_spec.rb
+++ spec/routes/api/cli/vm/restart_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Clover, "cli vm restart" do
       expect(cli(%w[vm eu-central-h1/test-vm restart])).to eq("Scheduled restart of VM with id #{@vm.ubid}\n")
     end.to change { Semaphore.where(strand_id: @vm.id, name: "restart").count }.from(0).to(1)
   end
+
+  it "raises error if running on AWS" do
+    @vm.update(location: Location[name: "us-east-1"])
+    expect do
+      expect(cli(%w[vm us-east-1/test-vm restart], status: 400)).to eq "! Unexpected response status: 400\nDetails: The restart action is not supported for VMs running on AWS\n"
+    end.to not_change { Semaphore.where(strand_id: @vm.id, name: "restart").count }
+  end
 end
--- spec/routes/api/cli/vm/start_spec.rb
+++ spec/routes/api/cli/vm/start_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Clover, "cli vm start" do
       expect(cli(%w[vm eu-central-h1/test-vm start])).to eq("Scheduled start of VM with id #{@vm.ubid}\n")
     end.to change { Semaphore.where(strand_id: @vm.id, name: "start").count }.from(0).to(1)
   end
+
+  it "raises error if running on AWS" do
+    @vm.update(location: Location[name: "us-east-1"])
+    expect do
+      expect(cli(%w[vm us-east-1/test-vm start], status: 400)).to eq "! Unexpected response status: 400\nDetails: The start action is not supported for VMs running on AWS\n"
+    end.to not_change { Semaphore.where(strand_id: @vm.id, name: "start").count }
+  end
 end
