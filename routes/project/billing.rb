7dea7e41c2b1c665d98766810d369f3da5ad35a3
Enes Cakir <enes@ubicloud.com>
2026-02-02 14:12:00 +0300

    Fix invoice ID lookup
    We are passing the `:id` symbol instead of the `id` variable. Since
    project invoices are ordered by creation date, this caused the query to
    always return the most recent invoice, regardless of the ID provided.
    
    As a result, customers saw the same invoice when clicking on any invoice
    in the console.

--- routes/project/billing.rb
+++ routes/project/billing.rb
@@ -169,7 +169,7 @@ class Clover
       end
 
       r.on "invoice", ["current", :ubid_uuid] do |id|
-        next unless (invoice = (id == "current") ? @project.current_invoice : @project.invoices_dataset.with_pk(:id))
+        next unless (invoice = (id == "current") ? @project.current_invoice : @project.invoices_dataset.with_pk(id))
 
         r.get true do
           if invoice.status == "current"
