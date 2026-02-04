d1616c36d061784feae153ed74afefd41008433d
Philip Dubé <philip.dube@clickhouse.com>
2026-02-04 03:56:58 +0000

    Improve postgres web restart test

--- spec/routes/web/project/postgres_spec.rb
+++ spec/routes/web/project/postgres_spec.rb
@@ -759,7 +759,9 @@ RSpec.describe Clover, "postgres" do
         expect(page).to have_content "Restart"
         click_button "Restart"
 
+        expect(page).to have_flash_notice "'#{pg.name}' will be restarted in a few seconds"
         expect(page.status_code).to eq(200)
+        expect(pg.servers.map { it.restart_set? }).to all(be true)
       end
 
       it "can cancel storage auto-scale" do
