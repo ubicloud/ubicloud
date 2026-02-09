9bae15d4bfd3b98be8cc3936f886410f2dba5936
mohi-kalantari <mohi.kalantari1@gmail.com>
2026-02-09 10:39:24 +0100

    Persist firewall rules on Kubernetes nodes across reboots
    Replaces the previous approach of applying iptables and nft rules
    at runtime with writing a complete nftables ruleset to
    /etc/nftables.conf and enabling the nftables systemd service. This
    ensures NAT masquerading and IPv6 pod access control rules survive
    node reboots.
    
    The ruleset includes a flush to start clean, an ip nat table for
    POSTROUTING masquerade, and an ip6 pod_access table for ingress
    and egress filtering.

--- prog/kubernetes/provision_kubernetes_node.rb
+++ prog/kubernetes/provision_kubernetes_node.rb
@@ -83,27 +83,36 @@ class Prog::Kubernetes::ProvisionKubernetesNode < Prog::Base
   label def bootstrap_rhizome
     nap 5 unless vm.strand.label == "wait"
 
-    vm.sshable.cmd("sudo iptables-nft -t nat -A POSTROUTING -s :private_ipv4 -o ens3 -j MASQUERADE", private_ipv4: vm.nics.first.private_ipv4)
-    vm.sshable.cmd("sudo nft --file -", stdin: <<TEMPLATE)
-table ip6 pod_access;
-delete table ip6 pod_access;
-table ip6 pod_access {
-  chain ingress_egress_control {
-    type filter hook forward priority filter; policy drop;
-    # allow access to the vm itself in order to not break the normal functionality of Clover and SSH
-    ip6 daddr #{vm.ip6} ct state established,related,new counter accept
-    ip6 saddr #{vm.ip6} ct state established,related,new counter accept
+    nft_rules = <<~NFT
+      #!/usr/sbin/nft -f
+      flush ruleset
 
-    # not allow new connections from internet but allow new connections from inside
-    ip6 daddr #{vm.ephemeral_net6} ct state established,related counter accept
-    ip6 saddr #{vm.ephemeral_net6} ct state established,related,new counter accept
+      table ip nat {
+        chain postrouting {
+          type nat hook postrouting priority 100;
+          ip saddr #{vm.nics.first.private_ipv4} oifname "ens3" masquerade
+        }
+      }
 
-    # used for internal private ipv6 communication between pods
-    ip6 saddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
-    ip6 daddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
-  }
-}
-TEMPLATE
+      table ip6 pod_access {
+        chain ingress_egress_control {
+          type filter hook forward priority filter; policy drop;
+          # allow access to the vm itself in order to not break the normal functionality of Clover and SSH
+          ip6 daddr #{vm.ip6} ct state established,related,new counter accept
+          ip6 saddr #{vm.ip6} ct state established,related,new counter accept
+
+          # not allow new connections from internet but allow new connections from inside
+          ip6 daddr #{vm.ephemeral_net6} ct state established,related counter accept
+          ip6 saddr #{vm.ephemeral_net6} ct state established,related,new counter accept
+
+          # used for internal private ipv6 communication between pods
+          ip6 saddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
+          ip6 daddr #{kubernetes_cluster.private_subnet.net6} ct state established,related,new counter accept
+        }
+      }
+    NFT
+    vm.sshable.cmd("sudo tee /etc/nftables.conf > /dev/null", stdin: nft_rules)
+    vm.sshable.cmd("sudo systemctl enable --now nftables")
     vm.sshable.cmd "sudo systemctl enable --now kubelet"
 
     bud Prog::BootstrapRhizome, {"target_folder" => "kubernetes", "subject_id" => vm.id, "user" => "ubi"}
