d9134dab894fd4da5ccd13b7677ce33e6ede53d6
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2026-02-10 12:46:00 +0100

    Add migration files for representative_until column

---
+++ migrate/20260211_resource_id_index_for_postgres_server.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  no_transaction
+
+  up do
+    alter_table(:postgres_server) do
+      add_index :resource_id, concurrently: true
+    end
+  end
+
+  down do
+    alter_table(:postgres_server) do
+      drop_index :resource_id, concurrently: true
+    end
+  end
+end
