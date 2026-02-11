27e54e9b36f45765faec356a172ea0ee9601ffe9
Jeremy Evans <jeremy@ubicloud.com>
2026-02-11 12:26:08 -0800

    Add Config.clover_admin_development_no_webauthn?
    If this is set in development mode, do not require webauthn be setup.
    Note that even in this case, the developer has the option of setting
    up webauthn.

--- config.rb
+++ config.rb
@@ -51,6 +51,7 @@ module Config
   mandatory :clover_session_secret, base64, clear: true
   mandatory :rack_env, string
 
+  override :clover_admin_development_no_webauthn, false, bool
   optional :clover_runtime_token_secret, base64, clear: true
   optional :kms_decrypt_clover_column_encryption_key_with_arn, string
   optional :heartbeat_url, string
