37e9c1d1af79ec89c22a26e6588e64d7c45b4cec
Jeremy Evans <jeremy@ubicloud.com>
2026-02-19 15:32:32 -0800

    Change stop semaphore to use soft stop followed by hard stop
    This asks the VM to shutdown nicely, then gives the VM a minute to shut
    down. If the VM does not shutdown nicely after a minute, it pulls the
    plug.
    
    Implement this using a stopping semaphore. If stop is set but stopping
    is not, that indicates a new request to stop. In that case, stopping
    is set, a soft shutdown is done, and it goes into the stopping loop.
    
    During the stopping loop, the VM checks whether it is running. If not,
    it decrements the stopping semaphore as it has stopped. If it is
    running, and it has been more than a minute since the the initial soft
    stop was attempted, it does a forced shutdown. If has been less than
    a minute, it again requests a soft shutdown. The reason it requests
    a soft shutdown every loop iteration is I saw cases where the initial
    soft shutdown request was ignored, but additional ones trigger the
    shutdown. Instead of using a frame entry to determine whether it has
    been over a minute, this uses the timestamp from the stopping
    semaphore, relying on the fact that semaphore ubids are timestamp
    ubids and not random ubids.
    
    If stop is set while stopping is already set, it means the VM is
    already attempting to shutdown, so no action is taken.
    
    Rearrange the code slightly so that admin_stop semaphore has the
    most priority, followed by nice stop, followed by stop, followed
    by stopping.

--- model/vm.rb
+++ model/vm.rb
@@ -40,7 +40,7 @@ class Vm < Sequel::Model
   plugin ResourceMethods, redacted_columns: :public_key
   plugin ProviderDispatcher, __FILE__
   plugin SemaphoreMethods, :destroy, :start_after_host_reboot, :prevent_destroy, :update_firewall_rules,
-    :checkup, :update_spdk_dependency, :waiting_for_capacity, :lb_expiry_started, :restart, :start, :stop, :migrate_to_separate_progs, :admin_stop
+    :checkup, :update_spdk_dependency, :waiting_for_capacity, :lb_expiry_started, :restart, :start, :stop, :migrate_to_separate_progs, :admin_stop, :stopping
   include HealthMonitorMethods
 
   include ObjectTag::Cleanup
