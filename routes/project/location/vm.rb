e634d7844e87ffaa019bad7b974e2df1b9370cf5
Jeremy Evans <jeremy@ubicloud.com>
2026-02-19 16:38:40 -0800

    Add Clover#vm_list helper method
    This uses the same approach as Clover#private_subnet_list. Instead
    of having #vm_list_dataset and #vm_list_api_response, use #vm_list
    both for web and api responses.
    
    This makes it so the eager loads for api and web are done at the
    same place. Code is also more localized and easier to follow.

--- routes/project/location/vm.rb
+++ routes/project/location/vm.rb
@@ -3,7 +3,7 @@
 class Clover
   hash_branch(:project_location_prefix, "vm") do |r|
     r.get api? do
-      vm_list_api_response(vm_list_dataset)
+      vm_list
     end
 
     r.on VM_NAME_OR_UBID do |vm_name, vm_id|
