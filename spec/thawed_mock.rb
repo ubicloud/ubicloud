040f4d42daf5fa3d7b45a7251164d84ef5f86a33
Jeremy Evans <jeremy@ubicloud.com>
2026-02-24 16:38:10 -0800

    Handle disconnect/connect errors in monitor worker threads

--- spec/thawed_mock.rb
+++ spec/thawed_mock.rb
@@ -126,6 +126,7 @@ module ThawedMock
   allow_mocking(InvoiceGenerator, :new)
   allow_mocking(Minio::Client, :new)
   allow_mocking(Minio::Crypto, :new)
+  allow_mocking(MonitorResourceType, :sleep, :raise)
   allow_mocking(RunpodClient, :new)
   allow_mocking(Scheduling::Allocator, :allocate)
   allow_mocking(Scheduling::Allocator::Allocation, :best_allocation, :candidate_hosts, :new, :random_score, :update_vm)
