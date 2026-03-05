304b9b4b722d7b881932e05067bbd253961dc34c
Jeremy Evans <jeremy@ubicloud.com>
2026-03-05 15:48:38 -0800

    Raise errors in unfrozen test mode for placeholders inside quotes
    shellescape only works correctly outside of quotes. Usage of
    shellescape inside quotes can result in broken code if the placeholder
    value needs escaping, and potentially security issues if the placeholder
    value is derived from user input.
    
    In unfrozen test mode, use a more complicated approach that will parse
    the shell command and raise an exception if the shell command uses
    placeholders inside a single or double quote. This was used to find all
    of the problems that the preceding commits fixed.
    
    Co-authored-by: Hadi Moshayedi <hadi@ubicloud.com>

--- spec/lib/net_ssh_spec.rb
+++ spec/lib/net_ssh_spec.rb
@@ -1,29 +1,66 @@
 # frozen_string_literal: true
 
 RSpec.describe NetSsh do
-  it ".command interpolates variables into string" do
-    expect(described_class.command("a :b :c d", b: 1, c: "e f")).to eq "a 1 e\\ f d"
+  commands = [:command]
+
+  if Config.unfrozen_test?
+    commands << :prod_command
+
+    it ".command raises for placeholder inside quotes" do
+      expect { described_class.command("a ':c'", c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+      expect { described_class.command('a ":c"', c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+    end
+
+    it ".command raises for unterminated quotes" do
+      expect { described_class.command(":c '", c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+      expect { described_class.command(':c "', c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+      expect { described_class.command(":c 'a", c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+      expect { described_class.command(':c "a', c: "a") }.to raise_error(NetSsh::PotentialInsecurity)
+    end
+
+    it ".command works with comments with '" do
+      expect(described_class.command("# isn't it fun\n:c", c: "e f")).to eq "# isn't it fun\ne\\ f"
+    end
+
+    it ".command works for placeholders after quotes" do
+      c = "e f"
+      expect(described_class.command("'' :c a", c:)).to eq "'' e\\ f a"
+      expect(described_class.command('"" :c', c:)).to eq "\"\" e\\ f"
+      expect(described_class.command("\\' :c", c:)).to eq "\\' e\\ f"
+      expect(described_class.command('\\" :c', c:)).to eq "\\\" e\\ f"
+      expect(described_class.command("'\"' :c", c:)).to eq "'\"' e\\ f"
+      expect(described_class.command('"\'" :c', c:)).to eq "\"'\" e\\ f"
+      expect(described_class.command('"\\"" :c', c:)).to eq "\"\\\"\" e\\ f"
+      expect(described_class.command("'\"'\"'\" :c", c:)).to eq "'\"'\"'\" e\\ f"
+      expect(described_class.command("'\\\"'\"'\" :c", c:)).to eq "'\\\"'\"'\" e\\ f"
+    end
   end
 
-  it ".command handles shelljoin interpolation" do
-    expect(described_class.command("a :b :shelljoin_c d", b: 1, shelljoin_c: ["e f", "g"])).to eq "a 1 e\\ f g d"
-  end
+  commands.each do |command|
+    it ".#{command} interpolates variables into string" do
+      expect(described_class.send(command, "a :b :c d", b: 1, c: "e f")).to eq "a 1 e\\ f d"
+    end
 
-  it ".command returns string when not given keyword" do
-    s = "a d"
-    expect(described_class.command(s)).to be s
-  end
+    it ".#{command} handles shelljoin interpolation" do
+      expect(described_class.send(command, "a :b :shelljoin_c d", b: 1, shelljoin_c: ["e f", "g"])).to eq "a 1 e\\ f g d"
+    end
 
-  it ".command raises for interpolated strings with keywords" do
-    c = ":c"
-    expect { described_class.command("a :b #{c} d", b: 1, c: "e f") }.to raise_error(NetSsh::PotentialInsecurity)
-    expect { described_class.command("a #{c}") }.to raise_error(NetSsh::PotentialInsecurity)
-  end
+    it ".#{command} returns string when not given keyword" do
+      s = "a d"
+      expect(described_class.send(command, s)).to be s
+    end
 
-  it ".command raises for non-strings" do
-    o = Object.new
-    expect { described_class.command(o) }.to raise_error(TypeError)
-    expect { described_class.command(o, b: 1) }.to raise_error(TypeError)
+    it ".#{command} raises for interpolated strings with keywords" do
+      c = ":c"
+      expect { described_class.send(command, "a :b #{c} d", b: 1, c: "e f") }.to raise_error(NetSsh::PotentialInsecurity)
+      expect { described_class.send(command, "a #{c}") }.to raise_error(NetSsh::PotentialInsecurity)
+    end
+
+    it ".#{command} raises for non-strings" do
+      o = Object.new
+      expect { described_class.send(command, o) }.to raise_error(TypeError)
+      expect { described_class.send(command, o, b: 1) }.to raise_error(TypeError)
+    end
   end
 
   it ".combine combines multiple commands" do
