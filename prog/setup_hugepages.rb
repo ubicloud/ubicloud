f3eaf5f3747e9af71a92a7549080ff9c31e93fa2
Hadi Moshayedi <hadi@ubicloud.com>
2026-03-04 21:15:02 -0800

    Adjust requested hugepage count by a safety ratio
    On a production host with 256 GB of RAM we observed only ~250 GB of
    total memory, indicating that usable memory can be roughly up to
    1/43 lower than the reported physical memory.
    
    To avoid overcommitting memory when allocating hugepages and leaving
    too little regular memory available, this change applies a safety ratio
    of 42/43 to physical memory instead of assuming the entire amount is
    usable.
    
    This ratio also satisfies our requirement of being able to fit
    premium-30 VMs on AX 102 hosts.
    
    We use the physical memory with a safety ratio instead of reported total
    memory to get more predictable number of hugepages.
    
    Note that this applies only to future host setups; existing hosts must
    be adjusted manually.

--- prog/setup_hugepages.rb
+++ prog/setup_hugepages.rb
@@ -8,7 +8,10 @@ class Prog::SetupHugepages < Prog::Base
 
     # Reserve 5G of overhead for the host. SPDK will use 2 of the hugepages +
     # upto about 1G of the 5G as not all SPDK allocations are from hugepages.
-    hugepage_cnt = vm_host.total_mem_gib - 5
+    # On production servers we have observed total memory to be up to ~1/43
+    # lower than the reported physical memory. We therefore use 42/43 as a
+    # safety ratio to avoid overcommitting memory for hugepages.
+    hugepage_cnt = vm_host.total_mem_gib * 42 / 43 - 5
 
     sshable.cmd("sudo sed -i '/^GRUB_CMDLINE_LINUX=\"/ s/\"$/ hugetlb_free_vmemmap=on default_hugepagesz=':hugepage_size' hugepagesz=':hugepage_size' hugepages=':hugepage_cnt'&/' /etc/default/grub", hugepage_size:, hugepage_cnt:)
     sshable.cmd("sudo update-grub")
