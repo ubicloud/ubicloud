ff280cebbed2c43229e03caae5e0d1e5c38e743e
Jeremy Evans <jeremy@ubicloud.com>
2026-03-06 13:43:35 -0800

    Allow admins to close other admin's accounts
    If during offboarding, an existing admin does not close their own
    account, this allows another admin to close it for them.

--- spec/routes/web/admin/admin_spec.rb
+++ spec/routes/web/admin/admin_spec.rb
@@ -1079,6 +1079,25 @@ RSpec.describe CloverAdmin do
     expect(page.all("#admin-list li").map(&:text)).to eq ["admin", "foo"]
   end
 
+  it "allows closing other admin accounts" do
+    DB[:admin_account].insert(login: "foo")
+    click_link "View Admin List"
+    expect(page.title).to eq "Ubicloud Admin - Admin List"
+    expect(page.all("#admin-list li").map(&:text)).to eq ["admin", "foo"]
+    select "foo"
+    expect(Clog).to receive(:emit).with("Admin account closed", {admin_account_closed: {account_closed: "foo", closer: "admin"}}).and_call_original
+    click_button "Close Admin Account"
+    expect(page).to have_flash_notice "Admin account \"foo\" closed."
+    expect(page.all("#admin-list li").map(&:text)).to eq ["admin"]
+
+    DB[:admin_account].insert(login: "foo")
+    page.refresh
+    DB[:admin_account].where(login: "foo").delete
+    select "foo"
+    click_button "Close Admin Account"
+    expect(page).to have_flash_error "Unable to close admin account for \"foo\"."
+  end
+
   it "shows unavailable VMs" do
     project = Project.create(name: "test")
     vm = create_vm(project_id: project.id, name: "active-vm")
