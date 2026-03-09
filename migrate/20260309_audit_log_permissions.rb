616c0b5cd81bca3f7774cb4f2e1244f1dc26c3ed
Jeremy Evans <jeremy@ubicloud.com>
2026-03-09 13:13:43 -0700

    Avoid unnecessary raw SQL in migration

--- migrate/20260309_audit_log_permissions.rb
+++ migrate/20260309_audit_log_permissions.rb
@@ -2,23 +2,15 @@
 
 Sequel.migration do
   up do
-    run <<~SQL
-      INSERT INTO
-        action_type (id, name)
-      VALUES
-        ('ffffffff-ff00-835a-802d-202b6d0e8200', 'Project:auditlog'); -- ttzzzzzzzz021g0pj0avd1t100
-    SQL
+    # ttzzzzzzzz021g0pj0avd1t100
+    from(:action_type).insert(id: "ffffffff-ff00-835a-802d-202b6d0e8200", name: "Project:auditlog")
 
-    run <<~SQL
-      INSERT INTO
-        applied_action_tag (tag_id, action_id)
-      VALUES
-        ('ffffffff-ff00-834a-87ff-ff82d2028210', 'ffffffff-ff00-835a-802d-202b6d0e8200'); -- add to Project:all
-    SQL
+    # Add to Project:all
+    from(:applied_action_tag).insert(tag_id: "ffffffff-ff00-834a-87ff-ff82d2028210", action_id: "ffffffff-ff00-835a-802d-202b6d0e8200")
   end
 
   down do
-    run "DELETE FROM applied_action_tag WHERE action_id = 'ffffffff-ff00-835a-802d-202b6d0e8200';"
-    run "DELETE FROM action_type WHERE id = 'ffffffff-ff00-835a-802d-202b6d0e8200';"
+    from(:applied_action_tag).where(action_id: "ffffffff-ff00-835a-802d-202b6d0e8200").delete
+    from(:action_type).where(id: "ffffffff-ff00-835a-802d-202b6d0e8200").delete
   end
 end
