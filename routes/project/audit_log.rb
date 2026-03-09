ae9bad8196f1cdc650f7706ca76c4a308ded4161
Jeremy Evans <jeremy@ubicloud.com>
2026-03-09 15:47:12 -0700

    Allow audit log pagination and searching by date
    Pagination works by looking for the next entry before the last entry
    (entries are shown in reverse chronological order).  We create a hash
    containing all filter criteria, and use that as query parameters in
    the next page link. We limit the query to one beyond the displayed
    limit, and pop the last record off if the results are over the
    displayed limit.  To handle fractional seconds in the next entry link,
    we have the pagination key timestamp part use float format. However,
    we need to be careful here to handle only up to usec, so we convert
    the float to a rational using round(6) when handling the parameter,
    and use %6N when formatting it.
    
    It may be possible to have multiple audit log entries sharing a
    timestamp (committed in the same transaction). To handle that case,
    order the audit log also by id, and for pagination, include the starting
    id in the pagination key, and exclude records with the same timestamp
    but a lower id.
    
    This also the user to set a per-page limit, but this is only used
    for testing and is not exposed in the UI currently.
    
    For date searching, limit to 3 month increments so at most 4 of the
    partitioned tables will be searched at a time. Also, make the end date
    for the search limited to 3 months on each side of the current date,
    allowing searching at most up to 6 months in the past. We'll probably
    expand this later to allow searching of older records, potentially
    on a per-project basis.
    
    For the action/account/objects links to work correctly after date
    searching has been added, include the end date in the link.
    
    While here, change the time display from ISO8601 to always being in
    UTC and only showing the date and time (no offset).
    
    Add an audit_log_content method to the specs to DRY things up.

--- routes/project/audit_log.rb
+++ routes/project/audit_log.rb
@@ -5,10 +5,12 @@ class Clover
     authorize("Project:auditlog", @project)
 
     r.get true do
-      ds = DB[:audit_log].where(project_id: @project.id).order(Sequel.desc(:at), :ubid_type, :action).limit(100)
+      ds = DB[:audit_log].where(project_id: @project.id).order(Sequel.desc(:at), :id, :ubid_type, :action)
       skip_query = false
+      next_page_params = @next_page_params = {}
 
       if (action = typecast_params.nonempty_str("action"))
+        next_page_params["action"] = action
         type, action = action.split("/")
         ds = if action
           ds.where(ubid_type: type, action:)
@@ -18,6 +20,7 @@ class Clover
       end
 
       if (subject = typecast_params.nonempty_str("subject"))
+        next_page_params["subject"] = subject
         if (subject_id = UBID.to_uuid(subject))
           ds = ds.where(subject_id:)
         elsif (subject_id = @project.accounts_dataset.where(Sequel[{name: subject}] | {email: subject}).get(:id))
@@ -28,6 +31,7 @@ class Clover
       end
 
       if (object = typecast_params.nonempty_str("object"))
+        next_page_params["object"] = object
         if (object_id = UBID.to_uuid(object))
           ds = ds.where(Sequel.pg_array_op(:object_ids).contains(Sequel.pg_array([object_id], :uuid)))
         else
@@ -35,10 +39,62 @@ class Clover
         end
       end
 
-      items = if skip_query
-        []
+      # How many months to show for a single request
+      @month_limit = month_limit = 3
+
+      today = Date.today
+      begin
+        end_date = typecast_params.date("end")
+      rescue Roda::RodaPlugins::TypecastParams::Error
+        bad_date = true
       else
-        ds.all
+        # Limit access to the prior 6 months by default
+        # (min date 3 months ago, showing the previous 3 months)
+        min_end_date = today << month_limit
+        if end_date && end_date.clamp(min_end_date, today >> month_limit) != end_date
+          bad_date = true
+        end
+      end
+
+      if bad_date
+        skip_query = true
+      else
+        end_date ||= today
+        @end_date = next_page_params["end"] = end_date
+        start_date = end_date << month_limit
+        if start_date >= min_end_date
+          @next_end_date = start_date
+        end
+        start_date += 1
+
+        # 1746082800.0 is May 1, 2025, before audit logging was added
+        ds = if (key = typecast_params.nonempty_str("pagination_key")) &&
+            (before, start_id = key.split("/", 2)) &&
+            start_id &&
+            (start_id = UBID.to_uuid(start_id)) &&
+            (before = before.to_f) > 1746082800
+          end_time = Time.at(before.to_r.round(6))
+          ds.where(Sequel[at: start_date.to_time...end_time] | (Sequel[at: end_time] & (Sequel[:id] >= start_id)))
+        else
+          ds.where(at: start_date...(end_date + 1))
+        end
+      end
+
+      if (limit = typecast_params.pos_int("limit"))
+        next_page_params["limit"] = limit
+      end
+
+      limit ||= 100
+      limit = limit.clamp(1, 100) + 1
+
+      if skip_query
+        items = []
+      else
+        items = ds.limit(limit).all
+        if items.length == limit
+          before_id = UBID.from_uuidish(items.pop[:id]).to_s
+          @pagination_key = "#{items.last[:at].strftime("%s.%6N")}/#{before_id}"
+        end
       end
 
       if api?
@@ -60,19 +116,17 @@ class Clover
         end
 
         items.each do |log|
-          log[:at] = log[:at].iso8601
-
           subject_id = log[:subject_id]
           subject_ubid = UBID.from_uuidish(subject_id).to_s
           subject_name = ubids[subject_id]&.name || subject_ubid
-          log[:subject] = "<a class=\"text-orange-600\" href=\"?subject=#{h subject_ubid}\">#{h subject_name}</a>"
+          log[:subject] = "<a class=\"text-orange-600\" href=\"?end=#{end_date}&amp;subject=#{h subject_ubid}\">#{h subject_name}</a>"
 
           log[:objects] = log[:object_ids].filter_map do |object_id|
             object_ubid = UBID.from_uuidish(object_id).to_s
             if (obj = ubids[object_id]) && obj.respond_to?(:name) && obj.respond_to?(:path)
-              "<a class=\"text-orange-600\" href=\"?object=#{h object_ubid}\">#{h obj.name}</a> (<a class=\"text-orange-600\" href=\"#{@project.path}#{obj.path}\">View</a>)"
+              "<a class=\"text-orange-600\" href=\"?end=#{end_date}&amp;object=#{h object_ubid}\">#{h obj.name}</a> (<a class=\"text-orange-600\" href=\"#{@project.path}#{obj.path}\">View</a>)"
             else
-              "<a class=\"text-orange-600\" href=\"?object=#{h object_ubid}\">#{h object_ubid}</a>"
+              "<a class=\"text-orange-600\" href=\"?end=#{end_date}&amp;object=#{h object_ubid}\">#{h object_ubid}</a>"
             end
           end
         end
