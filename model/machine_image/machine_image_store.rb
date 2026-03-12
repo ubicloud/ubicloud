ef21cac3b386507cb14d3c48f6e119dec13c682f
Hadi Moshayedi <hadi@ubicloud.com>
2026-03-11 22:32:30 -0700

    UMI: add model definitions for machine image relations.

---
+++ model/machine_image/machine_image_store.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "../../model"
+
+class MachineImageStore < Sequel::Model
+  plugin ResourceMethods, encrypted_columns: [:access_key, :secret_key]
+end
+
+# Table: machine_image_store
+# Columns:
+#  id          | uuid | PRIMARY KEY DEFAULT gen_random_ubid_uuid(640)
+#  project_id  | uuid | NOT NULL
+#  location_id | uuid | NOT NULL
+#  provider    | text | NOT NULL
+#  region      | text | NOT NULL
+#  endpoint    | text | NOT NULL
+#  bucket      | text | NOT NULL
+#  access_key  | text | NOT NULL
+#  secret_key  | text | NOT NULL
+# Indexes:
+#  machine_image_store_pkey                       | PRIMARY KEY btree (id)
+#  machine_image_store_project_id_location_id_key | UNIQUE btree (project_id, location_id)
+# Foreign key constraints:
+#  machine_image_store_location_id_fkey | (location_id) REFERENCES location(id)
+#  machine_image_store_project_id_fkey  | (project_id) REFERENCES project(id)
+# Referenced By:
+#  machine_image_version_metal | machine_image_version_metal_store_id_fkey | (store_id) REFERENCES machine_image_store(id)
--- ubid.rb
+++ ubid.rb
@@ -108,6 +108,9 @@ class UBID
   TYPE_LOCATION_AWS_AZ = "1a"
   TYPE_AWS_SUBNET = "as"
   # TYPE_AUDIT_LOG = "a1"
+  TYPE_MACHINE_IMAGE = "m1"
+  TYPE_MACHINE_IMAGE_VERSION = "mv"
+  TYPE_MACHINE_IMAGE_STORE = "m0"
 
   # Common entropy-based type for everything else
   TYPE_ETC = "et"
