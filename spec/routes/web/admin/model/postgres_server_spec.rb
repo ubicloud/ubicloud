bb218b359c032cafca72d50cd28f1b3befed68a7
Philip Dubé <philip.dube@clickhouse.com>
2026-03-14 20:44:11 +0000

    Add admin panel extras for PostgresServer & PostgresTimeline
    Display metrics like arcival backlog, wal lag, replay timestamp
    
    For timelines show backups
    
    Also add Recycle button to PostgresServer admin page

--- spec/routes/web/admin/model/postgres_server_spec.rb
+++ spec/routes/web/admin/model/postgres_server_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe CloverAdmin, "PostgresServer" do
   end
 
   it "displays the PostgresServer instance page correctly" do
+    expect(PostgresServer).to receive(:victoria_metrics_client).and_return(nil)
+
     click_link "PostgresServer"
     expect(page.status_code).to eq 200
     expect(page.title).to eq "Ubicloud Admin - PostgresServer - Browse"
@@ -20,5 +22,26 @@ RSpec.describe CloverAdmin, "PostgresServer" do
     click_link @instance.admin_label
     expect(page.status_code).to eq 200
     expect(page.title).to eq "Ubicloud Admin - PostgresServer #{@instance.ubid}"
+  end
+
+  it "can recycle a PostgresServer" do
+    expect(PostgresServer).to receive(:victoria_metrics_client).and_return(nil).twice
+
+    visit "/model/PostgresServer/#{@instance.ubid}"
+    click_link "Recycle"
+    click_button "Recycle"
+
+    expect(page).to have_content("Recycle scheduled for PostgresServer")
+    expect(Semaphore.where(strand_id: @instance.id, name: "recycle").count).to eq(1)
+  end
+
+  it "renders charts when metrics are available" do
+    tsdb_client = instance_double(VictoriaMetrics::Client)
+    expect(PostgresServer).to receive(:victoria_metrics_client).and_return(tsdb_client)
+    expect(tsdb_client).to receive(:query_range).twice.and_return([{"labels" => {}, "values" => [[Time.now.to_i, "5"]]}])
+
+    visit "/model/PostgresServer/#{@instance.ubid}"
+    expect(page.status_code).to eq 200
+    expect(page.body).to include("<svg")
   end
 end
