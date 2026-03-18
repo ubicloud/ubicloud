cc15bf9697145eb0357de773b41cd3db5eb1d481
Jeremy Evans <jeremy@ubicloud.com>
2026-03-18 15:51:03 -0700

    Combine the web account_spec into auth_spec
    These now have the same setup, and the account spec already had
    most specs dedicated to auth-like things, so it makes more sense
    for the specs to be in the same file.
    
    Tweak some existing auth specs to remove some login audit log
    entries, because the audit log entries added during the before
    hook are now removed (this was already done in other places in
    the account and auth specs, but not this place).

--- spec/routes/web/auth_spec.rb
+++ spec/routes/web/auth_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "spec_helper"
+require "webauthn/fake_client"
 
 RSpec.describe Clover, "auth" do
   def ip_hash(into = {})
@@ -402,7 +403,7 @@ RSpec.describe Clover, "auth" do
       visit "/dashboard"
 
       expect(page).to have_current_path("/dashboard")
-      expect(audit_log_hash).to eq({"login" => ip_hash("via" => "password")})
+      expect(audit_log_hash).to eq({})
     end
 
     it "can logout" do
@@ -411,7 +412,7 @@ RSpec.describe Clover, "auth" do
       click_button "Log out"
 
       expect(page.title).to eq("Ubicloud - Login")
-      expect(audit_log_hash).to eq({"login" => ip_hash("via" => "password"), "logout" => ip_hash})
+      expect(audit_log_hash).to eq({"logout" => ip_hash})
     end
 
     [true, false].each do |logged_in|
@@ -518,7 +519,7 @@ RSpec.describe Clover, "auth" do
         expect(page.title).to eq("Ubicloud - Change Password")
         expect(page).to have_flash_notice("Your password has been changed")
       end
-      expect(audit_log_hash).to eq({"login" => ip_hash("via" => "password"), "change_password" => ip_hash})
+      expect(audit_log_hash).to eq({"change_password" => ip_hash})
     end
 
     [true, false].each do |clear_last_password_entry|
@@ -576,7 +577,7 @@ RSpec.describe Clover, "auth" do
 
         expect(Account[email: TEST_USER_EMAIL]).to be_nil
         expect(DB[:access_tag].where(hyper_tag_id: account.id).count).to eq 0
-        expect(audit_log_hash).to eq({"login" => ip_hash("via" => "password"), "close_account" => ip_hash})
+        expect(audit_log_hash).to eq({"close_account" => ip_hash})
       end
     end
 
@@ -591,7 +592,230 @@ RSpec.describe Clover, "auth" do
 
       expect(page.title).to eq("Ubicloud - Close Account")
       expect(page).to have_flash_error("'Default' project has some resources. Delete all related resources first.")
-      expect(audit_log_hash).to eq({"login" => ip_hash("via" => "password")})
+      expect(audit_log_hash).to eq({})
+    end
+
+    it "show password change page" do
+      visit "/account/change-password"
+
+      expect(page.title).to eq("Ubicloud - Change Password")
+      expect(page).to have_content "Change Password"
+      expect(audit_log_hash).to eq({})
+    end
+
+    [true, false].each do |clear_last_password_entry|
+      it "allows setting up#{", authenticating with, unlocking," if clear_last_password_entry} and removing OTP authentication when password entry is #{"not " unless clear_last_password_entry}required" do
+        visit "/clear-last-password-entry" if clear_last_password_entry
+
+        visit "/account/multifactor-manage"
+        expect(page.title).to eq("Ubicloud - Multifactor Authentication")
+
+        click_link "Enable"
+        expect(page.title).to eq("Ubicloud - Setup One-Time Password")
+        totp = ROTP::TOTP.new(find_by_id("otp-secret").text)
+        fill_in "Authentication Code", with: totp.now
+        fill_in "Password", with: TEST_USER_PASSWORD if clear_last_password_entry
+        click_button "Enable One-Time Password Authentication"
+        expect(page).to have_flash_notice "One-time password authentication is now setup, please make note of your recovery codes"
+        expect(page.title).to eq("Ubicloud - Recovery Codes")
+
+        if clear_last_password_entry
+          DB[:account_otp_keys].update(last_use: Sequel.date_sub(Sequel::CURRENT_TIMESTAMP, seconds: 4600))
+          click_button "Log out"
+          visit "/login"
+          fill_in "Email Address", with: TEST_USER_EMAIL
+          click_button "Sign in"
+          fill_in "Password", with: TEST_USER_PASSWORD
+          click_button "Sign in"
+          expect(page.title).to eq("Ubicloud - 2FA - One-Time Password")
+          fill_in "Authentication Code", with: totp.now
+          click_button "Authenticate Using One-Time Password"
+          expect(page).to have_flash_notice("You have been logged in")
+
+          DB[:account_otp_keys].update(last_use: Sequel.date_sub(Sequel::CURRENT_TIMESTAMP, seconds: 4600))
+          click_button "Log out"
+          visit "/login"
+          fill_in "Email Address", with: TEST_USER_EMAIL
+          click_button "Sign in"
+          fill_in "Password", with: TEST_USER_PASSWORD
+          click_button "Sign in"
+          6.times do
+            expect(page.title).to eq("Ubicloud - 2FA - One-Time Password")
+            fill_in "Authentication Code", with: totp.now + "1"
+            click_button "Authenticate Using One-Time Password"
+          end
+          expect(page).to have_flash_error("TOTP authentication code use locked out due to numerous failures")
+          expect(Mail::TestMailer.deliveries.length).to eq 1
+          expect(Mail::TestMailer.deliveries.first.subject).to eq "Ubicloud Account One-Time Password Authentication Locked Out"
+
+          2.times do
+            expect(page.title).to eq("Ubicloud - One-Time Password Unlock")
+            fill_in "Authentication Code", with: totp.now
+            click_button "Authenticate Using One-Time Password to Unlock"
+            expect(page).to have_flash_notice("One-Time Password successful authentication, more successful authentication needed to unlock")
+            expect(page.title).to eq("Ubicloud - One-Time Password Unlock Not Available")
+            expect(page.response_headers["refresh"]).to match(/\A1[012]\d\z/)
+            expect(page).to have_content(/Deadline for next authentication: \d+ seconds/)
+            expect(page).to have_content(/Page will automatically refresh when authentication is possible \(in \d+ seconds\)\./)
+            DB[:account_otp_unlocks].update(next_auth_attempt_after: Sequel.date_sub(Sequel::CURRENT_TIMESTAMP, seconds: 200))
+            visit page.current_path
+          end
+
+          expect(page.title).to eq("Ubicloud - One-Time Password Unlock")
+          fill_in "Authentication Code", with: totp.now
+          click_button "Authenticate Using One-Time Password to Unlock"
+          expect(page).to have_flash_notice("One-Time Password authentication unlocked")
+          expect(Mail::TestMailer.deliveries.length).to eq 2
+          expect(Mail::TestMailer.deliveries.last.subject).to eq "Ubicloud Account One-Time Password Authentication Unlocked"
+
+          DB[:account_otp_keys].update(last_use: Sequel.date_sub(Sequel::CURRENT_TIMESTAMP, seconds: 4600))
+          expect(page.title).to eq("Ubicloud - 2FA - One-Time Password")
+          fill_in "Authentication Code", with: totp.now
+          click_button "Authenticate Using One-Time Password"
+          expect(page).to have_flash_notice("You have been logged in")
+
+          visit "/clear-last-password-entry"
+        end
+
+        visit "/account/multifactor-manage"
+        click_link "Disable"
+        expect(page.title).to eq("Ubicloud - Disable One-Time Password")
+        fill_in "Password", with: TEST_USER_PASSWORD if clear_last_password_entry
+        click_button "Disable One-Time Password Authentication"
+        expect(page).to have_flash_notice "One-time password authentication has been disabled"
+        if clear_last_password_entry
+          expect(audit_log_hash).to eq({
+            "login" => ip_hash("via" => "password"),
+            "logout" => ip_hash,
+            "otp_authentication_failure" => ip_hash,
+            "otp_disable" => ip_hash,
+            "otp_setup" => ip_hash,
+            "otp_unlock_auth_success" => ip_hash,
+            "two_factor_authentication" => ip_hash("via" => "totp")
+          })
+        else
+          expect(audit_log_hash).to eq({"otp_setup" => ip_hash, "otp_disable" => ip_hash})
+        end
+      end
+
+      it "allows setting up#{", authenticating," if clear_last_password_entry} and removing Webauthn authentication when password entry is #{"not " unless clear_last_password_entry}required" do
+        webauthn_client = WebAuthn::FakeClient.new("http://localhost:9292")
+        2.times do |i|
+          visit "/clear-last-password-entry" if clear_last_password_entry
+          visit "/account/multifactor-manage"
+          expect(page.title).to eq("Ubicloud - Multifactor Authentication")
+          click_link "Add"
+          expect(page.title).to eq("Ubicloud - Setup Security Key")
+          challenge = JSON.parse(page.find_by_id("webauthn-setup-form")["data-credential-options"])["challenge"]
+          fill_in "Key Name", with: "My Key #{i}"
+          fill_in "Password", with: TEST_USER_PASSWORD if clear_last_password_entry
+          fill_in "webauthn-setup", with: webauthn_client.create(challenge:).to_json, visible: false
+          click_button "Setup Security Key"
+          expect(page).to have_flash_notice "Security key is now setup, please make note of your recovery codes"
+          expect(page.title).to eq("Ubicloud - Recovery Codes")
+        end
+
+        if clear_last_password_entry
+          click_button "Log out"
+          visit "/login"
+          fill_in "Email Address", with: TEST_USER_EMAIL
+          click_button "Sign in"
+          fill_in "Password", with: TEST_USER_PASSWORD
+          click_button "Sign in"
+          expect(page.title).to eq("Ubicloud - 2FA - Security Keys")
+          challenge = JSON.parse(page.find_by_id("webauthn-auth-form")["data-credential-options"])["challenge"]
+          fill_in "webauthn_auth", with: webauthn_client.get(challenge:).to_json, visible: false
+          click_button "Authenticate Using Security Keys"
+          expect(page).to have_flash_notice "You have been logged in"
+        end
+
+        DB.transaction(rollback: :always) do
+          visit "/clear-last-password-entry" if clear_last_password_entry
+          visit "/account/multifactor-manage"
+          click_link "Remove"
+          expect(page.title).to eq("Ubicloud - Remove Security Key")
+          DB[:account_webauthn_keys].where(name: "My Key 1").delete
+          fill_in "Password", with: TEST_USER_PASSWORD if clear_last_password_entry
+          choose "My Key 1"
+          click_button "Remove Security Key"
+        end
+
+        expect(page).to have_flash_error "Error removing security key"
+        expect(find_by_id("webauthn_remove-error").text).to eq "Invalid security key to remove"
+
+        visit "/clear-last-password-entry" if clear_last_password_entry
+        visit "/account/multifactor-manage"
+        click_link "Remove"
+        fill_in "Password", with: TEST_USER_PASSWORD if clear_last_password_entry
+        choose "My Key 1"
+        click_button "Remove Security Key"
+        expect(page).to have_flash_notice "Security key has been removed"
+        if clear_last_password_entry
+          expect(audit_log_hash).to eq({
+            "login" => ip_hash("via" => "password"),
+            "logout" => ip_hash,
+            "webauthn_setup" => ip_hash("key_name" => "My Key 1"),
+            "webauthn_remove" => ip_hash("key_name" => "My Key 1"),
+            "two_factor_authentication" => ip_hash("via" => "webauthn", "key_name" => "My Key 0")
+          })
+        else
+          expect(audit_log_hash).to eq({"webauthn_setup" => ip_hash("key_name" => "My Key 1"), "webauthn_remove" => ip_hash("key_name" => "My Key 1")})
+        end
+      end
+    end
+
+    it "allows viewing and regenerating recovery codes" do
+      visit "/account/multifactor-manage"
+      click_link "Enable"
+      totp = ROTP::TOTP.new(find_by_id("otp-secret").text)
+      fill_in "Authentication Code", with: totp.now
+      click_button "Enable One-Time Password Authentication"
+
+      path = page.current_path
+      visit "/clear-last-password-entry"
+      visit path
+      fill_in "Password", with: TEST_USER_PASSWORD
+      click_button "View Authentication Recovery Codes"
+
+      recovery_codes = DB[:account_recovery_codes].select_map(:code)
+      expect(page.all("#recovery-codes-text div").map(&:text).sort).to eq recovery_codes.sort
+
+      DB[:account_recovery_codes].delete
+      visit path
+      expect(page.all("#recovery-codes-text div").to_a).to be_empty
+      click_button "Add Authentication Recovery Codes"
+
+      recovery_codes = DB[:account_recovery_codes].select_map(:code)
+      expect(page.all("#recovery-codes-text div").map(&:text).sort).to eq recovery_codes.sort
+      expect(audit_log_hash).to eq({"otp_setup" => ip_hash, "add_recovery_codes" => ip_hash})
+    end
+
+    it "allows removing all multifactor authentication methods" do
+      visit "/account/multifactor-manage"
+      click_link "Enable"
+      totp = ROTP::TOTP.new(find_by_id("otp-secret").text)
+      fill_in "Authentication Code", with: totp.now
+      click_button "Enable One-Time Password Authentication"
+      expect(page).to have_flash_notice "One-time password authentication is now setup, please make note of your recovery codes"
+
+      visit "/account/multifactor-manage"
+      click_link "Add"
+      webauthn_client = WebAuthn::FakeClient.new("http://localhost:9292")
+      challenge = JSON.parse(page.find_by_id("webauthn-setup-form")["data-credential-options"])["challenge"]
+      fill_in "Key Name", with: "My Key"
+      fill_in "webauthn-setup", with: webauthn_client.create(challenge:).to_json, visible: false
+      click_button "Setup Security Key"
+
+      visit "/account/multifactor-manage"
+      click_link "Remove All Multifactor Authentication Methods"
+      expect(page.title).to eq("Ubicloud - Remove All Multifactor Authentication Methods")
+      click_button "Remove All Multifactor Authentication Methods"
+      expect(page).to have_flash_notice "All multifactor authentication methods have been disabled"
+      expect(audit_log_hash).to eq({
+        "otp_setup" => ip_hash,
+        "webauthn_setup" => ip_hash("key_name" => "My Key"),
+        "two_factor_disable" => ip_hash
+      })
     end
   end
 
@@ -1271,5 +1495,12 @@ RSpec.describe Clover, "auth" do
         expect(audit_log_hash).to eq({})
       end
     end
+  end
+
+  it "can not access without login" do
+    visit "/account"
+
+    expect(page.title).to eq("Ubicloud - Login")
+    expect(audit_log_hash).to eq({})
   end
 end
