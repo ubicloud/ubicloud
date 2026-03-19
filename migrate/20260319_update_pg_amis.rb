1c1378951efd703801e92d7eec928a9815156728
furkan sahin <sahinffurkan@gmail.com>
2026-03-19 16:37:30 +0100

    Fix AMI IDs and SHA256 from latest build run
    The previous workflow run created this PR with stale AMI IDs and SHA256.
    Update to the correct values from run #23296659210.

--- migrate/20260319_update_pg_amis.rb
+++ migrate/20260319_update_pg_amis.rb
@@ -2,16 +2,16 @@
 
 Sequel.migration do
   ami_ids = [
-    ["us-west-2", "x64", "ami-0125323df7c96fec7", "ami-0ec7057ec5fad7e47"],
-    ["us-east-1", "x64", "ami-017704f7f2c78e223", "ami-046ea18f748f0b14a"],
-    ["us-east-2", "x64", "ami-0608478964ffd9a4a", "ami-01d90da2797cd161c"],
-    ["eu-west-1", "x64", "ami-01dad7228c73d1636", "ami-06b8099588ba2fbbf"],
-    ["ap-southeast-2", "x64", "ami-06405788df7045760", "ami-02863dc84159f6ff5"],
-    ["us-west-2", "arm64", "ami-038ee46bbf59f9f35", "ami-0d34af69c6e816e93"],
-    ["us-east-1", "arm64", "ami-0829b7d3b07ee6a1a", "ami-075dd67ffd7ea12a1"],
-    ["us-east-2", "arm64", "ami-0309428b3a5309e7f", "ami-057b0bf5bf0af62d4"],
-    ["eu-west-1", "arm64", "ami-0c60dd4b32d81ab78", "ami-0b62a8210720b7e39"],
-    ["ap-southeast-2", "arm64", "ami-093edf5ec5ac9e118", "ami-079943061900fc0e1"]
+    ["us-west-2", "x64", "ami-01f7840049eb2e5af", "ami-0ec7057ec5fad7e47"],
+    ["us-east-1", "x64", "ami-0c6987f020fa67987", "ami-046ea18f748f0b14a"],
+    ["us-east-2", "x64", "ami-0f421c5f7e01cd131", "ami-01d90da2797cd161c"],
+    ["eu-west-1", "x64", "ami-078b84f286dce69cf", "ami-06b8099588ba2fbbf"],
+    ["ap-southeast-2", "x64", "ami-013291c0e3a5e330b", "ami-02863dc84159f6ff5"],
+    ["us-west-2", "arm64", "ami-07a1d747d8cdedda8", "ami-0d34af69c6e816e93"],
+    ["us-east-1", "arm64", "ami-0b6916c061d067436", "ami-075dd67ffd7ea12a1"],
+    ["us-east-2", "arm64", "ami-0aad63561013ba18c", "ami-057b0bf5bf0af62d4"],
+    ["eu-west-1", "arm64", "ami-0706f2b0f56711ad2", "ami-0b62a8210720b7e39"],
+    ["ap-southeast-2", "arm64", "ami-0d06d9f1081e0f62f", "ami-079943061900fc0e1"]
   ]
 
   up do
