f5795913a1afe483f56d66348eba4fe2b9b3b4fb
Furkan Sahin <furkan@ubicloud.com>
2026-04-01 13:05:52 +0200

    Dispatch connect/disconnect subnet through providers
    Rename connect_subnet and disconnect_subnet to provider-prefixed
    methods so ProviderDispatcher routes them per location. AWS raises
    (connected subnets unsupported), metal keeps existing IPsec tunnel
    logic. Prepares for GCP which will also raise.

--- model/aws/private_subnet.rb
+++ model/aws/private_subnet.rb
@@ -2,5 +2,14 @@
 
 class PrivateSubnet < Sequel::Model
   module Aws
+    private
+
+    def aws_connect_subnet(subnet)
+      raise "Connected subnets are not supported for AWS"
+    end
+
+    def aws_disconnect_subnet(subnet)
+      raise "Connected subnets are not supported for AWS"
+    end
   end
 end
