6904af565da06edcf586f20ce4451f522453da34
Burak Yucesoy <2082070+byucesoy@users.noreply.github.com>
2026-04-02 10:46:37 +0200

    Return empty extra data for deleted PostgresServer resources
    While creating a page, we populate the extra data for PostgresServers.
    However, if the resource is deleted, we cannot access some of the data
    we need. In this case, it is OK to not populate the extra data because
    deletion of the resource means we would delete the server and resolve
    the page soon anyway.

--- model/strand.rb
+++ model/strand.rb
@@ -204,9 +204,8 @@ SQL
             {vm_host: vm_host.ubid, data_center: vm_host.data_center, location: vm_host.location.display_name, arch: vm_host.arch}
           when GithubRunner
             {label: sbj.label, installation: sbj.installation.ubid, vm: sbj.vm&.ubid, vm_host: sbj.vm&.vm_host&.ubid, data_center: sbj.vm&.vm_host&.data_center}
-          when PostgresServer
-            {resource_name: sbj.resource.name, role: sbj.primary? ? "primary" : "standby",
-             location: sbj.resource.location.display_name, needs_recycling: sbj.needs_recycling?}
+          when ->(s) { s.is_a?(PostgresServer) && s.resource }
+            {resource_name: sbj.resource.name, role: sbj.primary? ? "primary" : "standby", location: sbj.resource.location.display_name, needs_recycling: sbj.needs_recycling?}
           else
             {}
           end
