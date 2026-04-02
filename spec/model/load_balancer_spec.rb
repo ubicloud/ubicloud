cb240856973605c696904257a15e31d42e104774
Eren Basak <aamederen@gmail.com>
2026-04-02 11:17:53 +0300

    Make `LoadBalancer#vms_to_dns` distinct
    `LoadBalancer`s refresh dns records based on `vms_to_dns` call result
    and `vms_to_dns` return duplicate entries when there are multiple port
    mappings for a VM, which is common for Kubernetes deployments.
    
    We recently had a case where we had 130000 DNS records, mixed with
    tombstoned and non tombstoned ones, piled up and needed to be cleaned manually.
    It's not obvious if this code path was the reason but reducing the
    duplication is an improvement anyway.

--- spec/model/load_balancer_spec.rb
+++ spec/model/load_balancer_spec.rb
@@ -107,6 +107,13 @@ RSpec.describe LoadBalancer do
       }.to change { LoadBalancerVm.where(load_balancer_id: lb.id).count }.from(0).to(1)
         .and change { Semaphore.where(strand_id: lb.id, name: "rewrite_dns_records").count }.from(0).to(1)
     end
+
+    it "returns distinct VMs for vms_to_dns when a VM is attached through multiple ports" do
+      lb.add_vm(vm1)
+      lb.add_port(443, 8443)
+
+      expect(lb.reload.vms_to_dns.map(&:id)).to eq([vm1.id])
+    end
   end
 
   describe "evacuate_vm" do
