8b7cada828dc48e26ca1c400f9b4ab8a276f0a1a
Jeremy Evans <jeremy@ubicloud.com>
2026-04-03 12:50:42 -0700

    Ensure omniauth_provider is always a string
    Historically, in production it was a symbol, but when testing
    it was a string. That's because OmniAuth's `add_mock` method
    accepted a symbol as the provider, but converted it into a string:
    https://github.com/omniauth/omniauth/blob/2ad2d0dd9e15f8fd89814e00d3f50c61886d81cd/lib/omniauth.rb#L122
    
    In 97e0daea58e2f1c293db3abbdac83cb1032a5559, when we switched from
    per-OidcProvider omniauth strategies (registering the provider name
    as a ubid symbol), to a single :oidc strategy, we had the name of the
    strategy change dynamically, but return a string instead of a symbol.
    
    This caused a bug when deploying the change, which expected the
    provider name to be a string (since that is what the specs used),
    but failed when it was a symbol (:google). That bug was fixed by
    ec2f6eadec2167e04534f985723e17a5f0035cfa.
    
    It's best to standardize on a single type, and since omniauth's mocking
    forces string type, this standardizes on using a string.
    
    To implement the change, we use a :name option with a string argument
    when registering the :github and :google omniauth providers.
    
    This switches all use of `omniauth_provider.to_s` to
    `omniauth_provider`, since the value should always be a string.

--- helpers/web.rb
+++ helpers/web.rb
@@ -101,7 +101,6 @@ class Clover < Roda
   end
 
   def omniauth_provider_name(provider)
-    provider = provider.to_s
     omniauth_providers.each { |sym, name| return name if sym.name == provider }
     OidcProvider.name_for_ubid(provider) || provider
   end
