0069ade5d2ea91344adaae3c43d8cacc55cca128
Jeremy Evans <jeremy@ubicloud.com>
2026-04-14 14:45:32 -0700

    Move .postgres_test_location_options to Prog::Test::PostgresBase
    This was previously in Prog::Test::Base, but is now only used by
    subclasses of Prog::Test::PostgresBase.

--- prog/test/base.rb
+++ prog/test/base.rb
@@ -5,17 +5,4 @@ class Prog::Test::Base < Prog::Base
     strand.update(exitval: {msg:})
     hop_failed
   end
-
-  def self.postgres_test_location_options(provider)
-    if provider == "aws"
-      location = Location[provider: "aws", project_id: Config.local_e2e_postgres_test_project_id, name: "us-west-2"]
-      location.location_credential_aws ||
-        LocationCredentialAws.create_with_id(location, access_key: Config.e2e_aws_access_key, secret_key: Config.e2e_aws_secret_key)
-      family = "m8gd"
-      vcpus = 2
-      [location.id, Option.aws_instance_type_name(family, vcpus), Option::AWS_STORAGE_SIZE_OPTIONS[family][vcpus].first.to_i]
-    else
-      [Location::HETZNER_FSN1_ID, "standard-2", 128]
-    end
-  end
 end
