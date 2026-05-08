01cabd95785870c5829ba9168f98c12b17188bb0
Jeremy Evans <jeremy@ubicloud.com>
2026-05-08 12:31:27 -0700

    Consider protocol when sorting firewall rules

--- spec/model/firewall_rule_spec.rb
+++ spec/model/firewall_rule_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe FirewallRule do
     expect(fw_rule.ip6?).to be false
   end
 
-  it "<=> sorts by family, ip address (numerically), netmask, starting port, and ending port" do
+  it "<=> sorts by family, ip address (numerically), netmask, protocol, starting port, and ending port" do
     fws = []
     fws << (fw1 = described_class.create(cidr: "1.2.3.4/31", port_range: (22...32), firewall_id: fw.id))
     fws << (fw2 = described_class.create(cidr: "1.2.3.4/31", port_range: (22...31), firewall_id: fw.id))
@@ -27,5 +27,7 @@ RSpec.describe FirewallRule do
     expect(fws.sort).to eq [fw4, fw3, fw2, fw1, fw5]
     fws << (fw6 = described_class.create(cidr: "::/0", port_range: (21...33), firewall_id: fw.id))
     expect(fws.sort).to eq [fw4, fw3, fw2, fw1, fw5, fw6]
+    fws << (fw7 = described_class.create(cidr: "1.2.3.4/31", port_range: (22...31), protocol: "udp", firewall_id: fw.id))
+    expect(fws.sort).to eq [fw4, fw3, fw2, fw1, fw7, fw5, fw6]
   end
 end
