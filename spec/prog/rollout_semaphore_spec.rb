352af97f80f96981ef19869853938e8887e3fde0
Jeremy Evans <jeremy@ubicloud.com>
2026-06-17 11:17:50 -0700

    Change Prog::RolloutSemaphore to wait for semaphore decrement
    Previously, when Prog::RolloutSemaphore was run in increment mode (the
    default), it wouldn't wait until the semaphore is decremented. This
    can sort of defeat the purpose of a slow rollout, since you can end up
    with semaphore increments on potentially many resources before the
    results of the semaphore increment have taken effect.
    
    This changes the semaphore increment rollout to wait for the decrement
    of the semaphore from the current strand before moving to the next
    strand. For the previous behavior, you can set wait: false in assemble.
    
    Additionally, there are cases where a semaphore decrement by itself does
    not indicate successful completion of the task started by the semaphore
    increment. To handle that, the wait argument can be set to a string.
    This will make it so the rollout does not proceed to the next strand
    until the current strand no longer exists or is at the wait label.
    Typically, the wait label would be "wait", as it needs to be a
    steady-state label, because the checks are done via polling, they aren't
    edge-triggered.
    
    While waiting for the current strand, the code uses gap / 10, clamped to
    a minimum of 5 seconds and a maximum of 5 minutes, to ensure we aren't
    hammering the database or waiting too long between checks.

--- spec/prog/rollout_semaphore_spec.rb
+++ spec/prog/rollout_semaphore_spec.rb
@@ -22,6 +22,25 @@ RSpec.describe Prog::RolloutSemaphore do
       expect(frame["remaining"]).to eq(page_ids)
       expect(frame["next_increment_time"]).to be_within(5).of(Time.now.to_i)
       expect(frame["increment"]).to be true
+      expect(frame.fetch("wait_label")).to be true
+      expect(frame.fetch("current")).to be_nil
+    end
+
+    it "supports gap, initial_gap, initial_range, increment, and wait_label arguments" do
+      st = described_class.assemble(semaphore: "resolve", ids: page_ids, gap: 10, initial_gap: 15, initial_range: 1..5, increment: false, wait: "wait")
+      expect(st.label).to eq("start")
+      expect(st.prog).to eq("RolloutSemaphore")
+
+      frame = st.stack.first
+      expect(frame["semaphore"]).to eq("resolve")
+      expect(frame["gap"]).to eq(10)
+      expect(frame["initial_gap"]).to eq(15)
+      expect(frame["initial_num"]).to eq(1)
+      expect(frame["remaining"]).to eq(page_ids)
+      expect(frame["next_increment_time"]).to be_within(5).of(Time.now.to_i)
+      expect(frame["increment"]).to be false
+      expect(frame["wait_label"]).to eq "wait"
+      expect(frame.fetch("current")).to be_nil
     end
 
     it "raises for invalid semaphore" do
@@ -35,10 +54,23 @@ RSpec.describe Prog::RolloutSemaphore do
       expect { nx.start }.to nap(60 * 60)
     end
 
-    it "increments semaphore on next object and naps" do
+    it "increments semaphore on next object and naps if not waiting" do
+      refresh_frame(nx, new_values: {"wait_label" => false})
       expect { nx.start }.to nap(295...305)
         .and change { pages.first.reload.resolve_set? }.from(false).to(true)
       expect(st.stack[0]["next_increment_time"]).to be_within(5).of(Time.now.to_i + 300)
+      expect(st.stack[0]["completed"]).to eq [page_ids[0]]
+      expect(st.stack[0].fetch("current")).to be_nil
+      expect(st.stack[0]["remaining"]).to eq page_ids[1..]
+    end
+
+    it "increments semaphore on next object and hops to wait_current" do
+      expect { nx.start }.to hop("wait_current")
+        .and change { pages.first.reload.resolve_set? }.from(false).to(true)
+      expect(st.stack[0]["next_increment_time"]).to be_within(5).of(Time.now.to_i + 300)
+      expect(st.stack[0]["completed"]).to eq []
+      expect(st.stack[0]["current"]).to eq page_ids[0]
+      expect(st.stack[0]["remaining"]).to eq page_ids[1..]
     end
 
     it "decrements semaphore on next object and naps if increment is not true" do
@@ -47,6 +79,9 @@ RSpec.describe Prog::RolloutSemaphore do
       expect { nx.start }.to nap(295...305)
         .and change { pages.first.reload.resolve_set? }.from(true).to(false)
       expect(st.stack[0]["next_increment_time"]).to be_within(5).of(Time.now.to_i + 300)
+      expect(st.stack[0]["completed"]).to eq [page_ids[0]]
+      expect(st.stack[0].fetch("current")).to be_nil
+      expect(st.stack[0]["remaining"]).to eq page_ids[1..]
     end
 
     it "hops to destroy if there are no objects remaining" do
@@ -56,14 +91,46 @@ RSpec.describe Prog::RolloutSemaphore do
 
     it "naps using regular gap after passing the initial number of records" do
       refresh_frame(nx, new_values: {"initial_num" => 0})
-      expect { nx.start }.to nap(55...65)
+      expect { nx.start }.to hop("wait_current")
         .and change { pages.first.reload.resolve_set? }.from(false).to(true)
       expect(st.stack[0]["next_increment_time"]).to be_within(5).of(Time.now.to_i + 60)
+      expect(st.stack[0]["completed"]).to eq []
+      expect(st.stack[0]["current"]).to eq page_ids[0]
+      expect(st.stack[0]["remaining"]).to eq page_ids[1..]
     end
 
     it "naps if not yet at the next increment time" do
       refresh_frame(nx, new_values: {"next_increment_time" => Time.now.to_i + 10})
       expect { nx.start }.to nap(5...15)
+    end
+  end
+
+  describe "#wait_current" do
+    it "naps if semaphore still set for current strand" do
+      Semaphore.incr(page_ids[0], "resolve")
+      refresh_frame(nx, new_values: {"current" => page_ids[0]})
+      expect { nx.wait_current }.to nap(6)
+    end
+
+    it "naps if current strand not at wait_label" do
+      refresh_frame(nx, new_values: {"current" => page_ids[0], "wait_label" => "wait"})
+      expect { nx.wait_current }.to nap(6)
+    end
+
+    it "hops to start if semaphore no longer set and wait label is not a string" do
+      refresh_frame(nx, new_values: {"current" => page_ids[0]})
+      expect { nx.wait_current }.to hop("start")
+    end
+
+    it "hops to start if current strand no longer exists" do
+      Strand.where(id: page_ids[0]).destroy
+      refresh_frame(nx, new_values: {"current" => page_ids[0]})
+      expect { nx.wait_current }.to hop("start")
+    end
+
+    it "hops to start if current strand is at wait label" do
+      refresh_frame(nx, new_values: {"current" => page_ids[0], "wait_label" => "start"})
+      expect { nx.wait_current }.to hop("start")
     end
   end
 
