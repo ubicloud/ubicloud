0504c7bb6329604ab16a279e001c6ef3610e60e8
Amog Iska <amogiska@gmail.com>
2026-07-23 16:04:11 -0500

    Add postgres_disk_usage_monitor table

--- migrate/postgres_monitor_db.sql
+++ migrate/postgres_monitor_db.sql
@@ -5,3 +5,12 @@ CREATE UNLOGGED TABLE public.postgres_lsn_monitor (
 
 ALTER TABLE ONLY public.postgres_lsn_monitor
     ADD CONSTRAINT postgres_lsn_monitor_pkey PRIMARY KEY (postgres_server_id);
+
+CREATE UNLOGGED TABLE public.postgres_disk_usage_monitor (
+    postgres_server_id uuid NOT NULL,
+    data_disk_usage_percent smallint,
+    observed_at timestamp with time zone
+);
+
+ALTER TABLE ONLY public.postgres_disk_usage_monitor
+    ADD CONSTRAINT postgres_disk_usage_monitor_pkey PRIMARY KEY (postgres_server_id);
