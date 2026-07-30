9e8795d7f079257a217ff0468d5589e7ad198257
mohi-kalantari <mohi.kalantari1@gmail.com>
2026-07-30 12:13:01 +0200

    Page when a rekey pass stalls
    Register a 30-minute deadline when the leader consumes refresh_keys,
    auto-resolved on returning to wait.

--- spec/prog/vnet/metal/subnet_nexus_spec.rb
+++ spec/prog/vnet/metal/subnet_nexus_spec.rb
@@ -102,10 +102,12 @@ RSpec.describe Prog::Vnet::Metal::SubnetNexus do
       expect(leader_ps.refresh_keys_set?).to be true
     end
 
-    it "consumes refresh_keys and hops to refresh_keys as the leader" do
+    it "consumes refresh_keys, registers a deadline and hops to refresh_keys as the leader" do
       nx.incr_refresh_keys
       expect { nx.wait }.to hop("refresh_keys")
       expect(ps.refresh_keys_set?).to be false
+      expect(nx.strand.stack.first["deadline_target"]).to eq "wait"
+      expect(Time.parse(nx.strand.stack.first["deadline_at"])).to be_within(5).of(Time.now + 30 * 60)
     end
 
     it "triggers update_firewall_rules if when_update_firewall_rules_set?" do
