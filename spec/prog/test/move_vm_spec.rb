300c9f36a473d307dbdba85ac780e655a9a63402
Jeremy Evans <jeremy@ubicloud.com>
2026-07-31 16:48:04 -0700

    Fix issues in Prog::Test::MoveVm
    Reduce the number of labels by combining labels that have no reason to
    be separate. Rename labels to be similar to existing labels.
    
    Support the destroy semaphore, rename destroy_vms label to destroy.
    
    Avoid DB.transaction in label, since labels are already run inside a
    transaction.
    
    Make the Prog::Vm::Metal::MoveVm strand a child of the
    Prog::Test::MoveVm, to allow for simpler code using reap. This requires
    adding support for a parent_id argument to Prog::Vm::Metal::MoveVm.
    
    Reduce queries in destroy by using a query that includes both the old
    and new VMs, instead of separate queries for each.
    
    Cleanup the specs for these changes, and also using
    create_archive_ready_vm.

--- spec/prog/test/move_vm_spec.rb
+++ spec/prog/test/move_vm_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Prog::Test::MoveVm do
     it "creates a project and VM in the vm_host's location/arch and stages the strand" do
       st = described_class.assemble(vm_host)
       expect(st.prog).to eq("Test::MoveVm")
-      expect(st.label).to eq("wait_vm_running")
+      expect(st.label).to eq("start")
 
       frame = st.stack.first
       expect(frame).to include("vm_host_id" => vm_host.id, "markers" => [])
@@ -28,114 +28,72 @@ RSpec.describe Prog::Test::MoveVm do
     end
   end
 
-  describe "#wait_vm_running" do
-    let(:vm) { create_vm(display_state: "creating") }
-
-    before { refresh_frame(prog, new_values: {"vm_id" => vm.id}) }
-
-    it "naps while the vm is not running" do
-      expect { prog.wait_vm_running }.to nap(10)
+  describe "#start" do
+    it "naps until the vm is running" do
+      vm = create_vm(display_state: "creating")
+      refresh_frame(prog, new_values: {"vm_id" => vm.id})
+      expect { prog.start }.to nap(10)
     end
 
-    it "hops to write_markers once the vm is running" do
-      vm.update(display_state: "running")
-      expect { prog.wait_vm_running }.to hop("write_markers")
-    end
-  end
-
-  describe "#write_markers" do
-    it "writes marker files, records their checksums, and hops to prepare_to_move" do
+    it "writes marker files, records their checksums, prepares vm to move, and hops to move vm" do
       vm = create_vm(display_state: "running")
+      Strand.create_with_id(vm, prog: "Vm::Metal::Nexus", label: "wait")
       Sshable.create_with_id(vm.id, unix_user: "ubi", host: "1.2.3.4")
       refresh_frame(prog, new_values: {"vm_id" => vm.id})
 
-      expect(prog.sshable).to receive(:_cmd).with("sudo mkdir -p /opt/markers")
+      expect(prog.vm_sshable).to receive(:_cmd).with("sudo mkdir -p /opt/markers")
       described_class::MARKER_COUNT.times do |i|
-        expect(prog.sshable).to receive(:_cmd)
+        expect(prog.vm_sshable).to receive(:_cmd)
           .with("head -c 1M /dev/urandom | sudo tee /opt/markers/marker-#{i} | sha256sum")
           .and_return("sha-#{i}  x\n")
       end
 
-      expect { prog.write_markers }.to hop("prepare_to_move")
+      expect { prog.start }.to hop("move_vm")
+        .and change { vm.reload.prepare_to_move_set? }.from(false).to(true)
+        .and change { vm.reload.stop_set? }.from(false).to(true)
 
       expect(prog.markers).to eq(Array.new(described_class::MARKER_COUNT) { |i| ["/opt/markers/marker-#{i}", "sha-#{i}"] })
     end
   end
 
-  describe "#prepare_to_move" do
-    it "sets the prepare_to_move and stop semaphores and hops to wait_stopped_by_admin" do
+  describe "#move_vm" do
+    it "naps while the vm's strand hasn't reached stopped_by_admin" do
       vm = create_vm
       Strand.create_with_id(vm, prog: "Vm::Metal::Nexus", label: "wait")
       refresh_frame(prog, new_values: {"vm_id" => vm.id})
-
-      expect { prog.prepare_to_move }.to hop("wait_stopped_by_admin")
-
-      expect(vm.reload.prepare_to_move_set?).to be true
-      expect(vm.reload.stop_set?).to be true
-    end
-  end
-
-  describe "#wait_stopped_by_admin" do
-    let(:vm) {
-      vm = create_vm
-      Strand.create_with_id(vm, prog: "Vm::Metal::Nexus", label: "wait")
-      vm
-    }
-
-    before { refresh_frame(prog, new_values: {"vm_id" => vm.id}) }
-
-    it "naps while the vm's strand hasn't reached stopped_by_admin" do
-      expect { prog.wait_stopped_by_admin }.to nap(10)
+      expect { prog.move_vm }.to nap(10)
     end
 
-    it "hops to move_vm once the vm's strand reaches stopped_by_admin" do
-      vm.strand.update(label: "stopped_by_admin")
-      expect { prog.wait_stopped_by_admin }.to hop("move_vm")
-    end
-  end
-
-  describe "#move_vm" do
     it "assembles Prog::Vm::Metal::MoveVm and stores the new vm id and move strand id" do
-      StorageDevice.create(name: "target-vda", total_storage_gib: 100, available_storage_gib: 100, vm_host_id: vm_host.id)
-
-      project = Project.create(name: "move-vm-test-project")
-      source_host = create_vm_host(location_id: Location::HETZNER_FSN1_ID)
-      ready_vm = create_vm(project_id: project.id, location_id: Location::HETZNER_FSN1_ID, vm_host_id: source_host.id, name: "old-vm")
+      vm = create_archive_ready_vm(name: "test-name", public_key: "a a")
+      vm.incr_prepare_to_move
+      vm.strand.update(label: "stopped_by_admin")
+      vm.vm_host.update(total_cpus: 24)
       private_subnet = PrivateSubnet.create(name: "ps", location_id: Location::HETZNER_FSN1_ID, net6: "fd10:9b0b:6b4b:8fbb::/64",
-        net4: "1.1.1.0/26", state: "waiting", project_id: project.id)
+        net4: "1.1.1.0/26", state: "waiting", project_id: vm.project_id)
       Nic.create(private_subnet_id: private_subnet.id, private_ipv6: "fd10:9b0b:6b4b:8fbb:abc::", private_ipv4: "10.0.0.1",
         mac: "00:00:00:00:00:00", encryption_key: "0x736f6d655f656e6372797074696f6e5f6b6579", name: "old-vm-nic",
-        vm_id: ready_vm.id, state: "active")
-      vbb = create_vhost_block_backend(vm_host_id: source_host.id)
-      sd = StorageDevice.create(name: "vda", total_storage_gib: 100, available_storage_gib: 50, vm_host_id: source_host.id)
-      VmStorageVolume.create(vm_id: ready_vm.id, boot: true, size_gib: 40, disk_index: 0,
-        storage_device_id: sd.id, vhost_block_backend_id: vbb.id,
-        key_encryption_key_1_id: StorageKeyEncryptionKey.create_random(auth_data: "src").id,
-        vring_workers: 1, track_written: true)
-      Strand.create_with_id(ready_vm.id, prog: "Vm::Metal::Nexus", label: "stopped_by_admin")
-      ready_vm.incr_prepare_to_move
+        vm_id: vm.id, state: "active")
 
-      refresh_frame(prog, new_values: {"vm_id" => ready_vm.id})
+      prog = described_class.new(described_class.assemble(vm.vm_host))
+      refresh_frame(prog, new_values: {"vm_id" => vm.id})
+      expect { prog.move_vm }.to hop("wait_vm_moved")
 
-      expect { prog.move_vm }.to hop("wait_move_vm")
-
-      move_vm_strand = Strand[prog.move_vm_strand_id]
+      move_vm_strand = prog.strand.children.first
       expect(move_vm_strand.prog).to eq("Vm::Metal::MoveVm")
       expect(move_vm_strand.stack.first["subject_id"]).to eq(prog.new_vm_id)
-      expect(prog.new_vm_id).not_to eq(ready_vm.id)
+      expect(prog.new_vm_id).not_to eq(vm.id)
     end
   end
 
-  describe "#wait_move_vm" do
-    it "naps while the move strand still exists" do
-      move_strand = Strand.create(prog: "Vm::Metal::MoveVm", label: "start", stack: [{}])
-      refresh_frame(prog, new_values: {"move_vm_strand_id" => move_strand.id})
-      expect { prog.wait_move_vm }.to nap(10)
+  describe "#wait_vm_moved" do
+    it "naps while the move child strand still exists" do
+      Strand.create(prog: "Vm::Metal::MoveVm", label: "start", stack: [{}], parent_id: prog.strand.id, lease: Time.now + 100)
+      expect { prog.wait_vm_moved }.to nap(120)
     end
 
-    it "hops to verify_new_vm once the move strand has popped" do
-      refresh_frame(prog, new_values: {"move_vm_strand_id" => Strand.generate_uuid})
-      expect { prog.wait_move_vm }.to hop("verify_new_vm")
+    it "hops to verify_new_vm once the child strand no longer exists" do
+      expect { prog.wait_vm_moved }.to hop("verify_new_vm")
     end
   end
 
@@ -154,20 +112,20 @@ RSpec.describe Prog::Test::MoveVm do
     end
 
     it "hops to destroy_vms when all checksums match" do
-      expect(prog.new_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-0").and_return("sha-0  x\n")
-      expect(prog.new_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-1").and_return("sha-1  x\n")
+      expect(prog.new_vm_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-0").and_return("sha-0  x\n")
+      expect(prog.new_vm_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-1").and_return("sha-1  x\n")
 
-      expect { prog.verify_new_vm }.to hop("destroy_vms")
+      expect { prog.verify_new_vm }.to hop("destroy")
     end
 
     it "fails when a checksum doesn't match" do
-      expect(prog.new_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-0").and_return("wrong  x\n")
+      expect(prog.new_vm_sshable).to receive(:_cmd).with("sudo sha256sum /opt/markers/marker-0").and_return("wrong  x\n")
 
       expect { prog.verify_new_vm }.to hop("failed")
     end
   end
 
-  describe "#destroy_vms" do
+  describe "#destroy" do
     it "sets the destroy semaphore on both vms and hops to wait_resources_destroyed" do
       old_vm = create_vm
       new_vm = create_vm
@@ -175,7 +133,7 @@ RSpec.describe Prog::Test::MoveVm do
       Strand.create_with_id(new_vm, prog: "Vm::Metal::Nexus", label: "wait")
       refresh_frame(prog, new_values: {"vm_id" => old_vm.id, "new_vm_id" => new_vm.id})
 
-      expect { prog.destroy_vms }.to hop("wait_resources_destroyed")
+      expect { prog.destroy }.to hop("wait_resources_destroyed")
 
       expect(old_vm.reload.destroy_set?).to be true
       expect(new_vm.reload.destroy_set?).to be true
