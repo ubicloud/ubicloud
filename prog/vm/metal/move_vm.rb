ad582e438d948665e989a19405ff86cbf88fa234
Jeremy Evans <jeremy@ubicloud.com>
2026-08-14 12:22:20 -0700

    Prevent destroy of old and new vms when moving vms
    Use the existing prevent_destroy semaphore. However, if the
    prevent_destroy semaphore is already set, don't unset it in destroy.
    This still has issues if prevent_destroy is set manually after the
    Vm is moved. To prevent the issue, we would need to add a counting
    semaphore, where decr removes only one incr, instead of all.

--- prog/vm/metal/move_vm.rb
+++ prog/vm/metal/move_vm.rb
@@ -3,7 +3,7 @@
 class Prog::Vm::Metal::MoveVm < Prog::Base
   subject_is :vm
 
-  frame_reader :remote_storage_server_id
+  frame_reader :remote_storage_server_id, :old_vm_id, :unset_prevent_destroy
 
   def self.assemble(vm, vm_host, parent_id: nil)
     fail "Vm is not ready to move" unless vm.strand.label == "stopped_by_admin" && vm.prepare_to_move_set?
@@ -20,6 +20,7 @@ class Prog::Vm::Metal::MoveVm < Prog::Base
 
       old_name = vm.name
       vm.update(name: "moving-with-#{remote_storage_server.ubid}")
+      unset_prevent_destroy = !vm.prevent_destroy_set?
 
       new_vm = Prog::Vm::Nexus.assemble(
         vm.public_key,
@@ -39,12 +40,18 @@ class Prog::Vm::Metal::MoveVm < Prog::Base
       if vm.sshable
         Sshable.create_with_id(new_vm, unix_user: vm.sshable.unix_user, host: "temp_#{vm.id}", raw_private_key_1: vm.sshable.raw_private_key_1)
       end
+      Vm.incr_prevent_destroy([vm.id, new_vm.id])
 
       Strand.create(
         parent_id:,
         prog: "Vm::Metal::MoveVm",
         label: "start",
-        stack: [{"subject_id" => new_vm.id, "remote_storage_server_id" => remote_storage_server.id}],
+        stack: [{
+          "subject_id" => new_vm.id,
+          "remote_storage_server_id" => remote_storage_server.id,
+          "old_vm_id" => vm.id,
+          "unset_prevent_destroy" => unset_prevent_destroy,
+        }],
       )
     end
   end
@@ -70,6 +77,9 @@ class Prog::Vm::Metal::MoveVm < Prog::Base
 
   label def destroy
     RemoteStorageServer.incr_destroy(remote_storage_server_id)
+    ids = [@subject_id]
+    ids << old_vm_id if unset_prevent_destroy
+    Semaphore.where(strand_id: ids, name: "prevent_destroy").delete
     pop "vm moved"
   end
 end
