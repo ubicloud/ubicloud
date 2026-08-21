8fa147c812ac850605d09ca30a859adccc141c3f
Jeremy Evans <jeremy@ubicloud.com>
2026-08-21 10:44:47 -0700

    Make cli pg ca-certificates command handle no CA certs
    Previously, this resulted in a NoMethodError. Handle this case by
    returning an error with an explanation.
    
    Fixes a production exception.

--- cli-commands/pg/post/ca-certificates.rb
+++ cli-commands/pg/post/ca-certificates.rb
@@ -2,12 +2,19 @@
 
 class UbiCli
   on("pg").run_on("ca-certificates") do
-    desc "Print CA certificates for a PostgreSQL database"
+    desc "Print CA certificates for a PostgreSQL database (if available)"
 
     banner "ubi pg (location/pg-name | pg-id) ca-certificates"
 
-    run do
-      response(sdk_object.ca_certificates)
+    run do |_, cmd|
+      unless (certs = sdk_object.ca_certificates)
+        cmd.raise_failure(<<~END)
+          CA certificates are not available for this database, either because it uses
+          publicly signed certificates or because a CA certificate has not been generated
+          yet.
+        END
+      end
+      response(certs)
     end
   end
 end
