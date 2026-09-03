424563f242697a0ac8fc44089bc01d15c43f67c7
Jeremy Evans <jeremy@ubicloud.com>
2026-09-03 15:21:31 -0700

    Extract ResourceDiscount#matches? to ResourceMatchable module
    This will be used in an upcoming ResourceCredit model.

--- model/resource_discount.rb
+++ model/resource_discount.rb
@@ -6,14 +6,7 @@ class ResourceDiscount < Sequel::Model
   many_to_one :project, read_only: true
 
   plugin ResourceMethods
-
-  def matches?(line_item)
-    (resource_id.nil? || resource_id == line_item[:resource_id]) &&
-      (resource_type.nil? || resource_type == line_item[:resource_type]) &&
-      (resource_family.nil? || resource_family == line_item[:resource_family]) &&
-      (location.nil? || location == line_item[:location]) &&
-      (byoc.nil? || byoc == line_item[:byoc])
-  end
+  include ResourceMatchable
 end
 
 # Table: resource_discount
