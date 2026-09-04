462315403ddc4617cfa932edb48e75febd8f012b
Jeremy Evans <jeremy@ubicloud.com>
2026-09-04 10:31:19 -0700

    Add for_project and active_during dataset methods to ResourceMatchable
    Use these methods to simplify InvoiceGenerator#run and
    Project#has_valid_payment_method?.
    
    There was a slight difference between < and <= for the end_time
    check, however, because <= was used with CURRENT_TIMESTAMP, it
    should have no behavioral difference with <, so I used < for both.

--- lib/resource_matchable.rb
+++ lib/resource_matchable.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ResourceMatchable
+  module ClassMethods
+    Sequel::Plugins.def_dataset_methods(self, [:for_project, :active_during])
+  end
+
   module InstanceMethods
     def matches?(line_item)
       (resource_id.nil? || resource_id == line_item[:resource_id]) &&
@@ -12,6 +16,17 @@ module ResourceMatchable
 
     def wildcard?
       resource_id.nil? && resource_type.nil? && resource_family.nil? && location.nil? && byoc.nil?
+    end
+  end
+
+  module DatasetMethods
+    def for_project(project_id)
+      where(project_id:)
+    end
+
+    def active_during(begin_time, end_time)
+      where { active_from < end_time }
+        .where { (active_to > begin_time) | {active_to: nil} }
     end
   end
 end
